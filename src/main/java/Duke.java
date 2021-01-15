44c4d4eea32a1b478c71544c4084c7fd1b2b86a1
kewenlok <e0360650@u.nus.edu>
2021-01-15 18:40:46 +0800

    Add greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
+
+    private static void showDivider() {
+        System.out.println("-----------------------------------------------------");
+    }
+
+    private static void showGreeting() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    private static void showExit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        showDivider();
+        showGreeting();
+        showDivider();
+        showExit();
+        showDivider();
     }
 }
