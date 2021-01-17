e4c0198772ef456fb05e5eac0758451a73afed24
tantzexern <tantzexern@gmail.com>
2021-01-18 02:16:08 +0800

    add indentation after greet message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("What can I do for you?\n");
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
