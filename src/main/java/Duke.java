3080d07c74c45e0ed461f9170041157acbbfb7fa
huwenqi <huwenqi614@gmail.com>
2021-01-17 12:42:05 +0800

    Level 0. Greet
    Implement an initial skeletal version of the Duke that simply greets the user and exits.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,11 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String division_line = "____________________________________________________________\n";
+        String say_hi = "Hello! I'm Duke\n" +
+                        "What can I do for you?\n";
+        String say_bye = "Bye. Hope to see you again soon!\n";
+        System.out.println(division_line + say_hi + division_line);
+        System.out.println(say_bye + division_line);
     }
 }
