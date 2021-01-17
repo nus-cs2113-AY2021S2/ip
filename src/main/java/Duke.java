3706a7e55de8f3a683debb97c5a3e53d943e7f05
Wei Sheng Ong <ongweisheng30@gmail.com>
2021-01-17 15:34:22 +0800

    Added greeting message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
+        // logo/loading
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        // greeting
+        String greeting = "Hello! I'm Duke\n" + "What can I do for you?\n";
+        System.out.println(greeting);
     }
 }
