43e2ddf0b4d566f4d7c809573d7cd744271f0ac7
Wei Sheng Ong <ongweisheng30@gmail.com>
2021-01-17 15:45:06 +0800

    Added horizontal lines and changed the indentation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 public class Duke {
     public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
         // logo/loading
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,11 +8,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
         // greeting
-        String greeting = "Hello! I'm Duke\n" + "What can I do for you?\n";
+        String greeting = "Hello! I'm Duke\n" + "What can I do for you?";
         System.out.println(greeting);
+        System.out.println("____________________________________________________________");
         // exit message
-        String exitMessage = "Bye. Hope to see you again soon!\n";
+        String exitMessage = "Bye. Hope to see you again soon!";
         System.out.println(exitMessage);
+        System.out.println("____________________________________________________________");
     }
 }
