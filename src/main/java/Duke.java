e1008d8d77ce4727ad369c7be8648573c25fba67
jadenwjh <wongjunhong9723@gmail.com>
2021-01-17 15:03:40 +0800

    Add initial skeletal Hello and Bye message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String helloMsg = "Hello! I'm Duke\nWhat can I do for you?";
+        String byeMsg = "Bye. Hope to see you again soon!";
+
+        System.out.println(helloMsg);
+        System.out.println(byeMsg);
     }
 }
