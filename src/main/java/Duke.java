7a677b5fbb95e2c33895f51fd8c2a43681d0821b
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-01-17 14:14:24 +0800

    Added Increment: Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String greet= "____________________________________________________________\n"
+                + "Hello! I'm Duke\n"
+                +"What can I do for you?\n"
+                +"____________________________________________________________\n";
+        String bye="____________________________________________________________\n"
+                +"Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(greet);
+        System.out.println(bye);
     }
 }
