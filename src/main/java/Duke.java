a5d28bd50780bab2c4d6dbbd9354cf85cda33a70
Rizavur Rahman <rizavur@gmail.com>
2021-01-17 21:08:02 +0800

    Duke Level 0: Greet & bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
 public class Duke {
-    public static void main(String[] args) {
+    private static final String breakLine = "____________________________________________________________";
+
+    public static void showLogo() {
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(breakLine + "\n" + logo + "\n" + breakLine);
+    }
+    public static void greetMessage() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(breakLine);
+    }
+
+    public static void byeMessage() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(breakLine);
+    }
+    public static void main(String[] args) {
+        showLogo();
+        greetMessage();
+        byeMessage();
     }
 }
