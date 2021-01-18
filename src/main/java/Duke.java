5ce1ac6eda2a2b9d233b31ab7262934968f8e7c6
Oscar Lai <e0424620@u.nus.edu>
2021-01-18 20:39:36 +0800

    Week 0 Greeting: Update

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,11 @@
 public class Duke {
+
     public static void main(String[] args) {
+        greeting();
+    }
 
-        /*
-          String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-         */
+    public static void greeting(){
         String border = "____________________________________________________________";
-
         System.out.println(border + "\n" + "Hello! I'm Duke\nWhat can I do for you?");
         System.out.println(border + "\n" + "Bye. Hope to see you again soon!\n" + border);
     }
