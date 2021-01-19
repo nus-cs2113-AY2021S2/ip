8f547ba2dc4c658a4c5698f760d554483a2d8367
aliciatay-zls <e0424736@u.nus.edu>
2021-01-19 23:59:46 +0800

    Added greet and goodbye methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,15 @@
 public class Duke {
+    public static void greet() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________\n");
+    }
+    public static void goodbye() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +17,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
+        goodbye();
     }
 }
