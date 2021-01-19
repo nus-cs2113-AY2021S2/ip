354f08c5a82661d6fbb79c7b70ad62f899eaa703
Oscar Lai <e0424620@u.nus.edu>
2021-01-19 23:00:47 +0800

    Level 0. Greet: Update

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,23 @@
 public class Duke {
 
+    public static String border = "____________________________________________________________";
+
     public static void main(String[] args) {
         greeting();
+        exit();
     }
 
     public static void greeting(){
-        String border = "____________________________________________________________";
-        System.out.println(border + "\n" + "Hello! I'm Duke\nWhat can I do for you?");
+        String logo =  " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(border + "\n" + "Hello! I'm \n" + logo );
+        System.out.println("What can I do for you?");
+    }
+
+    public static void exit(){
         System.out.println(border + "\n" + "Bye. Hope to see you again soon!\n" + border);
     }
 }
