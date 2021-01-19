c384a2d99e286c0156dc5788b58a743ce72b1ebb
jovanhuang <jhtc98@hotmail.com>
2021-01-19 10:03:43 +0800

    Complete Level - 0 (Greet users and exit)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,19 @@
 public class Duke {
+
+    public static void printIntroMessage(){
+        System.out.println("________________________________");
+        System.out.println("Hello, I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println("________________________________");
+    }
+
+    public static void printExitMessage(){
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________");
+        System.exit(0);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,6 +21,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
+        printIntroMessage();
+        printExitMessage();
     }
 }
