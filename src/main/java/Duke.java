3c1066dbd76c3f9f0b23bed0848f175e1555bd63
NgManSing <E0678799@u.nus.edu>
2021-01-19 11:45:32 +0800

    [Week-2] Updated the code and added some comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,11 @@
+/**
+ * This class is to build a personal assistance chat-bot called "Duke"
+ *
+ * Week-2:
+ * The program is implemented to greet users and exits subsequently.
+ *
+ * @author NgManSing
+ */
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +13,13 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "----------------------------------------------------";
+        System.out.println(line);
+        System.out.println(logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
