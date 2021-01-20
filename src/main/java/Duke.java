94686768c3ebada70730082259ddd2bb6b1568a3
FarmZH98 <zhenghaofarm@gmail.com>
2021-01-21 03:32:39 +0800

    Level - 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,15 @@
 public class Duke {
+
+    public static void Level1(){
+        String text1 = "____________________________________________________________\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(text1);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +17,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Level1();
     }
 }
