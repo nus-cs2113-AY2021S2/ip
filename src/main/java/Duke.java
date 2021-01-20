f26b0d48405a3a83d88bf9e4607e52987c4d7eee
Song Yu <song0180@e.ntu.edu.sg>
2021-01-20 23:50:31 +0800

    Level 0. Greet: Add basic greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,23 @@
 public class Duke {
+    public static void printGreetings() {
+        System.out.println(
+                "__________________________________________\n" +
+                        " Welcome. I am your assistant Duke.\n" +
+                        " Just FYI, I am developed by Song Yu.\n" +
+                        " May I know what I can help you?\n" +
+                        "__________________________________________\n"
+        );
+    }
+
+    public static void printExitGreetings() {
+        System.out.println(
+                "__________________________________________\n" +
+                        " Thank you for getting in touch.\n" +
+                        " See you next time. :)\n" +
+                        "__________________________________________\n"
+        );
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +25,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printGreetings();
+        printExitGreetings();
     }
 }
