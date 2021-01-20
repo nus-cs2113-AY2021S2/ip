2e76150b61c9aa7c12c84d4946f285e42a46ad0b
Oscar Lai <77140576+oscarlai1998@users.noreply.github.com>
2021-01-20 09:46:40 +0800

    Update Duke.java
    Week 0: Update 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,23 @@
 public class Duke {
+    
+	public static String border = "____________________________________________________________";
+	public static String logo =  " ____        _        \n"
+			+ "|  _ \\ _   _| | _____ \n"
+			+ "| | | | | | | |/ / _ \\\n"
+			+ "| |_| | |_| |   <  __/\n"
+			+ "|____/ \\__,_|_|\\_\\___|\n";
 
-    public static String border = "____________________________________________________________";
+	public static void main(String[] args) {
+		System.out.println("Hello from\n" + logo);
+		greeting();
+		exit();
+	}
 
-    public static void main(String[] args) {
-        greeting();
-        exit();
-    }
+	public static void greeting(){ 
+		System.out.println(border + "\n" + "Hello! I'm Duke\nWhat can I do for you?");
+	}
 
-    public static void greeting(){
-        String logo =  " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(border + "\n" + "Hello! I'm \n" + logo );
-        System.out.println("What can I do for you?");
-    }
-
-    public static void exit(){
-        System.out.println(border + "\n" + "Bye. Hope to see you again soon!\n" + border);
-    }
+	public static void exit(){
+		System.out.println(border + "\n" + "Bye. Hope to see you again soon!\n" + border);
+	}
 }
