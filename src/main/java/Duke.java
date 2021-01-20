a9676e73aa4aa5db7557d453aa16174eba6fcf68
zihan9485 <e0523361@u.nus.edu>
2021-01-20 15:24:43 +0800

    Add Level-0 Increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        //Level 0: Greet
+        String hLine = "____________________________________________________________";
+        System.out.println(hLine);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(hLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(hLine);
     }
 }
