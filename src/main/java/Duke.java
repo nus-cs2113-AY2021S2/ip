04169fbf2c835fb1b7d0556070d4242f75c537e0
Yiwen_Qiu <60378963+e00426142@users.noreply.github.com>
2021-01-21 21:14:02 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
 
-        System.out.println("Hello I'm Duke\n");
-        System.out.println("What can I do for you?\n" );
+        System.out.println("Hello I'm Duke\n" + "What can I do for you?\n");
         System.out.println("Bye. Hope to see you again!\n");
     }
 }
