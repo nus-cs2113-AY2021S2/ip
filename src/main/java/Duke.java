98be444cf008e00a0d7cfe3f61142949173322fb
Yiwen_Qiu <60378963+e00426142@users.noreply.github.com>
2021-01-21 21:24:57 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,20 @@
 public class Duke {
     public static void main(String[] args) {
+        /*
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        */
+        String greet = "____________________________________________________________\n" +
+                " Hello! I'm Duke\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________";
+        String exit = "Bye. Hope to see you again!\n" +
+                "____________________________________________________________\n";
 
-        System.out.println("Hello I'm Duke\n" + "What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again!\n");
+        System.out.println(greet);
+        System.out.println(exit);
     }
 }
