ca776407317f637eef5aa40b683ae0760dfb099d
angjiahui <angjiahui@users.noreply.github.com>
2021-01-22 00:15:31 +0800

    Level-0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+//import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String[] stringArgs = new String[10];
+//        Scanner sc = new Scanner(System.in);
+//        stringArgs[0] = sc.nextLine();
+//
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+//        if (stringArgs[0] == "bye") {
+//            System.out.println("Bye. Hope to see you again soon!");
+//        }
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
