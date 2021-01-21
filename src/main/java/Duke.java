ea51a735141bc9120773740dac3ad03565a837ca
SeanGoh <sean28goh@outlook.com>
2021-01-21 20:05:01 +0800

    Add Increment Level 0 - ip (Greet)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
 public class Duke {
+
+    public static void printStartUpMessage() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void printExitMessage() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
+
+
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printStartUpMessage();
+        printExitMessage();
     }
 }
