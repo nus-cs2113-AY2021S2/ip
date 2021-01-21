3af66f23594c853fb73269c0b423270e294e9a11
LeeHanYongAndy <andyleehy2502@gmail.com>
2021-01-21 18:49:07 +0800

    Add methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,5 @@
 public class Duke {
-    public static void main(String[] args) {
+    public static void introduction() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,8 +11,15 @@ public class Duke {
         System.out.println("What can I do for you?");
         // Actions - To be added
         System.out.println("____________________________________________________________");
+
+    }
+    public static void ending() {
         // Completion & Exiting - To be added
         System.out.println("Bye. Hope to see you again soon!");
         System.out.print("____________________________________________________________");
+    }
+    public static void main(String[] args) {
+        introduction();
+        ending();
     }
 }
