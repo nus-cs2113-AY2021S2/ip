e46195a0c5ebf110481d98fede8780614dd89c85
brynagoh <brynagoh@gmail.com>
2021-01-21 20:30:13 +0800

    Add Duke's greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,11 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I am\n" + logo);
+
+        System.out.println("What can I do for you today?\n ");
+
+        System.out.println("Goodbye！See you again soon :)");
+
     }
 }
