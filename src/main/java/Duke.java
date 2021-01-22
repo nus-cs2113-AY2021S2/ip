5171a188ac8a39fda9e8b77cd5ee4bf5a46b9c11
iamakilahamed <akilahamed123@gmail.com>
2021-01-22 15:43:02 +0800

    Update to JARVIS

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,27 @@
+import java.util.concurrent.TimeUnit;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        // JARVIS initialisation (6 seconds waiting time)
+        System.out.println("------------------------------------------------");
+        System.out.println("Importing all preferences from home interface.");
+        TimeUnit.SECONDS.sleep(1);  // waits 1 second
+        System.out.println("Systems are now fully operational.");
+        TimeUnit.SECONDS.sleep(1);  // waits 1 second
+        System.out.print("LOADING");
+        TimeUnit.SECONDS.sleep(1);  // waits 1 second
+        for (int i = 0; i <= 2; i++) {
+            System.out.print(".");
+            TimeUnit.SECONDS.sleep(1);  // waits 1 second
+        }
         System.out.println();
-        System.out.println("------------------------------");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        // JARVIS greetings
+        System.out.println("------------------------------------------------");
+        System.out.println("    Hello, sir. J.A.R.V.I.S at your service.");
         System.out.println();
-        System.out.println("------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("------------------------------------------------");
+        System.out.println("    Goodbye, sir.");
         System.out.println();
-        System.out.println("------------------------------");
+        System.out.println("------------------------------------------------");
     }
 }
