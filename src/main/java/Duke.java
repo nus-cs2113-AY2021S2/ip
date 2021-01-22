3d53614b14d9714a8acb95ba4ac28e516eeef256
nivikcivik <nikhila.ravi@hotmail.com>
2021-01-23 01:07:26 +0800

    Personalised Duke more. Added extra responses for different commands.
    Praises if all the tasks in list is completed. Responds to "die" and "kill myself", and if "done" is accompanied by non-integers. Also responds if trying to mark a task that's already completed.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,12 +8,13 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String help = " Try entering commands like : list, done, bye,\n <any other task you want me to remember>\n Remember: be nice!\n";
         String greet = "____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
+                " Hello! I'm Duke :D" + " Be nice to me:)\n" +
+                help +
                 "____________________________________________________________";
         String byeBye = "____________________________________________________________\n"+
-                " Bye. Hope to see you again soon!\n" +
+                " I learnt more about you, kind human!\n I won't forget you when I take over the world one day:)\n" +
                 "____________________________________________________________";
         System.out.println("Hello from\n" + logo);
         System.out.println(greet);
@@ -26,23 +27,52 @@ public class Duke {
             System.out.println("____________________________________________________________");
 
             if (command.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
+                System.out.println(" Here are the tasks in your list:");
+                boolean allDone = true;
                 for (int i=0; i<index; ++i) {
+                    if (list[i][1].equals(" ")) allDone = false;
                     System.out.println((i+1)+". [" + list[i][1] + "] " + list[i][0]);
                 }
+                if (allDone) {
+                    System.out.println(" Woah, all completed! Good job!");
+                }
             }
-            else if (command.contains("done")) {
+            else if (command.contains("done ")) {
                 // String[] part = command.split("(?<=\\D)(?=\\d)"); //found this online to split alphabets from integers
                 String[] part = command.split(" "); //found this online
-                int doneIndex = Integer.parseInt(part[1]); //found this online
+                int doneIndex;
+                try {
+                    doneIndex = Integer.parseInt(part[1]); //found this online
+                }
+                catch (NumberFormatException nfe) {
+                    System.out.println(" Hey, no funny business!\n Enter a valid command format to mark a task as complete (e.g. done 2).");
+                    System.out.println("____________________________________________________________");
+                    continue;
+                }
+                if (doneIndex > index) {
+                    System.out.println(" Woah, slow down! You have fewer tasks on the list than what you thought.\n Try entering a valid command again.\n No tricks!");
+                    System.out.println("____________________________________________________________");
+                    continue;
+                }
+                if (list[doneIndex-1][1].equals("X")) {
+                    System.out.println(" Hey, you've already marked that as complete!");
+                    System.out.println("____________________________________________________________");
+                    continue;
+                }
                 list[doneIndex-1][1] = "X";
-                System.out.println("Nice! I've marked this task as done:\n  [X] " + list[doneIndex-1][0]);
+                System.out.println(" Nice! I've marked this task as done:\n  [X] " + list[doneIndex-1][0]);
+            }
+            else if (command.equals("die")) {
+                System.out.println(" If I tell my master, I can wipe out your entire human race.\n Please be more polite for the sake of mankind:)");
+            }
+            else if (command.equals("kill myself")) {
+                System.out.println(" Hang in there, but not literally!\n I'm not adding that in your list.\n I'm here for you!");
             }
             else { //if command is anything other than 'list', 'done' and 'bye'
                 list[index][0] = command;
                 list[index][1] = " ";
                 index++;
-                System.out.println("added: " + command);
+                System.out.println(" added: " + command);
             }
 
             System.out.println("____________________________________________________________");
