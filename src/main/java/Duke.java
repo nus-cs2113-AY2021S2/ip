38dff03d915ce16305380245b90e4172180f0f15
NgManSing <E0678799@u.nus.edu>
2021-01-23 01:24:14 +0800

    [Week 3] Update (Level-2)
    Updated userInputs saving and list function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,30 +1,44 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  * This class is to build a personal assistance chat-bot called "Duke"
- * 
+ * <p>
  * Week-2:
  * The program is implemented to greet users and exits subsequently.
  *
  * @author NgManSing
  */
 public class Duke {
+    private static final ArrayList<String> userInputs = new ArrayList<String>();
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        boolean isLoop = true;
-        String userInput;
 
         System.out.println("Hello! I am Kaman :)");
         System.out.println("What can I do for you?");
+        doWork();
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void doWork() {
+        String userInput;
+        boolean isLoop = true;
+        Scanner scan = new Scanner(System.in);
+
         while (isLoop) {
             System.out.print("User>");
             userInput = scan.nextLine();
             if (userInput.equals("bye")) {
                 isLoop = false;
+            } else if (userInput.equals("list")) {
+                System.out.println("List:");
+                for (int i = 0; i < userInputs.size(); i++) {
+                    System.out.println((i + 1) + ". " + userInputs.get(i));
+                }
             } else {
-                System.out.println(userInput);
+                System.out.println("Added: " + userInput);
+                userInputs.add(userInput);
             }
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
