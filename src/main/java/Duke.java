c9d8fbf6f243c91982b52ddb73ea7e1bd0848002
Brandon Foong <brandonfoongyankai@gmail.com>
2021-01-22 20:27:04 +0800

    Add Level 1 functionalities to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Duke");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t Hello! I'm Duke");
+        System.out.println("\t What can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\t Bye. Hope to see you again soon!");
+                System.out.println("\t____________________________________________________________");
+                break;
+            } else {
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\t " + command);
+                System.out.println("\t____________________________________________________________");
+            }
+        }
     }
 }
