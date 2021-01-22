ed4b55b63a448f3a21ca36d8faf7df3641758553
Howard Liu <howard@ixnet.work>
2021-01-22 17:04:38 +0800

    Complete greet, echo and exit features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,46 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static final String LONG_LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
         greeting();
+
+        Scanner in = new Scanner(System.in);
+        Boolean exit = false;
+        while (!exit) {
+            String line = in.nextLine();
+            switch(line) {
+            case "bye":
+                exit = true;
+                break;
+            default:
+                printLine(line);
+            }
+        }
+        in.close();
+
         goodbye();
     }
 
     public static void greeting() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
-        System.out.println("What can I do for you?");
+        printIndent(LONG_LINE);
+        printIndent("Hello! I'm Duke");
+        printIndent("What can I do for you?");
+        printIndent(LONG_LINE);
     }
 
     public static void goodbye() {
-        System.out.println("Bye. Hope to see you again soon!");
+        printLine("Bye. Hope to see you again soon!");
+    }
+
+    protected static void printLine(String line) {
+        printIndent(LONG_LINE);
+        printIndent(line);
+        printIndent(LONG_LINE);
+    }
+
+    protected static void printIndent(String line) {
+        System.out.println("    " + line);
     }
 }
