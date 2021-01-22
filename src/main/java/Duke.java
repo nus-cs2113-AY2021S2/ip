a59baeacd9cb66699d52a1a617932febd44e6064
NgManSing <E0678799@u.nus.edu>
2021-01-23 00:48:16 +0800

    [Week 3] Updated echo and bye function (Level-1)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
  * This class is to build a personal assistance chat-bot called "Duke"
- *
+ * <p>
  * Week-2:
  * The program is implemented to greet users and exits subsequently.
  *
@@ -8,18 +10,21 @@
  */
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String line = "----------------------------------------------------";
-        System.out.println(line);
-        System.out.println(logo);
-        System.out.println("Hello! I'm Duke");
+        Scanner scan = new Scanner(System.in);
+        boolean isLoop = true;
+        String userInput;
+
+        System.out.println("Hello! I am Kaman :)");
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        while (isLoop) {
+            System.out.print("User>");
+            userInput = scan.nextLine();
+            if (userInput.equals("bye")) {
+                isLoop = false;
+            } else {
+                System.out.println(userInput);
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
     }
 }
