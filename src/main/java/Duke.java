7a3c7c3fccf9a5f6aed634cace9960b182fd0a20
xseh <sehxinru@gmail.com>
2021-01-23 00:00:59 +0800

    Add echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            String command = in.nextLine();
+
+            //print bye statement and exit program
+            if (command.toLowerCase().equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            
+            //echos commands
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + command);
+            System.out.println("____________________________________________________________\n");
+        }
     }
 }
