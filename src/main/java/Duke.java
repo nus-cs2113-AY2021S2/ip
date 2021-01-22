f5be129966884d2978c574aab30febe269a55ef3
cswbibibi <caoshuwen1017@gmail.com>
2021-01-22 18:19:13 +0800

    first commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello, I'm Duke!\nWhat can i do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
