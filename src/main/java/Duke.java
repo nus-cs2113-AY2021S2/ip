8cbfdbd974381a8f180d37fa741108250c7296ac
Brandon Foong <brandonfoongyankai@gmail.com>
2021-01-22 20:33:01 +0800

    Add Level 2 functionalities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,18 +7,25 @@ public class Duke {
         System.out.println("\t What can I do for you?");
         System.out.println("\t____________________________________________________________");
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int numTasks = 0;
         while (true) {
             String command = sc.nextLine();
+            System.out.println("\t____________________________________________________________");
             if (command.equals("bye")) {
-                System.out.println("\t____________________________________________________________");
                 System.out.println("\t Bye. Hope to see you again soon!");
                 System.out.println("\t____________________________________________________________");
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < numTasks; i++) {
+                    System.out.println("\t " + (i + 1) + ". " + tasks[i]);
+                }
             } else {
-                System.out.println("\t____________________________________________________________");
-                System.out.println("\t " + command);
-                System.out.println("\t____________________________________________________________");
+                tasks[numTasks] = command;
+                numTasks = numTasks + 1;
+                System.out.println("\t added: " + command);
             }
+            System.out.println("\t____________________________________________________________");
         }
     }
 }
