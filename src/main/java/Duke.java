b36c7f2bca67f35a12e16f847caf74ce346e09cb
cloudy3 <cjingfeng98@gmail.com>
2021-01-23 15:57:05 +0800

    Allow user to add and list tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,6 +11,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<String>();
 
         System.out.println("~____________________________________________________________~");
         System.out.println("What's up! I'm Duke");
@@ -18,12 +20,20 @@ public class Duke {
 
         boolean isRunning = true;
         while (isRunning) {
-            String phrase = sc.next();
+            String phrase = sc.nextLine();
             if (phrase.equals("bye")) {
                 isRunning = false;
+            } else if (phrase.equals("list")) {
+                System.out.println("~____________________________________________________________~");
+                int i = 0;
+                for (String task : tasks) {
+                    System.out.println(++i + ". " + task);
+                };
+                System.out.println("~____________________________________________________________~\n");
             } else {
                 System.out.println("~____________________________________________________________~");
-                System.out.println(phrase);
+                System.out.println("added: " + phrase);
+                tasks.add(phrase);
                 System.out.println("~____________________________________________________________~\n");
             }
         }
