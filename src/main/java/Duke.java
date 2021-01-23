f40fa7d9e6145fcb62bfc797b0e909c36dd05800
NgManSing <E0678799@u.nus.edu>
2021-01-23 23:26:29 +0800

    [Week-3] level-2 (v3)
    The code is updated.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,32 +13,44 @@ public class Duke {
     private static final ArrayList<String> userInputs = new ArrayList<String>();
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean isLoop = true;
 
         System.out.println("Hello! I am Kaman :)");
         System.out.println("What can I do for you?");
-        doWork();
-        System.out.println("Bye. Hope to see you again soon!");
+        do {
+            System.out.print("User>");
+            String userInput = scan.nextLine();
+            switch (userInput) {
+            case "list":
+                showList();
+                break;
+            case "bye":
+                quitProgram();
+                isLoop = false;
+                break;
+            default:
+                addRecord(userInput);
+                break;
+            }
+        } while (isLoop);
+
+
     }
 
-    private static void doWork() {
-        String userInput;
-        boolean isLoop = true;
-        Scanner scan = new Scanner(System.in);
+    private static void addRecord(String userInput) {
+        System.out.println("Added: " + userInput);
+        userInputs.add(userInput);
+    }
 
-        while (isLoop) {
-            System.out.print("User>");
-            userInput = scan.nextLine();
-            if (userInput.equals("bye")) {
-                isLoop = false;
-            } else if (userInput.equals("list")) {
-                System.out.println("List:");
-                for (int i = 0; i < userInputs.size(); i++) {
-                    System.out.println((i + 1) + ". " + userInputs.get(i));
-                }
-            } else {
-                System.out.println("Added: " + userInput);
-                userInputs.add(userInput);
-            }
+    private static void showList() {
+        System.out.println("List:");
+        for (int i = 0; i < userInputs.size(); i++) {
+            System.out.println((i + 1) + ". " + userInputs.get(i));
         }
+    }
+
+    private static void quitProgram() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
