f6b07a6b9af88932d3cd7d0611ad8d206bda3298
8kdesign <that8kguy@gmail.com>
2021-01-23 10:42:01 +0800

    duke.java: add echo command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    static String LINE = "──────────────────────────────";
+    static String HELLO_MESSAGE = LINE + "\n"
+            + "8K: Hi there! 8K here.\n"
+            + "    How can I help you?\n"
+            + LINE;
+    static String BYE_MESSAGE = LINE + "\n"
+            + "8K: Bye bye! Have a nice day.\n"
+            + LINE;
+
     public static void main(String[] args) {
-        String message = "------------------------------\n"
-                + "Hi there! 8K here.\n"
-                + "How can I help you?\n"
-                + "------------------------------\n"
-                + "Bye bye! Have a nice day.\n"
-                + "------------------------------\n";
-        System.out.println(message);
+
+        Scanner in = new Scanner(System.in);
+        String input;
+
+        System.out.println(HELLO_MESSAGE);
+        input = in.nextLine();
+
+        while (!input.equalsIgnoreCase("bye")) {
+            System.out.println(LINE);
+            System.out.println("8K: " + input);
+            System.out.println(LINE);
+            input = in.nextLine();
+        }
+
+        System.out.println(BYE_MESSAGE);
     }
 }
