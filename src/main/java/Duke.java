d6520e437f8a383401393e96345c912b259f077b
Jing Wei <jingwei.chew@gmail.com>
2021-01-23 18:00:24 +0800

    [Week 3] Level 1 - Add Greeting, Echoing and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String welcomeText = "Hello! I'm Duke\n"
-                +   "What can I do for you?\n"
-                +   "Bye. Hope to see you again soon!";
-        System.out.println(logo + welcomeText);
+        String welcomeText = "Welcome to the Kwik-e-mart, my name is Apu\n"
+                +   "What can I get you?\n";
+        System.out.println(welcomeText);
+        Scanner scanner = new Scanner(System.in);
+        String nextCommand = scanner.nextLine();
+        while (!nextCommand.toLowerCase(Locale.ROOT).contains("bye")) {
+            System.out.println(">>> You said: " + nextCommand);
+            System.out.println("What else can I get you?");
+            nextCommand = scanner.nextLine();
+        }
+        String goodbyeText = "Goodbye, please come again!";
+        System.out.println(goodbyeText);
     }
 }
