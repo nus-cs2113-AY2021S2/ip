37a7d69c2db91bf36f4ce975266e8e9e81c4ae0f
8kdesign <that8kguy@gmail.com>
2021-01-23 11:24:58 +0800

    duke.java: add savedlist and help

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,21 +10,45 @@ public class Duke {
     static String BYE_MESSAGE = LINE + "\n"
             + "8K: Bye bye! Have a nice day.\n"
             + LINE;
+    static String HELP_MESSAGE = LINE + "\n"
+            + "8K: Bye - Exit programme.\n"
+            + "    Help - Show list of commands.\n"
+            + "    List - Show list of saved values.\n"
+            + LINE;
 
     public static void main(String[] args) {
+        String[] savedList = new String[100];
+        int savedListSize = 0;
 
         Scanner in = new Scanner(System.in);
-        String input;
+        String input = "";
 
         System.out.println(HELLO_MESSAGE);
-        input = in.nextLine();
 
-        while (!input.equalsIgnoreCase("bye")) {
-            System.out.println(LINE);
-            System.out.println("8K: " + input);
-            System.out.println(LINE);
+        do {
             input = in.nextLine();
-        }
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println(LINE);
+                for (int i = 0; i < savedListSize; i++) {
+                    System.out.println(Integer.toString(i+1) + ". " + savedList[i]);
+                }
+                System.out.println(LINE);
+            } else if (input.equalsIgnoreCase("help")) {
+                System.out.println(HELP_MESSAGE);
+            } else if (savedListSize < 100) {
+                savedList[savedListSize] = input;
+                savedListSize++;
+                System.out.println(LINE);
+                System.out.println("8K: Added \"" + input + "\" to list.");
+                System.out.println(LINE);
+            } else {
+                System.out.println(LINE);
+                System.out.println("8K: List is full.");
+                System.out.println(LINE);
+            }
+        } while (true);
 
         System.out.println(BYE_MESSAGE);
     }
