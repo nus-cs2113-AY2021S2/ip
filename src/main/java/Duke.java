bd232695b99d0e47302ac22561c39151d4d2142e
KevinNg <WNG052@e.ntu.edu.sg>
2021-01-23 16:47:50 +0800

    Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        startGreetings();
+        endGreetings();
+    }
+
+    public static void startGreetings() {
+        String greetings = "-------------------------------------\n"
+                         + "Hello! I'm Duke\n"
+                         + "What can I do for you?\n\n"
+                         + "-------------------------------------";
+        System.out.println(greetings);
+    }
+
+    public static void endGreetings() {
+        String exitStatements = "-------------------------------------\n"
+                              + "Bye. Hope to see you again soon!\n"
+                              + "-------------------------------------";
+        System.out.println(exitStatements);
     }
 }
