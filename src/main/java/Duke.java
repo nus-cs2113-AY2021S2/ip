79a600ab9bf057542beafe4d12b3782f07320f9b
Rizavur Rahman <rizavur@gmail.com>
2021-01-24 02:11:57 +0800

    Add features to echo user input and exit when 'bye' input is given.
    - Get user input using java scanner utility.
    - Also made minor change to 'breakline' constant as the previous version was not centred.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
-    private static final String breakLine = "____________________________________________________________";
+    private static final String BREAKLINE = "------------------------------------------------------------";
 
     public static void showLogo() {
         String logo = " ____        _        \n"
@@ -7,21 +9,40 @@ public class Duke {
                     + "| | | | | | | |/ / _ \\\n"
                     + "| |_| | |_| |   <  __/\n"
                     + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(breakLine + "\n" + logo + "\n" + breakLine);
+        System.out.println(BREAKLINE + "\n" + logo + "\n" + BREAKLINE);
     }
     public static void greetMessage() {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println(breakLine);
+        System.out.println(BREAKLINE);
     }
-
+    public static String getUserInput() {
+        System.out.print("> ");
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        System.out.println(BREAKLINE);
+        return input;
+    }
     public static void byeMessage() {
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(breakLine);
+        System.out.println(BREAKLINE);
     }
+
+
     public static void main(String[] args) {
         showLogo();
         greetMessage();
-        byeMessage();
+        while(true) {
+            String input = getUserInput();
+            switch (input) {
+            case "bye":
+                byeMessage();
+                return;
+            default:
+                System.out.println(input);
+                System.out.println(BREAKLINE);
+                break;
+            }
+        }
     }
 }
