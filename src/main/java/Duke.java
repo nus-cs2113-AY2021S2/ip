d41f7b21bc84b0fb714319e990f4338ae2a687df
NgManSing <E0678799@u.nus.edu>
2021-01-24 00:12:30 +0800

    [Week-3] Level-3 (v1)
    Added done feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  * @author NgManSing
  */
 public class Duke {
-    private static final ArrayList<String> userInputs = new ArrayList<String>();
+    private static final ArrayList<String> records = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -20,31 +20,53 @@ public class Duke {
         System.out.println("What can I do for you?");
         do {
             System.out.print("User>");
-            String userInput = scan.nextLine();
-            switch (userInput) {
+            String rawInput = scan.nextLine();
+            String[] inputs = rawInput.split(" ");
+            switch (inputs[0]) {
             case "list":
                 showList();
+                break;
+            case "done":
+                setDone(inputs);
                 break;
             case "bye":
                 quitProgram();
                 isLoop = false;
                 break;
             default:
-                addRecord(userInput);
+                addRecord(inputs[0]);
                 break;
             }
         } while (isLoop);
     }
 
+    private static void setDone(String[] inputs) {
+        if (inputs.length != 2) {
+            System.out.println("Invalid input! (Too many arguments)");
+            return;
+        }
+        int targetRecordIndex = Integer.parseInt(inputs[1]) - 1;
+        if (targetRecordIndex < 0) {
+            System.out.println("Invalid input! (Index should be at least 1)");
+            return;
+        }
+        String targetRecord = records.remove(targetRecordIndex);
+        String updatedRecord = "[X] " + targetRecord.substring(4);
+        records.add(targetRecordIndex, updatedRecord);
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("\t" + updatedRecord);
+
+    }
+
     private static void addRecord(String userInput) {
         System.out.println("Added: " + userInput);
-        userInputs.add(userInput);
+        records.add("[ ] " + userInput);
     }
 
     private static void showList() {
-        System.out.println("List:");
-        for (int i = 0; i < userInputs.size(); i++) {
-            System.out.println((i + 1) + ". " + userInputs.get(i));
+        System.out.println("Here is your task List:");
+        for (int i = 0; i < records.size(); i++) {
+            System.out.println((i + 1) + ". " + records.get(i));
         }
     }
 
