8a24e2ea10804b0dfb7d082e5e4dfd74c43c2456
SeanGoh <sean28goh@outlook.com>
2021-01-23 23:30:21 +0800

    Add Increment Level-3
    Create a new Task class to track the tasks, refactor some of the methods in the main run method to separate methods so that code is cleaner.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static String[] tasks = new String[100];
+    public static Task[] tasks = new Task[100];
     public static int taskCount = 0;
 
     public static void printStartUpMessage() {
@@ -26,19 +26,36 @@ public class Duke {
                 return;
             } else if (input.equals("list")) {
                 printTaskList();
+            } else if (input.contains("done")) {
+                markTaskDone(input);
             } else {
-                tasks[taskCount] = input;
-                taskCount++;
-                System.out.println("____________________________________________________________");
-                System.out.println("added: " + input);
-                System.out.println("____________________________________________________________");
+                addNewTask(input);
             }
         }
     }
 
-    public static void printTaskList() {
-        String[] taskList = Arrays.copyOf(tasks, taskCount);
+    public static void markTaskDone(String input) {
+        String[] parts = input.split(" ");
+        int taskIndex = Integer.parseInt(parts[1]) - 1;
+        tasks[taskIndex].markDone();
         System.out.println("____________________________________________________________");
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(tasks[taskIndex]);
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void addNewTask(String input){
+        tasks[taskCount] = new Task(input);
+        taskCount++;
+        System.out.println("____________________________________________________________");
+        System.out.println("added: " + input);
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void printTaskList() {
+        Task[] taskList = Arrays.copyOf(tasks, taskCount);
+        System.out.println("____________________________________________________________");
+        System.out.println("Here are the tasks in your list:");
         for(int i = 0; i < taskList.length; i++){
             System.out.printf("%d. %s%n", i+1, taskList[i]);
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void markDone(){
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", getStatusIcon(), getDescription());
+    }
+}
