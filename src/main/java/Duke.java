2e0e34d51c8bb1f6193a448db087c84da416af7c
cswbibibi <caoshuwen1017@gmail.com>
2021-01-24 11:53:19 +0800

    level-3 Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,30 +10,69 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello, I'm Duke!\nWhat can i do for you?\n");
+        System.out.println("Here are the things that i can do: ");
+        System.out.println("1. Echo");
+        System.out.println("2. Add task to your list & Mark task as done");
+        System.out.println("-1: Quit Duke");
 
-        String task;
         Scanner in = new Scanner(System.in);
-        int index = 0;
-        task = in.nextLine();
-        String[] tasks = new String[100];
+        boolean run = true;
+        while (run){
+            int choice = in.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Please input something and I will repeat it back to you:)");
+                    Scanner a = new Scanner(System.in);
+                    String sentence = a.nextLine();
+                    while (true) {
+                        if (sentence.equalsIgnoreCase("bye")){
+                            break;
+                        }
+                        else{
+                            System.out.println(sentence);
+                            sentence = a.nextLine();
+                        }
+                    }
+                    System.out.println("Thank you for using echo, you can now choose other function:)");
+                    break;
+                case 2:
+                    System.out.println("Please enter the task you want to add in to the task list.");
+                    System.out.println("Enter 'list' to view task list and mark task as done.");
+                    String task;
+                    Scanner b = new Scanner(System.in);
+                    int index = 0;
+                    task = b.nextLine();
+                    String[] tasks = new String[100];
+                    while (true) {
+                        if (task.equalsIgnoreCase("bye")) {
+                            System.out.println("Bye, hope to see you again again soon!");
+                            break;
+                        }
+                        else if (task.equals("list")) {
+                            System.out.print("Here are the tasks in your list: \n");
+                            for (int i = 0; i < index; i++) {
+                                System.out.println((i + 1) + ": " + tasks[i]);
+                            }
+                            System.out.println("Please enter the task number to be mark as done: ");
+                            Scanner c = new Scanner(System.in);
+                            int taskNo = c.nextInt();
+                            Task t = new Task (tasks[taskNo-1]);
+                            t.markAsDone();
+                            break;
+                        }
+                        else{
+                            System.out.println("Added: " + task.trim());
+                            tasks[index] = task;
+                            index++;
+                        }
+                        task = b.nextLine();
+                    }
+                    break;
+                case -1:
 
-        while (task != null){
-            if (task.equals("bye")){
-                System.out.println("Bye, hope to see you again again soon!");
-                break;
-            }
-            else if(task.equals("list")){
-                for(int i=0; i<index; i++)
-                {
-                    System.out.println((i+1) + ": " + tasks[i]);
-                }
-                task = in.nextLine();
-            }
-            else{
-                tasks[index] = task;
-                System.out.println("Added: " + task);
-                index++;
-                task = in.nextLine();
+                    run = false;
+                default:
+                    break;
             }
         }
     }
