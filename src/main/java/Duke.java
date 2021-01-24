b2aa75422771bfea936e2f64d398bb8951c0c2a8
Leow Xiao Xuan <leowxiaoxuan@gmail.com>
2021-01-24 16:23:46 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "____________________________________________________________\n"
                 + " Hello! I'm Duke\n"
                 + " What can I do for you?\n\n"
-                +"____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n\n"
                 +"____________________________________________________________\n";
         System.out.println(logo);
+        Scanner stringScanner = new Scanner(System.in);
+        String userQuery = stringScanner.nextLine();
+        String userQueryReturn = "";
+        while(!userQuery.equalsIgnoreCase("bye")){
+            userQueryReturn = "____________________________________________________________\n"
+                    + " " + userQuery + "\n"
+                    + "____________________________________________________________\n";
+            System.out.println(userQueryReturn);
+            userQuery = stringScanner.nextLine();
+        }
+        String bye = "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n\n"
+                +"____________________________________________________________\n";
+        System.out.println(bye);
     }
 }
