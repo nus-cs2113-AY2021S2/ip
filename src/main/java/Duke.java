76cbb7b646dc4fadba69ba08c650ab785f96b793
Leow Xiao Xuan <leowxiaoxuan@gmail.com>
2021-01-24 17:14:32 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,12 +10,25 @@ public class Duke {
         Scanner stringScanner = new Scanner(System.in);
         String userQuery = stringScanner.nextLine();
         String userQueryReturn = "";
+        String[] listItems = new String[100];
+        int itemCount = 0;
         while(!userQuery.equalsIgnoreCase("bye")){
-            userQueryReturn = "____________________________________________________________\n"
-                    + " " + userQuery + "\n"
-                    + "____________________________________________________________\n";
-            System.out.println(userQueryReturn);
-            userQuery = stringScanner.nextLine();
+            listItems[itemCount] = userQuery;
+            if(!userQuery.equalsIgnoreCase("list")) {
+                userQueryReturn = "____________________________________________________________\n"
+                        + " added: " + userQuery + "\n"
+                        + "____________________________________________________________\n";
+                System.out.println(userQueryReturn);
+                userQuery = stringScanner.nextLine();
+            }else{
+                System.out.println("____________________________________________________________");
+                for(int i=0; i<itemCount; i++){
+                    System.out.println(" "+String.valueOf(i+1)+". "+listItems[i]);
+                }
+                System.out.println("____________________________________________________________\n");
+                userQuery = stringScanner.nextLine();
+            }
+            itemCount++;
         }
         String bye = "____________________________________________________________\n"
                 + " Bye. Hope to see you again soon!\n\n"
