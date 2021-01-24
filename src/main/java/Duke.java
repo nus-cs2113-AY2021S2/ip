b597dee45feca53fdcb71f26df1dd265e67c5eac
Ong Wei Sheng <ongweisheng30@gmail.com>
2021-01-24 22:07:11 +0800

    Add handleCommand method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,53 @@
+import java.util.Scanner;
+
 public class Duke {
+    
+    public static String HORIZONTAL_LINE = "____________________________________________________________";
+    /**
+     * Handles commands from user
+     * @param command command from the user
+     */
+    public static void handleCommand(String command) {
+        switch (command) {
+        case "bye":
+            System.out.println(HORIZONTAL_LINE);
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(HORIZONTAL_LINE);
+            break;
+        default:
+            System.out.println(HORIZONTAL_LINE);
+            System.out.println(command);
+            System.out.println(HORIZONTAL_LINE);
+            break;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
         // logo/loading
+        System.out.println(HORIZONTAL_LINE);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
+        System.out.println(HORIZONTAL_LINE);
+
         // greeting
         String greeting = "Hello! I'm Duke\n" + "What can I do for you?";
         System.out.println(greeting);
-        System.out.println("____________________________________________________________");
-        // exit message
-        String exitMessage = "Bye. Hope to see you again soon!";
-        System.out.println(exitMessage);
-        System.out.println("____________________________________________________________");
+        System.out.println(HORIZONTAL_LINE);
+
+        String command; // receiving command input
+        boolean shouldExit = false;
+        Scanner in = new Scanner(System.in);
+        while (!shouldExit) {
+            command = in.nextLine();
+            if (command.equals("bye")) {
+                handleCommand(command);
+                shouldExit = true;
+            } else {
+                handleCommand(command);
+            }
+        }
     }
 }
