3eb435a5d880085210afc1b0475f3c98d1238177
Oscar Lai <e0424620@u.nus.edu>
2021-01-24 23:10:42 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,44 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Duke {
-    
+
 	public static final String border = "____________________________________________________________";
-	public static final String logo =  " ____        _        \n"
+	public static String logo =  " ____        _        \n"
 			+ "|  _ \\ _   _| | _____ \n"
 			+ "| | | | | | | |/ / _ \\\n"
 			+ "| |_| | |_| |   <  __/\n"
 			+ "|____/ \\__,_|_|\\_\\___|\n";
 
-	public static void greeting(){ 
-		System.out.println(border + "\n" + "Hello! I'm Duke\nWhat can I do for you?");
+	public static void printGreeting(){
+		System.out.println(border + "\n" + "Hello! I'm\n" + logo + "What can I do for you?");
 	}
 
-	public static void exit(){
+	public static void printExit(){
 		System.out.println(border + "\n" + "Bye. Hope to see you again soon!\n" + border);
 	}
-	
-	public static void main(String[] args) {
-		System.out.println("Hello from\n" + logo);
-		greeting();
-		exit();
+
+	public static String readInput(){
+		Scanner input = new Scanner(System.in);
+		return input.nextLine();
 	}
-	
+
+	public static void echo(){
+		while(true){
+			String input = readInput();
+			if(input.equals("bye")){
+				printExit();
+				System.exit(1);
+			}
+			else{
+				System.out.println(border + "\n" + input + "\n" + border);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		printGreeting();
+		echo();
+	}
+
 }
