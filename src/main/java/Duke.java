4d84a60d068509091e6b9d2fccabcf9f9c5ab9b7
LeeHanYongAndy <andyleehy2502@gmail.com>
2021-01-24 10:25:04 +0800

    Updated methods naming
    Changed to camelCase format

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,5 @@
 public class Duke {
-    public static void introduction() {
+    public static void printHelloStatement() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,17 +9,16 @@ public class Duke {
         System.out.println(logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        // Actions - To be added
         System.out.println("____________________________________________________________");
 
     }
-    public static void ending() {
-        // Completion & Exiting - To be added
+    public static void printBye() {
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.print("____________________________________________________________");
     }
     public static void main(String[] args) {
-        introduction();
-        ending();
+        printHelloStatement();
+        printBye();
     }
 }
