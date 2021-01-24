8d424019154e6fc4c156e2e345d434d7112d0105
Rizavur Rahman <rizavur@gmail.com>
2021-01-24 11:32:29 +0800

    Add the feature to store text entered by the user and display it back to them upon request.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     private static final String BREAKLINE = "------------------------------------------------------------";
-
+    private static List<String> userList = new ArrayList<String>();
     public static void showLogo() {
         String logo = " ____        _        \n"
                     + "|  _ \\ _   _| | _____ \n"
@@ -23,11 +25,23 @@ public class Duke {
         System.out.println(BREAKLINE);
         return input;
     }
+    public static void showList() {
+        int count = 1;
+        for (String i: userList) {
+            System.out.println(count + ". " + i);
+            count++;
+        }
+        System.out.println(BREAKLINE);
+    }
+    public static void addToList(String input){
+        userList.add(input);
+        System.out.println("Added: " + input);
+        System.out.println(BREAKLINE);
+    }
     public static void byeMessage() {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(BREAKLINE);
     }
-
 
     public static void main(String[] args) {
         showLogo();
@@ -38,9 +52,11 @@ public class Duke {
             case "bye":
                 byeMessage();
                 return;
+            case "list":
+                showList();
+                break;
             default:
-                System.out.println(input);
-                System.out.println(BREAKLINE);
+                addToList(input);
                 break;
             }
         }
