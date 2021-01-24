741dfeab0d428df94234eb15d915e2dbe842f3b1
hiongkaihan <kaihan00000@gmail.com>
2021-01-24 18:08:21 +0800

    add echo, greet, bye, logo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,58 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        // Logo
+
+    public static void printLine(){
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void printLineWithNewLine(){
+        System.out.println("____________________________________________________________\n");
+    }
+
+    public static void printLogo(){
+        printLine();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println(line);
-        // Greeting
+        printLineWithNewLine();
+    }
+    public static void printHello() {
+        printLine();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println(line);
-        // Bye
+        printLineWithNewLine();
+    }
+    public static void printBye() {
+        printLine();
         System.out.println("Bye. Hope to see you again soon!");
+        printLineWithNewLine();
+    }
+
+    public static void echoMessage(String line){
+        printLine();
         System.out.println(line);
+        printLineWithNewLine();
+    }
+
+    public static void main(String[] args) {
+
+        printLogo();
+        printHello();
+
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while(true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                printBye();
+                break;
+            }
+            echoMessage(line);
+        }
     }
 }
