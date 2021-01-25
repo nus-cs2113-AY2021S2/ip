f0df860ddc59f69fa86c6d8c3d9ef679b09dea2b
Oscar Lai <e0424620@u.nus.edu>
2021-01-25 18:02:16 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,42 +3,87 @@ import java.util.Scanner;
 
 public class Duke {
 
-	public static final String border = "____________________________________________________________";
-	public static String logo =  " ____        _        \n"
-			+ "|  _ \\ _   _| | _____ \n"
-			+ "| | | | | | | |/ / _ \\\n"
-			+ "| |_| | |_| |   <  __/\n"
-			+ "|____/ \\__,_|_|\\_\\___|\n";
+	private static final String border = "____________________________________________________________";
+	private static final String HelpMessage = "HELP COMMANDS\n"
+			+" list - view timetable\n"
+			+" bye  - terminate program\n";
+	public static int MAX_SIZE = 100;
+	public static Task[] Timetable = new Task[MAX_SIZE];
+	private static int counter = 0;
 
 	public static void printGreeting(){
-		System.out.println(border + "\n" + "Hello! I'm\n" + logo + "What can I do for you?");
+		String alfred = "⊂_ヽ\n"
+				+"　 ＼ ＼ what\n"
+				+"　　  ＼ ( ͡° ͜ʖ ͡°) can I \n"
+				+"　　　 >　   ⌒    ヽ\n"
+				+"　　　/ 　   へ      ＼\n"
+				+"　　/　　  /　  ＼   do for you?\n"
+				+"　　(　  (ヽ　　    ヽ _   つ\n"
+				+"   |  　| \\ \n"
+				+"　  | 丿 ＼ ⌒)\n"
+				+"　  | |　　) /\n"
+				+"   ノ )　　Lﾉ \n"
+				+"  (_／\n";
+		System.out.println(border + "\n" + "Good Day, I'm Alfred.\n" + alfred + border);
 	}
 
 	public static void printExit(){
-		System.out.println(border + "\n" + "Bye. Hope to see you again soon!\n" + border);
+		String wave = " __      __\n"
+				+"( _\\    /_ )\n"
+				+" \\ _\\  /_ / \n"
+				+"  \\ _\\/_ /_ _\n"
+				+"  |_____/_/ /|\n"
+				+"  (  (_)__)J-)\n"
+				+"  (  /`.,   /\n"
+				+"   \\/  ;   /\n"
+				+"    | === |See you again!\r\n";
+		System.out.println(border + "\n" + "Pleasure serving you...\n" + wave + border);
 	}
 
 	public static String readInput(){
-		Scanner input = new Scanner(System.in);
-		return input.nextLine();
+		Scanner in = new Scanner(System.in);
+		String input = in.nextLine();
+		return input;
 	}
 
-	public static void echo(){
+	public static void addTask(String input) {
+
+		Timetable[counter] = new Task(input);
+		counter++;
+		System.out.println("Added: " + input + "\n" + border);
+	}
+
+
+	public static void loop(){
 		while(true){
 			String input = readInput();
-			if(input.equals("bye")){
+			if(input.equalsIgnoreCase("bye")){
 				printExit();
 				System.exit(1);
 			}
-			else{
-				System.out.println(border + "\n" + input + "\n" + border);
+			else if(input.equalsIgnoreCase("help")) {
+				System.out.println(HelpMessage);
 			}
+			else if(input.equalsIgnoreCase("list")) {
+				printTask(Timetable);
+			}
+			else{
+				addTask(input);
+			}
+		}
+	}
+
+	private static void printTask(Task[] timetable) {
+		int i;
+		System.out.println("Timetable\n");
+		for(i=0;i<counter;i++) {
+			System.out.println(i+1 + ". " + timetable[i].getName());
 		}
 	}
 
 	public static void main(String[] args) {
 		printGreeting();
-		echo();
+		loop();
 	}
 
 }
