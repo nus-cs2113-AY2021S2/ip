2c5319d54f8f35d3c2fc002ec788bacd488c4b70
ivanchongzhien <icze2009@gmail.com>
2021-01-25 19:45:47 +0800

    Add level1 functionality
    Add while loop to echo user input, exits when user enters "bye"
    changed Duke to Diuk

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String helloMessage = "Hello! I\'m Duke \n" +
-                "What can I do for you?\n";
 
+        String helloMessage = "Hello! I\'m Diuk \n" + "What can I do for you?\n";
         String byeMessage = "Bye. Hope to see you again soon!\n";
-
         String line = "____________________________________________________________\n";
 
+        // Start - Greet user
         System.out.print(line);
         System.out.print(helloMessage);
+        System.out.print(line);
+
+        Scanner in = new Scanner(System.in);
+
+        // Loop to receive response
+        while (true){
+            String input = in.nextLine();
+
+            // check if exit
+            if(input.toUpperCase().equals("BYE")){
+                break;
+            }
+
+            // echo user command
+            System.out.println(input + '\n');
+        }
+
+
         System.out.print(line);
         System.out.print(byeMessage);
         System.out.print(line);
