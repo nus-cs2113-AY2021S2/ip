becb8490b97932ff996e63e75a13a2cd98281a9f
hazelhedmine <66232361+zelleh15@users.noreply.github.com>
2021-01-25 15:24:13 +0800

    added level 1, greet echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        String command;
+        String horizontalLine = "____________________________________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
+        System.out.println(horizontalLine);
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(horizontalLine);
+
+        while (!(command = userInput.nextLine()).equals("bye")) {
+            System.out.println(" " + command + "\n" + horizontalLine);
+        }
+        System.out.println(" Bye. Hope to see you again soon!\n" + horizontalLine);
     }
 }
