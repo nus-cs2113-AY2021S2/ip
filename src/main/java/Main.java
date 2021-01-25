83eb5d49b5ecd746f4d7d17297be52fac7133d41
Emkay16 <mabelkohjx@gmail.com>
2021-01-25 10:20:48 +0800

    Initial level-0 commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What  can I do for you?" + System.lineSeparator());
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
