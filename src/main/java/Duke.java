74406904c31f665a6dcde697ce5ff09da4b6f32c
KalanaManikaHennedige <mhennedige@gmail.com>
2021-01-25 15:20:37 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -14,8 +16,25 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println(divider);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(divider);
+        // the following is to fulfil the Level-1 increment
+        Scanner sc = new Scanner(System.in);
+
+        String input;
+        while (true) {
+            input = sc.nextLine();
+            if (!input.equals("bye")) {
+                System.out.println(divider);
+                System.out.println("Duke says: " + input);
+                System.out.println(divider);
+            }
+            else {
+                System.out.println(divider);
+                System.out.println("Bye! Hope to hear from you again soon!");
+                System.out.println(divider);
+                break;
+            }
+
+        }
 
     }
 }
