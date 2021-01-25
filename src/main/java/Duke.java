43ef4fba4fdfb184d54fd8070cbe885db01e2867
Tyuanyuan <190060010@qq.com>
2021-01-25 14:22:56 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,12 +15,23 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         boolean exit = false;
+        String[] storeList = new String[100];
+        int inputIndex = 0;
         while (!exit){
             String line = in.nextLine();
-            if (!line.equals("bye")){
-                System.out.println(line);
+            if (!line.equals("bye") && !line.equals("list")){
+                System.out.println("added: " + line);
+                storeList[inputIndex] = line;
+                inputIndex++;
+                continue;
             }
-            else {
+            else if (line.equals("list")){
+                for (int i=0; i<inputIndex; i++){
+                    System.out.println(i+1 + ": " + storeList[i]);
+                }
+                continue;
+            }
+            else if (line.equals("bye")) {
                 System.out.println("____________________________________________________________\n" +
                         " Bye. Hope to see you again soon!\n" +
                         "____________________________________________________________\n");
