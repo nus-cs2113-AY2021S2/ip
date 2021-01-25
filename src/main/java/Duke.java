00f0554b3ffbd77ae7faa8b36ca3b6784f47ea8e
jovanhuang <jhtc98@hotmail.com>
2021-01-25 16:37:47 +0800

    Refactor code and move codes from main method to helper methods.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,40 +4,9 @@ import java.util.Scanner;
 public class Duke {
 
     private static int taskCount = 0;
-    private static String[] list = new String[100];
-    private static Task[] tasksObjects = new Task[100];
+    private static Task[] tasksObjectsArray = new Task[100];
 
     public static void printIntroMessage(){
-        System.out.println("________________________________");
-        System.out.println("Hello, I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println();
-        System.out.println("________________________________");
-    }
-
-    public static void printExitMessage(){
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("________________________________");
-        System.exit(0);
-    }
-
-    public static void addTaskToList(Task task){
-        list[taskCount] = taskCount+1 +". [" + task.getStatusIcon() + "]" + task.getDescription();
-        tasksObjects[taskCount] = task;
-        taskCount += 1;
-    }
-
-    public static String[] removeNullFromStringList(String[] tasks){
-        return Arrays.copyOf(tasks, taskCount);
-    }
-
-    public static void printList(String[] list){
-        for (String item: list){
-            System.out.println(item);
-        }
-    }
-
-    public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -45,37 +14,66 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        printIntroMessage();
+        System.out.println("________________________________");
+        System.out.println("Hello, I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println("________________________________");
+    }
+    public static void printExitMessage(){
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________");
+        System.exit(0);
+    }
 
+    public static void addTaskToTaskList(Task task){
+        tasksObjectsArray[taskCount] = task;
+        taskCount += 1;
+        System.out.println("________________________________");
+        System.out.println("added: " + task.getDescription());
+        System.out.println("________________________________");
+    }
+
+    public static void markTaskAsDone(int taskNumber){
+        System.out.println("Nice! I've marked this task as done:");
+        tasksObjectsArray[taskNumber-1].markAsDone();
+        System.out.println("[" + tasksObjectsArray[taskNumber-1].getStatusIcon() + "]"
+                + tasksObjectsArray[taskNumber-1].getDescription());
+
+    }
+
+    public static Task[] removeNullFromList(Task[] tasks){
+        return Arrays.copyOf(tasks, taskCount);
+    }
+
+    public static void printTaskList(Task[] tasks){
+        int taskNumber = 1;
+        System.out.println("________________________________");
+        for (Task task: tasks){
+            System.out.println(taskNumber + "[" + task.getStatusIcon() + "]" + task.getDescription());
+            taskNumber +=1 ;
+        }
+        System.out.println("________________________________");
+    }
+
+    public static void main(String[] args) {
+        printIntroMessage();
         while (true){
             Scanner in = new Scanner(System.in);
             String input = in.nextLine();
-
             if (input.equals("list")){
-                System.out.println("________________________________");
-                printList(removeNullFromStringList(list));
-                System.out.println("________________________________");
+                printTaskList(removeNullFromList(tasksObjectsArray));
             }
             else if (input.equals("bye")) {
                 printExitMessage();
-                break;
             }
             else if (input.startsWith("done")) {
-                System.out.println("Nice! I've marked this task as done:");
                 int taskNumberDone = Integer.parseInt(input.substring(5));
-                System.out.println("[" + tasksObjects[taskNumberDone-1].getStatusIcon() + "]"
-                        + tasksObjects[taskNumberDone-1].getDescription());
-                tasksObjects[taskNumberDone-1].markAsDone();
-                list[taskNumberDone-1] = taskNumberDone +". [" + tasksObjects[taskNumberDone-1].getStatusIcon() + "]"
-                        + tasksObjects[taskNumberDone-1].getDescription();
-
+                markTaskAsDone(taskNumberDone);
             }
             else{
                 Task t = new Task(input);
-                addTaskToList(t);
-                System.out.println("________________________________");
-                System.out.println("added: " + t.getDescription());
-                System.out.println("________________________________");
+                addTaskToTaskList(t);
             }
         }
     }
