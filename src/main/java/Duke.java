4b092eb7a10be8dbce47355454dcb3b7b277fb20
Fuxi <fuxi_xia@u.nus.edu>
2021-01-25 21:34:10 +0800

    Implement Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Duke {
@@ -7,10 +6,15 @@ public class Duke {
     public static void main(String[] args) {
         printGreeting();
         printLine();
-        loopAddAndList();
+        loopAddListAndMark();
         sayGoodbye();
     }
-    public static void loopAddAndList() {
+
+    /**
+     * Loops through all features available.
+     * Returns if user types bye or number of tasks exceeds 100
+     */
+    public static void loopAddListAndMark() {
         while(true) {
             if(Task.totalTasks > 100) {
                 System.out.println("Maximum capacity reached");
@@ -20,20 +24,52 @@ public class Duke {
             if(line.equalsIgnoreCase("bye")) {
                 return;
             }
+            // If user wants to mark a task as done
+            if(line.substring(0, 4).equalsIgnoreCase("done")) {
+                // Check validity of input
+                if(isValidDone(line)) {
+                    int listNum = Integer.parseInt(line.substring(5));
+                    // Check for illegal access to out of bounds index
+                    if(listNum > Task.totalTasks) {
+                        System.out.println("Woah woah. Don't violate the rules aight. I'm watching you.");
+                        printLine();
+                        continue;
+                    }
+                    listNum -= 1;
+                    Tasks[listNum].setDone();
+                    System.out.println(Tasks[listNum].name + " set to done. You're well smart innit?");
+                } else {
+                    System.out.println("Aye aye that's the wrong format homie. " +
+                            "To set a task to done, type done <number>.");
+                }
+                printLine();
+                continue;
+            }
+            // If user wants to list all tasks
             if(line.equalsIgnoreCase("list")) {
                 listItems();
-            }
-            else {
+            } else {
+                // If user wants to add an item
                 addItem(line);
             }
         }
     }
+
+    /**
+     * Lists all items added to the list
+     */
     public static void listItems() {
+        System.out.println("Eez are the tings you added to the list");
         for(int i = 0; i < Task.totalTasks; i++) {
-            System.out.println(i+1 + ". " + Tasks[i].name);
+            System.out.println(i+1 + ".[" + Tasks[i].getStatusIcon() + "] " + Tasks[i].name);
         }
         printLine();
     }
+
+    /**
+     * Append new item to back of the list
+     * @param nameOfTask name of new item
+     */
     public static void addItem(String nameOfTask) {
         int current = Task.totalTasks;
         Tasks[current] = new Task(nameOfTask);
@@ -42,6 +78,18 @@ public class Duke {
         printLine();
     }
 
+    /**
+     * Checks if input is of valid format.
+     * Format should be "done<space><number>.
+     * @param line input string
+     * @return true if line is valid format, false otherwise
+     */
+    public static boolean isValidDone(String line) {
+        if(line.substring(5).matches("[0-9]+")) {
+            return true;
+        }
+        return false;
+    }
     public static void printGreeting() {
         System.out.println("Wagwan! I is Ali G. West side.");
         System.out.println("What is we chattin bout today?");
@@ -55,8 +103,6 @@ public class Duke {
     }
     public static String getInput() {
         Scanner in = new Scanner(System.in);
-        String line = in.nextLine();
-
-        return line;
+        return in.nextLine();
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,8 +1,22 @@
 public class Task {
-    public String name;
+    protected String name;
+    protected boolean isDone;
     public static int totalTasks = 0;
 
     public Task(String name) {
         this.name = name;
+        this.isDone = false;
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return (isDone? "\u2718" : " ");
+    }
+
+    public String getName() {
+        return name;
     }
 }
