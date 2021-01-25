e915fc4d76d5ebb90e277f52cbe7a09ae1f1d843
L-Irvin <leonardoirvinp@gmail.com>
2021-01-25 15:23:46 +0800

    add a task class and mark function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        ArrayList<String> inputs = new ArrayList<>();
+        ArrayList<Task> inputs = new ArrayList<>();
 
         String DECO_LINE = "____________________________________________________________";
         String HELLO_MESSAGE = " Hello! I'm Duke";
@@ -23,16 +23,35 @@ public class Duke {
             if (input.equals("list")) {
                 System.out.println(DECO_LINE);
                 for (int i = 0; i < inputs.size(); i++) {
-                    System.out.println(" " + (i + 1) + ". " + inputs.get(i));
+                    System.out.println(" " + (i + 1) + ". [" + inputs.get(i).getStatusIcon() + "] " + inputs.get(i).description);
+
                 }
                 System.out.println(DECO_LINE);
                 System.out.println();
+            } else if (input.split(" ")[0].equals("done")) {
+                System.out.println(DECO_LINE);
+                try {
+                    int ind = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (ind < inputs.size() && ind >= 0) {
+                        if (inputs.get(ind).isDone) {
+                            System.out.println("Task is already marked done!");
+                        } else {
+                            inputs.get(ind).markAsDone();
+                            System.out.println(" [" + inputs.get(ind).getStatusIcon() + "] " + inputs.get(ind).description);
+                        }
+                    } else {
+                        System.out.println("Index is out of boundary!");
+                    }
+                } catch (NumberFormatException nfe) {
+                    System.out.println("Format is wrong!");
+                }
+                System.out.println(DECO_LINE);
             } else {
                 System.out.println(DECO_LINE);
                 System.out.println(" added: " + input);
                 System.out.println(DECO_LINE);
                 System.out.println();
-                inputs.add(input);
+                inputs.add(new Task(input));
             }
             input = readinput.nextLine();
         }
