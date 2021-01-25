ea05cde709fc38011cbc413411a643bca6d5c137
Rye98 <zhong.shuhao@icloud.com>
2021-01-25 20:06:31 +0800

    Update Level-0 greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String greeting = "Hello! I'm Duke. \n"
+                + "What can I do for you?";
+        String sign_off = "Bye. Hope to see you again soon!";
+
+        System.out.println(greeting);
+        System.out.println(sign_off);
     }
+
+
 }
