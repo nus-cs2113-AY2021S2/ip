133573e890b485ca6a61d13edaa0f58ba161ca1f
BlubberMonster <e0406263@u.nus.edu>
2021-01-25 21:44:05 +0800

    Add Individual Project Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
+        String command;
+        Scanner in = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
-        System.out.println("Hi! I'm Duke");
-        System.out.println("What can I do for you today?");
+        System.out.println("    Hi! I'm Duke (:");
+        System.out.println("    What can I do for you today?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        command = in.nextLine();
+        do {
+            System.out.println("____________________________________________________________");
+            System.out.println("    " + command);
+            System.out.println("____________________________________________________________");
+            command = in.nextLine();
+        } while (!command.equals("bye"));
+        System.out.println("____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
