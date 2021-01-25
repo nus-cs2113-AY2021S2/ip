95d4174d5c4674e4389439748bb22b5d78cce079
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-01-25 20:54:03 +0800

    Added list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -36,10 +36,28 @@ public class Duke {
         System.out.println(bye);
 
     }
-    public static void echo(String line){
-        line = "____________________________________________________________"+ System.lineSeparator()
-                +line+System.lineSeparator()+"____________________________________________________________"+ System.lineSeparator();
-        System.out.println(line);
+
+    public static int addToList(String[] list, int listCount, String task){
+        list[listCount]= task;
+        System.out.println("____________________________________________________________");
+        System.out.println("added: "+task);
+        System.out.println("____________________________________________________________");
+        listCount++;
+        return listCount;
+    }
+    public static void printList(String[] list, int listCount){
+        for(int index =0; index< listCount;index++){
+            System.out.println("____________________________________________________________");
+            System.out.println((index+1)+". "+list[index]);
+        }
+        if(listCount==0){
+            System.out.println("____________________________________________________________");
+            System.out.println("This list is empty!!! YEEEEEEET!!!");
+            System.out.println("____________________________________________________________");
+        }
+        else{
+            System.out.println("____________________________________________________________");
+        }
 
     }
 
@@ -47,6 +65,8 @@ public class Duke {
         greet();
         boolean hasToExit = false;
         String line;
+        int listCount =0;
+        String[] list = new String[100];
         Scanner in = new Scanner(System.in);
         while(!hasToExit){
             line = in.nextLine();
@@ -55,8 +75,12 @@ public class Duke {
                 exit();
                 hasToExit = true;
                 break;
+            case "list":
+                printList(list,listCount);
+                break;
+
             default:
-                echo(line);
+                listCount=addToList(list,listCount,line);
                 break;
             }
         }
