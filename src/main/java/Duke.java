6db9aaec706a058da81a1e4dafba0ddafcd4d864
boonjuey <boonjuey98@gmail.com>
2021-01-25 20:39:41 +0800

    add greet, echo, exit commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -12,9 +14,32 @@ public class Duke {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+
+        Scanner in = new Scanner(System.in);
+        String command;
+        command = in.nextLine();
+        while(!command.equals("bye")){
+            switch(command){
+                case "list":
+                    System.out.println("____________________________________________________________");
+                    System.out.println("list");
+                    System.out.println("____________________________________________________________");
+                    break;
+                case "blah":
+                    System.out.println("____________________________________________________________");
+                    System.out.println("blah");
+                    System.out.println("____________________________________________________________");
+                    break;
+                case "bye":
+                    break;
+                default:
+                    break;
+            }
+            command = in.nextLine();
+        }
+
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
-
     }
 }
