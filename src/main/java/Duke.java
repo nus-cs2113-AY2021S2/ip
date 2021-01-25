e4f2b98a3036b7bbb0cf13cfb168c4213bb1240c
Lee Yang Peng <lee.yangpeng@dhs.sg>
2021-01-25 16:53:05 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         /**String logo = " ____        _        \n"
@@ -9,6 +11,15 @@ public class Duke {
          **/
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
+
+        String input;
+        Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+        while (!input.equals("bye")){
+            System.out.println(input + "\n");
+            input = in.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!\n");
 
     }
