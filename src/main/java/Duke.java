f566e30074c28075d4cf9a8e99cbe4cf563b80fa
Jo-Nah <jonahtham@u.nus.edu>
2021-01-26 00:18:34 +0800

    Added echo
    Customised greeting and exit to add a personal flavour

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,48 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        /**
+         * String logo = " ____        _        \n"
+         *       + "|  _ \\ _   _| | _____ \n"
+         *       + "| | | | | | | |/ / _ \\\n"
+         *       + "| |_| | |_| |   <  __/\n"
+         *       + "|____/ \\__,_|_|\\_\\___|\n";
+         * System.out.println("Hello from\n" + logo);
+         */
 
-        String greeting = "__________________________________________\n"
-                        + "Hello! I'm Duke\n"
-                        + "What can I do for you?\n"
-                        + "__________________________________________\n"
-                        + "Bye. Hope to see you again soon!\n"
-                        + "__________________________________________\n";
+        String logo = "**********************************\n"
+                + "*       Systems: [Online]        *\n"
+                + "*      Protocol: [Dominion]      *\n"
+                + "*          Race: [Terran]        *\n"
+                + "**********************************\n";
+
+        String greeting = "______________________________________________________\n"
+                + "Systems Accessed...\n"
+                + "Decrypting Overwrite...\n"
+                + "Welcome Commander, can I be of assistance, commander?\n"
+                + "______________________________________________________\n";
+
+        String exit = "______________________________________________________\n"
+                + "Good Bye Commander.\n"
+                + "______________________________________________________\n";
+
+        System.out.println(logo);
         System.out.println(greeting);
+
+        String input;
+        Scanner in = new Scanner(System.in);
+
+        while(true){
+            input = in.nextLine();
+            if(input.equalsIgnoreCase("bye")){
+                break;
+            }
+            String echo = "______________________________________________________\n"
+                    + "[Orders received] " + input + "\n"
+                    + "______________________________________________________\n";
+            System.out.println(echo);
+        }
+        System.out.println(exit);
     }
 }
