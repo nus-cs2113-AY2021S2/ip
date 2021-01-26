87e6483083583cfa4cf1f4e82ee350e539a4b9c0
xseh <sehxinru@gmail.com>
2021-01-26 18:25:27 +0800

    Update code syntax to follow standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-
         System.out.println("    ____________________________________________________________");
         System.out.println("     Hello! I'm Duke");
         System.out.println("     What can I do for you?");
@@ -11,48 +10,55 @@ public class Duke {
         Task[] list = new Task[100];
         int lengthOfList = 0;
 
-
         while (true) {
             Scanner in = new Scanner(System.in);
-            //remove trailing spaces
+            // Remove trailing spaces
             String command = in.nextLine().trim();
 
             switch (command.split(" ")[0].toLowerCase()) {
             case "bye":
-                //print bye statement and exit program
+                // Print bye statement and exit program
+                // Fallthrough
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     Bye. Hope to see you again soon!");
                 System.out.println("    ____________________________________________________________\n");
                 in.close();
                 return;
-
             case "list":
-            //print every task in list
+                // Print every task in list
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     Here are the tasks in your list: ");
                 for (int i = 0; i < lengthOfList; i++) {
-                    System.out.println(String.format("     %d.[%s] %s", i+1, list[i].getStatusIcon(), list[i].getDescription()));
+                    System.out.println(String.format("     %d.[%s] %s", i+1, list[i].getStatusIcon(), 
+                        list[i].getDescription()));
                 }
+
                 System.out.println("    ____________________________________________________________\n");
                 break;
             case "done":
+                System.out.println("    ____________________________________________________________");
                 int taskNumber = Integer.parseInt(command.substring(command.indexOf(" ")+1)) - 1;
+                // Ensure that the task number fit the length of task
                 if (taskNumber >= 0 && taskNumber < lengthOfList) {
                     list[taskNumber].setStatus();
-                    System.out.println("    ____________________________________________________________");
+                    
                     System.out.println("     Nice! I've marked this task as done: ");
-                    System.out.println(String.format("       [%s] %s", list[taskNumber].getStatusIcon(), list[taskNumber].getDescription()));
-                    System.out.println("    ____________________________________________________________");
+                    System.out.println(String.format("       [%s] %s", list[taskNumber].getStatusIcon(), 
+                        list[taskNumber].getDescription()));
+                } else {
+                    System.out.println("     Task not found!");
                 }
+                System.out.println("    ____________________________________________________________\n");
                 break;
             case "": 
+                // If no command detected, do nothing and await another command
                 break;
             default:
-                //add new Task to list
+                // Add new Task to list
                 list[lengthOfList] = new Task(command);
                 lengthOfList++;
 
-                //print notification of addition
+                // Print notification of addition
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     added: " + command);
                 System.out.println("    ____________________________________________________________\n");
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,14 @@
 public class Task {
+    /** Description of task */
     protected String description;
+    /** Boolean to track if task is completed */
     protected boolean isDone;
 
+    /**
+     * Creates a Task object with the given description. 
+     * 
+     * @param description Task description. 
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
@@ -23,7 +30,14 @@ public class Task {
         isDone = true;
     }
 
+    /**
+     * Returns the status icon depending on isDone boolean. 
+     * If the task is done (i.e. isDone is true), "X" is returned. 
+     * If the task is not done (i.e. isDone is false), " " is returned. 
+     * 
+     * @return Status icon
+     */
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); //return X or empty
+        return (isDone ? "X" : " ");
     }
 }
