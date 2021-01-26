bbc9e51c375837bad6a36b872ac92e74dbba351e
JonathanKhooTY <jonathan.khooty@gmail.com>
2021-01-26 22:54:07 +0800

    Update Duke.java with level-2 requirements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,27 +2,39 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static String echoCommand(){
-        Scanner in = new Scanner(System.in);
-        String command = in.nextLine();
-        System.out.println(command);
-        return command;
+    static String texts[] = new String[100];
+    static int textPosition = 0;
+
+
+    public static void storeText(String text){
+        texts[textPosition] = text;
+        textPosition++;
+        System.out.println("added: " + text);
+    }
+
+    public static void listArray(String[] texts){
+        int textNumber = 1;
+        for(String text:texts){
+            if(text != null){
+                System.out.println(textNumber + ". " + text);
+                textNumber++;
+            }
+        }
     }
 
     public static void main(String[] args) {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-         */
-
         System.out.println("Hello! I'm 343 Guilty Spark! You may call me Spark!");
         System.out.println("What can I do for you?");
-        while (!echoCommand().equalsIgnoreCase("Bye")){
-            echoCommand();
+        String text = "hi";
+        while(!text.equalsIgnoreCase("Bye")){
+            Scanner in = new Scanner(System.in);
+            text = in.nextLine();
+            if(text.equalsIgnoreCase("List")){
+                listArray(texts);
+            }
+            else {
+                storeText(text);
+            }
         }
         System.out.println("Bye! Hope to see you again soon!");
     }
