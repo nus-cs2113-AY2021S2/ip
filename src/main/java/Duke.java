678ea52b200a557ea574e9929637cbd75cd37c29
kwokyto <ryankwok@u.nus.edu>
2021-01-26 10:14:53 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void printWithBorder(String line) {
+        System.out.print("___________________________________________________\n");
+        System.out.print(line + "\n");
+        System.out.print("___________________________________________________\n");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,8 +14,13 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
-        System.out.println("___________________________________________________");
-        System.out.println("Bye. Hope to see you again!");
-        System.out.println("___________________________________________________");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (line != "bye") {
+            printWithBorder("Did I hear you correctly? You said: " + line);
+        }
+        printWithBorder("Sad to see you go! ): See you soon!");
     }
 }
