364afcc10cc21f4d3259ab079ef5c3b01953cc60
Emkay16 <mabelkohjx@gmail.com>
2021-01-26 17:03:26 +0800

    Add printList and add methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,19 +4,49 @@ public class Main {
     public static void printLine() {
         System.out.println("_____________________________________________________");
     }
+
+    public static void echo(String input) {
+        printLine();
+        System.out.println(input);
+        printLine();
+    }
+
+    public static void add(String input, String[] list, int index) {
+        list[index] = input.trim();
+        printLine();
+        System.out.println("\"" + list[index] + "\"" + " added to list");
+        printLine();
+    }
+
+    public static void printList(String[] list, int index) {
+        printLine();
+        for(int i=0; i<index; i++) {
+            System.out.print(i+1 + ") ");
+            System.out.println(list[i]);
+        }
+        printLine();
+    }
+
     public static void main(String[] args) {
         printLine();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         printLine();
 
+        String[] list = new String[100];
+        int index = 0;
+
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
 
-        while (!input.toLowerCase().equals("bye")) {
-            printLine();
-            System.out.println(input);
-            printLine();
+        while (!input.equalsIgnoreCase("bye")) {
+            if(input.equalsIgnoreCase(("list"))) {
+                printList(list, index);
+            }
+            else {
+                add(input, list, index);
+                index++;
+            }
             input = scan.nextLine();
         }
 
