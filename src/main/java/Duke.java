cb9c0880d6b7c6767bc8357c2d8da09ff104e057
douglaslewpc <reachdouglaslew@gmail.com>
2021-01-27 01:32:39 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -9,6 +11,17 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("");
+        boolean echoFlag = true;
+        while(echoFlag) {
+            Scanner in = new Scanner(System.in);
+            String line = in.nextLine();
+            if(line.equals("bye")){
+                echoFlag = false;
+            }
+            else {
+                System.out.println(line);
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
