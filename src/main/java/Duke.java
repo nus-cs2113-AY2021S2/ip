e1d45956c39108ee7d2c051cc581bf21fa463cb2
Jeremy Teo <jimmy.teo878@gmail.com>
2021-01-26 14:55:43 +0800

    Duke Level 0 - Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,16 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        System.out.println("Hello from\n" + logo);
+
+        // Strings
+        String lineDivider = "____________________________________________________________\n";
+        String dukeGreeting = lineDivider + " Hello! I'm Duke\n What can I do for you?\n" ;
+        String dukeFarewell =  lineDivider + " Bye. Hope to see you again soon!\n" + lineDivider;
+
+        System.out.println(dukeGreeting + dukeFarewell);
+
     }
+
+
 }
