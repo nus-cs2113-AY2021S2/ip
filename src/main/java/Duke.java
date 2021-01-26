9df43d3487e93ff1bc217ad7057f98402e91900f
angjiahui <angjiahui@users.noreply.github.com>
2021-01-27 00:34:43 +0800

    Add Greet,Echo,Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,22 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-//        String[] stringArgs = new String[10];
-//        Scanner sc = new Scanner(System.in);
-//        stringArgs[0] = sc.nextLine();
-//
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-//        if (stringArgs[0] == "bye") {
-//            System.out.println("Bye. Hope to see you again soon!");
-//        }
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
 
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+
+        Scanner sc = new Scanner(System.in);
+        String inputCommand = sc.nextLine();
+
+        while (!inputCommand.equals("bye")){
+            if (inputCommand.equals("list")){
+                System.out.println(inputCommand);
+            } else if (inputCommand.equals("blah")){
+                System.out.println(inputCommand);
+            }else if (inputCommand.equals("bye")){
+                break;
+            }
+            inputCommand = sc.nextLine();
+        }
+            System.out.println("Bye. Hope to see you again soon!");
     }
 }
