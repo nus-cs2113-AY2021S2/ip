48fbc0bfa8aefb5f795a62c294c7763adb724a12
hazelhedmine <66232361+zelleh15@users.noreply.github.com>
2021-01-27 01:45:59 +0800

    edited to follow coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,7 +10,7 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        Task[] taskList = new Task[100];
+        Task[] tasks = new Task[100];
         int numberOfTasks = 0;
 
         System.out.println("Hello from\n" + logo);
@@ -23,21 +23,22 @@ public class Duke {
             if (command.equals("list")) {
                 System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < numberOfTasks; i++) {
-                    System.out.println(" " + (i + 1) + "." + taskList[i].getStatus() + " " + taskList[i].getDescription());
+                    System.out.println(" " + (i + 1) + "." + tasks[i].getStatus()
+                            + " " + tasks[i].getDescription());
                 }
                 System.out.println(horizontalLine);
             } else {
                 String[] tokens = command.split(" ");
                 if (tokens[0].equals("done")) {
                     int taskNumber = Integer.parseInt(tokens[1]);
-                    Task task = taskList[taskNumber-1];
+                    Task task = tasks[taskNumber-1];
                     task.markAsDone();
                     System.out.println(horizontalLine + "\n Nice! I've marked this task as done:");
                     System.out.println("   [X] " + task.getDescription()  + "\n" + horizontalLine);
                 } else {
                     System.out.println(" added: " + command + "\n" + horizontalLine);
                     Task task = new Task(command);
-                    taskList[numberOfTasks] = task;
+                    tasks[numberOfTasks] = task;
                     numberOfTasks++;
                 }
             }
