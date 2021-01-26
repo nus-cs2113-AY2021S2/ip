a272065b727a9e00979a0ca4d782e461ffa1f2f2
angjiahui <angjiahui@users.noreply.github.com>
2021-01-27 01:00:23 +0800

    Add store text function
    Allow user to store text entered and display them back when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,14 +6,22 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String inputCommand = sc.nextLine();
+        String[] textList = new String[100];
+        int listNumber = 0;
 
         while (!inputCommand.equals("bye")){
             if (inputCommand.equals("list")){
-                System.out.println(inputCommand);
+                for (int i = 0; i < listNumber; i++){
+                    System.out.println(i + 1 + ". " + textList[i]);
+                }
             } else if (inputCommand.equals("blah")){
                 System.out.println(inputCommand);
             }else if (inputCommand.equals("bye")){
                 break;
+            }else {
+                textList[listNumber] = inputCommand;
+                listNumber++;
+                System.out.println("added: "+ textList[listNumber-1]);
             }
             inputCommand = sc.nextLine();
         }
