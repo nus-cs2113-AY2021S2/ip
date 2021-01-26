abc15fe53a0e05cc176a5de885bcef3c334bb901
isaharon <19249948+isaharon@users.noreply.github.com>
2021-01-26 18:48:43 +0800

    Add Increment: Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,46 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String hLine = "\u2500".repeat(60);
+    private static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        String hLine = "\u2500".repeat(60);
-        System.out.println(hLine);
+        Duke.greet();
+        Duke.readUserInput();
+        Duke.bye();
+    }
+
+    public static void greet() {
+        System.out.println(Duke.hLine);
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
-        System.out.println(hLine);
+        System.out.println(Duke.hLine);
+    }
+
+    public static void bye() {
+        System.out.println(Duke.hLine);
         System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(hLine);
+        System.out.println(Duke.hLine);
+    }
+
+    // Reads user input, does an action and stops reading when user inputs "bye"
+    public static void readUserInput() {
+        Scanner in = new Scanner(System.in);
+        String userInput;
+        userInput = in.nextLine();
+        while (!userInput.equals("bye")) {
+            Duke.echo(userInput);
+            userInput = in.nextLine();
+        }
+    }
+
+    public static void echo(String text) {
+        System.out.println(Duke.hLine);
+        System.out.println(text);
+        System.out.println(Duke.hLine);
     }
 }
