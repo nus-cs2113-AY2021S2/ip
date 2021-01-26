60f5ee050b3f3394d1caa7abcf50daf54cea805f
tzexern <tantzexern@gmail.com>
2021-01-27 00:16:23 +0800

    Add array isDoneList, method getStatusIcon markAsDone (Task.java). Add case in switch (Main.java)
    isDoneList is added to keep track of the state of the tasks.
    Added relevant methods in Task.java for more organized code.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,12 +16,16 @@ public class Duke {
 
         Scanner userInputScanner = new Scanner(System.in);
         String userInput = userInputScanner.nextLine();
-        while(!userInput.equals("bye")) {
-            switch (userInput) {
+        String[] userInputSplitted = userInput.split(" ");
+        while(!userInputSplitted[0].equals("bye")) {
+            switch (userInputSplitted[0]) {
             case "list":
                 System.out.println("_____________________________________________________");
                 Task.listTasks();
                 System.out.println("_____________________________________________________");
+                break;
+            case "done":
+                Task.markAsDone(userInputSplitted[1]);
                 break;
             default:
                 Task t = new Task(userInput);
@@ -30,6 +34,7 @@ public class Duke {
                 break;
             }
             userInput = userInputScanner.nextLine();
+            userInputSplitted = userInput.split(" ");
         }
         System.out.println("_____________________________________________________");
         System.out.println("Bye. Hope to see you again soon! :3");
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,27 +1,38 @@
 public class Task {
     protected String description;
-    protected boolean isDone;
+    private static boolean isDone;
     private static String[] taskList = new String[100];
+    private static boolean[] isDoneList = new boolean[100];
     private static int taskCount = 0;
 
     // Task Constructor
     public Task(String description){
         this.description = description;
-        System.out.println("after desc");
         this.isDone = false;
-        System.out.println("after isDone");
+    }
+
+    public static String getStatusIcon(int index){
+        return(isDoneList[index] ? "\u2713" : " ");
+    }
+
+    public static void markAsDone(String index){
+        isDoneList[Integer.parseInt(index)-1] = true;
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  " + "[" + getStatusIcon(Integer.parseInt(index)-1) + "] " + taskList[Integer.parseInt(index)-1]);
     }
 
     // Add Task to taskList and increment taskCount
     public void addTask(String description){
         taskList[taskCount] = description;
+        isDoneList[taskCount] = this.isDone;
         taskCount++;
     }
 
     public static void listTasks(){
         int index = 1;
         for(String t : taskList){
-            System.out.println(Integer.toString(index) + ". " + t);
+            System.out.print("");
+            System.out.println(Integer.toString(index) + ".[" + getStatusIcon(index-1) + "] " + t);
             index++;
             if(!(taskCount > index - 1)){
                 break;
