f52bcbd3cfb400107e1ad5b167aaf9ffebe4a249
aliciatay-zls <e0424736@u.nus.edu>
2021-01-26 23:12:20 +0800

    Modify code to comply with Java coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,81 +3,80 @@ import java.util.Scanner;
 public class Duke {
 
     // Messages from chatbot
-    private static final String border = "____________________________________________________________";
-    private static final String newline = System.lineSeparator();
+    private static final String BORDER = "____________________________________________________________";
+    private static final String NEWLINE = System.lineSeparator();
 
     public static void greet() {
-        System.out.println(border);
+        System.out.println(BORDER);
         System.out.println("Hello, I'm Panda!");
         System.out.println("What would you like to do today?");
-        System.out.println(border + newline);
+        System.out.println(BORDER + NEWLINE);
     }
 
-    public static void echo(String command) {
-        System.out.println(border);
-        System.out.println("\t" + "added: " + command);
-        System.out.println(border + newline);
+    public static void echo(String description) {
+        System.out.println(BORDER);
+        System.out.println("\t" + "added: " + description);
+        System.out.println(BORDER + NEWLINE);
     }
 
     public static void goodbye() {
-        System.out.println(border);
+        System.out.println(BORDER);
         System.out.println("Alright, goodbye!");
-        System.out.println(border);
+        System.out.println(BORDER);
     }
 
-    public static void printInvalidInput() {
-        System.out.println(border);
+    public static void printInvalidInputMessage() {
+        System.out.println(BORDER);
         System.out.println("I'm sorry, I don't quite understand. Can you try again?");
-        System.out.println(border + newline);
+        System.out.println(BORDER + NEWLINE);
     }
 
     // Actions that can be done
     private static Task[] tasks = new Task[100]; //arr of 100 objects of class Task
     private static int tasksCount = 0;
-    private static final String defaultStatus = " ";
-    private static final String doneStatus = "X";
+    private static final String DEFAULT_STATUS = " ";
+    private static final String DONE_STATUS = "X";
 
     public static void addToList(String description) {
-        tasks[tasksCount] = new Task(description, tasksCount+1, defaultStatus);
+        tasks[tasksCount] = new Task(description, tasksCount+1, DEFAULT_STATUS);
         tasksCount++;
     }
 
     public static void markInList(int id) {
-        tasks[id-1].setStatus(doneStatus);
-        System.out.println(border);
+        tasks[id-1].setStatus(DONE_STATUS);
+        System.out.println(BORDER);
         System.out.println("\t" + "Nice! I've marked this task as done:");
-        System.out.println("\t" + "[" + doneStatus + "] " + tasks[id-1].getItem());
-        System.out.println(border + newline);
+        System.out.println("\t" + "[" + DONE_STATUS + "] " + tasks[id-1].getItem());
+        System.out.println(BORDER + NEWLINE);
     }
 
     public static void printList() {
-        System.out.println(border);
+        System.out.println(BORDER);
         System.out.println("Here are the tasks in your list:");
         for (int i=0; i<tasksCount; i++) {
             System.out.println("\t" + tasks[i].getIndex() + ". "
                     + "[" + tasks[i].getStatus() + "] "
                     + tasks[i].getItem());
         }
-        System.out.println(border + newline);
+        System.out.println(BORDER + NEWLINE);
     }
 
     // Input validation
-    // This method checks for incomplete or invalid done command
+    // This method checks for incomplete or invalid done command, or non-existent item in the list
     public static boolean isNumeric(String[] tokens) {
         if (tokens.length != 2) {
-            printInvalidInput();
+            printInvalidInputMessage();
             return false;
         }
         try {
             Integer.parseInt(tokens[1]);
-        }
-        catch (NumberFormatException e) {
-            printInvalidInput();
+        } catch (NumberFormatException e) {
+            printInvalidInputMessage();
             return false;
         }
         int index = Integer.parseInt(tokens[1]);
-        if (index <=0 || index > tasksCount) { //item does not exist in list
-            printInvalidInput();
+        if (index <=0 || index > tasksCount) {
+            printInvalidInputMessage();
             return false;
         }
         return true;
@@ -88,23 +87,21 @@ public class Duke {
         greet();
 
         Scanner in = new Scanner(System.in);
-        String cmd = in.nextLine();
-        while (!cmd.equals("bye")) {
-            if (cmd.equals("list")) {
+        String description = in.nextLine();
+        while (!description.equals("bye")) {
+            if (description.equals("list")) {
                 printList();
-            }
-            else if (cmd.contains("done")) {
-                String[] tokens = cmd.split(" ", 0);
+            } else if (description.contains("done")) {
+                String[] tokens = description.split(" ", 0);
                 boolean isValid = isNumeric(tokens);
                 if (isValid) {
                     markInList(Integer.parseInt(tokens[1]));
                 }
+            } else {
+                echo(description);
+                addToList(description);
             }
-            else {
-                echo(cmd);
-                addToList(cmd);
-            }
-            cmd = in.nextLine();
+            description = in.nextLine();
         }
 
         goodbye();
