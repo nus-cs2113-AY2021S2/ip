4be1b78f65e98d9f1cd93cebe99ec0afeca15106
Emkay16 <mabelkohjx@gmail.com>
2021-01-26 13:16:15 +0800

    Add echo and exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void printLine() {
+        System.out.println("_____________________________________________________");
+    }
     public static void main(String[] args) {
+        printLine();
         System.out.println("Hello! I'm Duke");
-        System.out.println("What  can I do for you?" + System.lineSeparator());
+        System.out.println("What can I do for you?");
+        printLine();
+
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        while (!input.toLowerCase().equals("bye")) {
+            printLine();
+            System.out.println(input);
+            printLine();
+            input = scan.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
+        printLine();
     }
 }
