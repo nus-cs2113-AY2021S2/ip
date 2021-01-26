d077c54f7249763a12b61d2836d0d83730b89776
Jeremy Teo <jimmy.teo878@gmail.com>
2021-01-26 15:29:33 +0800

    Updated code with echo command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,26 @@
-
+import java.util.Scanner;
 
 public class Duke {
+    static final String lineDivider = "\t____________________________________________________________\n";
+    static final String dukeGreeting = lineDivider + "\t Hello! I'm Duke\n\t What can I do for you?\n" + lineDivider;
+    static final String dukeFarewell =  lineDivider + "\t Bye. Hope to see you again soon!\n" + lineDivider;
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void echos(){
+
+        String userInput = sc.nextLine();
+
+        while (!userInput.toLowerCase().equals("bye")){
+//            String echos = lineDivider + "\t" + userInput.toLowerCase() + "\n" + lineDivider;
+            String echos = String.format("%s\t %s\n%s",lineDivider,userInput.toLowerCase(),lineDivider);
+            System.out.println(echos);
+            userInput = sc.nextLine();
+        }
+
+        // Farewell procedure
+        System.out.println(dukeFarewell);
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,14 +29,12 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
 
-        // Strings
-        String lineDivider = "____________________________________________________________\n";
-        String dukeGreeting = lineDivider + " Hello! I'm Duke\n What can I do for you?\n" ;
-        String dukeFarewell =  lineDivider + " Bye. Hope to see you again soon!\n" + lineDivider;
 
-        System.out.println(dukeGreeting + dukeFarewell);
+        // Greetings
+        System.out.println(dukeGreeting);
 
+        // Do echo function
+        echos();
     }
-
 
 }
