a2e7b545d1edfbe4ee2d26b08d510890619a3c48
Song Yu <song0180@e.ntu.edu.sg>
2021-01-26 16:22:17 +0800

    Level 2. Echo: Add ToDoList class and ListItem class
    Add methods to add list item and print list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,53 +1,63 @@
 import java.util.Scanner;
 
+
 public class Duke {
+    public static final String DIVIDER_LINE_ONLY = "__________________________________________";
+    public static final String DIVIDER = "__________________________________________\n";
+
     public static void printGreetings() {
         System.out.println(
-                "__________________________________________\n"
-                        + " Welcome. I am your assistant Friday. ✧ʕ̢̣̣̣̣̩̩̩̩·͡˔·ོɁ̡̣̣̣̣̩̩̩̩✧ \n"
+                DIVIDER
+                        + " Welcome.\n"
+                        + " I am your assistant Friday. ✧ʕ̢̣̣̣̣̩̩̩̩·͡˔·ོɁ̡̣̣̣̣̩̩̩̩✧ \n"
                         + " Just FYI, I am developed by Song Yu.\n"
                         + " May I know what I can help you?\n"
-                        + "__________________________________________"
+                        + DIVIDER_LINE_ONLY
         );
     }
 
     public static void printExitGreetings() {
         System.out.println(
-                "__________________________________________\n"
+                DIVIDER
                         + " Thank you for getting in touch.\n"
                         + " See you next time. \n"
                         + "✧( ु•⌄• )◞ᴴᴬᵛᴱ ᴬ ᴳᴼᴼᴰ ᵀᴵᴹᴱ\n"
-                        + "__________________________________________"
+                        + DIVIDER_LINE_ONLY
         );
     }
 
     public static void echoInput(String userInput) {
         System.out.println(
-                "__________________________________________\n"
+                DIVIDER
                         + userInput + "\n"
-                        + "__________________________________________\n");
+                        + DIVIDER_LINE_ONLY
+        );
     }
 
     public static void main(String[] args) {
         printGreetings();
 
         Scanner sc = new Scanner(System.in);
+        ToDoList toDoList = new ToDoList();
         String userInput;
+
         // boolean variable to control the input loop
         boolean isRunning = true;
         while (isRunning) {
             // get user input
             userInput = sc.nextLine();
 
-            // exit if "bye" is received
-            if (userInput.equals("bye")) {
+            switch (userInput) {
+            case "bye":
                 printExitGreetings();
                 return;
+            case "list":
+                toDoList.printList();
+                break;
+            default:
+                toDoList.addListItem(userInput);
+                break;
             }
-
-            echoInput(userInput);
         }
-
-        printExitGreetings();
     }
 }
