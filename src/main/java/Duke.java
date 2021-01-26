334a74f7d3f4e8d434a1e93b808d0165cd7c4b75
Vikasharlani <e0523376@u.nus.edu>
2021-01-26 22:15:07 +0800

    Add Level-2 code
    Add Task class used to create new Tasks that are stored in taskList which is an array of Tasks.
    Add list command which loops through taskList to output tasks.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        Task[] taskList = new Task[100];
+        int counter = 0;
         String line;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -21,9 +23,19 @@ public class Duke {
                 System.out.println("\tBye. Hope to see you again soon!");
                 System.out.println("\t---------------------------------------------------------------------");
                 break;
-            } else {
+            } else if (line.equals("list")) {
                 System.out.println("\t---------------------------------------------------------------------");
-                System.out.println("\t" + line);
+                for (int i = 1; i <= counter; i++) {
+                    System.out.print("\t" + i + ". ");
+                    System.out.println(taskList[i-1].getDescription());
+                }
+                System.out.println("\t---------------------------------------------------------------------");
+            } else {
+                Task newTask = new Task(line);
+                taskList[counter] = newTask;
+                counter++;
+                System.out.println("\t---------------------------------------------------------------------");
+                System.out.println("\tadded: " + line);
                 System.out.println("\t---------------------------------------------------------------------");
             }
         }
