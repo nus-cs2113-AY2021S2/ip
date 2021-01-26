18912ae01de4cec01486ff48457a9a20244de24d
Song Yu <song0180@e.ntu.edu.sg>
2021-01-26 15:28:37 +0800

    Level 1. Echo: Add method to echo input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,53 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void printGreetings() {
         System.out.println(
-                "__________________________________________\n" +
-                        " Welcome. I am your assistant Duke.\n" +
-                        " Just FYI, I am developed by Song Yu.\n" +
-                        " May I know what I can help you?\n" +
-                        "__________________________________________\n"
+                "__________________________________________\n"
+                        + " Welcome. I am your assistant Friday. ✧ʕ̢̣̣̣̣̩̩̩̩·͡˔·ོɁ̡̣̣̣̣̩̩̩̩✧ \n"
+                        + " Just FYI, I am developed by Song Yu.\n"
+                        + " May I know what I can help you?\n"
+                        + "__________________________________________"
         );
     }
 
     public static void printExitGreetings() {
         System.out.println(
-                "__________________________________________\n" +
-                        " Thank you for getting in touch.\n" +
-                        " See you next time. :)\n" +
-                        "__________________________________________\n"
+                "__________________________________________\n"
+                        + " Thank you for getting in touch.\n"
+                        + " See you next time. \n"
+                        + "✧( ु•⌄• )◞ᴴᴬᵛᴱ ᴬ ᴳᴼᴼᴰ ᵀᴵᴹᴱ\n"
+                        + "__________________________________________"
         );
     }
 
+    public static void echoInput(String userInput) {
+        System.out.println(
+                "__________________________________________\n"
+                        + userInput + "\n"
+                        + "__________________________________________\n");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
         printGreetings();
+
+        Scanner sc = new Scanner(System.in);
+        String userInput;
+        // boolean variable to control the input loop
+        boolean isRunning = true;
+        while (isRunning) {
+            // get user input
+            userInput = sc.nextLine();
+
+            // exit if "bye" is received
+            if (userInput.equals("bye")) {
+                printExitGreetings();
+                return;
+            }
+
+            echoInput(userInput);
+        }
+
         printExitGreetings();
     }
 }
