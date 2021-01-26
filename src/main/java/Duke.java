3442b5cf5ec0c6006737eeba3f95bd46085f565d
gerardtwk <gerardtwk@gmail.com>
2021-01-26 23:31:01 +0800

    Level-2 implementation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,44 +1,70 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static final String DIVIDER = "____________________________________________________________";
+    private static final String[] STORED_TASKS = new String[100];
+    private static int storedTasksCount = 0;
+
     public static void main(String[] args) {
-        String divider  = "____________________________________________________________";
         String command;
         Scanner scanner = new Scanner(System.in);
 
-        welcomeMsg(divider);
+        welcomeMsg();
         do {
             command = scanner.nextLine();
-            if(!command.equals("bye")) {
-                echoCommand(divider, command);
-            }
+            handleCommand(command);
         } while(!command.equals("bye"));
-        exitMsg(divider);
+
         scanner.close();
     }
 
-    public static void welcomeMsg(String divider) {
+    public static void welcomeMsg() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(divider);
+        System.out.println(DIVIDER);
         System.out.println(logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println(divider);
+        System.out.println(DIVIDER);
     }
 
-    public static void echoCommand(String divider, String command) {
-        System.out.println(divider);
-        System.out.println(command);
-        System.out.println(divider);
+    public static void handleCommand(String command) {
+        switch(command) {
+        case "list":
+            displayStoredTasks();
+            break;
+
+        case "bye":
+            exitMsg();
+            break;
+
+        default:
+            storeTask(command);
+        }
     }
 
-    public static void exitMsg(String divider) {
-        System.out.println(divider);
+    public static void displayStoredTasks() {
+        System.out.println(DIVIDER);
+        for(int i=0; i<storedTasksCount; i++) {
+            System.out.printf("%d. %s\n", i+1, STORED_TASKS[i]);
+        }
+        System.out.println(DIVIDER);
+    }
+
+    public static void storeTask(String task) {
+        STORED_TASKS[storedTasksCount] = task;
+        storedTasksCount++;
+        System.out.println(DIVIDER);
+        System.out.println("added: " + task);
+        System.out.println(DIVIDER);
+    }
+
+    public static void exitMsg() {
+        System.out.println(DIVIDER);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(divider);
+        System.out.println(DIVIDER);
     }
 }
