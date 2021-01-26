472e63a17a3a0f8c1d2b6429632ab0808e99cd47
PingruiLi <lipingrui2001@gmail.com>
2021-01-26 11:12:24 +0800

    Add Level-1 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,12 +7,21 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+        System.out.println("\tHello from\n" + logo);
+        System.out.println("\t____________________________________________________________\n" +
+                "\tHello! I'm Duke\n" +
+                "\tWhat can I do for you?\n" +
+                "\t____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        while(!userInput.equals("bye")){
+            System.out.println("\t____________________________________________________________\n\t" +
+                    userInput +
+                    "\n\t____________________________________________________________");
+            userInput = sc.nextLine();
+        }
+        System.out.println("\t____________________________________________________________\n " +
+                "\tBye. Hope to see you again soon!" +
+                "\n\t____________________________________________________________");
     }
 }
