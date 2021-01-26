113448a8d264742f2b8028808a0c35646c77fa5a
jadenwjh <wongjunhong9723@gmail.com>
2021-01-26 18:12:27 +0800

    Add reply function to wrap replies with horizontal lines and indentations Add echoUser function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,52 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String horizontalLine = "    ____________________________________________________________";
+    private static final String helloMessage = "Hello! I'm Duke";
+    private static final String assistMessage = "What can I do for you?";
+    private static final String byeMessage = "Bye. Hope to see you again soon!";
+    private static final String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        String helloMsg = "Hello! I'm Duke\nWhat can I do for you?";
-        String byeMsg = "Bye. Hope to see you again soon!";
+        reply(new String[]{helloMessage, assistMessage});
+        
+        echoUser();
+    }
 
-        System.out.println(helloMsg);
-        System.out.println(byeMsg);
+    /**
+     * Wraps replies with horizontal lines and indentation.
+     */
+    public static void reply(String[] messages) {
+        String combined = "";
+        String indentation = "    ";
+        for (String message : messages) {
+            combined += indentation;
+            combined += message;
+            combined += "\n";
+        }
+        System.out.println(String.format("%s\n%s%s\n", horizontalLine, combined, horizontalLine));
+    }
+
+    /**
+     * Echo messages from user.
+     * If message is "bye", exits program.
+     */
+    public static void echoUser() {
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+
+        if (line.equals("bye")) {
+            reply(new String[]{byeMessage});
+            in.close();
+        } else {
+            reply(new String[]{line});
+            echoUser();
+        }
     }
 }
