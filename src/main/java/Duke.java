31c2e82e103b1e4433c4efb9f011115a7156da12
huachen <huachenw24@gmail.com>
2021-01-26 12:12:37 +0800

    add echo and bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String line = "\t________________________________________";
+    public static void echo() {
+        while (true) {
+            Scanner scannerObject = new Scanner(System.in);
+            String command = scannerObject.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(line);
+                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(line);
+            System.out.println("\t" + command);
+            System.out.println(line);
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        String line = "________________________________________";
         System.out.println(line);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println();
+        echo();
     }
 }
