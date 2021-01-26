c1e2b7f6d98ef90192e214d9adcd1dc4ea96a7f0
Vikasharlani <e0523376@u.nus.edu>
2021-01-26 21:43:50 +0800

    Add Level-1 code
    Add indentation for output (including Level-0)
    Implement echo of user input
    Add exit when user input is "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String line;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("---------------------------------------------------------------------");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("---------------------------------------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("---------------------------------------------------------------------");
+        System.out.println("\t---------------------------------------------------------------------");
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t---------------------------------------------------------------------");
+        while (true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("\t---------------------------------------------------------------------");
+                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println("\t---------------------------------------------------------------------");
+                break;
+            } else {
+                System.out.println("\t---------------------------------------------------------------------");
+                System.out.println("\t" + line);
+                System.out.println("\t---------------------------------------------------------------------");
+            }
+        }
     }
 }
