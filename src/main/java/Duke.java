fa8027c60da8cfcea8f4ae762a2d79048426bfba
angjiahui <angjiahui@users.noreply.github.com>
2021-01-27 02:23:14 +0800

    Add Level 3
    Add the ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,22 +6,40 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String inputCommand = sc.nextLine();
-        String[] textList = new String[100];
-        int listNumber = 0;
+        String[] textList = new String[101];
+        String[] doneList = new String[101];
+        int listNumber = 1;
+        String checkDone = null;
 
         while (!inputCommand.equals("bye")){
             if (inputCommand.equals("list")){
-                for (int i = 0; i < listNumber; i++){
-                    System.out.println(i + 1 + ". " + textList[i]);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 1; i < listNumber; i++){
+                    System.out.println(i + "." + "[" + doneList[i] + "] " + textList[i]);
                 }
-            } else if (inputCommand.equals("blah")){
+            } else if(inputCommand.equals("done")){
+
+            }else if (inputCommand.equals("blah")){
                 System.out.println(inputCommand);
             }else if (inputCommand.equals("bye")){
                 break;
             }else {
-                textList[listNumber] = inputCommand;
-                listNumber++;
-                System.out.println("added: "+ textList[listNumber-1]);
+                String trimmedCommand = inputCommand.trim();
+                if (trimmedCommand.length() > 4) {
+                    checkDone = trimmedCommand.substring(0, 4);
+                }
+                if("done".equals(checkDone) && (trimmedCommand.length() > 4)) {
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("[X] " + inputCommand);
+                    int doneIndex = Integer.parseInt(inputCommand.substring(5));
+                    doneList[doneIndex] = "X";
+
+                }else{
+                    textList[listNumber] = inputCommand;
+                    doneList[listNumber] = " ";
+                    System.out.println("added: " + textList[listNumber]);
+                    listNumber++;
+                }
             }
             inputCommand = sc.nextLine();
         }
