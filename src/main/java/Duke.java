dc2914e2c7932e81e53d279f2a7ece707b1deebd
PingruiLi <lipingrui2001@gmail.com>
2021-01-26 18:30:03 +0800

    Level-2 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,11 +13,23 @@ public class Duke {
                 "\tWhat can I do for you?\n" +
                 "\t____________________________________________________________");
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         String userInput = sc.nextLine();
         while(!userInput.equals("bye")){
-            System.out.println("\t____________________________________________________________\n\t" +
-                    userInput +
-                    "\n\t____________________________________________________________");
+            if(!userInput.equals("list")){
+                tasks[taskCount] = userInput;
+                System.out.println("\t____________________________________________________________\n" +
+                        "\tadded: " + userInput +
+                        "\n\t____________________________________________________________");
+                taskCount++;
+            }
+            else{
+                System.out.println("\t____________________________________________________________");
+                for(int i=0; i<taskCount; i++)
+                    System.out.println("\t" + (i+1) + "." + tasks[i]);
+                System.out.println("\t____________________________________________________________");
+            }
             userInput = sc.nextLine();
         }
         System.out.println("\t____________________________________________________________\n " +
