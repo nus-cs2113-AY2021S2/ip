389df3917d4c7dd4d7011a59a3ed4e4070ac3dce
Emkay16 <mabelkohjx@gmail.com>
2021-01-26 21:54:23 +0800

    Add markAsDone method & modify code for task class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,26 +5,53 @@ public class Main {
         System.out.println("_____________________________________________________");
     }
 
+    /*
     public static void echo(String input) {
         printLine();
         System.out.println(input);
         printLine();
+    }*/
+
+    public static void add(String input, Task[] list, int index) {
+        list[index] = new Task(input);
+        printLine();
+        System.out.println("\"" + list[index].getDesc() + "\"" + " added to list");
+        printLine();
     }
 
-    public static void add(String input, String[] list, int index) {
-        list[index] = input.trim();
-        printLine();
-        System.out.println("\"" + list[index] + "\"" + " added to list");
-        printLine();
-    }
-
-    public static void printList(String[] list, int index) {
+    public static void printList(Task[] list, int index) {
         printLine();
         for(int i=0; i<index; i++) {
-            System.out.print(i+1 + ") ");
-            System.out.println(list[i]);
+            if (i<9) {
+                System.out.print(" ");
+            }
+            System.out.print(i+1);
+            System.out.print(list[i].getStatusSymbol());
+            System.out.println(list[i].getDesc());
         }
         printLine();
+    }
+
+    public static void markAsDone(Task[] list, int taskNo, int max) {
+        if (taskNo <= max+1 && taskNo > 0) { //no. is valid
+            if (list[taskNo-1].getStatus() == true) {
+                printLine();
+                System.out.println("Task \"" + list[taskNo-1].getDesc() + "\" is already complete");
+                printLine();
+            }
+            else {
+                list[taskNo-1].check();
+                printLine();
+                System.out.println("Task completed!");
+                System.out.println("  " + list[taskNo - 1].getStatusSymbol() + list[taskNo - 1].getDesc());
+                printLine();
+            }
+        }
+        else {
+            printLine();
+            System.out.println("Please provide a valid argument");
+            printLine();
+        }
     }
 
     public static void main(String[] args) {
@@ -33,21 +60,39 @@ public class Main {
         System.out.println("What can I do for you?");
         printLine();
 
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int index = 0;
 
         Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
+        String in = scan.nextLine();
 
-        while (!input.equalsIgnoreCase("bye")) {
-            if(input.equalsIgnoreCase(("list"))) {
-                printList(list, index);
+
+        String[] input = in.split(" ");
+
+
+        while (!input[0].equalsIgnoreCase("bye")) {
+            if(input.length != 0) {
+                if(input[0].equalsIgnoreCase("list")) {
+                    printList(list, index);
+                }
+                else if (input[0].equalsIgnoreCase("done")) {
+                    if (input.length != 2) {
+                        printLine();
+                        System.out.println("Please provide 1 argument");
+                        printLine();
+                    }
+                    else {
+                        int ind = Integer.parseInt(input[1]);
+                        markAsDone(list, ind, index);
+                    }
+                }
+                else {
+                    add(in, list, index);
+                    index++;
+                }
             }
-            else {
-                add(input, list, index);
-                index++;
-            }
-            input = scan.nextLine();
+            in = scan.nextLine();
+            input = in.split(" ");
         }
 
         System.out.println("Bye. Hope to see you again soon!");
