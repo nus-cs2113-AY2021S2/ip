b79100b1ef832c1a1632d8f7bd8940d6efa0d9c4
tzexern <tantzexern@gmail.com>
2021-01-26 20:25:08 +0800

    Add Scanner
    Replaced previous command line input to scanning for user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -12,27 +14,30 @@ public class Duke {
         System.out.print("What can I do for you?\n");
         System.out.println("_____________________________________________________");
 
-        switch (args[0]) {
-        case "list":
-            System.out.println("_____________________________________________________");
-            System.out.println("list");
-            System.out.println("_____________________________________________________");
-            break;
-        case "blah":
-            System.out.println("_____________________________________________________");
-            System.out.println("blah");
-            System.out.println("_____________________________________________________");
-            break;
-        case "bye":
-            System.out.println("_____________________________________________________");
-            System.out.println("Bye. Hope to see you again soon! :3");
-            System.out.println("_____________________________________________________");
-            break;
-        default:
-            System.out.println("_____________________________________________________");
-            System.out.println("Uh oh, I don't understand what you have just typed :(");
-            System.out.println("The commands supported are: list, blah, bye");
-            System.out.println("_____________________________________________________");
+        Scanner userInputScanner = new Scanner(System.in);
+        String userInput = userInputScanner.nextLine();
+        while(!userInput.equals("bye")) {
+            switch (userInput) {
+            case "list":
+                System.out.println("_____________________________________________________");
+                System.out.println("list");
+                System.out.println("_____________________________________________________");
+                break;
+            case "blah":
+                System.out.println("_____________________________________________________");
+                System.out.println("blah");
+                System.out.println("_____________________________________________________");
+                break;
+            default:
+                System.out.println("_____________________________________________________");
+                System.out.println("Uh oh, I don't understand what you have just typed :(");
+                System.out.println("The commands supported are: list, blah, bye");
+                System.out.println("_____________________________________________________");
+            }
+            userInput = userInputScanner.nextLine();
         }
+        System.out.println("_____________________________________________________");
+        System.out.println("Bye. Hope to see you again soon! :3");
+        System.out.println("_____________________________________________________");
     }
 }
