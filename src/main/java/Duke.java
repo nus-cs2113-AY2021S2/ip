ea3185d852591e8c31a9bdf55963bd2d45eeb028
Jeremy Teo <jimmy.teo878@gmail.com>
2021-01-26 16:07:57 +0800

    Level 2. Add, List functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Duke {
@@ -18,8 +21,42 @@ public class Duke {
             userInput = sc.nextLine();
         }
 
-        // Farewell procedure
-        System.out.println(dukeFarewell);
+
+    }
+
+    public static void addList(){
+        System.out.print("Enter task: ");
+        String userInput = sc.nextLine();
+        ArrayList<String> taskList = new ArrayList<String>();
+
+        while (!userInput.toLowerCase().equals("bye")) {
+            if (!userInput.toLowerCase().equals("list")) { // userInput != list
+                if (taskList.contains(userInput)){ // handle duplicate task
+                    String taskExistMessage = String.format("%s\t \"%s\" already exists. \n%s", lineDivider, userInput, lineDivider);
+                    System.out.println(taskExistMessage);
+                }else{ // new task
+                    taskList.add(userInput);
+
+                    String addedMessage = String.format("%s\t added: %s\n%s", lineDivider, userInput, lineDivider);
+
+                    System.out.println(addedMessage + "\tEnter \"list\" to see full list. \n");
+                }
+            }else if(!taskList.isEmpty()) { // userInput == list
+                ListIterator<String> it = taskList.listIterator();
+                System.out.print(lineDivider);
+                while (it.hasNext()) {
+                    System.out.println("\t" + (it.nextIndex() + 1) + ". " + it.next());
+                }
+                System.out.println(lineDivider);
+            } else {
+                String emptyListMessage = String.format("%s\t List is empty!\n %s \n", lineDivider, lineDivider);
+                System.out.println(emptyListMessage);
+            }
+
+            System.out.print("Enter task: ");
+            userInput = sc.nextLine();
+        }
+
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -33,8 +70,16 @@ public class Duke {
         // Greetings
         System.out.println(dukeGreeting);
 
-        // Do echo function
-        echos();
+        // Do echo function - Level 1
+//        echos();
+
+        // Do Add, List function - Level 2
+        addList();
+
+        // Farewell procedure
+        System.out.println(dukeFarewell);
     }
+
+
 
 }
