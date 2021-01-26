221912d822967a3db2445bb2e65af934f3a7899d
LJ37 <tanlejun37@gmail.com>
2021-01-26 22:30:26 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,15 +9,34 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        Hello();
-        Bye();
+        Greet();
+        Scanner in = new Scanner(System.in);
+        String input;
+        input = in.nextLine();
+        while (!input.equals("bye")) {
+            Echo(input);
+            input = in.nextLine();
+        }
+        Exit();
     }
 
-    public static void Hello() {
-        System.out.println("Hello! I'm Duke" + System.lineSeparator() + "What can I do for you?" + System.lineSeparator());
+    public static void Greet() {
+        System.out.println("Hello! I'm Duke" + System.lineSeparator() + "What can I do for you?");
+        End();
     }
 
-    public static void Bye() {
+    public static void Exit() {
         System.out.println("Bye. Hope to see you again soon!" + System.lineSeparator());
+        End();
+    }
+
+    public static void End() {
+        System.out.println("____________________________________________________________" + System.lineSeparator());
+    }
+
+    public static void Echo(String input) {
+        System.out.println(input);
+        End();
+
     }
 }
