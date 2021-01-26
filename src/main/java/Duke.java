cc52a3fda8f8a3d2f0ae9713c75df860c3cce9a0
huachen <huachenw24@gmail.com>
2021-01-26 16:02:31 +0800

    add mark as done function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static String line = "\t________________________________________";
-    public static String[] tasks = new String[100];
+    public static Task[] tasks = new Task[100];
     public static int currentTask = 0;
 
     public static void takeInput() {
@@ -14,6 +14,8 @@ public class Duke {
                 break;
             } else if (command.equals("list")) {
                 list();
+            } else if (command.startsWith("done")) {
+                done(command.split(" ")[1]);
             } else {
                 add(command);
             }
@@ -23,34 +25,47 @@ public class Duke {
     public static void bye() {
         System.out.println(line);
         System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println(line);
-        System.out.println();
+        System.out.println(line + "\n");
     }
 
     public static void list() {
         System.out.println(line);
+        System.out.println("\tHere are the tasks in your list:");
         for (int i=0; i<currentTask; i++) {
-            System.out.println("\t" + Integer.toString(i+1) + ". " + tasks[i]);
+            System.out.println("\t" + Integer.toString(i+1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
         }
-        System.out.println(line);
-        System.out.println();
+        System.out.println(line + "\n");
     }
 
     public static void add(String task) {
-        tasks[currentTask] = task;
+        Task t = new Task(task);
+        tasks[currentTask] = t;
         currentTask++;
         System.out.println(line);
         System.out.println("\tadded: " + task);
-        System.out.println(line);
-        System.out.println();
+        System.out.println(line + "\n");
+    }
+
+    public static void done(String item) {
+        int i = Integer.parseInt(item)-1;
+        if (i >= currentTask || i < 0) {
+            System.out.println(line);
+            System.out.println("\tItem does not exist!");
+            System.out.println(line + "\n");
+        } else {
+            tasks[i].markAsDone();
+            System.out.println(line);
+            System.out.println("\tNice! I've marked this task as done:");
+            System.out.println("\t  [" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
+            System.out.println(line + "\n");
+        }
     }
 
     public static void main(String[] args) {
         System.out.println(line);
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
-        System.out.println(line);
-        System.out.println();
+        System.out.println(line + "\n");
         takeInput();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+}
