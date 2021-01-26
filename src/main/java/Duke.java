c54f85a89eb11698701a700444a15ff4fad1de7f
aliciatay-zls <e0424736@u.nus.edu>
2021-01-26 22:56:16 +0800

    Add ability to mark tasks as done
    Add input validation check for done commands
    Modify Task class constructor
    Add getters and setters to Task class
    Rearrange order of methods in Duke class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    // Messages from chatbot
     private static final String border = "____________________________________________________________";
     private static final String newline = System.lineSeparator();
 
@@ -23,23 +25,65 @@ public class Duke {
         System.out.println(border);
     }
 
-    private static String[] tasks = new String[100];
-    private static int tasksCount = 0;
+    public static void printInvalidInput() {
+        System.out.println(border);
+        System.out.println("I'm sorry, I don't quite understand. Can you try again?");
+        System.out.println(border + newline);
+    }
 
-    public static void addToList(String newItem) {
-        Task obj = new Task(newItem, tasksCount+1);
-        tasks[tasksCount] = obj.getItem();
+    // Actions that can be done
+    private static Task[] tasks = new Task[100]; //arr of 100 objects of class Task
+    private static int tasksCount = 0;
+    private static final String defaultStatus = " ";
+    private static final String doneStatus = "X";
+
+    public static void addToList(String description) {
+        tasks[tasksCount] = new Task(description, tasksCount+1, defaultStatus);
         tasksCount++;
+    }
+
+    public static void markInList(int id) {
+        tasks[id-1].setStatus(doneStatus);
+        System.out.println(border);
+        System.out.println("\t" + "Nice! I've marked this task as done:");
+        System.out.println("\t" + "[" + doneStatus + "] " + tasks[id-1].getItem());
+        System.out.println(border + newline);
     }
 
     public static void printList() {
         System.out.println(border);
+        System.out.println("Here are the tasks in your list:");
         for (int i=0; i<tasksCount; i++) {
-            System.out.println("\t" + tasks[i]);
+            System.out.println("\t" + tasks[i].getIndex() + ". "
+                    + "[" + tasks[i].getStatus() + "] "
+                    + tasks[i].getItem());
         }
         System.out.println(border + newline);
     }
 
+    // Input validation
+    // This method checks for incomplete or invalid done command
+    public static boolean isNumeric(String[] tokens) {
+        if (tokens.length != 2) {
+            printInvalidInput();
+            return false;
+        }
+        try {
+            Integer.parseInt(tokens[1]);
+        }
+        catch (NumberFormatException e) {
+            printInvalidInput();
+            return false;
+        }
+        int index = Integer.parseInt(tokens[1]);
+        if (index <=0 || index > tasksCount) { //item does not exist in list
+            printInvalidInput();
+            return false;
+        }
+        return true;
+    }
+
+    //Main method
     public static void main(String[] args) {
         greet();
 
@@ -48,6 +92,13 @@ public class Duke {
         while (!cmd.equals("bye")) {
             if (cmd.equals("list")) {
                 printList();
+            }
+            else if (cmd.contains("done")) {
+                String[] tokens = cmd.split(" ", 0);
+                boolean isValid = isNumeric(tokens);
+                if (isValid) {
+                    markInList(Integer.parseInt(tokens[1]));
+                }
             }
             else {
                 echo(cmd);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,27 @@
 public class Task {
     private String item;
-    private int count;
+    private int index;
+    private String status;
 
-    public Task(String item, int count) {
-        this.item = count + ". " + item;
-        this.count = count;
+    public Task(String item, int index, String status) {
+        this.item = item;
+        this.index = index;
+        this.status = status;
     }
 
     public String getItem() {
-        return item;
+        return this.item;
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
