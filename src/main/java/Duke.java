9ccd87e0384505244989e39782255e0377cf7473
tzexern <tantzexern@gmail.com>
2021-01-26 13:21:39 +0800

    Add Echoes and Exit
    Add switch statement to echo user input and exit on command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,8 +7,32 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
+        System.out.println("_____________________________________________________");
         System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.print("What can I do for you?\n");
+        System.out.println("_____________________________________________________");
+
+        switch (args[0]) {
+        case "list":
+            System.out.println("_____________________________________________________");
+            System.out.println("list");
+            System.out.println("_____________________________________________________");
+            break;
+        case "blah":
+            System.out.println("_____________________________________________________");
+            System.out.println("blah");
+            System.out.println("_____________________________________________________");
+            break;
+        case "bye":
+            System.out.println("_____________________________________________________");
+            System.out.println("Bye. Hope to see you again soon! :3");
+            System.out.println("_____________________________________________________");
+            break;
+        default:
+            System.out.println("_____________________________________________________");
+            System.out.println("Uh oh, I don't understand what you have just typed :(");
+            System.out.println("The commands supported are: list, blah, bye");
+            System.out.println("_____________________________________________________");
+        }
     }
 }
