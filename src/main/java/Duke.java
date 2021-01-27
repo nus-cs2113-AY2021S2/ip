8752a459b06768c34c6f5aee0de85e9b86864197
s-t-e-f <65395025+s-t-e-f@users.noreply.github.com>
2021-01-27 16:30:21 +0800

    Level -2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,20 +22,26 @@ public class Duke {
         String task = sc.nextLine();
 
         while (toContinueAddingTask) {
-
             if (task.equalsIgnoreCase("bye")) {
                 toContinueAddingTask = false;
-                System.out.println("bye");
-                System.out.println("------------------------------------------");
-                System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            tasksCount++;
-            tasksList.add(task);
-            System.out.println("    " + task);
-            task = sc.nextLine();
+            else if (task.equalsIgnoreCase("list")) {
+                System.out.println("list");
+                for (int i=0; i<tasksList.size(); i++){
+                    System.out.println("    " + (i+1) + ". " + tasksList.get(i));
+                }
+                System.out.println("------------------------------------------");
+                task = sc.nextLine();
+            }
+            else {
+                tasksCount++;
+                tasksList.add(task);
+                System.out.println("    added: " + task);
+                task = sc.nextLine();
+            }
         }
-
-
+        System.out.println("------------------------------------------");
+        System.out.println("    Bye. Hope to see you again soon!");
     }
 }
