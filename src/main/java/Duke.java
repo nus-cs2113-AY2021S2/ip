125c39ef8d44b079c153d375b33f49d4d2377000
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2021-01-27 23:34:13 +0800

    Add Echo, update Greet and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.printf("____________________________________________________________\n" +
-                "Hello! I'm Duke\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n");
-        System.out.printf("Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+        System.out.printf("Hello! I'm Duke\n" +
+                "What can I do for you?\n");
+        String line;
+        Scanner sc = new Scanner(System.in);
+        line = sc.nextLine();
+        while(!line.equals("bye")){
+            System.out.println(line);
+            line = sc.nextLine();
+        }
+        System.out.printf("Bye. Hope to see you again soon!\n");
     }
 }
