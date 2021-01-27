d8b50296a41fe95df1bb69c6fc7140b9fa811b05
beaniestanley <beanbeanstanley@gmail.com>
2021-01-27 17:38:23 +0800

    Added Level-0: Greet.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-    }
+
+        System.out.println("------------------------------------------");
+        System.out.println("Hello there! I'm Duke.");
+        System.out.println("What can I do for you?");
+        System.out.println("__________________________________________");
+        System.out.println("Goodbye. See you soon! ^.^");
+        System.out.println("__________________________________________");    }
 }
