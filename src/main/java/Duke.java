04d9390c8e70c39fae00849c0249a322e6c6f72b
Yiwen_Qiu <60378963+e00426142@users.noreply.github.com>
2021-01-27 22:53:27 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class Duke {
+    private static final String line = "____________________________________________________________";
+
     public static void main(String[] args) {
         /*
         String logo = " ____        _        \n"
@@ -16,19 +20,31 @@ public class Duke {
                 "____________________________________________________________\n";
 
         System.out.println(greet);
+        System.out.print(" What can I do for you?\n");
+        System.out.print(line);
 
-        String line = "";
+        String input = "";
         Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+        ArrayList<String> tasks = new ArrayList<>();
 
-        System.out.print(" What can I do for you?\n" +
-                "____________________________________________________________");
-        while(!line.equals("bye")) {
-            line = in.nextLine();
-            System.out.print("____________________________________________________________\n"+
-                    line+"\n"+
-                    "____________________________________________________________\n");
+        while (!input.equals("bye")) {
+            if (!input.equals("list")) {
+                tasks.add(input);
+                System.out.print("____________________________________________________________\n" +
+                        "added:" + input + "\n" +
+                        "____________________________________________________________\n");
+
+            } else if (input.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.print("____________________________________________________________\n" +
+                            Integer.toString(i+1) + "." + tasks.get(i) + "\n" +
+                            "____________________________________________________________\n");
+                }
+            }
+            input = in.nextLine();
         }
-
         System.out.println(exit);
+
     }
 }
