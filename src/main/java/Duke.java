c174aef58e4100441d5adde4d2adf6926a3a1d7e
KimIdeas8 <Kberleytay@gmail.com>
2021-01-28 03:07:26 +0800

    Add increment: level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
 
         String line = "____________________________________________________________";
@@ -6,6 +9,21 @@ public class Duke {
         System.out.println(line);
         System.out.println("Hello! I'm Duke"); //greets
         System.out.println("What can I do for you?");
+        System.out.println(line);
+
+        Scanner in = new Scanner(System.in);
+
+        //echoes user inputs:
+        String input = "";
+        input = in.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+            input = in.nextLine();
+        }
+
+        //exits with "bye":
         System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!"); //exits
         System.out.println(line);
