2efca0df4174f22416070d3114eaf2855bac2d50
rageqqq <chestersoh5@hotmail.com>
2021-01-27 23:05:10 +0800

    Level-1 Requirements
    Greet Echo Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,18 +2,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Duke {
-    public static void printList(String[] list){
-        for (String i : list){
-            if(!i.isEmpty()) {
-                System.out.println(i + "\n");
-            }
-        }
-    }
     public static void main(String[] args) {
-        int counter = 1;
-        String task;
-        String[] tasks =  new String[100];
-        Arrays.fill(tasks, "");
         String line;
         String dashline = "____________________________________________________________\n";
         String IntroMsg = " Hello! I'm Duke\n" +
@@ -23,17 +12,7 @@ public class Duke {
         System.out.println(dashline + IntroMsg + dashline);
         line = in.nextLine();
         while(!line.equals("bye")){
-            if(line.equals("list")){
-                System.out.println(dashline);
-                printList(tasks);
-                System.out.println(dashline);
-            }
-            else {
-                task = counter + ". " + line;
-                tasks[counter] = task;
-                counter++;
-                System.out.println(dashline + " added: " + line + "\n" + dashline);
-            }
+            System.out.println(dashline + " " + line + "\n" + dashline);
             line = in.nextLine();
         }
         System.out.println(dashline + OutMsg + dashline);
