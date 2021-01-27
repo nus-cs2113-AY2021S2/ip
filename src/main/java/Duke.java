f2dc5837f305b6cc27c104aaf69001fb590a5fba
Rye98 <zhong.shuhao@icloud.com>
2021-01-27 20:22:51 +0800

    Update echo
    condensed sectionDivider

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
 
 public class Duke {
-
+    static String sectionDivider = "____________________________________________________________";
     public static void main(String[] args) {
-        String greeting = "\t____________________________________________________________\n"
+        String greeting = "\t" + sectionDivider + "\n"
                 + "\tHello! I'm Duke. \n"
                 + "\tWhat can I do for you? \n"
-                + "\t____________________________________________________________";
-        String sign_off = "\t____________________________________________________________\n"
+                + "\t" + sectionDivider;
+        String sign_off = "\t" + sectionDivider + "\n"
                 + "\tBye. Hope to see you again soon!\n"
-                + "\t____________________________________________________________";
+                + "\t" + sectionDivider;
 
         System.out.println(greeting);
 
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while(line.toLowerCase().equals("bye") != true){
+            System.out.println("\t" + sectionDivider);
+            System.out.println("\t" + line);
+            System.out.println("\t" + sectionDivider);
+            line = in.nextLine();
+        }
         System.out.println(sign_off);
     }
 
