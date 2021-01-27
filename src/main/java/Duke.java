309366df1bfdfca6bd2a31e6b49a561fb37ac5e5
Owen yip <yipowen@yahoo.com.sg>
2021-01-27 16:21:45 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,7 +8,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm DUke\nWhatcan I do for you?\n");
+        System.out.println("Hello! I'm DUke\nWhat can I do for you?\n");
+        Scanner in = new Scanner(System.in);
+            while (true) {
+                String line;
+                line = in.nextLine();
+                System.out.println(line);
+                if (line.equals("bye")){
+                    break;
+                }
+            }
+
         System.out.println("____________________________________________________________\n");
         System.out.println("Bye. Hope to see you again soon!\n");
         System.out.println("____________________________________________________________\n");
