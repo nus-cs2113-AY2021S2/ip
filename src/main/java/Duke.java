c2a6b3ec4a608901299ceb6b52f7c065c7b44c4b
brynagoh <brynagoh@gmail.com>
2021-01-27 13:29:22 +0800

    Add task storage

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,13 +10,31 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I am\n" + logo);
 
-        System.out.println("What can I do for you today?\n ");
+        System.out.println("What can I do for you today?");
 
         Scanner sc = new Scanner(System.in);
+
+        String[] tasks = new String[100];
+        int numTasks = 0;
+
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                if (numTasks == 0) {
+                    System.out.println("No tasks yet!");
+                } else {
+                    for (int i = 0; i < numTasks; i++) {
+                        System.out.printf("%d. %s\n", i+1, tasks[i]);
+                    }
+                }
+
+            } else {
+                tasks[numTasks] = input;
+                numTasks++;
+                System.out.println("added: "+ input);
+            }
             input = sc.nextLine();
+
 
         }
 
