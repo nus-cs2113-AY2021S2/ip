3668652a0ce60bf56d4d489bfc775abb3b768d17
Vikasharlani <e0523376@u.nus.edu>
2021-01-27 21:32:18 +0800

    Add Level-3 code
    Add the ability to mark tasks as done
    Add boolean isDone to Task class to indicate whether task is done or not
    Add toString method to Task class
    Rename Task[] from taskList to tasksList (coding standard)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Task[] taskList = new Task[100];
+        Task[] tasksList = new Task[100];
         int counter = 0;
         String line;
         String logo = " ____        _        \n"
@@ -18,21 +18,33 @@ public class Duke {
         System.out.println("\t---------------------------------------------------------------------");
         while (true) {
             line = in.nextLine();
-            if (line.equals("bye")) {
+            line = line.trim();
+            String[] words = line.split(" ");
+            if (words[0].equals("done")) {
+                int index = Integer.parseInt(words[1]) - 1;
+                tasksList[index].markAsDone();
+                System.out.println("\t---------------------------------------------------------------------");
+                System.out.println("\tNice! I've marked this task as done:");
+                System.out.print("\t" + "  ");
+                System.out.println(tasksList[index]);
+                System.out.println("\t---------------------------------------------------------------------");
+            }
+            else if (line.equals("bye")) {
                 System.out.println("\t---------------------------------------------------------------------");
                 System.out.println("\tBye. Hope to see you again soon!");
                 System.out.println("\t---------------------------------------------------------------------");
                 break;
             } else if (line.equals("list")) {
                 System.out.println("\t---------------------------------------------------------------------");
+                System.out.println("\tHere are the tasks in your list:");
                 for (int i = 1; i <= counter; i++) {
                     System.out.print("\t" + i + ". ");
-                    System.out.println(taskList[i-1].getDescription());
+                    System.out.println(tasksList[i-1]);
                 }
                 System.out.println("\t---------------------------------------------------------------------");
             } else {
                 Task newTask = new Task(line);
-                taskList[counter] = newTask;
+                tasksList[counter] = newTask;
                 counter++;
                 System.out.println("\t---------------------------------------------------------------------");
                 System.out.println("\tadded: " + line);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,26 @@
 public class Task {
     protected String description;
+    protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+
+    public void markAsDone() {
+        isDone = true;
     }
 }
