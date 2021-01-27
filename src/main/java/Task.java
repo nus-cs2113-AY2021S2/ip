696f2ac635442463b4eaf6b2c3646275630fda79
Rye98 <zhong.shuhao@icloud.com>
2021-01-27 21:57:54 +0800

    Update Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,8 +1,17 @@
 public class Task {
-    private static Task[] tasks = {};
     private String description;
+    private boolean isDone;
 
     public Task(String description){
         this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone(){
+        this.isDone = true;
     }
 }
