40ab3675f076442987313a1260ecb54ef04c9227
SimJJ96 <simjingjie13@gmail.com>
2021-01-27 22:24:02 +0800

    Add Task and TaskManager class
    Task class: To track the status of individual task
    TaskManager class: To track a list of task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,38 +7,30 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String border = "    ____________________________________________________________\n";
-        String indent = "     ";
         String lineLowerCase;
         String line;
 
         Scanner in = new Scanner(System.in);
 
-        List<String> tasks = new ArrayList<>();
+        TaskManager taskManager = new TaskManager();
 
         System.out.println("Hello there! This is\n" + logo + "How may I help you ?\n");
 
         do {
             line = in.nextLine();
             lineLowerCase = line.toLowerCase();
-            switch(lineLowerCase) {
-            case "bye":
-                System.out.print(border + indent + "Good Bye. Hope to see you again soon!" + "\n" + border);
-                break;
-            case "list":
-                int index = 0;
+            int donePosition = line.indexOf("done");
 
-                System.out.print(border);
-                for (String task : tasks) {
-                    ++index;
-                    System.out.print(indent + index + ". " + task + "\n");
-                }
-                System.out.print(border);
-                break;
-            default:
-                tasks.add(line);
-                System.out.print(border + indent + "added: " + line + "\n" + border);
-                break;
+            if (lineLowerCase.equals("bye")) {
+                taskManager.endTaskManager();
+            } else if (lineLowerCase.equals("list")) {
+                taskManager.listTasks();
+            } else if (donePosition > -1) {
+                String[] words = line.split(" ");
+                int taskNumber = Integer.parseInt(words[1]);
+                taskManager.markAsDone(taskNumber);
+            } else {
+                taskManager.addTask(line);
             }
         } while (!lineLowerCase.equals("bye"));
     }
---
+++ src/main/java/TaskManager.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager {
+    private final List<Task> tasks = new ArrayList<>();
+    private static final String BORDER = "    ____________________________________________________________\n";
+
+    public void addTask (String description) {
+        Task task = new Task(description);
+        tasks.add(task);
+        System.out.print(BORDER + "\t" + "added: " + description + "\n" + BORDER);
+    }
+
+    public void endTaskManager() {
+        System.out.print(BORDER + "\t" + "Good Bye. Hope to see you again soon!" + "\n" + BORDER);
+    }
+
+    public void listTasks() {
+        System.out.print(BORDER + "\tHere are the task in your list:\n");
+        for (int i = 0; i < tasks.size(); ++i) {
+            int index = i+1;
+            System.out.print("\t" + index + "." + "[" + tasks.get(i).getStatusIcon() + "] " + tasks.get(i).getDescription() + "\n");
+        }
+        System.out.print(BORDER);
+    }
+
+    public void markAsDone(int taskNumber) {
+        if (taskNumber > tasks.size()) {
+            System.out.print(BORDER +"\t Invalid task. Please enter another task number\n" + BORDER);
+        } else {
+            taskNumber = taskNumber -1;
+            tasks.get(taskNumber).setAsDone();
+            System.out.print(BORDER + "\tNice! I've marked this as done:\n");
+            System.out.print("\t [" + tasks.get(taskNumber).getStatusIcon() + "] " + tasks.get(taskNumber).getDescription() + "\n" + BORDER);
+        }
+    }
+}
