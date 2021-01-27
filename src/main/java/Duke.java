b69a223b2f29fc8310a61083a16c67d02e87e2b7
brynagoh <brynagoh@gmail.com>
2021-01-27 15:51:04 +0800

    Add task status (done/not done)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,28 +14,41 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
 
-        String[] tasks = new String[100];
-        int numTasks = 0;
+        Task[] tasks = new Task[100];
 
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                if (numTasks == 0) {
+                if (Task.totalTasks == 0) {
                     System.out.println("No tasks yet!");
                 } else {
-                    for (int i = 0; i < numTasks; i++) {
-                        System.out.printf("%d. %s\n", i+1, tasks[i]);
+                    for (int i = 0; i < Task.totalTasks; i++) {
+                        System.out.printf("%d.[%s] %s\n", i+1, tasks[i].getStatusIcon(), tasks[i].getDescription());
+                    }
+                }
+
+            } else if (input.contains("done")) {
+                String[] inputArray = input.split(" ");
+                for (String word: inputArray) {
+                    if (word.equals("done")) {
+                        continue;
+                    } else {
+                        int completedIndex = Integer.parseInt(word);
+                        //ensure that the index given is valid
+                        if (completedIndex > 0 && completedIndex <= Task.totalTasks){
+                            tasks[completedIndex - 1].markAsDone();
+                        } else {
+                            System.out.printf("Task %d does not exist! Enter 'list' to view tasklist :)\n", completedIndex);
+                        }
+
                     }
                 }
 
             } else {
-                tasks[numTasks] = input;
-                numTasks++;
+                tasks[Task.totalTasks] = new Task(input);
                 System.out.println("added: "+ input);
             }
             input = sc.nextLine();
-
-
         }
 
         System.out.println("Goodbye！See you again soon :)");
---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public static int totalTasks = 0;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+        totalTasks++;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void markAsDone(){
+        if (!this.isDone) {
+            this.isDone = true;
+            System.out.println("Great! I've marked this task as done: ");
+            System.out.printf("[%s] %s\n", this.getStatusIcon(), this.description);
+        } else {
+            System.out.println("You already completed this task previously!");
+        }
+
+    }
+}
