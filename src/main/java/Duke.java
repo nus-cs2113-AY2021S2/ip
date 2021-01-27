a710370db4864154f7b615d07d97e255b80b4310
beaniestanley <beanbeanstanley@gmail.com>
2021-01-28 05:17:54 +0800

    Added Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,10 +9,21 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("------------------------------------------");
-        System.out.println("Hello there! I'm Duke.");
-        System.out.println("What can I do for you?");
-        System.out.println("__________________________________________");
-        System.out.println("Goodbye. See you soon! ^.^");
-        System.out.println("__________________________________________");    }
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\t------------------------------------------");
+        System.out.println("\tHello there! I'm Duke.");
+        System.out.println("\tWhat can I help you with?");
+        System.out.println("\t__________________________________________\n");
+
+        String userInput = sc.next();
+        while(!userInput.equals("bye")) {
+            System.out.println("\t------------------------------------------");
+            System.out.println("\t" + userInput);
+            System.out.println("\t__________________________________________\n");
+            userInput = sc.next();
+        }
+        System.out.println("\t------------------------------------------");
+        System.out.println("\tSee you soon! Goodbye! ^.^");
+        System.out.println("\t__________________________________________");    }
 }
