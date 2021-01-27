e5ced07760ea84aa25c42b18880918df0f9d0a6b
LAPTOP-VRGPN0FU\MingS <e0425961@u.nus.edu>
2021-01-27 23:11:32 +0800

    A-CodingStandard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ public class Duke {
     public static String getStatus(boolean isDone) {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
+
     public static void main(String[] args) {
 /*        String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -27,17 +28,16 @@ public class Duke {
                 break;
             } else if (word.equals("list")) {
                 for (int i = 0; i < lists.size(); i++) {
-                    System.out.println(i+1 + ".[" + getStatus (lists.get(i).isDone) +"] "+ lists.get(i).description);
+                    System.out.println(i + 1 + ".[" + getStatus(lists.get(i).isDone) + "] " + lists.get(i).description);
                 }
-            } else if (word.contains("done")){
-                String numStr = word.substring(word.indexOf(" ")+1);
+            } else if (word.contains("done")) {
+                String numStr = word.substring(word.indexOf(" ") + 1);
                 int num = Integer.parseInt(numStr);
-                System.out.println("Nice! I've marked this task as done:" );
-                Task d = new Task (lists.get(num-1).description);
-                lists.get(num-1).isDone = d.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                Task d = new Task(lists.get(num - 1).description);
+                lists.get(num - 1).isDone = d.markAsDone();
 
-            }
-            else {
+            } else {
                 Task t = new Task(word);
                 lists.add(t);
                 System.out.println("added: " + word);
