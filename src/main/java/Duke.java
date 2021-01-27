eda2506f1e6c209075575e10e134a2c8769b7e27
stefanie <65395025+s-t-e-f@users.noreply.github.com>
2021-01-27 15:49:39 +0800

    Level-1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,6 +10,21 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        System.out.println("------------------------------------------");
+
+        Scanner sc = new Scanner(System.in);
+
+        String task1 = sc.nextLine();
+        System.out.println(task1);
+
+        String task2 = sc.nextLine();
+        System.out.println(task2);
+
+        System.out.println("bye");
+        System.out.println("------------------------------------------");
+
+
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
