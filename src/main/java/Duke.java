65dc52a763c8d2a2532ba03dfaa2b00970be8ef5
justinaquak <justinaquak@gmail.com>
2021-01-29 00:28:10 +0800

    finish level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,7 +8,13 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo);
         System.out.println("Hello from Duke!");
+        String task;
+        Scanner sc = new Scanner(System.in);
         System.out.println("What can I do for you?");
+        while (!(task = sc.nextLine()).equals("Bye")){
+            System.out.println(task);
+            System.out.println("What can I do for you?");
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
