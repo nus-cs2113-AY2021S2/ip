678a715d5c96b5d43018c63e0d5bfeb7d7ea413a
CabbageTime <cabbagetime@outlook.com>
2021-01-28 23:56:06 +0800

    Add skeletal version of Duke that greets, echo the users and exit when "bye" is input.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String command = "";
         System.out.println("Hello! I'm Duke\n" +
                 "What can I do for you?");
-        System.out.println("\nBye. Hope to see you again soon!");
+        do{
+            command = in.nextLine();
+            System.out.println(command);
+        }while(!command.equals("bye"));
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
