5e29e53ab512179ccc11f0a9b756f4b5322bf5ab
justinaquak <justinaquak@gmail.com>
2021-01-29 00:51:23 +0800

    finish level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,12 +11,27 @@ public class Duke {
         System.out.println(logo);
         System.out.println("Hello from Duke!");
         String task;
+        Set<String> list = new HashSet<String>();
         Scanner sc = new Scanner(System.in);
         System.out.println("What can I do for you?");
         while (!(task = sc.nextLine()).equals("Bye")){
-            System.out.println(task);
-            System.out.println("What can I do for you?");
+            if(task.equals("list")) {
+                int taskNumber =1;
+                for (String todo : list){
+                    System.out.println(taskNumber+". " + todo);
+                    taskNumber++;
+                }
+                System.out.println("What can I do for you?");
+            }
+            else {
+                list.add(task);
+                System.out.println("added: " + task);
+                System.out.println("What can I do for you?");
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
+
+// TODO: make it case insensitive
+// TODO: add tasks in chronological order
