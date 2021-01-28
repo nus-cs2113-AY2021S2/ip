de47a21da5d452850c815eeb5350f5b70778e111
liping-eng <wongliping9962@gmail.com>
2021-01-28 21:39:15 +0800

    add greet, echo, exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +11,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        //skeletal version of Duke
+        String userInput;
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        do {
+            userInput = SCANNER.nextLine();
+            System.out.println(userInput);
+            System.out.println("------------------------------------");
+        } while (!userInput.equals("bye"));
+
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
