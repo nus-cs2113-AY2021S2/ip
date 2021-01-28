7d78e5911476b95df67b8201765868158655943a
liping-eng <wongliping9962@gmail.com>
2021-01-28 22:55:47 +0800

    add done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,47 +2,62 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    public static ArrayList<String> myItems = new ArrayList<String>();
-    public static final Scanner SCANNER = new Scanner(System.in);
+public static ArrayList<String> myItems = new ArrayList<String>();
+public static ArrayList<Boolean> itemStatuses = new ArrayList<Boolean>();
+public static final Scanner SCANNER = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+public static void main(String[] args) {
+    String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    System.out.println("Hello from\n" + logo);
 
-        //skeletal version of Duke
-        String userInput = "";
+    //skeletal version of Duke
+    String userInput = "";
 
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+    System.out.println("Hello! I'm Duke");
+    System.out.println("What can I do for you?");
 
-        while(true) {
-            userInput = SCANNER.nextLine();
-            System.out.println("------------------------------------");
-            if(userInput.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else if (userInput.startsWith("list")) {
-                for(int i=0; i< myItems.size(); i++) {
-                    System.out.println(i+1 + ". " + myItems.get(i));
+    while(true) {
+        userInput = SCANNER.nextLine();
+        System.out.println("------------------------------------");
+        if(userInput.trim().equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+            break;
+        } else if (userInput.startsWith("list")) {
+            System.out.println("Here are the tasks in your list: ");
+            for(int i=0; i< myItems.size(); i++) {
+                System.out.print(i+1 + ".[");
+                if(itemStatuses.get(i) == true) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
                 }
-            } else {
-                myItems.add(userInput);
-                System.out.println("added: " + userInput);
+                System.out.println("] " + myItems.get(i));
             }
-            System.out.println("------------------------------------");
+        } else if (userInput.trim().startsWith("done")) {
+            //String = userInput.substring(4);
+            int processedInput;
+            processedInput = Integer.parseInt(userInput.replaceAll("[^0-9]","")) -1;
+            itemStatuses.set(processedInput,true);
+            System.out.println("Nice! I've marked this task as done: ");
+            System.out.println("[X] " + myItems.get(processedInput));
+        } else {
+            myItems.add(userInput);
+            System.out.println("added: " + userInput);
+            itemStatuses.add(false);
         }
-     /*   do {
-            userInput = SCANNER.nextLine();
-
-            System.out.println(userInput);
-            System.out.println("------------------------------------");
-        } while (!userInput.equals("bye"));
-*/
-
-
+        System.out.println("------------------------------------");
     }
+ /*   do {
+        userInput = SCANNER.nextLine();
+
+        System.out.println(userInput);
+        System.out.println("------------------------------------");
+    } while (!userInput.equals("bye"));*/
+
+
+}
 }
