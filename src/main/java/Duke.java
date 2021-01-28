3c0e9208892a981e27c9dcc368b9dc38a8f8b896
s-t-e-f <65395025+s-t-e-f@users.noreply.github.com>
2021-01-28 10:53:59 +0800

    Level-3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,15 @@ import java.util.ArrayList;
 
 public class Duke {
 
-    static ArrayList<String> tasksList = new ArrayList<> ();
+    static ArrayList<Task> tasksList = new ArrayList<> ();
     static int tasksCount = 0 ;
+
+    public static void printTaskList(ArrayList<Task> taskList) {
+        System.out.println("Here are the tasks in your list: ");
+        for (int i=0; i<tasksList.size(); i++){
+            System.out.println("    " + (i+1) + ". [" + taskList.get(i).getStatusIcon() + "] " + tasksList.get(i).getDescription());
+        }
+    }
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -27,19 +34,23 @@ public class Duke {
                 break;
             }
             else if (task.equalsIgnoreCase("list")) {
-                System.out.println("list");
-                for (int i=0; i<tasksList.size(); i++){
-                    System.out.println("    " + (i+1) + ". " + tasksList.get(i));
-                }
+                printTaskList(tasksList);
                 System.out.println("------------------------------------------");
-                task = sc.nextLine();
+            }
+            else if (task.contains("done")){
+                int idx = Integer.parseInt(String.valueOf(task.charAt(5)));
+                tasksList.get(idx-1).markAsDone();
+                System.out.println("------------------------------------------");
+                System.out.println("    Nice! I've marked this task as done: ");
+                System.out.println("    [" + tasksList.get(idx-1).getStatusIcon() + "] " + tasksList.get(idx-1).getDescription());
             }
             else {
+                Task t = new Task(task);
                 tasksCount++;
-                tasksList.add(task);
+                tasksList.add(t);
                 System.out.println("    added: " + task);
-                task = sc.nextLine();
             }
+            task = sc.nextLine().trim();
         }
         System.out.println("------------------------------------------");
         System.out.println("    Bye. Hope to see you again soon!");
