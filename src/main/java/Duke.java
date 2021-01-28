a26cacdbd2cc24e09c3cb1f5019c09310ce3464f
heyjinwei <chuah.jin.wei@gmail.com>
2021-01-28 19:14:16 +0800

    First commit with echoMode

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void printSeparator() {
+        for(int i = 0; i < 40; i++) {
+            System.out.print('-');
+        }
+        System.out.print('\n');
+    }
+
+    public static void echoMode() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (!line.equals("bye")){
+            System.out.print(line + '\n');
+            printSeparator();
+            line = in.nextLine();
+        }
+        System.out.print("My cover's blown!\n");
+        printSeparator();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /* String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        */
+        printSeparator();
+        System.out.print("Greetings, fellow humans!\nI am CI.\nHow may I help you?\n");
+        printSeparator();
+        echoMode();
     }
 }
