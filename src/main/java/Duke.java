b452061b2086e97bbe3a533f98ad079fe6064394
JoviYeung92 <joviyeung1992@gmail.com>
2021-01-28 12:46:37 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,22 +2,36 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+
         String line;
+        String []list= new String[100];
         Scanner in = new Scanner(System.in);
+        int counter = 0;
 
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println("Hello! I'm Duke\nWhat can i do for you?\n");
 
-        while(true){
+
+
+
+        while(true) {
 
             line = in.nextLine();
 
-            if(line.equals("bye")) {
+            if(line.equals("bye")){
                 break;
-            }else{
-                System.out.println(line);
             }
 
+            else if(line.equals("list")){
+                int count=1;
+                for(int i=0;i<counter;i++){
+                    System.out.println((i+1)+". " +list[i]);
+                }
+            }
+            else {
+                list[counter++] = line;
+                System.out.println("added: "+line);
+            }
         }
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
