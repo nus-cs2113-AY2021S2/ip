ad5ec0692817e052ed0a7b25a257ef7941b38d86
Chiamjiaen <64964101+Chiamjiaen@users.noreply.github.com>
2021-01-28 22:45:37 +0800

    Add greet, echo, exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 
 public class Duke {
+    static Scanner myObj = new Scanner(System.in);
+    static String LINEBREAK = "____________________________________________________________\n";
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,8 +13,12 @@ public class Duke {
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Duke\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+                "____________________________________________________________\n");
+        String text = myObj.nextLine();
+        while (!text.equals("bye")) {
+            System.out.println(LINEBREAK + text + "\n" + LINEBREAK);
+            text = myObj.nextLine();
+        }
+        System.out.println(LINEBREAK + "Bye, Hope to see you again soon!\n" + LINEBREAK);
     }
 }
