abaf30ccdaeb3e5d131731c3453ad9dcbcd6fb3f
liping-eng <wongliping9962@gmail.com>
2021-01-28 22:00:34 +0800

    add items and list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-
+    public static ArrayList<String> myItems = new ArrayList<String>();
     public static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -13,17 +14,35 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         //skeletal version of Duke
-        String userInput;
+        String userInput = "";
+
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
-        do {
+        while(true) {
             userInput = SCANNER.nextLine();
+            System.out.println("------------------------------------");
+            if(userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else if (userInput.startsWith("list")) {
+                for(int i=0; i< myItems.size(); i++) {
+                    System.out.println(i+1 + ". " + myItems.get(i));
+                }
+            } else {
+                myItems.add(userInput);
+                System.out.println("added: " + userInput);
+            }
+            System.out.println("------------------------------------");
+        }
+     /*   do {
+            userInput = SCANNER.nextLine();
+
             System.out.println(userInput);
             System.out.println("------------------------------------");
         } while (!userInput.equals("bye"));
+*/
 
 
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
