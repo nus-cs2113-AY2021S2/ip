a3b535efbcd5bf5631047e833832bcf635cf30a2
justinaquak <justinaquak@gmail.com>
2021-01-29 21:26:44 +0800

    change list feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,39 +19,48 @@ public class Duke {
         int taskNumber = 0;
         while (!(taskInput = sc.nextLine()).equals("bye")){
 //            taskInput = sc.nextLine();
-            int dividerPosition = taskInput.indexOf(" ");
-            String taskCommand = taskInput.toLowerCase().substring(0,dividerPosition);
-            String taskDescription = taskInput.toLowerCase().substring(dividerPosition);
-            switch (taskCommand){
-            case "list":
+            if (taskInput.toLowerCase().equals("list")){
                 System.out.println("This is your current list:");
                 for (int i = 0; i < taskNumber; i++){
                     int displayedTask = i + 1;
                     System.out.println(displayedTask + ": " + Tasks[i].getStatusIcon() + Tasks[i].getTaskDescription());
                 }
-                break;
+            }
+            else {
+                int dividerPosition = taskInput.indexOf(" ");
+                String taskCommand = taskInput.toLowerCase().substring(0, dividerPosition);
+                String taskDescription = taskInput.toLowerCase().substring(dividerPosition);
+                switch (taskCommand) {
+                    //            case "list":
+                    //                System.out.println("This is your current list:");
+                    //                for (int i = 0; i < taskNumber; i++){
+                    //                    int displayedTask = i + 1;
+                    //                    System.out.println(displayedTask + ": " + Tasks[i].getStatusIcon() + Tasks[i].getTaskDescription());
+                    //                }
+                    //                break;
 
-            case "todo":
-                Tasks[taskNumber] = new Task(taskDescription);
-                System.out.println("added: " + taskDescription);
-                taskNumber++;
-                break;
+                    case "todo":
+                        Tasks[taskNumber] = new Task(taskDescription);
+                        System.out.println("added: " + taskDescription);
+                        taskNumber++;
+                        break;
 
-            case "deadline":
-                break;
+                    case "deadline":
+                        break;
 
-            case "done":
-                int taskNumberCompleted = Integer.parseInt(taskDescription.trim()) - 1;
-                Tasks[taskNumberCompleted].markAsDone();
-                System.out.println("Nice! I've marked this task as done!");
-                System.out.println(Tasks[taskNumberCompleted].getStatusIcon() + Tasks[taskNumberCompleted].getTaskDescription());
-                break;
+                    case "done":
+                        int taskNumberCompleted = Integer.parseInt(taskDescription.trim()) - 1;
+                        Tasks[taskNumberCompleted].markAsDone();
+                        System.out.println("Nice! I've marked this task as done!");
+                        System.out.println(Tasks[taskNumberCompleted].getStatusIcon() + Tasks[taskNumberCompleted].getTaskDescription());
+                        break;
 
-//            case "bye":
-//                break;
+                    //            case "bye":
+                    //                break;
 
-            default:
-                break;
+                    default:
+                        break;
+                }
             }
             System.out.println("What can I do for you?");
         }
