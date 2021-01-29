5607016d38adcff4fea6bfc3b33942f2a695499a
ivanchongzhien <icze2009@gmail.com>
2021-01-29 20:29:50 +0800

    Add Task class to repo
    Forgot to track this file in previous commits

---
+++ src/main/java/Task.java
@@ -0,0 +1,41 @@
+public class Task {
+    private String job;
+    private boolean isDone;
+
+    public static int taskCount = 0;
+
+    public Task() {
+        this.job = null;
+        this.isDone = false;
+    }
+
+    public Task(String inputJob){
+        this.job = inputJob;
+        this.isDone = false;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public void printTask(){
+        String doneBox = "[X] ";
+        String emptyBox = "[ ] ";
+
+        String output = this.isDone ? doneBox : emptyBox;
+        output += this.job;
+        System.out.println(output);
+    }
+}
