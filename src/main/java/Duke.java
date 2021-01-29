010d35f7fcd705140fdf688503e8ef7c31a74f0e
ivanchongzhien <icze2009@gmail.com>
2021-01-29 20:27:18 +0800

    Refactored code to try follow SLAM
    Extracted many new methods - methods are inside Duke class, no new classes created

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,94 +1,142 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    private static final int MAX_TASK = 100;
+
     public static void main(String[] args) {
+        printHello();
+        executeCommand();
+        printBye();
+    }
 
-        String helloMessage = "Hello I'm Diuk! \nWhat would you like to do today?\n";
-        String byeMessage = "Bye! Hit me up if you feel like being productive again ;)\n";
-        String line = "____________________________________________________________\n";
-
-        // Start - Greets user
-        System.out.print(line);
-        System.out.print(helloMessage);
-        System.out.print(line);
-
+    private static void executeCommand() {
         Scanner in = new Scanner(System.in);
 
-        Task[] taskList = new Task[100];
+        Task[] taskList = new Task[MAX_TASK];
 
         //Loop to receive response
         while (true){
             String input = in.nextLine();
 
             // BYE command
-            if(input.equalsIgnoreCase("BYE")){
-                break;
+            if(isSameInput(input, "bye")){
+                return;
             }
 
             // LIST command
-            if(input.equalsIgnoreCase("LIST")){
-                int numbering = 1;
-
-                // error handling - no jobs
-                if(Task.taskCount == 0){
-                    System.out.println("No tasks yet! What would you like to do today?\n");
-                }
-                else {
-                    for (int i = 0; i < Task.taskCount; i++) {
-                        System.out.print(numbering + ". ");
-                        taskList[i].printTask();
-                        numbering++;
-                    }
-                    System.out.println();
-                }
+            if(isSameInput(input, "list")){
+                runList(taskList);
             }
 
             // DONE command
             else if(input.toUpperCase().startsWith("DONE")){
-                String[] word = input.split(" ");
-
-                // handles invalid input
-                if(word.length != 2){
-                    System.out.println("Wrong format! Enter in the format: \"done [number]\"");
-                    System.out.println("Make sure number is a valid integer! \n");
-                    continue;
-                }
-
-                int jobNumber = Integer.parseInt(word[1]) - 1;
-
-                if(jobNumber < Task.taskCount && jobNumber >= 0){
-                    taskList[jobNumber].setDone(true);
-                    System.out.print("Congrats! You've completed: \n   ");
-                    taskList[jobNumber].printTask();
-                    System.out.println();
-                }
-                else if(Task.taskCount == 0){
-                    System.out.println("You don't have any tasks yet! Enter a task \n");
-                }
-                // smaller
-                else if(jobNumber < 0){
-                    System.out.println("Enter a valid job number. Use the list command to view your current tasks.\n");
-                }
-
-                // larger
-                else {
-                    System.out.println("You don't have that many jobs! Use the list command to view your current tasks.\n");
-                }
+                runDone(taskList, input);
             }
 
             // Adds new task
             else {
-                // stores user command as job
-                Task newTask = new Task(input);
-                taskList[Task.taskCount] = newTask;
-                Task.taskCount++;
-                System.out.println("Added to list: " + newTask.getJob() + '\n');
+                runAdd(taskList, input);
             }
         }
 
+
+    }
+
+    private static void runAdd(Task[] taskList, String input) {
+        // stores user command as job
+        Task newTask = new Task(input);
+        taskList[Task.taskCount] = newTask;
+        Task.taskCount++;
+        printTaskAdded(newTask.getJob());
+    }
+
+    private static void runDone(Task[] taskList, String input) {
+        String[] word = input.split(" ");
+
+        // handles invalid input
+        if(word.length != 2){
+            printInvalidInputWarning();
+            return;
+        }
+
+        int jobNumber = Integer.parseInt(word[1]) - 1;
+
+        if(jobNumber < Task.taskCount && jobNumber >= 0){
+            markJobAsDone(taskList[jobNumber]);
+        }
+        else if(Task.taskCount == 0){
+            printNoTaskWarning();
+        }
+        else{
+            printInvalidTaskWarning(jobNumber);
+        }
+    }
+
+    private static void runList(Task[] taskList) {
+        int numbering = 1;
+
+        // error handling - no jobs
+        if(Task.taskCount == 0){
+            System.out.println("No tasks yet! What would you like to do today?\n");
+        }
+        else {
+            for (int i = 0; i < Task.taskCount; i++) {
+                System.out.print(numbering + ". ");
+                taskList[i].printTask();
+                numbering++;
+            }
+            System.out.println();
+        }
+    }
+
+    private static boolean isSameInput(String input, String command){
+        return input.equalsIgnoreCase(command);
+    }
+
+    private static void markJobAsDone(Task task) {
+        task.setDone(true);
+        System.out.print("Congrats! You've completed: \n   ");
+        task.printTask();
+        System.out.println();
+    }
+
+    private static void printHello() {
+        // Start - Greets user
+        String line = "____________________________________________________________\n";
+        String hello_message = "Hello I'm Diuk! \nWhat would you like to do today?\n";
+
         System.out.print(line);
-        System.out.print(byeMessage);
+        System.out.print(hello_message);
         System.out.print(line);
+    }
+
+    private static void printBye() {
+        String line = "____________________________________________________________\n";
+        String bye_message = "Bye! Hit me up if you feel like being productive again ;)\n";
+
+        System.out.print(line);
+        System.out.print(bye_message);
+        System.out.print(line);
+    }
+
+    private static void printInvalidInputWarning() {
+        System.out.println("Wrong format! Enter in the format: \"done [number]\"");
+        System.out.println("Make sure number is a valid integer! \n");
+    }
+
+    private static void printTaskAdded(String task) {
+        System.out.println("Added to list: " + task + '\n');
+    }
+
+    private static void printNoTaskWarning() {
+        System.out.println("You don't have any tasks yet! Enter a task \n");
+    }
+
+    private static void printInvalidTaskWarning(int jobNumber) {
+        String smaller = "Enter a valid job number. Use the list command to view your current tasks.\n";
+        String larger = "You don't have that many jobs! Use the list command to view your current tasks.\n";
+        System.out.println(jobNumber < 0 ? smaller : larger);
     }
 
 }
