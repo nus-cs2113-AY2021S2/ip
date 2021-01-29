862b1f09e620092af9d4bec43681893ca052c05b
limwenfeng <wilfred.limwf@gmail.com>
2021-01-29 21:17:29 +0800

    Level 2
    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Duke {
-    private static final Scanner SCANNER = new Scanner(System.in);
+    private static Scanner SCANNER = new Scanner(System.in);
+    private static Vector<String> entries = new Vector<String>();
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -20,8 +22,12 @@ public class Duke {
                 exitDuke();
                 break;
             }
+            if (userCommand.equalsIgnoreCase("list")){
+                listOutEntries();
+            } else {
             printLine();
-            echoUserCommand(userCommand);
+            addUserCommandToEntries(userCommand);
+            }
             printLine();
         }
     }
@@ -36,12 +42,26 @@ public class Duke {
         System.out.println("---------------------------------------------------");
     }
 
-    private static void echoUserCommand(String userCommand) {
+    private static void addUserCommand(String userCommand) {
         System.out.println(userCommand);
     }
 
     private static String getUserInput() {
         String inputLine = SCANNER.nextLine();
         return inputLine;
+    }
+
+    private static void addUserCommandToEntries(String userCommand) {
+        String newEntries = userCommand;
+        entries.add(userCommand);
+        System.out.println("Added: " + userCommand);
+    }
+
+    private static void listOutEntries() {
+        int i = 0;
+        while (i < entries.size()) {
+            System.out.println(i + ". " + entries.get(i));
+            i++;
+        }
     }
 }
