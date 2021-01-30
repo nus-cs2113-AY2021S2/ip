fb366930e2b515f040d1ada7eaca3edad5c8e795
LeeHanYongAndy <andyleehy2502@gmail.com>
2021-01-30 20:55:37 +0800

    update SLAP
    Implemented SLAP and updated my mushroom head logo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,15 +14,33 @@ public class Duke {
     }
 
     public static void printHelloStatement() {
+        String logo = "           ____\n" +
+                "       _.-'78o `\"`--._\n" +
+                "   ,o888o.  .o888o,   ''-.\n" +
+                " ,88888P  `78888P..______.]\n" +
+                "/_..__..----\"\"        __.'\n" +
+                "`-._       /\"\"| _..-''\n" +
+                "    \"`-----\\  `\\\n" +
+                "            |   ;.-\"\"--..\n" +
+                "            | ,8o.  o88. `.\n" +
+                "            `;888P  `788P  :\n" +
+                "      .o\"\"-.|`-._         ./\n" +
+                "     J88 _.-/    \";\"-P----'\n" +
+                "     `--'\\`|     /  /\n" +
+                "         | /     |  |\n" +
+                "         \\|     /   |akn\n" +
+                "          `-----`---'\n";
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+         */
         printDividerLine();
         System.out.println(logo);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("Welcome Mushroom!");
+        System.out.println("What can Mushroom Head do for you?");
         printDividerLine();
     }
 
@@ -56,51 +74,53 @@ public class Duke {
         }
     }
 
-    public static int getTaskIndex(String userInput) {
+    private static void performAction(int commandCode, String userInput, Task task) {
+        // No Fallthrough intended in this switch statement
+        switch (commandCode) {
+        case INPUT_CODE_EXIT:
+            printBye();
+            break;
+        case INPUT_CODE_LIST:
+            printDividerLine();
+            task.listTask();
+            printDividerLine();
+            break;
+        case INPUT_CODE_DONE:
+            printDividerLine();
+            int index = getTaskIndexFromUserInput(userInput);
+            task.markDone(index);
+            printDividerLine();
+            break;
+        case INPUT_CODE_INVALID:
+            printDividerLine();
+            System.out.println("You have enter an invalid command, please try again.");
+            printDividerLine();
+            break;
+        default:
+            printDividerLine();
+            task.addTask(userInput);
+            printDividerLine();
+            break;
+        }
+    }
+
+    private static int getTaskIndexFromUserInput(String userInput) {
         String[] words = userInput.split(" ");
         int indexResult = Integer.parseInt(words[1]);
         return indexResult;
     }
 
     public static void main(String[] args) {
-        printHelloStatement();
-
         int commandCode;
         String userInput;
         Scanner in = new Scanner(System.in);
         Task task = new Task();
 
+        printHelloStatement();
         do {
             userInput = in.nextLine();
             commandCode = getCommandCode(userInput);
-
-            // No Fallthrough intended in this switch statement
-            switch (commandCode) {
-            case INPUT_CODE_EXIT:
-                printBye();
-                break;
-            case INPUT_CODE_LIST:
-                printDividerLine();
-                task.listTask();
-                printDividerLine();
-                break;
-            case INPUT_CODE_DONE:
-                printDividerLine();
-                int index = getTaskIndex(userInput);
-                task.markDone(index);
-                printDividerLine();
-                break;
-            case INPUT_CODE_INVALID:
-                printDividerLine();
-                System.out.println("You have enter an invalid command, please try again.");
-                printDividerLine();
-                break;
-            default:
-                printDividerLine();
-                task.addTask(userInput);
-                printDividerLine();
-                break;
-            }
+            performAction(commandCode, userInput, task);
         } while (commandCode != INPUT_CODE_EXIT);
     }
 }
