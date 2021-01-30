214819d9e10b4a7910c3fdcc7653d051cc76e764
lowwilliam <2595903341@qq.com>
2021-01-30 20:27:23 +0800

    Finish level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static String inputValue[] = new String [100];
     public static boolean inputStatus[] = new boolean [100];
+    public static String inputState[] = new String [100];
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -20,11 +21,15 @@ public class Duke {
     public static void interaction(){
         int listIndex = 0;
         String deleteString = "done ";
+        String stateTodo = "todo ";
+        String stateDeadline = "deadline ";
+        String stateEvent = "event ";
         do{
             Scanner input = new Scanner(System.in);
             input.useDelimiter("\n");
             inputValue[listIndex] = input.next();
             inputStatus[listIndex] = false;
+            inputState[listIndex] = "T";
             if(inputValue[listIndex].equals("bye")){
                 System.out.println("____________________________________________________________\n"+
                 "Bye. Hope to see you again soon!"+"\n"+
@@ -34,28 +39,61 @@ public class Duke {
                 for (int i = 0; i<listIndex; i++){
                     int index = i+1;
                     if (inputStatus[i]==true){
-                        System.out.println(index +".[x] "+ inputValue[i]);
+                        System.out.println(index +".["+inputState[i]+"]"+"[x] "+ inputValue[i]);
                     }else{
-                        System.out.println(index +".[ ] "+ inputValue[i]);
+                        System.out.println(index +".["+inputState[i]+"]"+"[ ] "+ inputValue[i]);
                     }
                 }
                 System.out.println("____________________________________________________________\n");
             }else if(inputValue[listIndex].startsWith(deleteString)){
-                int delStrLength = deleteString.length();
-                inputValue[listIndex] = inputValue[listIndex].substring(delStrLength);
+                inputValue[listIndex] = deletePrefix(inputValue[listIndex],deleteString);
                 int doneIndex = Integer.parseInt(inputValue[listIndex]);
                 System.out.println("____________________________________________________________\n"+
                 "Nice! I've marked this task as done: \n"+
-                "[X] "+ inputValue[doneIndex-1]+"\n"+
+                "["+inputState[doneIndex-1]+"]"+"[X] "+ inputValue[doneIndex-1]+"\n"+
                 "____________________________________________________________\n");
                 inputStatus[doneIndex-1] = true;
             }else{
+                if(inputValue[listIndex].startsWith(stateEvent)){
+                    inputState[listIndex] = "E";
+                    inputValue[listIndex] = deletePrefix(inputValue[listIndex],stateEvent);
+                    inputValue[listIndex] = inputValue[listIndex].replace("/", "(");
+                    inputValue[listIndex] = inputValue[listIndex].replace("at", "at:");
+                    inputValue[listIndex] = inputValue[listIndex]+")";
+                }else if(inputValue[listIndex].startsWith(stateTodo)){
+                    inputState[listIndex] = "T";
+                    inputValue[listIndex] = deletePrefix(inputValue[listIndex],stateTodo);
+                }else if(inputValue[listIndex].startsWith(stateDeadline)){
+                    inputState[listIndex] = "D";
+                    inputValue[listIndex] = deletePrefix(inputValue[listIndex],stateDeadline);
+                    inputValue[listIndex] = inputValue[listIndex].replace("/", "(");
+                    inputValue[listIndex] = inputValue[listIndex].replace("by", "by:");
+                    inputValue[listIndex] = inputValue[listIndex]+")";
+                }
                 System.out.println("____________________________________________________________\n"+
-                "added: "+inputValue[listIndex]+"\n"+
+                "Got it. I've added this task:\n"+
+                "["+inputState[listIndex]+"]"+"[ ] "+inputValue[listIndex]+"\n"+
+                "Now you have "+(listIndex+1)+" tasks in the list.\n"+
                 "____________________________________________________________\n");
                 listIndex+=1;
                 inputValue[listIndex]=inputValue[listIndex-1];
             }
            }while(!inputValue[listIndex].equals("bye"));
     }
+
+    public static String deletePrefix(String input, String prefix){
+        int prefixLength = prefix.length();
+        input = input.substring(prefixLength);
+        return input;
+    }
+    // public static String inputState(int state){
+    //     String returnValue = null;
+    //     if(state == 0){
+    //         returnValue = 
+    //     }else if(state == 1){
+
+    //     }else if(state ==2){
+
+    //     }
+    // }
 }
