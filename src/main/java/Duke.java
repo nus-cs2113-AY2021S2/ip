ddc8b2449e9fd16c1aebfe570750e08dc7ebe1d1
lowwilliam <2595903341@qq.com>
2021-01-30 17:45:38 +0800

    Finish Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static String inputValue[] = new String [100];
+    public static boolean inputStatus[] = new boolean [100];
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -18,9 +19,12 @@ public class Duke {
 
     public static void interaction(){
         int listIndex = 0;
+        String deleteString = "done ";
         do{
             Scanner input = new Scanner(System.in);
+            input.useDelimiter("\n");
             inputValue[listIndex] = input.next();
+            inputStatus[listIndex] = false;
             if(inputValue[listIndex].equals("bye")){
                 System.out.println("____________________________________________________________\n"+
                 "Bye. Hope to see you again soon!"+"\n"+
@@ -29,9 +33,22 @@ public class Duke {
                 System.out.println("____________________________________________________________\n");
                 for (int i = 0; i<listIndex; i++){
                     int index = i+1;
-                    System.out.println(index +". "+ inputValue[i]);
+                    if (inputStatus[i]==true){
+                        System.out.println(index +".[x] "+ inputValue[i]);
+                    }else{
+                        System.out.println(index +".[ ] "+ inputValue[i]);
+                    }
                 }
                 System.out.println("____________________________________________________________\n");
+            }else if(inputValue[listIndex].startsWith(deleteString)){
+                int delStrLength = deleteString.length();
+                inputValue[listIndex] = inputValue[listIndex].substring(delStrLength);
+                int doneIndex = Integer.parseInt(inputValue[listIndex]);
+                System.out.println("____________________________________________________________\n"+
+                "Nice! I've marked this task as done: \n"+
+                "[X] "+ inputValue[doneIndex-1]+"\n"+
+                "____________________________________________________________\n");
+                inputStatus[doneIndex-1] = true;
             }else{
                 System.out.println("____________________________________________________________\n"+
                 "added: "+inputValue[listIndex]+"\n"+
