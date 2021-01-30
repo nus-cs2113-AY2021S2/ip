90fbdb0ca71c23733d71de97c36d6aa4e964f3d6
jianningzhuang <jianning.personal@gmail.com>
2021-01-30 15:35:00 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,8 +6,8 @@ public class Duke {
     public static int index = 0;
 
     public static void greet() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
+        String logo = " ____        _\n"
+                + "|  _ \\ _   _| | _____\n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
