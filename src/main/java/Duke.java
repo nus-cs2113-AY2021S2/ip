62886352bc98dca787303be1973c1d7ea523d63f
kewenlok <e0360650@u.nus.edu>
2021-01-30 11:47:01 +0800

    validateIndexNumber method: Fix minor bug

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -120,6 +120,7 @@ public class Duke {
             isInRange = checkIsInRange(indexNumber);
         } else {
             showInvalidInputFormatError();
+            return isValidIndexNumber;
         }
 
         if (isInRange) {
