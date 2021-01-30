80bb6466591c31c743c761c1bf080b1c99d3c97b
Chihui <NGCH0118@e.ntu.edu.sg>
2021-01-30 17:13:30 +0800

    add todo, deadline and event methods. add done function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,33 +1,55 @@
 import java.util.*;
+
 public class Duke{
     Scanner sc = new Scanner(System.in);
     Task[] t = new Task[100];
-    int taskNo = 0;
+    private static int taskNo = 0;
 
     public void run() {
 
-        String choice;
+        String input;
+        String[] switchChoice;
         do {
             Ui.printMenu();
-            choice = sc.nextLine();
-            switch (choice) {
-
-            case "1":
-                enterTask();
+            input = sc.nextLine().trim().toLowerCase();
+            switchChoice = input.toLowerCase().split(" ", 2);
+            switch (switchChoice[0]){
+            case "todo":
+                enterToDo(switchChoice[1]);
                 break;
-            case "2":
+            case "deadline":
+                enterDeadline(switchChoice[1]);
+                break;
+            case "event":
+                enterEvent(switchChoice[1]);
+                break;
+            case "list":
                printDescription();
                 break;
-            case "3":
-                markDone();
+            case "done":
+                markDone(switchChoice[1]);
                 break;
             }
-        } while (!choice.equals("bye"));
+        } while (!input.equals("bye"));
     }
-    private void enterTask() {
-        t[taskNo] = new Task();
-        System.out.println("Please enter you task!");
-        t[taskNo].setDescription(sc.nextLine());
+
+
+    private void enterDeadline(String input) {
+        String[] inputSplit = input.split("/by");
+        t[taskNo] = new Deadline(inputSplit[0], inputSplit[1]);
+        t[taskNo].setTaskNo(taskNo);
+        taskNo++;
+    }
+
+    private void enterEvent(String input) {
+        String[] inputSplit = input.split("/at");
+        t[taskNo] = new Event(inputSplit[0], inputSplit[1]);
+        t[taskNo].setTaskNo(taskNo);
+        taskNo++;
+    }
+
+    private void enterToDo(String input) {
+        t[taskNo] = new Todo(input);
         t[taskNo].setTaskNo(taskNo);
         taskNo++;
     }
@@ -35,22 +57,15 @@ public class Duke{
     private void printDescription(){
         System.out.println("Select this to print description");
         for (int i = 0; i < taskNo; i++) {
-            System.out.println((t[i].getTaskNo() + 1) + "." + t[i].getStatusIcon() + t[i].getDescription());
+            System.out.println((t[i].getTaskNo() + 1) + ". " + t[i].toString());
         }
+        System.out.println("You have " + (taskNo+1) + " task(s) in the list." );
+
     }
 
-    private void markDone(){
-        System.out.println("Please enter then task number you want to mark as done ");
-        String value = sc.nextLine().replaceAll("[^0-9]", "");
-        int newValue = Integer.parseInt(value);
+    private void markDone(String input){
+        int newValue = Integer.parseInt(input);
         t[newValue - 1].setDone(true);
         System.out.println("Task sucessfully marked as done!");
-    }
-
-
-    public static void main(String[] args) {
-        Duke dk = new Duke();
-        Ui ui = new Ui();
-        dk.run();
     }
 }
