3367ee53cb8830025b711b9e641c779c7e60e789
isaharon <isabharon@gmail.com>
2021-01-31 11:41:26 +0800

    Fix bug and refactor some code
    Fixed checking of user input command without splitting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,49 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static final int MAX_TASKS = 100;
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static Task[] tasks = new Task[MAX_TASKS];
+
     public static void main(String[] args) {
         Menu.greet();
-        Task[] tasks = new Task[100];
-
-        Scanner in = new Scanner(System.in);
-        String userInput;
-        userInput = in.nextLine();
-        while (!userInput.equals("bye")) {
-            if (userInput.startsWith("list")) {
-                handleList(tasks);
-            } else if (userInput.startsWith("done")) {
-                handleDone(userInput, tasks);
-            } else {
-                handleAdd(userInput, tasks);
-            }
-            userInput = in.nextLine();
+        while (true) {
+            String userInput = getUserInput();
+            executeUserCommand(userInput);
         }
-        Menu.bye();
     }
 
-    public static void handleList(Task[] tasks) {
+    private static void executeUserCommand(String userInput) {
+        String[] commandAndArgs = getCommandAndArgs(userInput);
+        String commandType = commandAndArgs[0];
+        String commandArgs = commandAndArgs[1];
+        switch (commandType) {
+        case "list":
+            handleList();
+            break;
+        case "done":
+            handleDone(commandArgs);
+            break;
+        case "bye":
+            handleExit();
+            break;
+        default:
+            handleAdd(userInput);
+        }
+    }
+
+    private static String getUserInput() {
+        String userInput;
+        userInput = SCANNER.nextLine();
+        return userInput;
+    }
+
+    private static String[] getCommandAndArgs(String userInput) {
+        String[] split = userInput.split("\\s+", 2);
+        return split.length == 2 ? split : new String[] { split[0], "" };
+    }
+
+    private static void handleList() {
         String outputText;
         outputText = "Here are the tasks in your list: ";
         for (int i = 0; i < Task.getNumberOfTasks(); i++) {
@@ -34,36 +56,36 @@ public class Duke {
         Menu.echo(outputText);
     }
 
-    public static void handleDone(String userInput, Task[] tasks) {
+    private static void handleDone(String taskNumberString) {
+        int taskNumber = Integer.parseInt(taskNumberString);
         String outputText;
-        String[] words = userInput.split(" ");
-        if (words.length != 2) {
-            outputText = "You did not input valid task number to be marked as done!";
+        if (taskNumber < 1 || taskNumber > Task.getNumberOfTasks()) {
+            outputText = "Task number invalid!";
         } else {
-            int taskNumber = Integer.parseInt(words[1]);
-            if (taskNumber < 1 || taskNumber > Task.getNumberOfTasks()) {
-                outputText = "Task number is invalid!";
+            Task task = tasks[taskNumber - 1];
+            if (!task.isDone) {
+                task.setDone(true);
+                outputText = "Nice! I've marked this task as done:" +
+                        System.lineSeparator() +
+                        "\t" +
+                        task.toString();
             } else {
-                Task task = tasks[taskNumber - 1];
-                if (!task.isDone) {
-                    task.setDone(true);
-                    outputText = "Nice! I've marked this task as done:" +
-                            System.lineSeparator() +
-                            "\t" +
-                            task.toString();
-                } else {
-                    outputText = "Task already marked as done!";
-                }
+                outputText = "Task already marked as done!";
             }
         }
         Menu.echo(outputText);
     }
 
-    public static void handleAdd(String userInput, Task[] tasks) {
+    private static void handleAdd(String taskDescription) {
         String outputText = " added: ";
-        Task t = new Task(userInput);
+        Task t = new Task(taskDescription);
         tasks[Task.getNumberOfTasks() - 1] = t;
-        outputText += userInput;
+        outputText += taskDescription;
         Menu.echo(outputText);
+    }
+
+    private static void handleExit() {
+        Menu.bye();
+        System.exit(0);
     }
 }
