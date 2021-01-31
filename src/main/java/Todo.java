9bb3d04053025c10f6df0e07bf2079cebcb93ec0
LeeHanYongAndy <andyleehy2502@gmail.com>
2021-01-31 17:32:30 +0800

    test pushing new class file

---
+++ src/main/java/Todo.java
@@ -0,0 +1,12 @@
+public class Todo extends Task {
+    private static final String TASK_SYMBOL_TODO = "T";
+
+    public Todo(String taskDescrption) {
+        super(taskDescrption);
+    }
+
+    @Override
+    public String getTaskSymbol() {
+        return TASK_SYMBOL_TODO;
+    }
+}
