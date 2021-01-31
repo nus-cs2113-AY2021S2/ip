aeba016f4cce6574b14de876522c8ee4472256f0
KimIdeas8 <Kberleytay@gmail.com>
2021-01-31 21:56:04 +0800

    Edit 5 of increment: Level 3

---
+++ src/main/java/com/testsmarker/tasks.java
@@ -0,0 +1,25 @@
+package com.testsmarker;
+
+public class tasks {
+
+    protected String description;
+    protected boolean isDone;
+
+    public tasks(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+}
