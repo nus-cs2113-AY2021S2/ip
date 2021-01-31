cb6348a139d1dbefbb596053c698293952d29b5a
Leow Xiao Xuan <leowxiaoxuan@gmail.com>
2021-01-31 20:35:55 +0800

    Added codes for level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,38 +10,37 @@ public class Duke {
         Scanner stringScanner = new Scanner(System.in);
         String userQuery = stringScanner.nextLine();
         String userQueryReturn = "";
-        //String[] listItems = new String[100];
         ArrayList<Task> taskItems = new ArrayList<Task>();
         int itemCount = 0;
         while(!userQuery.equalsIgnoreCase("bye")){
-            taskItems.add(new Task(userQuery));
             if(userQuery.contains("done")){
                 int doneIndex = Integer.parseInt(userQuery.substring(5));
                 taskItems.get(doneIndex-1).setStatus(true);
                 userQueryReturn = "____________________________________________________________\n"
                         +" Nice! I've marked this task as done: \n"
-                        + " ["+taskItems.get(doneIndex-1).getStatusIcon()+"] "+taskItems.get(doneIndex-1).getDescription()+"\n"
+                        + " ["+taskItems.get(doneIndex-1).getTypeIcon()+"]["+taskItems.get(doneIndex-1).getStatusIcon()+"] "+taskItems.get(doneIndex-1).getDescription()+"\n"
                         + "____________________________________________________________\n";
                 System.out.println(userQueryReturn);
                 userQuery = stringScanner.nextLine();
             }else if(!userQuery.equalsIgnoreCase("list")) {
+                taskItems.add(new Task(userQuery));
+                itemCount++;
                 userQueryReturn = "____________________________________________________________\n"
-                        + " added: " + userQuery + "\n"
+                        + " Got it. I've added this task: \n"
+                        + "   ["+taskItems.get(taskItems.size()-1).getTypeIcon()+"]["+taskItems.get(taskItems.size()-1).getStatusIcon()+"] "+taskItems.get(taskItems.size()-1).getDescription()+"\n"
+                        + " Now you have "+taskItems.size()+" tasks in the list.\n"
                         + "____________________________________________________________\n";
                 System.out.println(userQueryReturn);
                 userQuery = stringScanner.nextLine();
-                taskItems.add(new Task(userQuery));
-                itemCount++;
             }else{
                 System.out.println("____________________________________________________________");
                 System.out.println(" Here are the tasks in your list: ");
                 for(int i=0; i<itemCount; i++){
-                    System.out.println(" "+String.valueOf(i+1)+".["+taskItems.get(i).getStatusIcon()+"] "+taskItems.get(i).getDescription());
+                    System.out.println(" "+String.valueOf(i+1)+".["+taskItems.get(i).getTypeIcon()+"]["+taskItems.get(i).getStatusIcon()+"] "+taskItems.get(i).getDescription());
                 }
                 System.out.println("____________________________________________________________\n");
                 userQuery = stringScanner.nextLine();
             }
-            //itemCount++;
         }
         String bye = "____________________________________________________________\n"
                 + " Bye. Hope to see you again soon!\n\n"
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,35 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected String typeIcon;
+    protected String deadline = "";
 
     public Task(String description) {
-        this.description = description;
-        this.isDone = false;
+        if(description.contains("todo")||description.contains("deadline")||description.contains("event")) {
+            String command = description.substring(0, description.indexOf(' '));
+            String content = description.substring(description.indexOf(' ') + 1);
+
+            this.description = content;
+            this.isDone = false;
+            switch (command) {
+            case "todo":
+                typeIcon = "T";
+                break;
+            case "deadline":
+                typeIcon = "D";
+                deadline = content.substring(content.indexOf('/')+4);
+                break;
+            case "event":
+                typeIcon = "E";
+                deadline = content.substring(content.indexOf('/')+4);
+                break;
+            default:
+                typeIcon = " ";
+            }
+        }else{
+            this.description = description;
+            this.isDone = false;
+        }
     }
 
     public String getStatusIcon() {
@@ -12,11 +37,24 @@ public class Task {
         return (isDone ? "X" : " "); //in the example, X means completed and space means incomplete
     }
 
+    public String getTypeIcon() {
+        return typeIcon;
+    }
+
     public void setStatus(boolean isDone){
         this.isDone = isDone;
     }
 
     public String getDescription() {
-        return description;
+        String content;
+        switch (typeIcon) {
+        case "D":
+            return description.substring(0,description.indexOf('/')) + " (by: " + deadline + ")";
+        case "E":
+            return description.substring(0,description.indexOf('/')) + " (at: " + deadline + ")";
+        case "T":
+        default:
+            return description;
+        }
     }
 }
