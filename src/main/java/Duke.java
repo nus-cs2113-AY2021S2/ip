c4917361538e7d1d3a9d1ba3bae4dff1e5089997
calvinlow7 <calvinlow7@gmail.com>
2021-02-01 09:43:07 +0800

    Commiting Week 3 Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,11 +9,27 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Duke");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        echo();
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
+    }
+
+    public static void echo() {
+        String userInput;
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+
+            userInput = sc.nextLine();
+
+            if (userInput.toLowerCase().equals("bye")) break;
+
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t" + userInput);
+            System.out.println("\t____________________________________________________________");
+        }
     }
 }
