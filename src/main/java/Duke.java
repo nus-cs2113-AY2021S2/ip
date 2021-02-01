13ab98380ced8de8c4afb2ce6a6952a034535b95
calvinlow7 <calvinlow7@gmail.com>
2021-02-01 14:49:51 +0800

    Committing changes made for Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -13,12 +14,14 @@ public class Duke {
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
-        echo();
+        toDoList();
         System.out.println("\tBye. Hope to see you again soon!");
         System.out.println("\t____________________________________________________________");
     }
 
-    public static void echo() {
+    public static void toDoList() {
+        String[] list = new String[100];
+        int itemNo = 0;
         String userInput;
         while (true) {
             Scanner sc = new Scanner(System.in);
@@ -27,9 +30,21 @@ public class Duke {
 
             if (userInput.toLowerCase().equals("bye")) break;
 
-            System.out.println("\t____________________________________________________________");
-            System.out.println("\t" + userInput);
-            System.out.println("\t____________________________________________________________");
+            else if (userInput.toLowerCase().equals("list")){
+                System.out.println("\t____________________________________________________________");
+                for (int i = 0; i < itemNo; i++){
+                    System.out.printf("%d. %s\n", i+1, list[i]);
+                }
+                System.out.println("\t____________________________________________________________");
+            }
+
+            else {
+                list[itemNo] = userInput;
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\t added:read " + userInput);
+                System.out.println("\t____________________________________________________________");
+                itemNo++;
+            }
         }
     }
 }
