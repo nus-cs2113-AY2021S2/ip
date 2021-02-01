e9600171e6717678f68ce560d5e1b388c2140125
violinyap <violinyap@gmail.com>
2021-02-01 23:21:50 +0800

    add increment Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,9 +7,22 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm \n" + logo );
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner scan = new Scanner(System.in);
+        String text = "";
+        text = scan.nextLine();
+        while (!text.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(text);
+            System.out.println("____________________________________________________________");
+            text = scan.nextLine();
+        }
+
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
