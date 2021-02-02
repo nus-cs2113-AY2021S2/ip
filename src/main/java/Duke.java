6e01542d179222b8cfc3d02f4f5847c3c1bb3d7f
e0699194 <e0699194@u.nus.edu>
2021-02-02 21:32:56 +0800

    Improve code quality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,20 +13,21 @@ public class Duke {
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
-        toDoList();
+        taskTracker();
         System.out.println("\tBye. Hope to see you again soon!");
         System.out.println("\t____________________________________________________________");
     }
 
-    public static void toDoList() {
+    public static void taskTracker() {
         Task[] tasks = new Task[100];
-        int itemNo = 0;
+        int indexOfTask = 0;
         String userInput;
         Scanner sc = new Scanner(System.in);
 
         running:
         while (sc.hasNextLine()) {
             userInput = sc.nextLine();
+            //Split user input to retrieve task type (i.e. event, deadline) and task description.
             String[] strings = userInput.split(" ",2);
             String taskType = strings[0].toLowerCase();
             String taskDescription = null;
@@ -36,57 +37,61 @@ public class Duke {
 
             switch (taskType) {
             case ("todo"):
-                tasks[itemNo] = new Todo(taskDescription);
+                tasks[indexOfTask] = new Todo(taskDescription);
                 System.out.println("\t____________________________________________________________");
                 System.out.println("\tGot it! I've added this task:");
-                System.out.println("\t" + tasks[itemNo].toString());
-                itemNo++;
-                System.out.printf("\tNow you have %d tasks in the list.\n", itemNo);
+                System.out.println("\t" + tasks[indexOfTask].toString());
+                indexOfTask++;
+                System.out.printf("\tNow you have %d tasks in the list.\n", indexOfTask);
                 System.out.println("\t____________________________________________________________");
                 break;
 
             case ("deadline"):
+                // taskDescription now contains task description and it's deadline date and/or time
+                // Split task description and deadline date and/or time
                 strings = taskDescription.split(" /by ",2);
                 taskDescription = strings[0];
-                String by = strings[1];
-                tasks[itemNo] = new Deadline(taskDescription, by);
+                String deadlineDate = strings[1];
+                tasks[indexOfTask] = new Deadline(taskDescription, deadlineDate);
                 System.out.println("\t____________________________________________________________");
                 System.out.println("\tGot it! I've added this task:");
-                System.out.println("\t" + tasks[itemNo].toString());
-                itemNo++;
-                System.out.printf("\tNow you have %d tasks in the list.\n", itemNo);
+                System.out.println("\t" + tasks[indexOfTask].toString());
+                indexOfTask++;
+                System.out.printf("\tNow you have %d tasks in the list.\n", indexOfTask);
                 System.out.println("\t____________________________________________________________");
                 break;
 
             case ("event"):
+                // taskDescription now contains task description and it's event date and/or timeslot
+                // Split task description and event's date and/or timeslot
                 strings = taskDescription.split(" /at ",2);
                 taskDescription = strings[0];
-                String at = strings[1];
-                tasks[itemNo] = new Event(taskDescription,at);
+                String eventDate = strings[1];
+                tasks[indexOfTask] = new Event(taskDescription,eventDate);
                 System.out.println("\t____________________________________________________________");
                 System.out.println("Got it! I've added this task:");
-                System.out.println("\t" + tasks[itemNo].toString());
-                itemNo++;
-                System.out.printf("Now you have %d tasks in the list.\n", itemNo);
+                System.out.println("\t" + tasks[indexOfTask].toString());
+                indexOfTask++;
+                System.out.printf("Now you have %d tasks in the list.\n", indexOfTask);
                 System.out.println("\t____________________________________________________________");
                 break;
 
             case ("list"):
                 System.out.println("\t____________________________________________________________");
                 System.out.println("Here are the tasks in your tasks:");
-                for (int i = 0; i < itemNo; i++) {
+                for (int i = 0; i < indexOfTask; i++) {
                     System.out.printf("\t%d. %s\n", i + 1, tasks[i].toString());
                 }
                 System.out.println("\t____________________________________________________________");
-                itemNo++;
+                indexOfTask++;
                 break;
 
             case ("done"):
-                int taskIndex = Integer.parseInt(taskDescription) - 1;
-                tasks[taskIndex].setDone();
+                int indexOfTaskDone = Integer.parseInt(taskDescription) - 1;
+                tasks[indexOfTaskDone].setDone();
                 System.out.println("\t____________________________________________________________");
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("\t" + tasks[taskIndex].toString());
+                System.out.println("\t" + tasks[indexOfTaskDone].toString());
                 System.out.println("\t____________________________________________________________");
                 break;
 
