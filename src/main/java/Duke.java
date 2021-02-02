059580c2aebb2460b32580a2059bda9d4dd45fc9
douglaslewpc <reachdouglaslew@gmail.com>
2021-02-03 03:33:19 +0800

    ToDo, Event, Deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,23 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static Task[] tasks = new Task[100];
+
     public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("--------------------------------------------------------------------------------");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("");
+        System.out.println(" ");
+        System.out.println("--------------------------------------------------------------------------------");
         boolean isOn = true;
         int numOfCompletedTasks = 0;
         int numOfTasks = 0;
-        Task[] listArray = new Task[100];
         int n = 0;
         while(isOn) {
             Scanner in = new Scanner(System.in);
@@ -17,24 +26,126 @@ public class Duke {
             if(line.equals("bye")){
                 isOn = false;
             }
+            else if(words[0].equals("todo")){
+                String description = "";
+                int wordsCount = words.length;
+                for(String word:words){
+                    wordsCount--;
+                    if(word.equals("todo")){
+                        continue;
+                    }
+                    else if(wordsCount==0){
+                        description = description + word;
+                    }
+                    else{
+                        description = description + word + " ";
+                    }
+                }
+                tasks[n] = new Todo(description);
+                System.out.println("    --------------------------------------------------------------------------------");
+                System.out.println("    Got it. I've added this task:");
+                System.out.println("      "+tasks[n]);
+                n++;
+                System.out.println("    Now you have " + n +" tasks in the list.");
+                System.out.println(" ");
+                System.out.println("    --------------------------------------------------------------------------------");
+            }
+            else if(words[0].equals("deadline")){
+                String description = "";
+                String by = "";
+                boolean byFlag = false;
+                int wordsCount = words.length;
+                for(String word:words){
+                    wordsCount--;
+                    if(word.equals("/by")){
+                        byFlag = true;
+                        continue;
+                    }
+                    if(word.equals("deadline")){
+                        continue;
+                    }
+                    if(byFlag && wordsCount==0){
+                        by = by + word;
+                    }
+                    else if(byFlag){
+                        by = by + word + " ";
+                    }
+                    else{
+                        description = description + word + " ";
+                    }
+                }
+                tasks[n] = new Deadline(description,by);
+                System.out.println("    --------------------------------------------------------------------------------");
+                System.out.println("    Got it. I've added this task:");
+                System.out.println("      "+tasks[n]);
+                n++;
+                System.out.println("    Now you have " + n +" tasks in the list.");
+                System.out.println(" ");
+                System.out.println("    --------------------------------------------------------------------------------");
+            }
+            else if(words[0].equals("event")){
+                String description = "";
+                String at = "";
+                boolean atFlag = false;
+                int wordsCount = words.length;
+                for(String word:words){
+                    wordsCount--;
+                    if(word.equals("/at")){
+                        atFlag = true;
+                        continue;
+                    }
+                    if(word.equals("event")){
+                        continue;
+                    }
+                    if(atFlag && wordsCount==0){
+                        at = at + word;
+                    }
+                    else if(atFlag){
+                        at = at + word + " ";
+                    }
+                    else{
+                        description = description + word + " ";
+                    }
+                }
+                tasks[n] = new Event(description,at);
+                System.out.println("    --------------------------------------------------------------------------------");
+                System.out.println("    Got it. I've added this task:");
+                System.out.println("      "+tasks[n]);
+                n++;
+                System.out.println("    Now you have " + n +" tasks in the list.");
+                System.out.println(" ");
+                System.out.println("    --------------------------------------------------------------------------------");
+            }
             else if(words[0].equals("done")){
                 int taskNum = Integer.parseInt(words[1])-1;
-                listArray[taskNum].markAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[" + "\u2718" + "] " + listArray[taskNum].getTask());
+                tasks[taskNum].markAsDone();
+                System.out.println("    --------------------------------------------------------------------------------");
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println("      " + tasks[taskNum]);
+                System.out.println(" ");
+                System.out.println("    --------------------------------------------------------------------------------");
             }
             else if(line.equals("list")){
-                System.out.println("Here are the tasks in your list:");
+                System.out.println("    --------------------------------------------------------------------------------");
+                System.out.println("    Here are the tasks in your list:");
                 for(int i=0;i<n;i++){
-                    System.out.println((i + 1) + ". [" + listArray[i].getStatusIcon() + "] " + listArray[i].getTask());
+                    System.out.println("    "+(i + 1) + "." + tasks[i]);
                 }
+                System.out.println(" ");
+                System.out.println("    --------------------------------------------------------------------------------");
             }
             else {
-                listArray[n] = new Task(line);
+                tasks[n] = new Task(line);
                 n++;
-                System.out.println("added: " + line);
+                System.out.println("    --------------------------------------------------------------------------------");
+                System.out.println("    added: " + line);
+                System.out.println(" ");
+                System.out.println("    --------------------------------------------------------------------------------");
             }
         }
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("    --------------------------------------------------------------------------------");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println(" ");
+        System.out.println("    --------------------------------------------------------------------------------");
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,12 @@
 public class Task {
-    private String description;
-    private boolean isDone;
+    protected String description;
+    protected boolean isDone;
 
     public Task(String description){
         this.description = description;
         this.isDone = false;
     }
+
     public void markAsDone(){
         isDone = true;
     }
@@ -13,7 +14,17 @@ public class Task {
         return description;
     }
     public String getStatusIcon(){
-        return (isDone ? "\u2718":" ");
+        return (isDone ? "X":" ");
+    }
+
+    @Override
+    public String toString(){
+        if(isDone){
+            return "[X] " + description;
+        }
+        else{
+            return "[ ] " + description;
+        }
     }
 
 }
