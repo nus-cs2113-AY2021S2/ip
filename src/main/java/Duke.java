4ce471d1b13eb7d0b0f9e201eac01955ddef3fa6
CabbageTime <cabbagetime@outlook.com>
2021-02-02 10:41:09 +0800

    Add code for mark tasks as done, and change tasks to a custom class.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,37 +1,53 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Vector;
 
 public class Duke {
-    public static void printTasks(List<String> inputs){
+    public static void takeCommand(String command,List<Task> tasks){
+        String[] subStrs=command.split(" ");
+
+        switch(subStrs[0]){
+        case "list":
+            printTasks(tasks);
+            break;
+        case "bye":
+            System.out.println("Bye. Hope to see you again soon!");
+            break;
+        case "done":
+            int taskNo = Integer.parseInt(subStrs[1]);
+            Task temp = tasks.get(taskNo-1);
+            temp.markAsDone();
+            tasks.set(taskNo-1,temp);
+            break;
+        default:
+            Task task = new Task(command);
+            tasks.add(task);
+            System.out.println("added: " + command);
+            break;
+        }
+    }
+
+    public static void printTasks(List<Task> tasks){
         int i = 1;
-        for(String input:inputs){
-            System.out.println(i + ". " + input);
+        System.out.println("Here are the tasks in your list:");
+        for(Task task:tasks){
+            System.out.println(i + ".[" + task.getStatusIcon() +
+                    "] " + task.getDescription());
             ++i;
         }
     }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in).useDelimiter(" ");
         String command = "";
-        List<String> tasks = new Vector<String>();
+        List<Task> tasks = new ArrayList<Task>();
 
         System.out.println("Hello! I'm Duke\n" +
                 "What can I do for you?");
         do{
             command = in.nextLine();
-            switch(command){
-            case "list":
-                printTasks(tasks);
-                break;
-            case "bye":
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            default:
-                tasks.add(command);
-                System.out.println("added: " + command);
-                break;
-            }
+            takeCommand(command,tasks);
         }while(!command.equals("bye"));
     }
 }
