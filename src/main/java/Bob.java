fab8067e0a4795968953c66bf2b61eae96155301
Ivan <ivan.t.mailbox@gmail.com>
2021-02-03 22:46:44 +0800

    Refactored main class and added Command enum

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -9,75 +9,17 @@ public class Bob {
         goodbyeMessage();
     }
 
+    private static void welcomeMessage() {
+        String welcome = "____________________________________________________________\n" +
+                " Hello! I'm Bob 😀\n" +
+                " If you need anything hit me up fam 😌\n";
+        System.out.println(welcome);
+    }
+
+
     private static void scanInput() {
         Scanner in = new Scanner(System.in);
         scanLoop(in);
-    }
-
-    /**
-     * Scanner loop until bye command
-     */
-    private static void scanLoop(Scanner in) {
-        boolean isScanning = true;
-        while (isScanning) {
-            String line = in.nextLine();
-            isScanning = scanSwitch(line);
-        }
-    }
-
-    /**
-     * String before first space taken as command rest as args
-     */
-    private static boolean scanSwitch(String line) {
-        String[] lineArr = line.split(" ");
-        boolean isScanning = true;
-
-        switch (lineArr[0]) {
-        case "list":
-            taskList.printList();
-            break;
-        case "done":
-            taskList.completeTask(Integer.parseInt(lineArr[1]));
-            break;
-        case "todo":
-            addTodo(line);
-            break;
-        case "deadline":
-            addDeadline(line);
-            break;
-        case "event":
-            addEvent(line);
-            break;
-        case "bye":
-            isScanning = false;
-            break;
-        default: //regular task
-            taskList.addToList(new Task(line));
-        }
-        return isScanning;
-    }
-
-    private static void addTodo(String line) {
-        String label = line.substring("todo".length() + 1);
-        taskList.addToList(new Todo(label));
-    }
-
-    private static void addEvent(String line) {
-        final String startTimeMarker = "/at";
-        int divider = line.indexOf(startTimeMarker);
-        String dueDate = line.substring(divider + startTimeMarker.length() + 1);
-        String label = line.substring("event".length() + 1 ,divider);
-
-        taskList.addToList(new Event(label, dueDate));
-    }
-
-    private static void addDeadline(String line) {
-        final String dueTimeMarker = "/by";
-        int divider = line.indexOf(dueTimeMarker);
-        String dueDate = line.substring(divider + dueTimeMarker.length() + 1);
-        String label = line.substring("deadline".length() + 1 ,divider);
-
-        taskList.addToList(new Deadline(label, dueDate));
     }
 
     private static void goodbyeMessage() {
@@ -87,10 +29,129 @@ public class Bob {
         System.out.println(goodbye);
     }
 
-    private static void welcomeMessage() {
-        String welcome = "____________________________________________________________\n" +
-                " Hello! I'm Bob 😀\n" +
-                " If you need anything hit me up fam 😌\n";
-        System.out.println(welcome);
+    /**
+     * Scanner loop until bye command
+     */
+    private static void scanLoop(Scanner in) {
+        boolean isScanning = true;
+        while (isScanning) {
+            String inputString = in.nextLine();
+            isScanning = scanSwitch(inputString);
+        }
+    }
+
+    /**
+     * String before first space taken as command rest as args
+     */
+    private static boolean scanSwitch(String inputString) {
+        String[] inputStringArr = inputString.split(" ");
+        boolean isScanning = true;
+        Command commandType = getCommandType(inputStringArr[0]);
+
+        switch (commandType) {
+        case LIST:
+            printList();
+            break;
+        case DONE:
+            completeTask(inputStringArr[1]);
+            break;
+        case TODO:
+            addTodo(inputString);
+            break;
+        case DEADLINE:
+            addDeadline(inputString);
+            break;
+        case EVENT:
+            addEvent(inputString);
+            break;
+        case BYE:
+            isScanning = false;
+            break;
+        default: //regular task
+            addTask(inputString);
+        }
+        return isScanning;
+    }
+
+    private static void addTask(String inputString) {
+        taskList.addToList(new Task(inputString));
+    }
+
+    private static void completeTask(String s) {
+        taskList.completeTask(Integer.parseInt(s));
+    }
+
+    private static void printList() {
+        taskList.printList();
+    }
+
+    /**
+     * Takes a string and returns a enum Command
+     * Treats an unknown command as regular task
+     */
+    private static Command getCommandType(String commandString) {
+        Command command;
+        switch (commandString) {
+        case "list":
+            command = Command.LIST;
+            break;
+        case "done":
+            command = Command.DONE;
+            break;
+        case "todo":
+            command = Command.TODO;
+            break;
+        case "deadline":
+            command = Command.DEADLINE;
+            break;
+        case "event":
+            command = Command.EVENT;
+            break;
+        case "bye":
+            command = Command.BYE;
+            break;
+        default:
+            command =  Command.TASK;
+        }
+        return command;
+    }
+
+    private static void addTodo(String command) {
+        String label = command.substring("todo".length() + 1);
+        taskList.addToList(new Todo(label));
+    }
+
+    /**
+     * Phrases command into label and startTime parts
+     * Followed by creating event object
+     */
+    private static void addEvent(String command) {
+        final String startTimeMarker = "/at";
+        String commandType = Command.EVENT.name().toLowerCase();
+
+        String[] commandArray = command.split(startTimeMarker);
+        String label = getLabel(commandArray[0],commandType);
+        String startTime = commandArray[1].trim();
+
+        taskList.addToList(new Event(label, startTime));
+    }
+
+    /**
+     * Phrases command into label and dueTime parts
+     * Followed by creating Deadline object
+     */
+    private static void addDeadline(String command) {
+        final String dueTimeMarker = "/by";
+        String commandType = Command.DEADLINE.name().toLowerCase();
+
+        String[] commandArray = command.split(dueTimeMarker);
+        String label = getLabel(commandArray[0],commandType);
+        String dueTime = commandArray[1].trim();
+
+        taskList.addToList(new Deadline(label, dueTime));
+    }
+
+    private static String getLabel(String string, String commandType) {
+        return string.replaceFirst(commandType,"").trim();
     }
 }
