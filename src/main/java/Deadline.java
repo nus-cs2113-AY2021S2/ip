683ce8a64c99eb8b81e9b0b56dcd3db79d913c09
Vikasharlani <e0523376@u.nus.edu>
2021-02-03 18:48:15 +0800

    Add Level-4 Code, A-CodeQuality
    Add ToDo, Deadline and Event Classes that inherit from Task class
    Refactor code in Duke class:
    -Remove magic literals by adding constants
    -Extract methods to print welcome message, exit message, new task, completion of task, and list of tasks
    -Extract method (inputLoop) to replace while loop in main method

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,18 @@
+public class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : " ");
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,55 +1,107 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    private static final int TODO_DONE_LENGTH = 5;
+    private static final int DEADLINE_LENGTH = 9;
+    private static final int EVENT_LENGTH = 6;
+    private static final int BY_AT_LENGTH = 3;
+    private static Task[] tasksList = new Task[100];
+    private static int taskCounter = 0;
+
     public static void main(String[] args) {
+        showWelcomeMessage();
+        inputLoop();
+    }
+
+    private static void inputLoop() {
         Scanner in = new Scanner(System.in);
-        Task[] tasksList = new Task[100];
-        int counter = 0;
         String line;
+        while (true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                showExitMessage();
+                break;
+            } else if (line.equals("list")) {
+                printTasksList();
+                continue;
+            } else if (line.startsWith("done")) {
+                int index = Integer.parseInt(line.substring(TODO_DONE_LENGTH)) - 1;
+                tasksList[index].markAsDone();
+                printDoneTask(index);
+                continue;
+            } else if (line.startsWith("todo")) {
+                String description = line.substring(TODO_DONE_LENGTH);
+                Task newTask = new ToDo(description);
+                tasksList[taskCounter] = newTask;
+            } else if (line.startsWith("deadline")) {
+                int index = line.indexOf("/");
+                String description = line.substring(DEADLINE_LENGTH, index - 1);
+                String by = line.substring(index + BY_AT_LENGTH).trim();
+                Task newTask = new Deadline(description, by);
+                tasksList[taskCounter] = newTask;
+            } else if (line.startsWith("event")) {
+                int index = line.indexOf("/");
+                String description = line.substring(EVENT_LENGTH, index - 1);
+                String at = line.substring(index + BY_AT_LENGTH).trim();
+                Task newTask = new Event(description, at);
+                tasksList[taskCounter] = newTask;
+            }
+            taskCounter++;
+            printNewTask();
+        }
+    }
+
+    private static void printDoneTask(int index) {
+        printLine();
+        System.out.println("\tNice! I've marked this task as done:");
+        System.out.print("\t" + "  ");
+        System.out.println(tasksList[index]);
+        printLine();
+    }
+
+    private static void printTasksList() {
+        printLine();
+        System.out.println("\tHere are the tasks in your list:");
+        for (int i = 1; i <= taskCounter; i++) {
+            System.out.print("\t" + i + ". ");
+            System.out.println(tasksList[i-1]);
+        }
+        printLine();
+    }
+
+    private static void printNewTask() {
+        printLine();
+        System.out.println("\tGot it. I've added this task:");
+        System.out.println("\t  " + tasksList[taskCounter-1]);
+        if (taskCounter == 1) {
+            System.out.println("\tNow you have " + taskCounter + " task in the list.");
+        } else {
+            System.out.println("\tNow you have " + taskCounter + " tasks in the list.");
+        }
+        printLine();
+    }
+
+    private static void printLine() {
+        System.out.println("\t---------------------------------------------------------------------");
+    }
+
+    private static void showWelcomeMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("\t---------------------------------------------------------------------");
+        printLine();
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
-        System.out.println("\t---------------------------------------------------------------------");
-        while (true) {
-            line = in.nextLine();
-            line = line.trim();
-            String[] words = line.split(" ");
-            if (words[0].equals("done")) {
-                int index = Integer.parseInt(words[1]) - 1;
-                tasksList[index].markAsDone();
-                System.out.println("\t---------------------------------------------------------------------");
-                System.out.println("\tNice! I've marked this task as done:");
-                System.out.print("\t" + "  ");
-                System.out.println(tasksList[index]);
-                System.out.println("\t---------------------------------------------------------------------");
-            }
-            else if (line.equals("bye")) {
-                System.out.println("\t---------------------------------------------------------------------");
-                System.out.println("\tBye. Hope to see you again soon!");
-                System.out.println("\t---------------------------------------------------------------------");
-                break;
-            } else if (line.equals("list")) {
-                System.out.println("\t---------------------------------------------------------------------");
-                System.out.println("\tHere are the tasks in your list:");
-                for (int i = 1; i <= counter; i++) {
-                    System.out.print("\t" + i + ". ");
-                    System.out.println(tasksList[i-1]);
-                }
-                System.out.println("\t---------------------------------------------------------------------");
-            } else {
-                Task newTask = new Task(line);
-                tasksList[counter] = newTask;
-                counter++;
-                System.out.println("\t---------------------------------------------------------------------");
-                System.out.println("\tadded: " + line);
-                System.out.println("\t---------------------------------------------------------------------");
-            }
-        }
+        printLine();
+    }
+
+    private static void showExitMessage() {
+        printLine();
+        System.out.println("\tBye. Hope to see you again soon!");
+        printLine();
     }
 }
