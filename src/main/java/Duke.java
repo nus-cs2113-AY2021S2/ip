4dbae55fff60f957ed54b510ee458d181e2935f4
Emily <tockemily@yahoo.com.sg>
2021-02-03 14:27:21 +0800

    Level-0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,22 +9,23 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n" );
-        Scanner in = new Scanner(System.in);
-        String input;
-        String[] data = new String[100];
-        int a=0;
-        input = in.nextLine();
-        while(!input.contains("bye")){
-            data[a++] = input;
-            System.out.println("added: "+ input);
-            input = in.nextLine();
-            if(input.contains("list")){
-                for(int i=0; i<a; i++) {
-                    System.out.println(i + "." + " " + data[i]);
-                }
-            }
-        }
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+//        Scanner in = new Scanner(System.in);
+//        String input;
+//        String[] data = new String[100];
+//        int a = 0;
+//        input = in.nextLine();
+//        while (!input.contains("bye")) {
+//            data[a++] = input;
+//            System.out.println("added: " + input);
+//            input = in.nextLine();
+//            if (input.contains("list")) {
+//                for (int i = 0; i < a; i++) {
+//
+//                    System.out.println(i + "." + " " + data[i]);
+//                }
+//            }
+//        }
         System.out.println("Bye. Hope to see you again soon!");
 
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,14 @@
-public class Task {
-}
+//public class Task {
+//    protected String description;
+//    protected boolean isDone;
+//
+//    public Task(String description) {
+//        this.description = description;
+//        this.isDone = false;
+//    }
+//
+//    public String getStatusIcon() {
+//        return (isDone ? " " : "\u2718"); //return tick or X symbols
+//    }
+//
+//}
