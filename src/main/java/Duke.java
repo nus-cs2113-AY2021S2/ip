e1906dda959c9a260314520153d4f69b94e96e18
Emily <tockemily@yahoo.com.sg>
2021-02-03 23:54:37 +0800

    Update level 3 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,42 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
+
+
 public class Duke {
+    private static final String divider = "_______________________________\n";
+    private static final ArrayList<Task> tasks = new ArrayList<>();
+
+    private static void addTask(String taskName){
+        Task currentTask = new Task(taskName);
+        tasks.add(currentTask);
+        System.out.println(divider + "added " + currentTask.description + "\n" + divider);
+    }
+
+    private static void listTask(){
+        System.out.println(divider);
+        System.out.println("Here are the tasks in your list:");
+        for(int i=0; i<tasks.size(); ++i){
+            System.out.println((i+1) + "." +
+                    "[" + tasks.get(i).getStatusIcon()+"] "+
+                    tasks.get(i).description+ "\n");
+        }
+        System.out.println(divider);
+    }
+
+    private static void markTaskDone(int index){
+        Task currentTask = tasks.get(index-1);
+        currentTask.markAsDone();
+        System.out.println(divider + "Nice! I've marked this task as done:"+
+                "\n["+ currentTask.getStatusIcon()+ "]" + currentTask.description +
+                "\n" + divider);
+    }
+
+    private static void exitDuke(){
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,23 +46,24 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
-//        Scanner in = new Scanner(System.in);
-//        String input;
-//        String[] data = new String[100];
-//        int a = 0;
-//        input = in.nextLine();
-//        while (!input.contains("bye")) {
-//            data[a++] = input;
-//            System.out.println("added: " + input);
-//            input = in.nextLine();
-//            if (input.contains("list")) {
-//                for (int i = 0; i < a; i++) {
-//
-//                    System.out.println(i + "." + " " + data[i]);
-//                }
-//            }
-//        }
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner in = new Scanner(System.in);
+        String input;
+
+        while (in.hasNext()) {
+            input = in.nextLine();
+            if (input.equals("list")) {
+                listTask();
+            }else if(input.contains("done")){
+                int index = Integer.parseInt(input.substring(5));
+                markTaskDone(index);
+            }else if(input.equals("bye")){
+                exitDuke();
+                break;
+            }else{
+                addTask(input);
+            }
+        }
+        in.close();
 
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,20 @@
-//public class Task {
-//    protected String description;
-//    protected boolean isDone;
-//
-//    public Task(String description) {
-//        this.description = description;
-//        this.isDone = false;
-//    }
-//
-//    public String getStatusIcon() {
-//        return (isDone ? " " : "\u2718"); //return tick or X symbols
-//    }
-//
-//}
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2718" : " "); //if done return X
+    }
+    public void markAsDone(){
+        this.isDone = true;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+}
