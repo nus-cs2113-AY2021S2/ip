276eaa4b3ac3f1dfb8f967569f684aab311a71dc
rageqqq <chestersoh5@hotmail.com>
2021-02-03 21:44:09 +0800

    Code Quality
    -changed taskcounter to 0-based
    -renamed variables to make it easier to understand
    -created method for dashline printing and task addition to remove repeated code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,70 +1,89 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void printList(Task[] Tasks, Integer IDCounter){
-        for (int i=1; i<=IDCounter-1; ++i){
+    public static void printList(Task[] Tasks, Integer totalTasks){
+        for (Integer i=0; i<totalTasks; ++i){
             Task task = Tasks[i];
-            System.out.println(i + "." + task.toString());
+            Integer taskNumber = i+1;
+            System.out.println(taskNumber + "." + task.toString());
         }
     }
+    public static int printTaskAdded(Task[] Tasks, Integer totalTasks){
+        printDashLine();
+        System.out.println(" Got it. I've added this task:\n" + Tasks[totalTasks].toString());
+        totalTasks++;
+        System.out.println("Now you have " + totalTasks + " tasks in the list.");
+        printDashLine();
+        return totalTasks;
+    }
+    public static void printDashLine(){
+        System.out.println("____________________________________________________________");
+    }
     public static void main(String[] args) {
-        Integer IDCounter = 1;
-        Task[] Tasks =  new Task[101];
+        Integer totalTasks = 0;
+        Task[] Tasks =  new Task[100];
         String Line;
-        String DashLine = "____________________________________________________________\n";
-        String IntroMessage = " Hello! I'm Duke\n" + " What can I do for you?\n";
-        String OutroMessage = "Bye. Hope to see you again soon!\n";
+        String IntroMessage = " Hello! I'm Duke\n" + " What can I do for you?";
+        String OutroMessage = "Bye. Hope to see you again soon!";
         Scanner Input = new Scanner(System.in);
-        System.out.println(DashLine + IntroMessage + DashLine);
+        printDashLine();
+        System.out.println(IntroMessage);
+        printDashLine();
         Line = Input.nextLine();
-        while(!Line.equals("bye")){
-            if(Line.equals("list")){
-                System.out.println(DashLine + "Here are the tasks in your list:");
-                printList(Tasks, IDCounter);
-                System.out.println(DashLine);
+        boolean notBye = !Line.equals("bye");
+        while(notBye){
+            String[] words = Line.split(" ");
+            boolean isList = Line.equals("list");
+            boolean isDone = words[0].equals("done");
+            boolean isTodo = words[0].equals("todo");
+            boolean isDeadline = words[0].equals("deadline");
+            boolean isEvent = words[0].equals("event");
+            if(isList){
+                printDashLine();
+                System.out.println("Here are the tasks in your list:");
+                printList(Tasks, totalTasks);
+                printDashLine();
+            }
+            else if(isDone) {
+                int taskNumber = Integer.parseInt(words[1]) - 1;
+                Tasks[taskNumber].isDone = true;
+                printDashLine();
+                System.out.println("Nice! I've marked this task as done:\n" + " " + Tasks[taskNumber].getStatus() + " " + Tasks[taskNumber].getDescription());
+                printDashLine();
+            }
+            else if (isTodo){
+                Line = Line.replace("todo ", "");
+                ToDo toDo = new ToDo(Line);
+                Tasks[totalTasks] = toDo;
+                totalTasks = printTaskAdded(Tasks,totalTasks);
+            }
+            else if (isDeadline){
+                Line = Line.replace("deadline ", "");
+                words = Line.split("/by ");
+                Deadline deadline = new Deadline(words[0], words[1]);
+                Tasks[totalTasks] = deadline;
+                totalTasks = printTaskAdded(Tasks,totalTasks);
+            }
+            else if (isEvent){
+                Line = Line.replace("event ", "");
+                words = Line.split("/at ");
+                Event event = new Event(words[0], words[1]);
+                Tasks[totalTasks] = event;
+                totalTasks = printTaskAdded(Tasks,totalTasks);
             }
             else {
-                String[] words = Line.split(" ");
-                if (words[0].equals("done")) {
-                    int i = Integer.parseInt(words[1]);
-                    Tasks[i].isDone = true;
-                    System.out.println(DashLine + "Nice! I've marked this task as done:\n" + " " + Tasks[i].getStatus() + "\n" + " " + Tasks[i].getDescription() + "\n" + DashLine);
-                }
-                else if (words[0].equals("todo")){
-                    Line = Line.replace("todo ", "");
-                    ToDo toDo = new ToDo(Line);
-                    Tasks[IDCounter] = toDo;
-                    IDCounter++;
-                    String TaskCount = String.valueOf(IDCounter-1);
-                    System.out.println(DashLine + " Got it. I've added this task:\n" + toDo.toString() + "\n" + "Now you have " + TaskCount + " tasks in the list.\n" + DashLine);
-                }
-                else if (words[0].equals("deadline")){
-                    Line = Line.replace("deadline ", "");
-                    words = Line.split("/by ");
-                    Deadline deadline = new Deadline(words[0], words[1]);
-                    Tasks[IDCounter] = deadline;
-                    IDCounter++;
-                    String TaskCount = String.valueOf(IDCounter-1);
-                    System.out.println(DashLine + " Got it. I've added this task:\n" + deadline.toString() + "\n" + "Now you have " + TaskCount + " tasks in the list.\n" + DashLine);
-                }
-                else if (words[0].equals("event")){
-                    Line = Line.replace("event ", "");
-                    words = Line.split("/at ");
-                    Event event = new Event(words[0], words[1]);
-                    Tasks[IDCounter] = event;
-                    IDCounter++;
-                    String TaskCount = String.valueOf(IDCounter-1);
-                    System.out.println(DashLine + " Got it. I've added this task:\n" + event.toString() + "\n" + "Now you have " + TaskCount + "tasks in the list.\n" + DashLine);
-                }
-                else {
-                    Task t = new Task(Line);
-                    Tasks[IDCounter] = t;
-                    IDCounter++;
-                    System.out.println(DashLine + " added: " + Line + "\n" + DashLine);
-                }
+                Task t = new Task(Line);
+                Tasks[totalTasks] = t;
+                totalTasks++;
+                printDashLine();
+                System.out.println(" added: " + Line);
+                printDashLine();
             }
             Line = Input.nextLine();
+            notBye = !Line.equals("bye");
         }
-        System.out.println(DashLine + OutroMessage + DashLine);
+        printDashLine();
+        System.out.println(OutroMessage);
+        printDashLine();
     }
 }
