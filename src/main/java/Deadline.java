4ffd9453dd7197013b25dec059209a8e308b7def
angjiahui <angjiahui@users.noreply.github.com>
2021-02-04 00:55:44 +0800

    Add Event, Todo and Deadline class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,20 @@
+public class Deadline extends Task {
+
+    protected String dateTime;
+
+    public Deadline(String description, String dateTime) {
+        super(description);
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public String getTaskType() {
+        return "[D]";
+    }
+
+    @Override
+    public String getDateTime() {
+        return "(by: " + dateTime + ")";
+    }
+}
+
