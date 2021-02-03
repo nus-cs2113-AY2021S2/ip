0d1c2c60fa29cc3a7a19175a4dff2184e07101a5
zihan9485 <e0523361@u.nus.edu>
2021-02-04 00:48:28 +0800

    Add Level-4 Increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,11 +2,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Duke {
-    public static void lineBreak(){
-        final String HORIZONTAL_LINE = "____________________________________________________________";
-        System.out.println(HORIZONTAL_LINE);
-    }
-
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -20,11 +15,14 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         lineBreak();
+        //TODO: Refactor and Extract Method
+
         //Level 3: Mark as Done
         Scanner in = new Scanner(System.in);
         String userInput;
-        Task[] taskList = new Task[100];
+        Task[] taskList = new Task[100]; //TODO: Remove magic literals, refactor and extract constant
         int listCount = 0;
+        //TODO: Edit to coincide with SLAP abstraction
         while (true){
             userInput = in.nextLine();
             if (userInput.equals("list")){
@@ -32,7 +30,7 @@ public class Duke {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < listCount; i++){
                     Task currentTask = taskList[i];
-                    System.out.println(i+1 + ".[" + currentTask.getStatusIcon() + "] " + currentTask.description);
+                    System.out.println(i+1 + "." + currentTask.toString());
                 }
                 lineBreak();
                 continue;
@@ -44,21 +42,42 @@ public class Duke {
                 currentTask.markAsDone();
                 lineBreak();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  [" + currentTask.getStatusIcon() + "] " + currentTask.description);
+                System.out.println("  " + currentTask.toString());
                 lineBreak();
                 continue;
             }
-            if (userInput.equals("bye")){
+
+            if (userInput.equals("bye")) {
                 break;
             }
+
             lineBreak();
-            taskList[listCount++] = new Task(userInput);
-            System.out.println("added: " + userInput);
+            System.out.println("Got it. I've added this task:");
+
+            if (userInput.startsWith("todo")) {
+                taskList[listCount++] = new Todo(userInput.substring(5));
+            } else if (userInput.startsWith("deadline")) {
+                int dividerPosition = userInput.indexOf("/by");
+                String by = userInput.substring(dividerPosition + 4);
+                taskList[listCount++] = new Deadline(userInput.substring(9, dividerPosition - 1), by);
+            } else if (userInput.startsWith("event")) {
+                int dividerPosition = userInput.indexOf("/at");
+                String by = userInput.substring(dividerPosition + 4);
+                taskList[listCount++] = new Event(userInput.substring(6, dividerPosition - 1), by);
+            }
+
+            System.out.println("  " + taskList[listCount - 1].toString());
+            System.out.println("Now you have " + listCount + " tasks in the list.");
             lineBreak();
         }
 
         lineBreak();
         System.out.println("Bye. Hope to see you again soon!");
         lineBreak();
+    }
+
+    public static void lineBreak() {
+        final String HORIZONTAL_LINE = "____________________________________________________________";
+        System.out.println(HORIZONTAL_LINE);
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,16 +2,21 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
-    public Task(String description){
+    public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
-    public String getStatusIcon(){
+    public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718");
     }
 
-    public void markAsDone(){
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + description;
+    }
+
+    public void markAsDone() {
         isDone = true;
     }
 }
