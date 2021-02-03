ab3f87e85e76c805df5dd965bf278ff1b5ea9a62
brynagoh <brynagoh@gmail.com>
2021-02-03 20:07:14 +0800

    Refactor code
    moves logic out of main method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,44 @@ import java.util.Scanner;
 
 public class Duke {
 
+    private static Task[] tasks = new Task[100];
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void listTasks() {
+        if (Task.totalTasks == 0) {
+            System.out.println("No tasks yet!");
+        } else {
+            for (int i = 0; i < Task.totalTasks; i++) {
+                System.out.printf("%d.[%s] %s\n", i+1, tasks[i].getStatusIcon(), tasks[i].getDescription());
+            }
+        }
+    }
+
+    public static void markTasksAsDone(String input) {
+        String[] inputArray = input.split(" ");
+
+        //completedIndex holds the index of valid integer(s) in inputArray (indicating index in tasklist)
+        int completedIndex;
+        for (String word: inputArray) {
+            if (word.equals("done")) {
+                continue;
+            } else {
+                completedIndex = Integer.parseInt(word);
+                //ensure that the index given is valid
+                if (completedIndex > 0 && completedIndex <= Task.totalTasks){
+                    tasks[completedIndex - 1].markAsDone();
+                } else {
+                    System.out.printf("Task %d does not exist! Enter 'list' to view tasklist :)\n", completedIndex);
+                }
+            }
+        }
+    }
+
+    public static void addTask(String input) {
+        tasks[Task.totalTasks] = new Task(input);
+        System.out.println("added: "+ input);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -12,46 +50,18 @@ public class Duke {
 
         System.out.println("What can I do for you today?");
 
-        Scanner sc = new Scanner(System.in);
-
-        Task[] tasks = new Task[100];
-
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                if (Task.totalTasks == 0) {
-                    System.out.println("No tasks yet!");
-                } else {
-                    for (int i = 0; i < Task.totalTasks; i++) {
-                        System.out.printf("%d.[%s] %s\n", i+1, tasks[i].getStatusIcon(), tasks[i].getDescription());
-                    }
-                }
-
+                listTasks();
             } else if (input.contains("done")) {
-                String[] inputArray = input.split(" ");
-                for (String word: inputArray) {
-                    if (word.equals("done")) {
-                        continue;
-                    } else {
-                        int completedIndex = Integer.parseInt(word);
-                        //ensure that the index given is valid
-                        if (completedIndex > 0 && completedIndex <= Task.totalTasks){
-                            tasks[completedIndex - 1].markAsDone();
-                        } else {
-                            System.out.printf("Task %d does not exist! Enter 'list' to view tasklist :)\n", completedIndex);
-                        }
-
-                    }
-                }
-
+                markTasksAsDone(input);
             } else {
-                tasks[Task.totalTasks] = new Task(input);
-                System.out.println("added: "+ input);
+                addTask(input);
             }
             input = sc.nextLine();
         }
 
         System.out.println("Goodbye！See you again soon :)");
-
     }
 }
