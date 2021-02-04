1d3fe4fd6b54eac2a8f0f2e31e7dff125c404592
JonathanKhooTY <jonathan.khooty@gmail.com>
2021-02-04 08:57:30 +0800

    Modify runtest files

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -12,4 +12,11 @@ public class Task {
         return(isDone?"\u2713":"\u2718");
     }
 
+
+    /*
+    public String toString(){
+        return "added: " + ".["+ getStatusIcon() + "] " + description;
+    }
+
+     */
 }
