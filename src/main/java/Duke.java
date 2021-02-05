89fbd8526560ab1b2de2c4b4433b2606de11d056
cloudy3 <cjingfeng98@gmail.com>
2021-02-05 12:16:56 +0800

    Fixed Minor Bugs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,20 +3,15 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printLogo();
 
         Scanner sc = new Scanner(System.in);
         ArrayList<Task> tasks = new ArrayList<Task>();
 
-        System.out.println("~____________________________________________________________~");
+        printDivider();
         System.out.println("What's up! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("~____________________________________________________________~\n");
+        printEndDivider();
 
         boolean isRunning = true;
         while (isRunning) {
@@ -27,42 +22,42 @@ public class Duke {
                 isRunning = false;
             } else if (phrase.equals("list")) {
                 // List all tasks
-                System.out.println("~____________________________________________________________~");
+                printDivider();
                 System.out.println("Here are the tasks in your list:");
                 int i = 0;
                 for (Task task : tasks) {
                     System.out.println(++i + ". " + task.toString());
                 };
-                System.out.println("~____________________________________________________________~\n");
+                printEndDivider();
             } else if (phrase.startsWith("done ")) {
                 // Set a task as done
                 int taskIndex = phrase.charAt(phrase.length()-1) - '0';
 
-                if (taskIndex < tasks.size() || taskIndex > tasks.size()) {
-                    System.out.println("~____________________________________________________________~");
+                if (taskIndex > tasks.size()) {
+                    printDivider();
                     System.out.println("Oops task " + taskIndex + " does not exist! Try again mate!");
-                    System.out.println("~____________________________________________________________~");
+                    printEndDivider();
                 } else {
-                    System.out.println("~____________________________________________________________~");
+                    printDivider();
                     System.out.println("Nice! I've marked this task as done:");
                     Task task = tasks.get(taskIndex - 1);
                     task.markAsDone();
                     System.out.println("[" + task.getStatusIcon() + "] " + task.getDescription());
-                    System.out.println("~____________________________________________________________~\n");
+                    printEndDivider();
                 }
             } else if (phrase.startsWith("todo ")) {
                 // Add a To-Do
-                System.out.println("~____________________________________________________________~");
+                printDivider();
                 phrase = phrase.substring(5);
                 Todo todo = new Todo(phrase);
                 System.out.println("Got it! I've added this task:");
                 tasks.add(todo);
                 System.out.println(todo);
                 System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-                System.out.println("~____________________________________________________________~\n");
+                printEndDivider();
             } else if (phrase.startsWith("deadline ")) {
                 // Add a Deadline
-                System.out.println("~____________________________________________________________~");
+                printDivider();
                 int dividerPosition = phrase.indexOf("/");
                 String by = phrase.substring(dividerPosition+4);
                 phrase = phrase.substring(9, dividerPosition);
@@ -71,10 +66,10 @@ public class Duke {
                 tasks.add(deadline);
                 System.out.println(deadline);
                 System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-                System.out.println("~____________________________________________________________~\n");
+                printEndDivider();
             } else if (phrase.startsWith("event ")) {
                 // Add an Event
-                System.out.println("~____________________________________________________________~");
+                printDivider();
                 int dividerPosition = phrase.indexOf("/");
                 String eventTime = phrase.substring(dividerPosition+4);
                 phrase = phrase.substring(6, dividerPosition);
@@ -83,17 +78,35 @@ public class Duke {
                 tasks.add(event);
                 System.out.println(event);
                 System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-                System.out.println("~____________________________________________________________~\n");
+                printEndDivider();
             } else {
                 // Invalid Task
-                System.out.println("~____________________________________________________________~");
+                printDivider();
                 System.out.println("Sorry mate I do not understand your request. Please specify task :)");
-                System.out.println("~____________________________________________________________~");
+                printEndDivider();
             }
         }
         // Exits Program
-        System.out.println("\n~____________________________________________________________~");
+        printEndDivider();
         System.out.println("Alright cheers mate!");
+        printDivider();
+    }
+
+    private static void printLogo() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+
+    private static void printDivider() {
         System.out.println("~____________________________________________________________~");
     }
+
+    private static void printEndDivider() {
+        System.out.println("~____________________________________________________________~\n");
+    }
+
 }
