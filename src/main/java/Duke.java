564563fc09936171e406cf6ab12939bc8bcffdac
iamakilahamed <akilahamed123@gmail.com>
2021-02-06 20:58:58 +0800

    Handle exceptions
    Handle some exceptions such as invalid command, invalid task and empty list.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,13 @@ public class Duke {
     public static void main(String[] args) throws InterruptedException {
         Jarvis jarvis = new Jarvis();
         jarvis.startJarvis();
-        jarvis.performTask();
+        while (true) {
+            try {
+                jarvis.performTask();
+            } catch (InvalidCommandException exception) {
+                System.out.println("\tSorry, sir. I do not recognise this command.");
+                jarvis.printDivider();
+            }
+        }
     }
 }
--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 
 public class Jarvis {
     private final Scanner in = new Scanner(System.in);    // create Scanner object
-    private final String DIVIDER = "------------------------------------------------";
     ArrayList<Task> tasks = new ArrayList<>();   // create ArrayList to store all the tasks
 
     public Jarvis() {}
 
+    public void printDivider() {
+        String DIVIDER = "------------------------------------------------";
+        System.out.println(DIVIDER);
+    }
+
     // initialisation of JARVIS (just for fun)
     public void startJarvis() throws InterruptedException {
-        System.out.println(DIVIDER);
+        printDivider();
         System.out.println("Importing all preferences from home interface.");
         int DELAY = 500;    // 0.5 seconds delay
         TimeUnit.MILLISECONDS.sleep(DELAY);
@@ -24,9 +28,9 @@ public class Jarvis {
             TimeUnit.MILLISECONDS.sleep(DELAY);
         }
         System.out.println();
-        System.out.println(DIVIDER);
+        printDivider();
         System.out.println("\tHello, sir. J.A.R.V.I.S at your service.");
-        System.out.println(DIVIDER);
+        printDivider();
     }
 
     // print these if task is successfully added in the ArrayList
@@ -34,13 +38,14 @@ public class Jarvis {
         System.out.println("\tGot it. I've added this task:");
         System.out.println("\t\t" + task.toString());
         System.out.println("\tNow you have " + tasks.size() + " tasks in the list.");
-        System.out.println(DIVIDER);
+        printDivider();
     }
 
     // JARVIS stops running
     public void exitJarvis() {
         System.out.println("\tGoodbye, sir.");
-        System.out.println(DIVIDER);
+        printDivider();
+        System.exit(1);
     }
 
     // adds Todo task to the ArrayList
@@ -74,21 +79,21 @@ public class Jarvis {
     }
 
     // prints out the entire list of tasks
-    public void runList() {
+    public void runList() throws EmptyListException {
         if (tasks.size() != 0) {
             System.out.println("\tHere are the tasks in your list, sir:");
             for (int i = 0; i < tasks.size(); i++) {
                 Task task = tasks.get(i);
                 System.out.println(String.format("\t\t%d. ", i + 1) + task.toString());
             }
-        } else {    // if there is no task in the ArrayList
-            System.out.println("\tYou do not have any pending task, sir.");
+            printDivider();
+        } else {    // if there is no task in the ArrayList, throw EmptyListException
+            throw new EmptyListException();
         }
-        System.out.println(DIVIDER);
     }
 
     // marks the task as done if the task exist
-    public void runDone(String command) {
+    public void runDone(String command) throws InvalidTaskException {
         String description = command.replaceFirst("done ", "");
         int taskNumber = Integer.parseInt(description.substring(0, 1));
         if (taskNumber <= tasks.size()) {
@@ -96,40 +101,41 @@ public class Jarvis {
             task.setTaskStatus(true);
             System.out.println("\tWell done, sir! I've marked this task as done:");
             System.out.println("\t\t" + task.toString());
-        } else {    // if the task is not in the ArrayList
-            System.out.println("\tSorry, sir.");
-            System.out.println("\tI am unable to retrieve the information from the database.");
-            System.out.println("\tWould you like to add the task instead, sir?");
+            printDivider();
+        } else {    // if the task is not in the ArrayList, throw InvalidTaskException
+            throw new InvalidTaskException();
         }
-        System.out.println(DIVIDER);
-    }
-
-    // Echo back to user
-    public void runEcho(String command) {
-        System.out.println("\t" + command);
-        System.out.println(DIVIDER);
     }
 
     // JARVIS main task manager
-    public void performTask() {
-        while (true) {
-            String command = in.nextLine();
-            if (command.startsWith("bye")) {    // bye
-                exitJarvis();
-                return;
-            } else if (command.startsWith("todo")) {     // todo
-                runTodo(command);
-            } else if (command.startsWith("deadline")) {    // deadline
-                runDeadline(command);
-            } else if (command.startsWith("event")) {    // event
-                runEvent(command);
-            } else if (command.startsWith("list")) {    // list
+    public void performTask() throws InvalidCommandException {
+        String command = in.nextLine();
+        if (command.startsWith("bye")) {    // bye
+            exitJarvis();
+        } else if (command.startsWith("todo")) {     // todo
+            runTodo(command);
+        } else if (command.startsWith("deadline")) {    // deadline
+            runDeadline(command);
+        } else if (command.startsWith("event")) {    // event
+            runEvent(command);
+        } else if (command.startsWith("list")) {    // list
+            try {
                 runList();
-            } else if (command.startsWith("done")) {    // done
-                runDone(command);
-            } else {    // echo
-                runEcho(command);
+            } catch (EmptyListException exception) {
+                System.out.println("\tYou do not have any pending task, sir.");
+                printDivider();
             }
+        } else if (command.startsWith("done")) {    // done
+            try {
+                runDone(command);
+            } catch (InvalidTaskException exception) {
+                System.out.println("\tSorry, sir.");
+                System.out.println("\tI am unable to retrieve the information from the database.");
+                System.out.println("\tWould you like to add the task instead?");
+                printDivider();
+            }
+        } else {    // if invalid command, throw InvalidCommandException
+            throw new InvalidCommandException();
         }
     }
 }
