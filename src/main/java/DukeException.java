aa61ee06962b86dc742716cb2d67415c0d7caeec
JethroPhuah <jethrophuah@gmail.com>
2021-02-06 13:56:05 +0800

    update

--- src/main/java/DukeException.java
+++
@@ -1,9 +0,0 @@
-public class DukeException extends Exception {
-    public DukeException(String message) {
-        super("OOPS!!! The description of a/an " + message + " cannot be empty.");
-    }
-
-    public DukeException() {
-        super();
-    }
-}
