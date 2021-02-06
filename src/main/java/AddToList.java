f5a00f8b435050c5947329edef19652cbd1c515a
baggiiiie <apple@Baggie.local>
2021-02-06 14:24:27 +0800

    Modify Level 5

--- src/main/java/AddToList.java
+++ src/main/java/AddToList.java
@@ -6,41 +6,35 @@ public class AddToList extends Duke {
             lists[listIndex][2] = task;
             lists[listIndex][3] = " ";
             TaskAddedText();
-        } else {
-            if (task.contains("/")) {
-                String[] TaskAndTime= task.split("/", 2);
-                if (taskType.equalsIgnoreCase("deadline")) {
-                    lists[listIndex][0] = "D";
-                    lists[listIndex][1] = " ";
-                    lists[listIndex][2] = TaskAndTime[0].trim();
-                    lists[listIndex][3] = " (by:" + TaskAndTime[1].trim() + ")";
-                    TaskAddedText();
-                } else if (taskType.equalsIgnoreCase("event")) {
-                    lists[listIndex][0] = "E";
-                    lists[listIndex][1] = " ";
-                    lists[listIndex][2] = TaskAndTime[0].trim();
-                    lists[listIndex][3] = " (at:" + TaskAndTime[1].trim() + ")";
-                    TaskAddedText();
-                }
-            } else {
-                DukeException.taskWithoutTime();
+        } else if (task.contains("/")) {
+            String[] TaskAndTime = task.split("/", 2);
+            if (taskType.equalsIgnoreCase("deadline")) {
+                lists[listIndex][0] = "D";
+                lists[listIndex][1] = " ";
+                lists[listIndex][2] = TaskAndTime[0].trim();
+                lists[listIndex][3] = " (by:" + TaskAndTime[1].trim() + ")";
+                TaskAddedText();
+            } else if (taskType.equalsIgnoreCase("event")) {
+                lists[listIndex][0] = "E";
+                lists[listIndex][1] = " ";
+                lists[listIndex][2] = TaskAndTime[0].trim();
+                lists[listIndex][3] = " (at:" + TaskAndTime[1].trim() + ")";
+                TaskAddedText();
             }
+        } else {
+            DukeException.taskWithoutTime();
         }
     }
 
     public static void TaskAddedText() {
-    System.out.println(" Task added! ^_^");
+        System.out.println(" Task added! ^_^");
         PrintList(listIndex, listIndex + 1);
-        listIndex += 1;
+        ++listIndex;
     }
 
-    public static boolean keywordCheck(String keyword){
-        if (keyword.equalsIgnoreCase("deadline")||
-            keyword.equalsIgnoreCase("event") ||
-            keyword.equalsIgnoreCase(("todo"))){
-            return true;
-        } else {
-            return false;
-        }
+    public static boolean keywordCheck(String keyword) {
+        return keyword.equalsIgnoreCase("deadline") ||
+                keyword.equalsIgnoreCase("event") ||
+                keyword.equalsIgnoreCase("todo");
     }
 }
