c8824e67481a8daa37eb840303a00e234735356e
lamzf1998 <73275294+lamzf1998@users.noreply.github.com>
2021-02-06 22:03:15 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,13 +14,16 @@ public class Duke {
     /*Methods to process the input of the user.
     If input is not valid, returns string nonsense.
     The type of task is indicated by the first substring and the task description is found after the first blank space.
-    If a forward slash present,  ths substring is split into the task and the end date which is indicated by the slash.*/
+    If a forward slash present,  ths substring is split into the task and the end date which is indicated by the slash.
+    Exception handling includes cases where no tasks are specified, invalid inputs, no date or time being specified (for event and deadline)*/
     public static String extractTask(String input) {
         String Task;
         if (input.equalsIgnoreCase("list")) {
             return "list";
         } else if (input.equalsIgnoreCase("bye")) {
             return "bye";
+        } else if (input.contains("done")) {
+            return "done";
         } else if (input.contains("todo") || input.contains("deadline") || input.contains("event")) {
             int indexOfSpace = input.indexOf(" ");
             if (indexOfSpace == -1) {
@@ -46,8 +49,10 @@ public class Duke {
             return "list";
         } else if (input.equalsIgnoreCase("bye")) {
             return "bye";
+        } else if (input.contains("done")) {
+            return "done";
         } else if (input.contains("todo")) {
-            return null;
+            return "todo";
         } else if (input.contains("deadline") || input.contains("event")) {
             int indexOfSpace = input.indexOf(" ");
             String subString = input.substring(indexOfSpace + 1);
@@ -55,12 +60,18 @@ public class Duke {
                 int indexOfSlash = subString.indexOf("/");
                 String subStringDate = subString.substring(indexOfSlash);
                 int indexNext = subStringDate.indexOf(" ");
-                Date = subStringDate.substring(indexNext + 1);
+                if (indexNext == -1) {
+                    return "missing";
+                } else {
+                    Date = subStringDate.substring(indexNext + 1);
+                    return Date;
+                }
             } else {
-                Date = null;
-                    }
+                return "missing";
             }
-        return Date;
+        } else {
+            return "nonsense";
+        }
     }
 
     public static void main(String[] args) {
@@ -78,6 +89,7 @@ public class Duke {
              /* If input is "bye", system exits with message.
              If input is "list", list of tasks will be displayed.
              If input is "done", the task number to be marked as done.
+             Exception handling for "done" includes not indicating task number and input task number out of range.
              If input is "todo", classify task as ToDo.
              If input is "Deadline", classify task as Deadline.
              If input is "Event", classify task as Event.
@@ -85,6 +97,11 @@ public class Duke {
 
             if (stringTask.contains("retry")) {
                 System.out.println("Oi allow it fam! Why you got no tasks? Are you dumb? Try again... you melon!");
+                continue;
+            }
+
+            if (stringDate.contains("missing")) {
+                System.out.println("Oi you forgot your date yea? Type in your time period boi!");
                 continue;
             }
 
@@ -101,11 +118,21 @@ public class Duke {
                 }
             } else if (input.contains("done")) {
                 int indexSpace = input.indexOf(" ");
-                String numberString = input.substring(indexSpace + 1);
-                int taskNumber = Integer.parseInt(numberString);
-                taskList[taskNumber - 1].markAsDone();
-                System.out.println("Awwww yeah! I've marked this task as done... brrrrrap brrrrrap: ");
-                System.out.println(taskList[taskNumber - 1].printDescription());
+                if (indexSpace == -1) {
+                    System.out.println("I see you forget how to type numbers yea... you donut! Type in your task number man!");
+                    continue;
+                } else {
+                    String numberString = input.substring(indexSpace + 1);
+                    int taskNumber = Integer.parseInt(numberString);
+                    if (taskList[taskNumber - 1] == null) {
+                        System.out.println("Are you blind? There is no such task number you Tori!");
+                        continue;
+                    } else {
+                        taskList[taskNumber - 1].markAsDone();
+                        System.out.println("Awwww yeah! I've marked this task as done... brrrrrap brrrrrap: ");
+                        System.out.println(taskList[taskNumber - 1].printDescription());
+                    }
+                }
             } else if (input.contains("todo")) {
                 taskList[count] = new toDo(stringTask);
                 System.out.println("Ayy I got you my brother. I've added this ting: ");
