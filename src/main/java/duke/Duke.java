b16671441e10fa73a0057856680aaf0656195ee5
Howard Liu <howard@ixnet.work>
2021-02-07 22:33:03 +0800

    Improved invalid input handling by using custom Exception

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Vector;
 
+import duke.exception.InvalidInputException;
+import duke.exception.InvalidInputException.InputExceptionType;
 import duke.task.Deadline;
 import duke.task.Event;
 import duke.task.Task;
@@ -24,32 +26,41 @@ public class Duke {
 
         while (!isExit) {
             String line = in.nextLine();
-            String[] arguments = line.split(" ");
+            // Split the line by any whitespaces characters (including spaces, tabs etc.)
+            String[] arguments = line.split("\\s+");
+            // If first argument (command) is empty, there are empty spaces typed in at the front - so we remove it
+            if (arguments[0].isEmpty()) {
+                arguments = Arrays.copyOfRange(arguments, 1, arguments.length);
+            }
 
             printlnWithIndent(LONG_LINE);
 
-            switch(arguments[0]) {
-            case "bye":
-                byeHandler();
-                isExit = true;
-                break;
-            case "list":
-                listHandler(tasks);
-                break;
-            case "done":
-                doneHandler(tasks, arguments);
-                break;
-            case "deadline":
-                deadlineHandler(tasks, arguments);
-                break;
-            case "event":
-                eventHandler(tasks, arguments);
-                break;
-            case "todo":
-                todoHandler(tasks, arguments);
-                break;
-            default:
-                invalidCommandHandler(tasks, line);
+            try {
+                switch(arguments[0]) {
+                case "bye":
+                    byeHandler();
+                    isExit = true;
+                    break;
+                case "list":
+                    listHandler(tasks);
+                    break;
+                case "done":
+                    doneHandler(tasks, arguments);
+                    break;
+                case "deadline":
+                    deadlineHandler(tasks, arguments);
+                    break;
+                case "event":
+                    eventHandler(tasks, arguments);
+                    break;
+                case "todo":
+                    todoHandler(tasks, arguments);
+                    break;
+                default:
+                    throw new InvalidInputException(InputExceptionType.UNKNOWN_COMMAND);
+                }
+            } catch (Exception e) {
+                printlnWithIndent("Oops! " + e.getMessage());
             }
 
             printlnWithIndent(LONG_LINE);
@@ -81,7 +92,7 @@ public class Duke {
     }
 
     // Mark a task to be done with index specified in arguments[1]
-    protected static void doneHandler(Vector<Task> tasks, String[] arguments) {
+    protected static void doneHandler(Vector<Task> tasks, String[] arguments) throws InvalidInputException {
         if (arguments.length < 2) {
             // An index must be provided for the task to be marked "done"
             printlnWithIndent("You will need to give me an index, like this: `done 2`.");
@@ -90,7 +101,7 @@ public class Duke {
                 int index = Integer.parseInt(arguments[1]);
                 if (index > tasks.size() || index < 1) {
                     // This index is out of the boundary of our database
-                    throw new IllegalArgumentException();
+                    throw new InvalidInputException(InputExceptionType.INDEX_OUT_OF_BOUND);
                 }
 
                 Task task = tasks.get(index - 1);
@@ -100,49 +111,44 @@ public class Duke {
                 printlnWithIndent("Nice! I've marked this task as done:");
                 printlnWithIndent("\t" + task);
             } catch (NumberFormatException e) {
-                printlnWithIndent("Index provided is not a proper number.");
-            } catch (IllegalArgumentException e) {
-                printlnWithIndent("Task with this index is not found in our database.");
+                throw new InvalidInputException(InputExceptionType.NOT_INTEGER, e);
             }
         }
     }
 
     // Create a deadline task
-    protected static void deadlineHandler(Vector<Task> tasks, String[] arguments) {
+    protected static void deadlineHandler(Vector<Task> tasks, String[] arguments) throws InvalidInputException {
         int i = findIndex(arguments, "/by");
-        if (i != -1) {
+        if (i != -1 && i + 1 != arguments.length) {
             String description = String.join(" ", Arrays.copyOfRange(arguments, 1, i));
             String by = String.join(" ", Arrays.copyOfRange(arguments, i + 1, arguments.length));
             tasks.add(new Deadline(description, by));
             printNewTask(tasks);
         } else {
-            printlnWithIndent("You must specify a deadline after /by");
+            // Either /by is not found at all, or no dates are following /by
+            throw new InvalidInputException(InputExceptionType.NO_BY_DATE);
         }
     }
 
     // Create an event task
-    protected static void eventHandler(Vector<Task> tasks, String[] arguments) {
+    protected static void eventHandler(Vector<Task> tasks, String[] arguments) throws InvalidInputException {
         int i = findIndex(arguments, "/at");
-        if (i != -1) {
+        if (i != -1 && i + 1 != arguments.length) {
             String description = String.join(" ", Arrays.copyOfRange(arguments, 1, i));
             String at = String.join(" ", Arrays.copyOfRange(arguments, i + 1, arguments.length));
             tasks.add(new Event(description, at));
             printNewTask(tasks);
         } else {
-            printlnWithIndent("You must specify a event time after /at");
+            // Either /at is not found at all, or no dates are following /at
+            throw new InvalidInputException(InputExceptionType.NO_AT_DATE);
         }
     }
 
     // Create a todo task
-    protected static void todoHandler(Vector<Task> tasks, String[] arguments) {
+    protected static void todoHandler(Vector<Task> tasks, String[] arguments) throws InvalidInputException {
         String description = String.join(" ", Arrays.copyOfRange(arguments, 1, arguments.length));
         tasks.add(new ToDo(description));
         printNewTask(tasks);
-    }
-
-    // Save a new task with no specific type in the task list
-    protected static void invalidCommandHandler(Vector<Task> tasks, String description) {
-        printlnWithIndent("I cannot recognize what your command means.");
     }
 
     /*
--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -1,9 +1,11 @@
 package duke.task;
 
+import duke.exception.InvalidInputException;
+
 public class Deadline extends Task {
     protected String by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, String by) throws InvalidInputException {
         super(description);
         this.by = by;
         typeIcon = "D";
