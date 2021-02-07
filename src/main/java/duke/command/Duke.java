403fbdce64dcae3e5cccdcac72f3d587a5f3a42b
kangxinwang <juliawangkangxin@gmail.com>
2021-02-07 18:51:13 +0800

    divide classes into packages

--- src/main/java/duke/command/Duke.java
+++ src/main/java/duke/command/Duke.java
@@ -1,4 +1,10 @@
-import java.util.Arrays;
+package duke.command;
+
+import duke.task.Deadline;
+import duke.task.Event;
+import duke.task.Task;
+import duke.task.Todo;
+
 import java.util.Scanner;
 public class Duke {
     private static final int TASK_MAX_SIZE = 100;
