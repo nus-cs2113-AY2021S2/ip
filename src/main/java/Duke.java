a24af8b3c188d50c79b8893ba6c49d895fec0713
s-t-e-f <65395025+s-t-e-f@users.noreply.github.com>
2021-02-08 22:37:46 +0800

    Level-5: Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,59 +1,102 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 public class Duke {
 
     static ArrayList<Task> tasks = new ArrayList<> ();
     static int tasksCount = 0 ;
+    static final int TODOLENGTH = 4;
+    static final int EVENTLENGTH = 5;
+    static final int DEADLINELENGTH = 8;
+    enum TaskType {
+        TODO,
+        EVENT,
+        DEADLINE
+    }
 
-    public static void printTaskList(ArrayList<Task> tasks) {
+    public static void printTaskList() {
         System.out.println("Here are the tasks in your list: ");
-        for (int i=0; i<tasks.size(); i++){
+        for (int i=0; i<tasks.size(); i++) {
             System.out.println("    " + (i+1) + ". " + tasks.get(i));
         }
     }
 
+    public static void printErrorMsgInvalidInput() {
+        System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+    }
+
+    public static void addDeadline(String userInput) throws DukeException {
+        if (userInput.length() == DEADLINELENGTH ) {
+            throw new DukeException(TaskType.DEADLINE);
+        }
+        String description, by;
+        userInput = userInput.substring(DEADLINELENGTH+1).trim();
+        if (userInput.contains("/")){
+            int idx = userInput.indexOf('/');
+            description =userInput.substring(0, idx);
+            by = userInput.substring(idx+3).trim();
+        }
+        else {
+            description =userInput;
+            by = "";
+        }
+
+        Deadline d = new Deadline(description, by);
+        tasks.add(d);
+    }
+
+    public static void addEvent(String userInput) throws DukeException {
+        if (userInput.length() == EVENTLENGTH) {
+            throw new DukeException(TaskType.EVENT);
+        }
+        String description, at;
+        userInput = userInput.substring(EVENTLENGTH+1).trim();
+        if (userInput.contains("/")){
+            int idx = userInput.indexOf('/');
+            description =userInput.substring(0, idx);
+            at = userInput.substring(idx+3).trim();
+        }
+        else {
+            description =userInput;
+            at = "";
+        }
+
+        Event e = new Event(description, at);
+        tasks.add(e);
+    }
+
+    public static void addTodo(String userInput) throws DukeException {
+        if (userInput.length() == TODOLENGTH) {
+            throw new DukeException(TaskType.TODO);
+        }
+        String description = userInput.substring(TODOLENGTH+1);
+        Todo t = new Todo(description);
+        tasks.add(t);
+    }
+
+
     public static void addTask(String userInput) {
-        String description = "";
-        if (userInput.substring(0,8).equalsIgnoreCase("deadline")){
-            String by = "";
-            userInput = userInput.substring(9).trim();
-            if (userInput.contains("/")){
-                int idx = userInput.indexOf('/');
-                description =userInput.substring(0, idx);
-                by = userInput.substring(idx+3).trim();
+        try {
+            if (userInput.startsWith("deadline")){
+                addDeadline(userInput);
+            }
+            else if (userInput.startsWith("event")){
+                addEvent(userInput);
+            }
+            else if (userInput.startsWith("todo")){
+                addTodo(userInput);
             }
             else {
-                description =userInput;
+                printErrorMsgInvalidInput();
+                return;
             }
-
-            Deadline d = new Deadline(description, by);
-            tasks.add(d);
+            System.out.println("    Got it. I've added this task: \n      " + tasks.get(tasksCount));
+            tasksCount++;
+            System.out.println("    Now you have " + tasksCount + " tasks in the list.");
+        } catch (DukeException e) {
+            e.printErrorTaskCannotBeEmpty();
         }
-        else if (userInput.substring(0,5).equalsIgnoreCase("event")){
-            String at = "";
-            userInput = userInput.substring(6).trim();
-            if (userInput.contains("/")){
-                int idx = userInput.indexOf('/');
-                description =userInput.substring(0, idx);
-                at = userInput.substring(idx+3).trim();
-            }
-            else {
-                description =userInput;
-            }
-
-            Event e = new Event(description, at);
-            tasks.add(e);
-        }
-        else if (userInput.substring(0,4).equalsIgnoreCase("todo")){
-            description = userInput.substring(5);
-            Todo t = new Todo(description);
-            tasks.add(t);
-        }
-        System.out.println("    Got it. I've added this task: \n      " + tasks.get(tasksCount));
-        tasksCount++;
-        System.out.println("    Now you have " + tasksCount + " tasks in the list.");
-
     }
 
     public static void main(String[] args) {
@@ -77,7 +120,7 @@ public class Duke {
                 break;
             }
             else if (task.equalsIgnoreCase("list")) {
-                printTaskList(tasks);
+                printTaskList();
                 System.out.println("------------------------------------------");
             }
             else if (task.contains("done")){
@@ -88,14 +131,7 @@ public class Duke {
                 System.out.println("    " + tasks.get(idx-1));
             }
             else {
-                try {
-                    addTask(task);
-                }
-                catch (Exception e) {
-                    System.out.println("    Invalid input! To add task: Please start with 'todo'/ 'deadline' / 'event'. ");
-                    System.out.println("                   To view the list of tasks: Enter 'list'. ");
-                    System.out.println("                   To end the application: Enter 'bye'. ");
-                }
+                addTask(task);
             }
             task = sc.nextLine().trim();
         }
--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class DukeException {
+/* to handle exceptions specific to Duke */
+
+public class DukeException extends Exception {
+
+    private Duke.TaskType taskType;
+    public DukeException(Duke.TaskType taskType) {
+        this.taskType = taskType;
+    }
+    public void printErrorTaskCannotBeEmpty() {
+        System.out.println("OOPS!!! The description of a " + taskType + " cannot be empty.");
+    }
+
 }
