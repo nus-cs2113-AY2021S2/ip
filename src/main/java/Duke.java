52e1818a0beebc8ae547bffafb638388e21a27ce
aliciatay-zls <e0424736@u.nus.edu>
2021-02-09 22:26:49 +0800

    Refactor code, increase readability after PR review

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,30 +5,30 @@ public class Duke {
     /** Constants used for displaying messages */
     private static final String BORDER = "____________________________________________________________";
     private static final String NEWLINE = System.lineSeparator();
-    public static final String LOGO =
-                      "                                      ,::::," + "\n"
-                    + "                          ,,,,:::::::':::::::" + "\n"
-                    + "        ,::::.     ..:::~~           \\::::::" + "\n"
-                    + "       ::::::::::~''      ':       __     ':." + "\n"
-                    + "       ::::/          __    .o.. : u ::     ':." + "\n"
-                    + "         ::,        :: u :.' '. ' ':::'     '::    ,, .::,," + "\n"
-                    + "          `::       ':::' /.  : .\\         .::   ::::::::::" + "\n"
-                    + "          ::.               '':'            ::'  :::,'''.:::'" + "\n"
-                    + "          `::                  :          ,::'  ,:::',,,':::'" + "\n"
-                    + "           `::,                 .     ..::::,, ::::::::::::'" + "\n"
-                    + "              '::::++,....      :..::::~     ':::::::::::'" + "\n"
-                    + "              :::::::::::::::::'~   .         ''::::::'" + "\n"
-                    + "              ::::::::::::::::::.      .         `::::'" + "\n"
-                    + "              ::::::::::::::::::::,     .          ::" + "\n"
-                    + "               ::::::::::::::::::::      :         ::" + "\n"
-                    + "                 ::::::::::::::::'      .       .::'" + "\n"
-                    + "                :: '::::::::::::'       .     .:':" + "\n"
-                    + "                 ::    ~~::::''        . ,,,:::::::::::::" + "\n"
-                    + "                  :::,,,,,,,,,,,....::::::::::::::::::::::" + "\n"
-                    + "                  :::::::::::::::::: :::::::::::::::::::::" + "\n"
-                    + "                 .::::::::::::::::::,  :::::::::::::::::''" + "\n"
-                    + "                .:::::::::::::::::::::,  :::::::::::::'" + "\n"
-                    + "                `::::::::::::::::::::::'" + "\n";
+    public static final String LOGO = """
+                                                          ,::::,
+                                              ,,,,:::::::':::::::
+                            ,::::.     ..:::~~           \\::::::
+                           ::::::::::~''      ':       __     ':.
+                           ::::/          __    .o.. : u ::     ':.
+                             ::,        :: u :.' '. ' ':::'     '::    ,, .::,,
+                              `::       ':::' /.  : .\\         .::   ::::::::::
+                              ::.               '':'            ::'  :::,'''.:::'
+                              `::                  :          ,::'  ,:::',,,':::'
+                               `::,                 .     ..::::,, ::::::::::::'
+                                  '::::;,,...       :..::::~     ':::::::::::'
+                                  :::::::::::::::::'~   .         ''::::::'
+                                  ::::::::::::::::::.      .         `::::'
+                                  ::::::::::::::::::::,     .          ::
+                                   ::::::::::::::::::::      :         ::
+                                     ::::::::::::::::'      .       .::'
+                                    :: '::::::::::::'       .     .:':
+                                     ::    ~~::::''        . ,,,:::::::::::::
+                                      :::,,,,,,,,,,,....::::::::::::::::::::::
+                                      :::::::::::::::::: :::::::::::::::::::::
+                                     .::::::::::::::::::,  :::::::::::::::::''
+                                    .:::::::::::::::::::::,  :::::::::::::'
+                                    `::::::::::::::::::::::' """;
 
 
     /** Constants used to set status of a task */
@@ -43,74 +43,78 @@ public class Duke {
 
     /** Methods that display messages */
     public static void greet() {
-        System.out.println(BORDER);
-        System.out.println(LOGO);
-        System.out.println("Hello, I'm Panda!");
-        System.out.println("What would you like to do today?");
-        System.out.println("Tip: use \"help\" to view all valid commands");
-        System.out.println(BORDER + NEWLINE);
+        String greetString = BORDER + NEWLINE
+                + LOGO + NEWLINE
+                + "Hello, I'm Panda!" + NEWLINE
+                + "What would you like to do today?" + NEWLINE
+                + "Tip: use \"help\" to view all valid commands" + NEWLINE
+                + BORDER + NEWLINE + NEWLINE;
+        System.out.print(greetString);
     }
 
     public static void goodbye() {
-        System.out.println(BORDER);
-        System.out.println("Alright, goodbye!");
-        System.out.println(BORDER);
+        String goodbyeString = BORDER + NEWLINE
+                + "Alright, goodbye!" + NEWLINE
+                + BORDER + NEWLINE;
+        System.out.print(goodbyeString);
     }
 
     public static void printInvalidInputMessage() {
-        System.out.println(BORDER);
-        System.out.println("I'm sorry, I don't quite understand. Can you try again?");
-        System.out.println(BORDER + NEWLINE);
+        String invalidInputString = BORDER + NEWLINE
+                + "I'm sorry, I don't quite understand. "
+                + "Can you try again?" + NEWLINE
+                + BORDER + NEWLINE + NEWLINE;
+        System.out.print(invalidInputString);
     }
 
     public static void help() {
-        System.out.println(BORDER);
-        System.out.println("HELP PAGE");
-        System.out.println("This is the list of all valid commands:" + "\n");
-        System.out.println("\thelp");
-        System.out.println("\t - displays all valid commands" + "\n");
-        System.out.println("\tbye");
-        System.out.println("\t- stops the task manager" + "\n");
-        System.out.println("\tlist");
-        System.out.println("\t- displays all tasks in the list" + "\n");
-        System.out.println("\ttodo     | <task>");
-        System.out.println("\t- adds specified task to the list" + "\n");
-        System.out.println("\tdeadline | <task>  | /by | <deadline>");
-        System.out.println("\t- adds specified task and deadline and to the list" + "\n");
-        System.out.println("\tevent    | <task>  | /at | <timing>");
-        System.out.println("\t- adds specified task and timing to the list" + "\n");
-        System.out.println("\tdone     | <index>");
-        System.out.println("\t- marks existing task matching the specified index"
-                + "as completed in the list" + "\n");
-        System.out.println("<> indicates an input field and | is a field separator.");
-        System.out.println(BORDER + NEWLINE);
+        String helpString = BORDER + NEWLINE
+                + "HELP PAGE" + NEWLINE
+                + "This is the list of all valid commands:" + NEWLINE + NEWLINE
+                + "\thelp" + NEWLINE
+                + "\t - displays all valid commands" + NEWLINE + NEWLINE
+                +"\tbye" + NEWLINE
+                + "\t- stops the task manager" + NEWLINE + NEWLINE
+                + "\tlist" + NEWLINE
+                + "\t- displays all tasks in the list" + NEWLINE + NEWLINE
+                + "\ttodo     | <task>" + NEWLINE
+                + "\t- adds specified task to the list" + NEWLINE + NEWLINE
+                + "\tdeadline | <task>  | /by | <deadline>" + NEWLINE
+                + "\t- adds specified task and deadline and to the list" + NEWLINE + NEWLINE
+                + "\tevent    | <task>  | /at | <timing>" + NEWLINE
+                + "\t- adds specified task and timing to the list" + NEWLINE + NEWLINE
+                + "\tdone     | <index>" + NEWLINE
+                + "\t- marks existing task matching the specified index"
+                + "as completed in the list" + NEWLINE  + NEWLINE
+                + "<> indicates an input field and | is a field separator." + NEWLINE
+                + BORDER + NEWLINE + NEWLINE;
+        System.out.print(helpString);
     }
 
     /** Methods that print part of or full list */
     public static void echo() {
-        System.out.println(BORDER);
-        System.out.println("New task added: ");
+        System.out.print(BORDER + NEWLINE);
+        System.out.print("New task added: " + NEWLINE);
         System.out.print("\t");
         tasks[tasksCount-1].printTask();
-        System.out.print("\n");
-//        System.out.println("There are " + tasksCount + " tasks in your list.");
+        System.out.print(NEWLINE);
         System.out.print("There ");
         System.out.print(tasksCount > 1 ? "are " : "is ");
         System.out.print(tasksCount);
         System.out.print(tasksCount > 1 ? " tasks" : " task");
-        System.out.println(" in your list.");
-        System.out.println(BORDER + NEWLINE);
+        System.out.print(" in your list." + NEWLINE);
+        System.out.print(BORDER + NEWLINE + NEWLINE);
     }
 
     public static void printList() {
-        System.out.println(BORDER);
-        System.out.println("Here are the tasks in your list:");
+        System.out.print(BORDER + NEWLINE);
+        System.out.print("Here are the tasks in your list:" + NEWLINE);
         for (int i=0; i<tasksCount; i++) {
             System.out.print("\t" + tasks[i].getIndex() + ". ");
             tasks[i].printTask();
-            System.out.print("\n");
+            System.out.print(NEWLINE);
         }
-        System.out.println(BORDER + NEWLINE);
+        System.out.print(BORDER + NEWLINE + NEWLINE);
     }
 
 
@@ -141,17 +145,23 @@ public class Duke {
 
     public static void markInList(int id) {
         tasks[id-1].setStatus(DONE_STATUS);
-        System.out.println(BORDER);
-        System.out.println("Nice! This task is now done:");
+        System.out.print(BORDER + NEWLINE);
+        System.out.print("Nice! This task is now done:" + NEWLINE);
         System.out.print("\t");
         tasks[id-1].printTask();
-        System.out.print("\n");
-        System.out.println(BORDER + NEWLINE);
+        System.out.print(NEWLINE + BORDER + NEWLINE + NEWLINE);
     }
 
 
     /** Methods that check if user inputs are valid commands */
     public static void processInput(String userInput) {
+        if (userInput.equals("list")) {
+            printList();
+            return;
+        } else if (userInput.equals("help")) {
+            help();
+            return;
+        }
         userInput = userInput.trim();
         String[] tokens = userInput.split(" ", 2);
         boolean isValid = true;
@@ -170,14 +180,14 @@ public class Duke {
             addTodo(tokens[1]);
             break;
         case "deadline":
-            isValid = hasDeadlineOrTiming(" /by ", tokens);
+            isValid = isDeadlineOrTiming(" /by ", tokens);
             if (isValid) {
                 int position = tokens[1].indexOf("/by");
                 addDeadline(tokens[1].substring(0, position - 1), tokens[1].substring(position + 4));
             }
             break;
         case "event":
-            isValid = hasDeadlineOrTiming(" /at ", tokens);
+            isValid = isDeadlineOrTiming(" /at ", tokens);
             if (isValid) {
                 int position = tokens[1].indexOf("/at");
                 addEvent(tokens[1].substring(0, position - 1), tokens[1].substring(position + 4));
@@ -204,7 +214,7 @@ public class Duke {
         return isPossibleIndex && isValidIndex;
     }
 
-    public static boolean hasDeadlineOrTiming(String keyword, String[] tokens) {
+    public static boolean isDeadlineOrTiming(String keyword, String[] tokens) {
         if (!tokens[1].contains(keyword)) {
             return false;
         }
@@ -225,13 +235,7 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         String userInput = in.nextLine();
         while (!userInput.equals("bye")) {
-            if (userInput.equals("list")) {
-                printList();
-            } else if (userInput.equals("help")) {
-                help();
-            } else {
-                processInput(userInput);
-            }
+            processInput(userInput);
             userInput = in.nextLine();
         }
         goodbye();
