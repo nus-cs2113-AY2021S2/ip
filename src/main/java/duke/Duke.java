48ccc37aa0dbc340a56290d6f2456c1246aa1ffa
Mark Low <nedgangk@gmail.com>
2021-02-09 21:11:38 +0800

    Move classes into its packages

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,3 +1,10 @@
+package duke;
+
+import duke.task.Deadline;
+import duke.task.Event;
+import duke.task.Task;
+import duke.task.Todo;
+
 public class Duke {
 
     private static final String COMMAND_LIST_WORD = "list";
