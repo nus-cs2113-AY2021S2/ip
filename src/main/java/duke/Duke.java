331159bf7dd69ca85df07511c0457ee576fa0580
Brandon Foong <brandonfoongyankai@gmail.com>
2021-02-09 21:16:01 +0800

    Convert each command into its own function Improve error handling with DukeException

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -11,44 +11,10 @@ public class Duke {
 
     private static final String LINE_DIVIDER = "\t____________________________________________________________";
 
-
     public static void main(String[] args) {
         DukePrinter.printWelcomeMessage();
         interactWithUser();
         DukePrinter.printExitMessage();
-    }
-
-    private static void addTodo(ArrayList<Task> tasks, String description) {
-        Todo todo = new Todo(description);
-        tasks.add(todo);
-        DukePrinter.printTaskAdded(tasks, todo);
-    }
-
-    private static void addDeadline(ArrayList<Task> tasks, String description, String dueDate) {
-        Deadline deadline = new Deadline(description, dueDate);
-        tasks.add(deadline);
-        DukePrinter.printTaskAdded(tasks, deadline);
-    }
-
-    private static void addEvent(ArrayList<Task> tasks, String description, String eventDate) {
-        Event event = new Event(description, eventDate);
-        tasks.add(event);
-        DukePrinter.printTaskAdded(tasks, event);
-    }
-
-    private static void markTaskAsDone(ArrayList<Task> tasks, int taskNumber) {
-        /* Enforce that taskNumber is valid */
-        if (taskNumber < 1 || taskNumber > tasks.size()) {
-            DukePrinter.printInvalidArgumentsMessage();
-            return;
-        }
-        /* Change from 1-based indexing to 0-based indexing */
-        taskNumber = taskNumber - 1;
-        tasks.get(taskNumber).markAsDone();
-        System.out.println(LINE_DIVIDER);
-        System.out.println("\t Nice! I've marked this task as done: ");
-        System.out.println("\t   " + tasks.get(taskNumber));
-        System.out.println(LINE_DIVIDER);
     }
 
     private static void interactWithUser() {
@@ -57,7 +23,7 @@ public class Duke {
         while (!isDoneReadingInputs) {
             String[] commandTokens = DukeReader.readUserInput();
             try {
-                isDoneReadingInputs = parseCommandTokens(tasks, isDoneReadingInputs, commandTokens);
+                isDoneReadingInputs = executeUserInput(tasks, commandTokens);
             } catch (DukeException dukeException) {
                 String errorMessage = dukeException.getMessage();
                 DukePrinter.printErrorMessage(errorMessage);
@@ -67,15 +33,16 @@ public class Duke {
         }
     }
 
-    private static boolean parseCommandTokens(
-            ArrayList<Task> tasks, boolean isDoneReadingInputs, String[] commandTokens)
+    private static boolean executeUserInput(
+            ArrayList<Task> tasks, String[] commandTokens)
             throws DukeException, NumberFormatException {
         if (commandTokens.length == 0) {
             throw new DukeException(
                     "Please enter a command.\n" +
-                    "Try using \"help\" for a list of commands."
+                            "Try using \"help\" for a list of commands."
             );
         }
+        boolean isDoneReadingInputs = false;
         switch (commandTokens[0]) {
         case DukeCommands.BYE_COMMAND:
             isDoneReadingInputs = true;
@@ -87,62 +54,16 @@ public class Duke {
             DukePrinter.printHelpMessage();
             break;
         case DukeCommands.DONE_COMMAND:
-            if (commandTokens.length < 2) {
-                throw new DukeException(
-                        "Please give me more details about the task!"
-                );
-            }
-            int taskNumber = Integer.parseInt(commandTokens[1]);
-            markTaskAsDone(tasks, taskNumber);
+            markTaskAsDone(tasks, commandTokens);
             break;
         case DukeCommands.TODO_COMMAND:
-            if (commandTokens.length < 2) {
-                throw new DukeException(
-                        "Please give me more details about the task!"
-                );
-            }
-            if (commandTokens[1].equals("")) {
-                throw new DukeException(
-                        "The description of a task can't be empty. Please try again."
-                );
-            }
-            addTodo(tasks, commandTokens[1]);
+            addTodo(tasks, commandTokens);
             break;
         case DukeCommands.DEADLINE_COMMAND:
-            if (commandTokens.length < 3) {
-                throw new DukeException(
-                        "Please give me more details about the task!"
-                );
-            }
-            if (commandTokens[1].equals("")) {
-                throw new DukeException(
-                        "The description of a task can't be empty. Please try again."
-                );
-            }
-            if (commandTokens[2].equals("")) {
-                throw new DukeException(
-                        "Please specify a deadline for the task."
-                );
-            }
-            addDeadline(tasks, commandTokens[1], commandTokens[2]);
+            addDeadline(tasks, commandTokens);
             break;
         case DukeCommands.EVENT_COMMAND:
-            if (commandTokens.length < 3) {
-                throw new DukeException(
-                        "Please give me more details about the task!"
-                );
-            }
-            if (commandTokens[1].equals("")) {
-                throw new DukeException(
-                        "The description of a task can't be empty. Please try again."
-                );
-            }
-            if (commandTokens[2].equals("")) {
-                throw new DukeException(
-                        "Please specify a date for the event."
-                );
-            }
-            addEvent(tasks, commandTokens[1], commandTokens[2]);
+            addEvent(tasks, commandTokens);
             break;
         default:
             /* Unknown command, prompt user to use the "help" command */
@@ -152,4 +73,89 @@ public class Duke {
         return isDoneReadingInputs;
     }
 
+    private static void addTodo(ArrayList<Task> tasks, String[] arguments) throws DukeException {
+        if (arguments.length < 2) {
+            throw new DukeException(
+                    "Please give me more details about the task!"
+            );
+        }
+        if (arguments[1].equals("")) {
+            throw new DukeException(
+                    "The description of a task can't be empty. Please try again."
+            );
+        }
+        String description = arguments[1];
+        Todo todo = new Todo(description);
+        tasks.add(todo);
+        DukePrinter.printTaskAdded(tasks, todo);
+    }
+
+    private static void addDeadline(ArrayList<Task> tasks, String[] arguments) throws DukeException {
+        if (arguments.length < 3) {
+            throw new DukeException(
+                    "Please give me more details about the task!"
+            );
+        }
+        if (arguments[1].equals("")) {
+            throw new DukeException(
+                    "The description of a task can't be empty. Please try again."
+            );
+        }
+        if (arguments[2].equals("")) {
+            throw new DukeException(
+                    "Please specify a deadline for the task."
+            );
+        }
+        String description = arguments[1];
+        String dueDate = arguments[2];
+        Deadline deadline = new Deadline(description, dueDate);
+        tasks.add(deadline);
+        DukePrinter.printTaskAdded(tasks, deadline);
+    }
+
+    private static void addEvent(ArrayList<Task> tasks, String[] arguments) throws DukeException {
+        if (arguments.length < 3) {
+            throw new DukeException(
+                    "Please give me more details about the task!"
+            );
+        }
+        if (arguments[1].equals("")) {
+            throw new DukeException(
+                    "The description of a task can't be empty. Please try again."
+            );
+        }
+        if (arguments[2].equals("")) {
+            throw new DukeException(
+                    "Please specify a date for the event."
+            );
+        }
+        String description = arguments[1];
+        String eventDate = arguments[2];
+        Event event = new Event(description, eventDate);
+        tasks.add(event);
+        DukePrinter.printTaskAdded(tasks, event);
+    }
+
+    private static void markTaskAsDone(ArrayList<Task> tasks, String[] arguments)
+            throws DukeException, NumberFormatException {
+        if (arguments.length < 2) {
+            throw new DukeException(
+                    "Please give me more details about the task!"
+            );
+        }
+        int taskNumber = Integer.parseInt(arguments[1]);
+        /* Enforce that taskNumber is valid */
+        if (taskNumber < 1 || taskNumber > tasks.size()) {
+            throw new DukeException(
+                    "That's an invalid task number!"
+            );
+        }
+        /* Change from 1-based indexing to 0-based indexing */
+        taskNumber = taskNumber - 1;
+        tasks.get(taskNumber).markAsDone();
+        System.out.println(LINE_DIVIDER);
+        System.out.println("\t Nice! I've marked this task as done: ");
+        System.out.println("\t   " + tasks.get(taskNumber));
+        System.out.println(LINE_DIVIDER);
+    }
 }
--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -15,7 +15,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin duke.Duke < INPUT_2.TXT > ACTUAL.TXT
+java -classpath ..\bin duke.Duke < INPUT_1.TXT > ACTUAL.TXT
 
 REM compare the output to the expected output
-FC ACTUAL.TXT EXPECTED_2.TXT
+FC ACTUAL.TXT EXPECTED_1.TXT
