0f5088f397389aff47907bd727c887adb7ab00af
douglaslewpc <reachdouglaslew@gmail.com>
2021-02-10 23:44:24 +0800

    Implemented try and catch statements to handle exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static Task[] tasks = new Task[100];
+    public static final int maxTasks = 100;
+    private static final String border = "    ―――――――――――――――――――――――――――――――――――――――――――――――――";
+    private static Task[] tasks = new Task[maxTasks];
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         welcomeMessage();
-        String[] commands = {"bye","deadline","todo","event","done","list"};
+        Scanner in = new Scanner(System.in);
         boolean isOn = true;
         int numOfCompletedTasks = 0;
         int numOfTasks = 0;
@@ -14,120 +15,146 @@ public class Duke {
         while(isOn && in.hasNextLine()) {
             String line = in.nextLine();
             String[] words = line.split(" ");
-            if(line.equals("bye")){
-                System.out.println("    --------------------------------------------------------------------------------");
-                System.out.println("    Bye. Hope to see you again soon!");
-                System.out.println(" ");
-                System.out.println("    --------------------------------------------------------------------------------");
-                isOn = false;
-            } else if(words[0].equals("todo")){
-                String description = "";
-                int wordsCount = words.length;
-                if(words.length==1){
-                    System.out.println("    --------------------------------------------------------------------------------");
-                    System.out.println("    OOPS!! The description of a todo cannot be empty.");
-                    System.out.println("    --------------------------------------------------------------------------------");
-                    continue;
+            try{
+                if(line.equals("bye")){
+                    System.out.println(border);
+                    System.out.println("    Bye. Hope to see you again soon!");
+                    System.out.println(" ");
+                    System.out.println(border);
+                    isOn = false;
+                } else if(words[0].equals("todo")){
+                    String description = "";
+                    try{
+                        int wordsCount = words.length;
+                        if(words.length==1){
+                            throw new DukeException("missing_parameter");
+                        }
+                        for(String word:words){
+                            wordsCount--;
+                            if(word.equals("todo")){
+                                continue;
+                            } else if(wordsCount==0){
+                                description = description + word;
+                            } else{
+                                description = description + word + " ";
+                            }
+                        }
+                        tasks[n] = new Todo(description);
+                        System.out.println(border);
+                        System.out.println("    Got it. I've added this task:");
+                        System.out.println("      "+tasks[n]);
+                        n++;
+                        System.out.println("    Now you have " + n +" tasks in the list.");
+                        System.out.println(" ");
+                        System.out.println(border);
+                    }catch (DukeException e) {
+                        System.out.println(border);
+                        System.out.println("    OOPS!! The description of a todo cannot be empty.");
+                        System.out.println(border);
+                    }
+                } else if(words[0].equals("deadline")){
+                    String description = "";
+                    String by = "";
+                    try{
+                        boolean byFlag = false;
+                        int wordsCount = words.length;
+                        if(wordsCount==1){
+                            throw new DukeException("missing_parameter");
+                        }
+                        for(String word:words){
+                            wordsCount--;
+                            if(word.equals("/by")){
+                                byFlag = true;
+                                continue;
+                            }
+                            if(word.equals("deadline")){
+                                continue;
+                            }
+                            if(byFlag && wordsCount==0){
+                                by = by + word;
+                            } else if(byFlag){
+                                by = by + word + " ";
+                            } else{
+                                description = description + word + " ";
+                            }
+                        }
+                        tasks[n] = new Deadline(description,by);
+                        System.out.println(border);
+                        System.out.println("    Got it. I've added this task:");
+                        System.out.println("      "+tasks[n]);
+                        n++;
+                        System.out.println("    Now you have " + n +" tasks in the list.");
+                        System.out.println(" ");
+                        System.out.println(border);
+                    }catch (DukeException e){
+                        System.out.println(border);
+                        System.out.println("    OOPS!! The description of the deadline cannot be empty.");
+                        System.out.println(border);
+                    }
+
+                } else if(words[0].equals("event")){
+                    String description = "";
+                    String at = "";
+                    try{
+                        boolean atFlag = false;
+                        int wordsCount = words.length;
+                        if(wordsCount==1){
+                            throw new DukeException("missing_parameter");
+                        }
+                        for(String word:words){
+                            wordsCount--;
+                            if(word.equals("/at")){
+                                atFlag = true;
+                                continue;
+                            }
+                            if(word.equals("event")){
+                                continue;
+                            }
+                            if(atFlag && wordsCount==0){
+                                at = at + word;
+                            } else if(atFlag){
+                                at = at + word + " ";
+                            } else{
+                                description = description + word + " ";
+                            }
+                        }
+                        tasks[n] = new Event(description,at);
+                        System.out.println(border);
+                        System.out.println("    Got it. I've added this task:");
+                        System.out.println("      "+tasks[n]);
+                        n++;
+                        System.out.println("    Now you have " + n +" tasks in the list.");
+                        System.out.println(" ");
+                        System.out.println(border);
+                    }catch (DukeException e){
+                        System.out.println(border);
+                        System.out.println("    OOPS!! The description of the deadline cannot be empty.");
+                        System.out.println(border);
+                    }
+
+                } else if(words[0].equals("done")){
+                    int taskNum = Integer.parseInt(words[1])-1;
+                    tasks[taskNum].markAsDone();
+                    System.out.println(border);
+                    System.out.println("    Nice! I've marked this task as done:");
+                    System.out.println("      " + tasks[taskNum]);
+                    System.out.println(" ");
+                    System.out.println(border);
+                } else if(line.equals("list")){
+                    System.out.println(border);
+                    System.out.println("    Here are the tasks in your list:");
+                    for(int i=0;i<n;i++){
+                        System.out.println("    "+(i + 1) + "." + tasks[i]);
+                    }
+                    System.out.println(" ");
+                    System.out.println(border);
+                } else {
+                    throw new DukeException("unrecognized_input");
                 }
-                for(String word:words){
-                    wordsCount--;
-                    if(word.equals("todo")){
-                        continue;
-                    } else if(wordsCount==0){
-                        description = description + word;
-                    } else{
-                        description = description + word + " ";
-                    }
-                }
-                tasks[n] = new Todo(description);
-                System.out.println("    --------------------------------------------------------------------------------");
-                System.out.println("    Got it. I've added this task:");
-                System.out.println("      "+tasks[n]);
-                n++;
-                System.out.println("    Now you have " + n +" tasks in the list.");
-                System.out.println(" ");
-                System.out.println("    --------------------------------------------------------------------------------");
-            }
-            else if(words[0].equals("deadline")){
-                String description = "";
-                String by = "";
-                boolean byFlag = false;
-                int wordsCount = words.length;
-                for(String word:words){
-                    wordsCount--;
-                    if(word.equals("/by")){
-                        byFlag = true;
-                        continue;
-                    }
-                    if(word.equals("deadline")){
-                        continue;
-                    }
-                    if(byFlag && wordsCount==0){
-                        by = by + word;
-                    } else if(byFlag){
-                        by = by + word + " ";
-                    } else{
-                        description = description + word + " ";
-                    }
-                }
-                tasks[n] = new Deadline(description,by);
-                System.out.println("    --------------------------------------------------------------------------------");
-                System.out.println("    Got it. I've added this task:");
-                System.out.println("      "+tasks[n]);
-                n++;
-                System.out.println("    Now you have " + n +" tasks in the list.");
-                System.out.println(" ");
-                System.out.println("    --------------------------------------------------------------------------------");
-            } else if(words[0].equals("event")){
-                String description = "";
-                String at = "";
-                boolean atFlag = false;
-                int wordsCount = words.length;
-                for(String word:words){
-                    wordsCount--;
-                    if(word.equals("/at")){
-                        atFlag = true;
-                        continue;
-                    }
-                    if(word.equals("event")){
-                        continue;
-                    }
-                    if(atFlag && wordsCount==0){
-                        at = at + word;
-                    } else if(atFlag){
-                        at = at + word + " ";
-                    } else{
-                        description = description + word + " ";
-                    }
-                }
-                tasks[n] = new Event(description,at);
-                System.out.println("    --------------------------------------------------------------------------------");
-                System.out.println("    Got it. I've added this task:");
-                System.out.println("      "+tasks[n]);
-                n++;
-                System.out.println("    Now you have " + n +" tasks in the list.");
-                System.out.println(" ");
-                System.out.println("    --------------------------------------------------------------------------------");
-            } else if(words[0].equals("done")){
-                int taskNum = Integer.parseInt(words[1])-1;
-                tasks[taskNum].markAsDone();
-                System.out.println("    --------------------------------------------------------------------------------");
-                System.out.println("    Nice! I've marked this task as done:");
-                System.out.println("      " + tasks[taskNum]);
-                System.out.println(" ");
-                System.out.println("    --------------------------------------------------------------------------------");
-            } else if(line.equals("list")){
-                System.out.println("    --------------------------------------------------------------------------------");
-                System.out.println("    Here are the tasks in your list:");
-                for(int i=0;i<n;i++){
-                    System.out.println("    "+(i + 1) + "." + tasks[i]);
-                }
-                System.out.println(" ");
-                System.out.println("    --------------------------------------------------------------------------------");
-            } else {
-                System.out.println("    --------------------------------------------------------------------------------");
-                System.out.println("    OOPS!! I'm sorry, but I don't know what that means");
-                System.out.println("    --------------------------------------------------------------------------------");
+            }catch(DukeException e){
+                System.out.println(border);
+                System.out.println("    OOPS!! I'm sorry, but I don't know what that means.");
+                System.out.println(border);
             }
         }
     }
