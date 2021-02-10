46e63d68cc16113d2d48dff2fbbe46b27db0c80d
Yiwen_Qiu <60378963+e00426142@users.noreply.github.com>
2021-02-10 15:33:53 +0800

    Revert "Revert "level 5 increment on branch""
    This reverts commit c2afab4b71e432cad80322ad291399cef5337364.

---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public abstract class Task {
+    protected String description;
+    protected boolean isDone;
+
+
+
+    public Task(String description) {
+        this.description = description;
+        isDone = false;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+    public void markAsDone(){
+        isDone = true;
+    }
+    public abstract String getType();
+
+
+    public String toString(){
+        return "description: " + description;
+    }
+
+}
