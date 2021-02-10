2ca77132839b89cca46acb619e1ca6801a8d05e6
VInci-Hu <huwenqi614@gmail.com>
2021-02-10 17:15:44 +0800

    organize methods by OO standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,50 +13,46 @@ public class Duke {
     final static String PARTINGS = "\tBye. Hope you have done your work next time I see you!\n" +
             "\tAh, and also remember to take care of yourself and sleep early :)\n";
     private static boolean isExiting = false;
+    private static Task[] tasks = new Task[100];   // list of task items
+    private static int taskCount = 0;
 
     /**
      * Deals with raw input, extracts keyword and calls respective methods.
      * @param   prompt  is the raw input string
      * */
-    public static void processPrompt(String prompt) {
+    public static void processPrompt(String prompt) throws IllegalKeywordException {
         String keyword = prompt.contains(" ") ? prompt.split(" ")[0] : prompt;
-        System.out.print(DIVLINE);
-        switch(keyword){
-        case "bye":
-            break;
-        case "list":
-            break;
-        case "done":
-            break;
-        case "deadline":
-            break;
-        case "event":
-            break;
-        default:
-            break;
+        switch (keyword) {
+            case "bye":
+                break;
+            case "list":
+                displayList(tasks, taskCount);
+                break;
+            case "done":
+                try {
+                    int taskIndex = Integer.parseInt(prompt.substring(5)) - 1;
+                    completeTask(tasks, taskIndex);
+                } catch (NumberFormatException e) {
+                    System.out.println(DIVLINE + " :( OOPS!!! You are not specifying a valid task number." + DIVLINE);
+                }
+                break;
+            case "deadline":
+                addToList(tasks, prompt.substring(9), "deadline");
+                break;
+            case "event":
+                addToList(tasks, prompt.substring(6), "event");
+                break;
+            case "todo":
+                addToList(tasks, prompt.substring(5), "todo");
+                break;
+            default:
+                throw new IllegalKeywordException();
         }
-
-//            if (prompt.equals("list")) {
-//                displayList(tasks, taskIndex);
-//            } else if (prompt.startsWith("done")) {
-//                completeTask(tasks, prompt.substring(5));
-//            } else if (prompt.startsWith("todo")) {
-//                addToList(tasks, prompt.substring(5), taskIndex, "todo");
-//                taskIndex++;
-//            } else if (prompt.startsWith("deadline")) {
-//                addToList(tasks, prompt.substring(9), taskIndex, "deadline");
-//                taskIndex++;
-//            } else if (prompt.startsWith("event")) {
-//                addToList(tasks, prompt.substring(6), taskIndex, "event");
-//                taskIndex++;
-//            } else {
-//                System.out.println("Invalid prompt. Please try other keywords.");
-//            }
-        System.out.print(DIVLINE);
-        return;
+         return;
     }
 
-    public static void displayList(Task[] tasks, int taskIndex) {
+    private static void displayList(Task[] tasks, int taskIndex) {
+        System.out.println(DIVLINE);
         if (taskIndex == 0) {
             System.out.println("\tYou haven't noted down anything yet.");
             return;
@@ -65,46 +61,47 @@ public class Duke {
         for (int i = 0; i < taskIndex; i++) {
             System.out.println("\t" + (i + 1) + "." + tasks[i].toString());
         }
+        System.out.println(DIVLINE);
     }
 
-    private static void addToList(Task[] tasks, String prompt, int taskIndex, String taskType) {
+    private static void addToList(Task[] tasks, String prompt, String taskType) {
         int cutOffPoint = 0;
         switch (taskType) {
         case "todo":
-            tasks[taskIndex] = new Todo(prompt);
+            tasks[taskCount] = new Todo(prompt);
             break;
         case "deadline":
             cutOffPoint = prompt.indexOf("/by");
-            tasks[taskIndex] = new Deadline(prompt.substring(0, cutOffPoint - 1), prompt.substring(cutOffPoint + 4));
+            tasks[taskCount] = new Deadline(prompt.substring(0, cutOffPoint - 1), prompt.substring(cutOffPoint + 4));
             break;
         case "event":
             cutOffPoint = prompt.indexOf("/at");
-            tasks[taskIndex] = new Event(prompt.substring(0, cutOffPoint - 1), prompt.substring(cutOffPoint + 4));
+            tasks[taskCount] = new Event(prompt.substring(0, cutOffPoint - 1), prompt.substring(cutOffPoint + 4));
             break;
         }
-        System.out.println("\tGot it. I've added this task:\n" +
-                "\t  " + tasks[taskIndex].toString());
-        System.out.println("\tNow you have " + (taskIndex + 1) + " tasks in the list.");
+        System.out.println(DIVLINE + "\tGot it. I've added this task:\n" +
+                "\t  " + tasks[taskCount].toString());
+        System.out.println("\tNow you have " + (taskCount + 1) + " tasks in the list."+ DIVLINE);
     }
 
-    private static void completeTask(Task[] tasks, String substring) {
-        int taskIndex = Integer.parseInt(substring) - 1;
+    private static void completeTask(Task[] tasks, int taskIndex) {
         tasks[taskIndex].markAsDone();
         System.out.println("\tNice! I've marked this task as done: \n" +
                 "\t" + tasks[taskIndex].toString());
-        //bug here: does not deal with overflow values of input
     }
 
     public static void main(String[] args) {
-        Task[] tasks = new Task[100];   // list of task items
         System.out.print(DIVLINE + GREETINGS + DIVLINE);
         Scanner in = new Scanner(System.in);
         int taskIndex = 0;
         while (!isExiting) {
-            String prompt = in.nextLine();
+            try{
+                String prompt = in.nextLine();
+                processPrompt(prompt);
+            } catch (IllegalKeywordException e) {
+                System.out.println(DIVLINE + "OOPS!!! I'm sorry, but I don't know what that means :-(" + DIVLINE);
+            }
         }
-
-
         System.out.println(DIVLINE + PARTINGS + DIVLINE);
     }
 
