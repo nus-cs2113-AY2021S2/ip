c8aa4216fb95cbe2cb0f54d0b5898d135ab6e8ce
geezzzyyy <zhuoyange@gmail.com>
2021-02-10 18:21:33 +0800

    improve code quality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,54 +23,74 @@ public class Duke {
                 break;
             }else if(command.equals("list")){
                 System.out.println("____________________________________________________________");
-                System.out.println("Here are the tasks in your list:");
-                for(int index=0; index<i;index++){
-                    System.out.print(index+1+". ");
-                    System.out.println(tasks[index]);
-                }
+                listTasks(i, tasks);
                 System.out.println("____________________________________________________________");
             }else if(command.contains("done")){
-                int doneNumber=Integer.parseInt(command.substring(5));
-                System.out.println("Nice! I've marked this task as done: ");
-                tasks[doneNumber-1].markAsDone();
+                doneTasks(tasks, command);
                 System.out.println("____________________________________________________________");
             } else if(command.contains("todo")){
                 System.out.println("____________________________________________________________");
-                tasks[i] = new Todo(command.substring(5));
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[i]);
-                System.out.println("Now you have "+ Task.getTaskCount() +" task(s) in the list.");
+                todoTask(i, tasks, command);
                 System.out.println("____________________________________________________________");
                 i++;
             }else if(command.contains("deadline")) {
                 System.out.println("____________________________________________________________");
                 //Deadline t = new Deadline(8,command.indexOf("/by"));
-                String description = command.substring(8, command.indexOf("/by"));
-                String by = command.substring(command.indexOf("/by") + 4);
-                tasks[i] = new Deadline(description, by);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[i]);
-                System.out.println("Now you have "+ Task.getTaskCount() +" task(s) in the list.");
+                deadlineTasks(i, tasks, command);
                 System.out.println("____________________________________________________________");
                 i++;
             }else if(command.contains("event")) {
                 System.out.println("____________________________________________________________");
                 //Deadline t = new Deadline(8,command.indexOf("/by"));
-                String description = command.substring(6, command.indexOf("/at"));
-                String at = command.substring(command.indexOf("/at") + 4);
-                tasks[i] = new Event(description, at);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[i]);
-                System.out.println("Now you have "+ Task.getTaskCount() +" task(s) in the list.");
+                eventTask(i, tasks, command);
                 System.out.println("____________________________________________________________");
                 i++;
             }else{
                 System.out.println("____________________________________________________________");
-                System.out.println("Invalid command please enter again");
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 System.out.println("____________________________________________________________");
             }
         }
 
 
+    }
+
+    private static void deadlineTasks(int i, Task[] tasks, String command) {
+        String description = command.substring(8, command.indexOf("/by"));
+        String by = command.substring(command.indexOf("/by") + 4);
+        tasks[i] = new Deadline(description, by);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(tasks[i]);
+        System.out.println("Now you have "+ Task.getTaskCount() +" task(s) in the list.");
+    }
+
+    private static void eventTask(int i, Task[] tasks, String command) {
+        String description = command.substring(6, command.indexOf("/at"));
+        String at = command.substring(command.indexOf("/at") + 4);
+        tasks[i] = new Event(description, at);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(tasks[i]);
+        System.out.println("Now you have "+ Task.getTaskCount() +" task(s) in the list.");
+    }
+
+    private static void todoTask(int i, Task[] tasks, String command) {
+        tasks[i] = new Todo(command.substring(5));
+        System.out.println("Got it. I've added this task:");
+        System.out.println(tasks[i]);
+        System.out.println("Now you have "+ Task.getTaskCount() +" task(s) in the list.");
+    }
+
+    private static void doneTasks(Task[] tasks, String command) {
+        int doneNumber=Integer.parseInt(command.substring(5));
+        System.out.println("Nice! I've marked this task as done: ");
+        tasks[doneNumber-1].markAsDone();
+    }
+
+    private static void listTasks(int i, Task[] tasks) {
+        System.out.println("Here are the tasks in your list:");
+        for(int index = 0; index< i; index++){
+            System.out.print(index+1+". ");
+            System.out.println(tasks[index]);
+        }
     }
 }
