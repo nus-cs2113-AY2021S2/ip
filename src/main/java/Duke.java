34e92ba8950dd2869796eb302d235bff26fd4ef4
Krithigha24 <krithigha242000@gmail.com>
2021-02-12 00:07:25 +0800

    deal with errors such as incorrect inputs entered by the use

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,60 +2,36 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        boolean isExit = false;
-        Task[] tasks = new Task[100];
-        int count = 0;
-        showWelcomeMessage();
+        MessagePrinter.showWelcomeMessage();
 
-        while (!isExit) {
-            String userInput = getUserInput();
-            printBorder();
-            if (userInput.equals("bye")) {
-                System.out.println("     Bye. Hope to see you again soon!");
-                isExit = true;
-            } else if (userInput.equals("list")) {
-                if (count == 0) {
-                    System.out.println("     List is empty");
-                }
-                for (int i = 0; i < count; i++) {
-                    System.out.print("     " + (i + 1));
-                    System.out.println(". " + tasks[i]);
-                }
-            } else if (userInput.contains("done")) {
-                int value = Integer.parseInt(userInput.replaceAll("[^0-9]", ""));
-                if (value > count) {
-                    System.out.println("     Task " + value + " does not exist!");
-                } else {
-                    tasks[value - 1].markAsDone();
-                    System.out.println("     Nice! I've marked this task as done: ");
-                    System.out.print("      ");
-                    System.out.println(tasks[value - 1]);
+        while (true) {
+            String fullCommand = getUserInput();
+            String[] partOfCommand = fullCommand.split(" ");
+
+            if (fullCommand.equals("bye")) {
+                MessagePrinter.showByeMessage();
+                break;
+            } else if (fullCommand.equals("list")) {
+                TaskManager.printList();
+            } else if (fullCommand.contains("done")) {
+                try {
+                    TaskManager.displayTaskDone(Integer.parseInt(partOfCommand[1]));
+                } catch (IndexOutOfBoundsException oob) {
+                    MessagePrinter.printBorder();
+                    System.out.println("     ☹ OOPS!!! Please specify which task number is done");
+                    MessagePrinter.printBorder();
+                } catch (NullPointerException npe) {
+                    MessagePrinter.printBorder();
+                    System.out.println("     ☹ OOPS!!! There is no such task number");
+                    MessagePrinter.printBorder();
+                } catch ( DukeException de){
+                    MessagePrinter.printBorder();
+                    System.out.println("     ☹ OOPS!!! This task is already marked as done");
+                    MessagePrinter.printBorder();
                 }
             } else {
-                if (userInput.contains("todo")) {
-                    System.out.println("     Got it. I've added this task: ");
-                    Task a = new Todo(userInput.substring(5));
-                    tasks[count] = a;
-                    count++;
-                    System.out.println("      " + a);
-                } else if (userInput.contains("deadline")) {
-                    System.out.println("     Got it. I've added this task: ");
-                    Task b = new Deadline(userInput.substring(9, userInput.indexOf("/by")), userInput.substring(userInput.indexOf("/by") + 4));
-                    tasks[count] = b;
-                    count++;
-                    System.out.println("      " + b);
-                } else if (userInput.contains("event")) {
-                    System.out.println("     Got it. I've added this task: ");
-                    Task c = new Event(userInput.substring(6, userInput.indexOf("/at")), userInput.substring(userInput.indexOf("/at") + 4));
-                    tasks[count] = c;
-                    count++;
-                    System.out.println("      " + c);
-                } else {
-                    System.out.println("     !INVALID TASK! Please specify the type of task");
-                }
-                System.out.println("     Now you have " + count + " tasks in the list.");
+                TaskManager.addNewTask(partOfCommand, fullCommand);
             }
-            printBorder();
         } //while loop
     } //main method
 
@@ -64,21 +40,4 @@ public class Duke {
         return scanner.nextLine();
     }
 
-    private static void showWelcomeMessage() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        printBorder();
-        System.out.println("     Hello! I'm Duke ");
-        System.out.println("     What can I do for you?");
-        printBorder();
-    }
-
-    private static void printBorder() {
-        System.out.println("    ____________________________________________________________");
-    }
 }
