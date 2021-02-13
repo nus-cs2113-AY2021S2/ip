33858af1e579ef6b5a1df1b151a3f07e4f06fab0
Manika <mhennedige@gmail.com>
2021-02-13 23:21:15 +0800

    Update skeleton for tomorrow

--- src/main/java/duke/savedata/Save.java
+++ src/main/java/duke/savedata/Save.java
@@ -1,5 +1,22 @@
 package duke.savedata;
 
 public class Save {
-    
+
+
+    public static void addTodo() {
+        // todo add todo task from txt file
+
+    }
+
+    public static void addDeadline() {
+        // todo add deadline task from txt file
+    }
+
+    public static void addEvent() {
+        // todo add event task from txt file
+    }
+
+    public static void saveTasks() {
+        // todo save list of current tasks to txt file
+    }
 }
