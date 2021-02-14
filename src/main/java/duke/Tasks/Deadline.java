2b24872fe21b00deef2684a7250c7efab7e263f4
lamzf1998 <73275294+lamzf1998@users.noreply.github.com>
2021-02-14 14:01:14 +0800

    Update Deadline.java

--- src/main/java/duke/Tasks/Deadline.java
+++ src/main/java/duke/Tasks/Deadline.java
@@ -2,9 +2,8 @@ package duke.Tasks;
 
 public class Deadline extends Task {
     protected String byDate;
-    protected String taskType = "D";
 
-    public Deadline() {};
+    public Deadline() {}
 
     public Deadline(String description, String by) {
         super(description);
@@ -13,10 +12,6 @@ public class Deadline extends Task {
 
     public String getByDate() {
         return byDate;
-    }
-
-    public String getTaskType() {
-        return taskType;
     }
 
     public String printDescription() {
