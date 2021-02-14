92489975d9bc1bb364036d8474b8c739d0d048ac
iamakilahamed <akilahamed123@gmail.com>
2021-02-14 13:10:29 +0800

    Read jarvis.txt

--- src/main/java/jarvis/Duke.java
+++ src/main/java/jarvis/Duke.java
@@ -1,11 +1,21 @@
 package jarvis;
 
 import jarvis.exception.InvalidCommandException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) throws InterruptedException {
         Jarvis jarvis = new Jarvis();
         jarvis.startJarvis();
+
+        try {
+            printFileContents();
+        } catch (FileNotFoundException exception) {
+            System.out.println("File not found");
+        }
+
         while (true) {
             try {
                 jarvis.performTask();
@@ -13,6 +23,14 @@ public class Duke {
                 System.out.println("\tSorry, sir. I do not recognise this command.");
                 jarvis.printDivider();
             }
+        }
+    }
+
+    private static void printFileContents() throws FileNotFoundException {
+        File file = new File("data/jarvis.txt");
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNext()) {
+            System.out.println(scanner.nextLine());
         }
     }
 }
