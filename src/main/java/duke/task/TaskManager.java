9166f4e8f4ac6f98be093d7aee10ab11733ec03e
SimJJ96 <simjingjie13@gmail.com>
2021-02-15 15:51:14 +0800

    Add deleteTask method in TaskManager.java

--- src/main/java/duke/task/TaskManager.java
+++ src/main/java/duke/task/TaskManager.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 public class TaskManager {
     private final List<Task> taskList = new ArrayList<>();
-    private static final String BORDER = "\t____________________________________________________________\n";
+    private static final String BORDER = "\t________________________________________________________________\n";
 
     private static final int ARRAY_INDEX_FOR_DESCRIPTION = 0;
     private static final int ARRAY_INDEX_FOR_DATE = 1;
+    private static final int EXTRA_INDEX_FOR_ARRAY = 1;
 
     public void addToDo (String description) {
         Task task = new ToDo(description);
@@ -47,20 +48,30 @@ public class TaskManager {
     }
 
     public void listTasks() {
-        System.out.print(BORDER + "\tHere are the task in your list:\n");
+        System.out.print(BORDER + "\t Here are the task in your list:\n");
         for (int i = 0; i < taskList.size(); ++i) {
             int indexOfList = i+1;
-            System.out.print("\t" + indexOfList + "." + taskList.get(i) + "\n");
+            System.out.print("\t " + indexOfList + "." + taskList.get(i) + "\n");
         }
         System.out.print(BORDER);
     }
 
-    public void markAsDone(int taskNumber) {
+    public void markAsDone(String description) {
+        int taskNumber = Integer.parseInt(description)-EXTRA_INDEX_FOR_ARRAY;
         taskList.get(taskNumber).setAsDone();
         System.out.print(BORDER
                 + "\t Nice! I've marked this as done:\n"
-                + "\t [" + taskList.get(taskNumber).getStatusIcon() + "] "
-                + taskList.get(taskNumber).getDescription() + "\n"
+                + "\t   " + taskList.get(taskNumber).toString() + "\n"
+                + BORDER);
+    }
+
+    public void deleteTask(String description) {
+        int taskNumber = Integer.parseInt(description)-EXTRA_INDEX_FOR_ARRAY;
+        System.out.print(BORDER
+                + "\t Noted! I've removed this task:\n"
+                + "\t   " + taskList.get(taskNumber).toString() + "\n");
+        taskList.remove(taskNumber);
+        System.out.print("\t Now you have " + taskList.size() + " tasks in the list.\n"
                 + BORDER);
     }
 }
