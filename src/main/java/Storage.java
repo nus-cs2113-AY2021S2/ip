f058dd93717d0f34c38ba0a5fe197c44c7875eee
LAPTOP-VRGPN0FU\MingS <e0425961@u.nus.edu>
2021-02-15 19:49:32 +0800

    push to branch-level07

---
+++ src/main/java/Storage.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Storage {
+    protected String fileName;
+
+    public Storage(String fileName){
+        this.fileName = fileName;
+    }
+
+    public void loadFile(){
+
+    }
+
+    public void writeToFile(){
+
+    }
+
+    public void createFile(){
+
+    }
+
+
+}
