fdc4314fe4627c2f04e316c9cf0e146d7cea338c
JethroPhuah <jethrophuah@gmail.com>
2021-02-15 19:27:41 +0800

    Update

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -57,6 +57,7 @@ public class Duke {
             strings = dc.processInput(in);
 
         } while (!in.equals("bye"));
+
         System.out.println("--------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("--------------------------------------------");
