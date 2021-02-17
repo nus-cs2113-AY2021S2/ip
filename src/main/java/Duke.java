ecf8fa6be186e00a7bef68ca4c285d80c8549928
Rye98 <zhong.shuhao@icloud.com>
2021-02-17 18:43:00 +0800

    Added deleteTask method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,7 +16,7 @@ public class Duke {
                 + "\t" + SECTION_DIVIDER;
         System.out.println(sign_off);
     }
-    private static final String COMMAND_WORDS = "list" + "done" + "todo" + "deadline" + "event";
+    private static final String COMMAND_WORDS = "list" + "done" + "todo" + "deadline" + "event" + "delete";
 
     static ArrayList<Task> tasks = new ArrayList<>();
 
@@ -55,12 +55,18 @@ public class Duke {
 
     public static void markTaskAsDone(String taskDescription){
         // Marks one of the tasks as done.
-        System.out.println("\t Nice! I've marked this task as done: ");
         String[] input = taskDescription.split(" ");        // input = ["Done", taskIndex]
         int taskIndex = Integer.parseInt(input[1]);
-        tasks.get(taskIndex - 1).markAsDone();
-        String output = String.format("%02d. [%s] %s", taskIndex, tasks.get(taskIndex - 1).getStatusIcon(), tasks.get(taskIndex - 1).getDescription());
-        System.out.println("\t" + output);
+        try {
+            tasks.get(taskIndex - 1).markAsDone();
+            System.out.println("\t Nice! I've marked this task as done: ");
+            String output = String.format("%02d. [%s] %s", taskIndex, tasks.get(taskIndex - 1).getStatusIcon(), tasks.get(taskIndex - 1).getDescription());
+            System.out.println("\t" + output);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("\t Invalid task number!");
+        } catch (NumberFormatException e) {
+            System.out.println("\t Please enter the task number instead!");
+        }
     }
 
     public static void listTasks(){
@@ -71,6 +77,22 @@ public class Duke {
             String output = String.format("%02d. %s", i+1, t);
             System.out.println("\t" + output);
             i++;
+        }
+    }
+
+    public static void deleteTask(String taskDescription){
+        // Deletes one of the tasks
+        String[] input = taskDescription.split(" ");        // input = ["Done", taskIndex]
+        int taskIndex = Integer.parseInt(input[1]);
+        try {
+            String taskInformation = tasks.get(taskIndex-1).toString();
+            tasks.remove(taskIndex - 1);
+            System.out.println("\t Noted. I have removed this task. ");
+            System.out.println("\t" + taskInformation);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("\t Invalid task number!");
+        } catch (NumberFormatException e) {
+            System.out.println("\t Please enter the task number instead!");
         }
     }
 
@@ -103,6 +125,9 @@ public class Duke {
             listTasks();
         } else if (line.toLowerCase().contains("done")){
             markTaskAsDone(line);
+        } else if (line.toLowerCase().contains("delete")) {
+            // Insert code for deleting task
+            deleteTask(line);
         } else {
             try {
                 addTask(line);
