16de212bae9e3ef7e39308468d4f3875ef8211c3
geezzzyyy <zhuoyange@gmail.com>
2021-02-17 20:40:00 +0800

    add save function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,28 @@
 import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Duke {
+
+    private static void printFileContents(String filePath) throws FileNotFoundException {
+        File f = new File(filePath); // create a File for the given file path
+        Scanner s = new Scanner(f); // create a Scanner using the File as the source
+        while (s.hasNext()) {
+            System.out.println(s.nextLine());
+        }
+    }
+
+    private static void writeToFile(String filePath, Task[] tasks, int i) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        //System.out.println(tasks.length);
+        for (int j=0; j<=i; j++) {
+            fw.write(tasks[j].toString()+"\n");
+        }
+        //fw.write(textToAdd);
+        fw.close();
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -11,6 +34,14 @@ public class Duke {
                         "Hello! I'm Duke\n"+
                         "What can I do for you?\n"+
                         "____________________________________________________________");
+
+        try {
+            printFileContents("data/duke.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("task list doesn't exist yet");
+            createDirAndFile();
+        }
+
         Scanner sc = new Scanner(System.in);
         int i=0;
         Task[] tasks = new Task[100];
@@ -55,6 +86,23 @@ public class Duke {
 
     }
 
+    private static void createDirAndFile() {
+        try {
+            System.out.println("Creating file: data/duke.txt");
+            File newDir = new File("data");
+            newDir.mkdir();
+            File f = new File("data/duke.txt");
+            if (f.createNewFile()) {
+                System.out.println("File created: " + f.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
     private static void deadlineTasks(int i, Task[] tasks, String command) {
         try {
             String description = command.substring(8, command.indexOf("/by"));
@@ -63,8 +111,11 @@ public class Duke {
             System.out.println("Got it. I've added this task:");
             System.out.println(tasks[i]);
             System.out.println("Now you have "+ Task.getTaskCount() +" task(s) in the list.");
+            writeToFile("data/duke.txt", tasks, i);
         } catch (StringIndexOutOfBoundsException e) {
             System.out.println("OOPS!!! The description of a deadline must include /by and description.");
+        }catch (IOException e) {
+            System.out.println("Something went wrong when writing: " + e.getMessage());
         }
     }
 
@@ -76,8 +127,11 @@ public class Duke {
             System.out.println("Got it. I've added this task:");
             System.out.println(tasks[i]);
             System.out.println("Now you have "+ Task.getTaskCount() +" task(s) in the list.");
+            writeToFile("data/duke.txt", tasks, i);
         } catch (StringIndexOutOfBoundsException e) {
             System.out.println("OOPS!!! The description of a event must include /at and description.");
+        }catch (IOException e) {
+            System.out.println("Something went wrong when writing: " + e.getMessage());
         }
     }
 
@@ -87,8 +141,11 @@ public class Duke {
             System.out.println("Got it. I've added this task:");
             System.out.println(tasks[i]);
             System.out.println("Now you have "+ Task.getTaskCount() +" task(s) in the list.");
+            writeToFile("data/duke.txt", tasks, i);
         } catch (StringIndexOutOfBoundsException e) {
             System.out.println("OOPS!!! The description of a todo cannot be empty.");
+        }catch (IOException e) {
+            System.out.println("Something went wrong when writing: " + e.getMessage());
         }
     }
 
@@ -97,6 +154,7 @@ public class Duke {
             int doneNumber=Integer.parseInt(command.substring(5));
             System.out.println("Nice! I've marked this task as done: ");
             tasks[doneNumber-1].markAsDone();
+            //writeToFile("data/duke.txt", tasks);
         } catch (NumberFormatException e) {
             System.out.println("OOPS!!! A number is expected after command done");
         }catch (ArrayIndexOutOfBoundsException e) {
