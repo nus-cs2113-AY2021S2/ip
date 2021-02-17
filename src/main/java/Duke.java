69ad356c42376873872468517ad150afa1bf1b9b
geezzzyyy <zhuoyange@gmail.com>
2021-02-17 19:18:10 +0800

    introduce Arraylist

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.lang.reflect.Array;
 import java.util.*;
 public class Duke {
+    private static ArrayList<Task> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -13,39 +16,43 @@ public class Duke {
                         "____________________________________________________________");
         Scanner sc = new Scanner(System.in);
         int i=0;
-        Task[] tasks = new Task[100];
+//        Task[] tasks = new Task[100];
+
         while(true){
             String command = sc.nextLine();
-            if(command.equals("bye")){
+            if (command.equals("bye")) {
                 System.out.println("____________________________________________________________\n"+
                                     "Bye. Hope to see you again soon!\n"+
                                     "____________________________________________________________");
                 break;
-            }else if(command.equals("list")){
+            }else if (command.equals("list")) {
                 System.out.println("____________________________________________________________");
-                listTasks(i, tasks);
+                listTasks();
                 System.out.println("____________________________________________________________");
-            }else if(command.contains("done")){
-                doneTasks(tasks, command);
+            }else if (command.contains("done")) {
+                doneTasks(command);
                 System.out.println("____________________________________________________________");
-            } else if(command.contains("todo")){
+            } else if (command.contains("todo")) {
                 System.out.println("____________________________________________________________");
-                todoTask(i, tasks, command);
-                System.out.println("____________________________________________________________");
-                i++;
-            }else if(command.contains("deadline")) {
-                System.out.println("____________________________________________________________");
-                //Deadline t = new Deadline(8,command.indexOf("/by"));
-                deadlineTasks(i, tasks, command);
+                todoTask(command);
                 System.out.println("____________________________________________________________");
                 i++;
-            }else if(command.contains("event")) {
+            }else if (command.contains("deadline")) {
                 System.out.println("____________________________________________________________");
-                //Deadline t = new Deadline(8,command.indexOf("/by"));
-                eventTask(i, tasks, command);
+                deadlineTasks(command);
                 System.out.println("____________________________________________________________");
                 i++;
-            }else{
+            }else if (command.contains("event")) {
+                System.out.println("____________________________________________________________");
+                eventTask(command);
+                System.out.println("____________________________________________________________");
+                i++;
+            }else if (command.contains("delete")) {
+                System.out.println("____________________________________________________________");
+                //eventTask(i, tasks, command);
+                System.out.println("____________________________________________________________");
+
+            } else {
                 System.out.println("____________________________________________________________");
                 System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
                 System.out.println("____________________________________________________________");
@@ -55,48 +62,51 @@ public class Duke {
 
     }
 
-    private static void deadlineTasks(int i, Task[] tasks, String command) {
+    private static void deadlineTasks(String command) {
         try {
-            String description = command.substring(8, command.indexOf("/by"));
+            String description = command.substring(9, command.indexOf("/by"));
             String by = command.substring(command.indexOf("/by") + 4);
-            tasks[i] = new Deadline(description, by);
+            Task task = new Deadline(description, by);
+            tasks.add(task);
             System.out.println("Got it. I've added this task:");
-            System.out.println(tasks[i]);
+            System.out.println(task);
             System.out.println("Now you have "+ Task.getTaskCount() +" task(s) in the list.");
         } catch (StringIndexOutOfBoundsException e) {
             System.out.println("OOPS!!! The description of a deadline must include /by and description.");
         }
     }
 
-    private static void eventTask(int i, Task[] tasks, String command) {
+    private static void eventTask(String command) {
         try {
             String description = command.substring(6, command.indexOf("/at"));
             String at = command.substring(command.indexOf("/at") + 4);
-            tasks[i] = new Event(description, at);
+            Task task = new Event(description, at);
+            tasks.add(task);
             System.out.println("Got it. I've added this task:");
-            System.out.println(tasks[i]);
+            System.out.println(task);
             System.out.println("Now you have "+ Task.getTaskCount() +" task(s) in the list.");
         } catch (StringIndexOutOfBoundsException e) {
             System.out.println("OOPS!!! The description of a event must include /at and description.");
         }
     }
 
-    private static void todoTask(int i, Task[] tasks, String command) {
+    private static void todoTask(String command) {
         try {
-            tasks[i] = new Todo(command.substring(5));
+            Task task = new Todo(command.substring(5));
+            tasks.add(task);
             System.out.println("Got it. I've added this task:");
-            System.out.println(tasks[i]);
+            System.out.println(task);
             System.out.println("Now you have "+ Task.getTaskCount() +" task(s) in the list.");
         } catch (StringIndexOutOfBoundsException e) {
             System.out.println("OOPS!!! The description of a todo cannot be empty.");
         }
     }
 
-    private static void doneTasks(Task[] tasks, String command) {
+    private static void doneTasks(String command) {
         try {
             int doneNumber=Integer.parseInt(command.substring(5));
             System.out.println("Nice! I've marked this task as done: ");
-            tasks[doneNumber-1].markAsDone();
+            tasks.get(doneNumber-1).markAsDone();
         } catch (NumberFormatException e) {
             System.out.println("OOPS!!! A number is expected after command done");
         }catch (ArrayIndexOutOfBoundsException e) {
@@ -104,11 +114,11 @@ public class Duke {
         }
     }
 
-    private static void listTasks(int i, Task[] tasks) {
+    private static void listTasks() {
         System.out.println("Here are the tasks in your list:");
-        for(int index = 0; index< i; index++){
-            System.out.print(index+1+". ");
-            System.out.println(tasks[index]);
+        for(Task t: tasks){
+            System.out.print(tasks.indexOf(t)+1+". ");
+            System.out.println(t);
         }
     }
 }
