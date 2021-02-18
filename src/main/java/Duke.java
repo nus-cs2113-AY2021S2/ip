068f0bd9c8267b452ff7f40ee4cec3489d24230c
Chiamjiaen <e0426220@u.nus.edu>
2021-02-18 12:09:37 +0800

    add parser and error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,57 +1,28 @@
-import java.util.Scanner;
-
 public class Duke {
-    static Scanner scanner = new Scanner(System.in);
-    static String LINEBREAK = "____________________________________________________________\n";
 
-    public static void printLine() {
-        System.out.print(LINEBREAK);
-    }
+    static String LINEBREAK = "____________________________________________________________\n";
+    static String LOGO = "Hello from\n"
+            + " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    static String GREETING =  " Hello! I'm Duke\n"
+            + " What can I do for you?\n";
+    static String GOODBYE = "Bye, Hope to see you again soon!\n";
 
     public static void main(String[] args) {
         printWelcome();
-
-        String text = scanner.nextLine();
-        List list = new List();
-
-        while (!text.equals("bye")) {
-            String[] stringTokens = text.split(" ", 2);
-            printLine();
-            switch (stringTokens[0]) {
-                case "list":
-                    list.printList();
-                    break;
-                case "done":
-                    list.markDone(Integer.parseInt(stringTokens[1]));
-                    break;
-                case "todo":
-                    list.addTask(stringTokens[1]);
-                    break;
-                case "deadline":
-                    list.addDeadline(stringTokens[1]);
-                    break;
-                case "event":
-                    list.addEvent(stringTokens[1]);
-                    break;
-                default:
-                    System.out.println("no such command!");
-            }
-            printLine();
-            text = scanner.nextLine();
-        }
-        System.out.println(LINEBREAK + "Bye, Hope to see you again soon!\n" + LINEBREAK);
+        Parser.newInstance();
+        printGoodbye();
     }
 
     public static void printWelcome() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println(LINEBREAK +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                LINEBREAK);
+        System.out.println(LOGO);
+        System.out.println(LINEBREAK + GREETING + LINEBREAK);
+    }
+
+    public static void printGoodbye() {
+        System.out.println(LINEBREAK + GOODBYE + LINEBREAK);
     }
 }
