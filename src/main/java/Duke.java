2ad019adabb3e2af73c55b2ec01152b38b7976e4
cswbibibi <caoshuwen1017@gmail.com>
2021-02-18 11:47:11 +0800

    Exception, CodingStandard commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,140 +16,107 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Here are the things that i can do: ");
-        System.out.println("1. Echo");
-        System.out.println("2. Add task to your list & Mark task as done");
-        System.out.println("-1: Quit Duke");
+        System.out.println("Add task to your list - todo, deadline and event tasks");
+        System.out.println("list: View all tasks in the list");
+        System.out.println("done: Done task and delete: Delete task");
+        System.out.println("bye: quite Duke");
 
-        Scanner in = new Scanner(System.in);
         String filePath = "C:/Users/XPS/Desktop/Uni drives me crazy/Y2S2/NUS exchange/CS2113 Software Engineering/ip/duke.txt";
-        boolean run = true;
-        while (run) {
-            int choice = in.nextInt();
-            switch (choice) {
-            case 1:
-                System.out.println("Please input something and I will repeat it back to you");
-                Scanner a = new Scanner(System.in);
-                String sentence = a.nextLine();
-                while (true) {
-                    if (sentence.equalsIgnoreCase("bye")) {
-                        break;
-                    } else {
-                        System.out.println(sentence);
-                        sentence = a.nextLine();
-                    }
-                }
-                System.out.println("Thank you for using echo, " +
-                        "you can now choose other function :)");
-                break;
-            case 2:
-                System.out.println("Please enter the task you want to add in to the task list.");
-                Task[] tasks = new Task[MAX_NO_OF_TASKS];
-                int index = 0;
-                String task;
-                Scanner b = new Scanner(System.in);
-                task = b.nextLine();
-                while (true) {
-                    if (task.equalsIgnoreCase("bye")) {
-                        break;
-                    } else if (task.equals("list")) {
-                        for (int i = 0; i < index; i++) {
-                            System.out.println((i + 1) + "." + tasks[i].toString());
-                        }
-                    } else if (task.contains("done")) {
-                        try {
-                            String taskNo = task.substring(task.length() - 1);
-                            int new_taskNo = Integer.parseInt(taskNo);
-                            tasks[new_taskNo - 1].setTaskStatus(true);
-                            System.out.println("Nice! I've marked this task as done:");
-                            System.out.println(tasks[new_taskNo - 1].toString());
-                            replaceTXT(tasks[new_taskNo - 1].getDescription());
-                        } catch (StringIndexOutOfBoundsException e) {
-                            System.out.println(":( OOps!! The number of task to be done" +
-                                    "cannot be empty");
-                        } catch (IOException e) {
-                            //handle exception
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    } else if (task.contains("todo")) {
-                        try {
-                            String taskName = task.substring(5);
-                            tasks[index] = new Todo(taskName, 'T');
-                            index = addTaskMessage(index, tasks[index]);
-                            String textToAppend = "T | 0 | " + taskName;
-                            appendToFile(filePath, textToAppend);
-                        } catch (StringIndexOutOfBoundsException e) {
-                            System.out.println(":( OOps!! The description of a" +
-                                    "todo cannot be empty");
-                        } catch (FileNotFoundException e) {
-                            System.out.println(":( OOps!! The file you are looking for does not exist.");
-                        } catch (IOException e) {
-                            System.out.println(":( OOps!! Something went wrong: " + e.getMessage());
-                        }
-                    } else if (task.contains("deadline")) {
-                        try {
-                            int slash_sign = task.indexOf("/");
-                            String ddl_taskName = task.substring(9, slash_sign - 1);
-                            String by = task.substring(slash_sign + 3);
-                            tasks[index] = new Deadline(ddl_taskName, 'D', by);
-                            index = addTaskMessage(index, tasks[index]);
-                            String textToAppend = "D | 0 | " + ddl_taskName + " | " + by;
-                            appendToFile(filePath, textToAppend);
-                        } catch (StringIndexOutOfBoundsException e) {
-                            System.out.println(":( OOps!! The description of a" +
-                                    "deadline cannot be empty");
-                        } catch (FileNotFoundException e) {
-                            System.out.println(":( OOps!! The file you are looking for does not exist.");
-                        } catch (IOException e) {
-                            System.out.println(":( OOps!! Something went wrong: " + e.getMessage());
-                        }
-                    } else if (task.contains("event")) {
-                        try {
-                            int slash_sign = task.indexOf("/");
-                            String event_taskName = task.substring(6, slash_sign - 1);
-                            String at = task.substring(slash_sign + 3);
-                            tasks[index] = new Event(event_taskName, 'E', at);
-                            index = addTaskMessage(index, tasks[index]);
-                            String textToAppend = "E | 0 | " + event_taskName + " | " + at;
-                            appendToFile(filePath, textToAppend);
-                        } catch (StringIndexOutOfBoundsException e) {
-                            System.out.println(":( OOps!! The description of a" +
-                                    "event cannot be empty");
-                        } catch (FileNotFoundException e) {
-                            System.out.println(":( OOps!! The file you are looking for does not exist.");
-                        } catch (IOException e) {
-                            System.out.println(":( OOps!! Something went wrong: " + e.getMessage());
-                        }
-                    } else if (task.contains("delete")) {
-                        int delete_index = Integer.parseInt(task.substring(7));
-                        Task delete_task = tasks[delete_index];
-                        removeTaskMessage(delete_task);
-                        index -= 1;
-                        System.out.println("Now you have " + index + " tasks in the list.");
-                    } else {
-                        System.out.println(":( OOps!! I'm sorry, " +
-                                "but i don't know what that means...");
-                    }
-                    /*
-                    else{
-                        //add tasks with no specific task type
-                        tasks[index] = new Task(task);
-                        index = addTaskMessage(index, tasks[index]);
-                    }
-                    */
-                    task = b.nextLine();
-                }
+        System.out.println("Please enter the task you want to add in to the task list.");
+        Task[] tasks = new Task[MAX_NO_OF_TASKS];
+        int index = 0;
+        String task;
+        Scanner b = new Scanner(System.in);
+        task = b.nextLine();
+        while (true) {
+            if (task.equalsIgnoreCase("bye")) {
                 System.out.println("Thank you for using Tasks function, " +
                         "you can now choose other function :)");
                 break;
-            case (-1):
-                System.out.println("Goodbye, hope to see you again!");
-                run = false;
-                break;
+            } else if (task.equals("list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.println((i + 1) + "." + tasks[i].toString());
+                }
+            } else if (task.contains("done")) {
+                try {
+                    String taskNo = task.substring(5);
+                    int new_taskNo = Integer.parseInt(taskNo);
+                    tasks[new_taskNo - 1].setTaskStatus(true);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(tasks[new_taskNo - 1].toString());
+                    replaceTXT(tasks[new_taskNo - 1].getDescription());
+                } catch (StringIndexOutOfBoundsException e) {
+                    System.out.println(":( OOps!! The number of task to be done" +
+                            "cannot be empty");
+                } catch (IOException e) {
+                    //handle exception
+                }
+            } else if (task.contains("todo")) {
+                try {
+                    String taskName = task.substring(5);
+                    tasks[index] = new Todo(taskName, 'T');
+                    index = addTaskMessage(index, tasks[index]);
+                    String textToAppend = "T | 0 | " + taskName;
+                    appendToFile(filePath, textToAppend);
+                } catch (StringIndexOutOfBoundsException e) {
+                    System.out.println(":( OOps!! The description of a" +
+                            "todo cannot be empty");
+                } catch (FileNotFoundException e) {
+                    System.out.println(":( OOps!! The file you are looking for does not exist.");
+                } catch (IOException e) {
+                    System.out.println(":( OOps!! Something went wrong: " + e.getMessage());
+                }
+            } else if (task.contains("deadline")) {
+                try {
+                    int slash_sign = task.indexOf("/");
+                    String ddl_taskName = task.substring(9, slash_sign - 1);
+                    String by = task.substring(slash_sign + 3);
+                    tasks[index] = new Deadline(ddl_taskName, 'D', by);
+                    index = addTaskMessage(index, tasks[index]);
+                    String textToAppend = "D | 0 | " + ddl_taskName + " | " + by;
+                    appendToFile(filePath, textToAppend);
+                } catch (StringIndexOutOfBoundsException e) {
+                    System.out.println(":( OOps!! The description of a" +
+                            "deadline cannot be empty");
+                } catch (FileNotFoundException e) {
+                    System.out.println(":( OOps!! The file you are looking for does not exist.");
+                } catch (IOException e) {
+                    System.out.println(":( OOps!! Something went wrong: " + e.getMessage());
+                }
+            } else if (task.contains("event")) {
+                try {
+                    int slash_sign = task.indexOf("/");
+                    String event_taskName = task.substring(6, slash_sign - 1);
+                    String at = task.substring(slash_sign + 3);
+                    tasks[index] = new Event(event_taskName, 'E', at);
+                    index = addTaskMessage(index, tasks[index]);
+                    String textToAppend = "E | 0 | " + event_taskName + " | " + at;
+                    appendToFile(filePath, textToAppend);
+                } catch (StringIndexOutOfBoundsException e) {
+                    System.out.println(":( OOps!! The description of a" +
+                            "event cannot be empty");
+                } catch (FileNotFoundException e) {
+                    System.out.println(":( OOps!! The file you are looking for does not exist.");
+                } catch (IOException e) {
+                    System.out.println(":( OOps!! Something went wrong: " + e.getMessage());
+                }
+            } else if (task.contains("delete")) {
+                try {
+                    int delete_index = Integer.parseInt(task.substring(7));
+                    Task delete_task = tasks[delete_index - 1];
+                    removeTaskMessage(delete_task);
+                    index -= 1;
+                    System.out.println("Now you have " + index + " tasks in the list.");
+                }  catch (StringIndexOutOfBoundsException e) {
+                    System.out.println(":( OOps!! The description of a" +
+                            "todo cannot be empty"); }
+            } else {
+                System.out.println(":( OOps!! I'm sorry, " +
+                        "but i don't know what that means...");
             }
+            task = b.nextLine();
         }
     }
-
     private static void removeTaskMessage(Task delete_task) {
         System.out.println("Noted. I've removed this task: ");
         System.out.println("  " + delete_task.toString());
@@ -163,7 +130,7 @@ public class Duke {
         return index;
     }
 
-    private static void appendToFile(String filePath, String textToAppend) throws IOException, FileNotFoundException {
+    private static void appendToFile(String filePath, String textToAppend) throws IOException {
         FileWriter fw = new FileWriter(filePath, true);
         fw.write(textToAppend + "\n");
         fw.close();
@@ -173,7 +140,6 @@ public class Duke {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String line = "";
         String oldtext = "";
-        String goal = null;
         while ((line = reader.readLine()) != null) {
             oldtext += line + "\r\n";
         }
