ca0ddacbe55a5cc4a4881e9281e9eaefa376bafd
Chenling CUI <cuichenling2@gmail.com>
2021-02-18 14:30:46 +0800

    Exception-handling revised feb 18

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,11 +4,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Duke {
-    //public static String[] to_do_list= new String[100];
-    //public static Task finish_goal = new Task("finish goal in list");
+    public static int num_of_goals = 0;
+
+    public static Scanner in = new Scanner(System.in);
+    public static String user_input;
     public static Task t[] = new Task[100];
+    public static final int DONE_START = 5;
+    public static final int TODO_START = 5;
+    public static final int EVENT_START = 6;
+    public static final int DEADLINE_START = 9;
+
 
     public static void show_welcome_msg() {
         String logo = " ____        _        \n"
@@ -18,225 +26,267 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________\n" +
+
                 " Hello! I'm Duke\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n");
     }
 
-    public static void show_list(int num) {
-        System.out.println(" ____________________________________________________________\n" +
-                "     Here are the tasks in your list:");
-        for (int i = 0; i < num; i++) {
-            System.out.println((i + 1) + ". " + "[" + t[i].getSymbol() + "]" + "[" + t[i].getStatusIcon() + "] " + t[i].getDescription());
-        }
+    public static void show_exit_msg() {
+        System.out.println("Bye. Hope to see you again soon!\n");
     }
 
-    public static String userDone(String user_done) {
-        switch (user_done) {
-            case "todo":
-                return "T";
-            case "event":
-                return "E";
-            case "deadline":
-                return "D";
-            default:
-                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                break;
-        }
-
-        return "";
-
+    private static String getString(Scanner in) {
+        String user_input;
+        user_input = in.nextLine();
+        return user_input;
 
     }
 
-    public static boolean noInput(String input) {
-        if (input.equals("")) {
-            System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+
+    private static boolean isList() {
+        if (user_input.equals("list")) {
             return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean isTodo() {
+        if (user_input.length() > 4) {
+            if (user_input.substring(0, 4).equals("todo")) {
+                return true;
+            }
         }
         return false;
-
-
     }
 
-    public static void printTask(String taskKind, String substring, int num_of_goals, String ddl, String prep) {
-        System.out.println("Got it. I've added this task: ");
-        if (ddl == null) {
-            System.out.println("[" + taskKind + "]" + "[" + " " + "]" + " " + substring);
+    private static boolean isDone() {
+        if (user_input.length() > 4) {
+            if (user_input.substring(0, 4).equals("done")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isEvent() {
+        if (user_input.length() > 5) {
+            if (user_input.substring(0, 5).equals("event")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isDeadline() {
+        if (user_input.length() > 8) {
+            if (user_input.substring(0, 8).equals("deadline")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isBye() {
+        if (user_input.equals("bye")) {
+            return true;
         } else {
-            System.out.println("[" + taskKind + "]" + "[" + " " + "]" + " " + substring + "(" + prep + ":" + ddl + ")");
-        }
-
-        System.out.println("Now you have " + num_of_goals + " task in your list!");
-    }
-
-    public static int totalNum() {
-        int count = 0;
-        for (int i = 0; i < t.length; i++) {
-            if (t[i] != null) {
-                count = count + 1;
-            }
-
-        }
-        return count;
-    }
-
-    public static void delete(int index) {
-        System.out.println("Noted. I've removed this task: ");
-        System.out.println("[" + t[index].symbol + "]" + "[" + " " + "]" + " " + t[index].description + "(" + t[index].preposition + ":" + t[index].ddl + ")");
-        for (int i = index; i < t.length - 1; i++) {
-            t[i] = t[i + 1];
-        }
-        System.out.println("Now you have " + totalNum() + " task in your list!");
-    }
-
-    private static void printFileContents(String filePath) throws FileNotFoundException {
-        File f = new File(filePath); // create a File for the given file path
-        Scanner s = new Scanner(f); // create a Scanner using the File as the source
-        while (s.hasNext()) {
-            System.out.println(s.nextLine());
+            return false;
         }
     }
 
-    public static void loadFile() {
-        try {
-            printFileContents("/Users/chenlingcui/Desktop/CS2113/duke.txt");
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
+    private static void repeatTaskAdded() {
+        System.out.println(t[num_of_goals]);
+        System.out.println("Now you have" + num_of_goals + " tasks in the list.");
+    }
+
+    private static int getDeadlineIndex(String input) {
+        int index = user_input.indexOf('/');
+        return index;
+    }
+
+    private static String getDeadline(String user_input) {
+        int timeIndex = getDeadlineIndex(user_input);
+        String deadline = user_input.substring(timeIndex + 3, user_input.length());
+        return deadline;
+    }
+
+    private static void plusNumOfGoals() {
+        num_of_goals++;
+    }
+
+    private static void doneMessage(int taskIndex) {
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println("[" + t[taskIndex].getStatusIcon() + "] " + t[taskIndex].getDescription() + "\n");
+    }
+
+    private static void listTaskMsg() {
+        System.out.println("Here are the tasks in your list:");
+    }
+
+    private static void enumerateTasks() {
+        for (int i = 0; i < num_of_goals; i++) {
+            System.out.println((i + 1) + "." + t[i + 1].toString());
         }
     }
 
-    //private static void appendToFile(String filePath, String textToAppend) throws IOException {
-    //  FileWriter fw = new FileWriter(filePath, true); // create a FileWriter in append mode
-    //fw.write(textToAppend);
-    //fw.close();
-    //}
-
-    private static void writeToFile(String filePath, String textToAdd) throws IOException {
-        FileWriter fw = new FileWriter(filePath);
-        fw.write(textToAdd);
-        fw.close();
+    private static int getDoneTaskIndex(String user_input) {
+        int taskIndex = Integer.parseInt(user_input.substring(DONE_START, user_input.length()));
+        return taskIndex;
     }
 
-    private static void appendToFile(String filePath, String textToAppend) throws IOException {
-        FileWriter fw = new FileWriter(filePath, true); // create a FileWriter in append mode
-        fw.write(textToAppend);
-        fw.close();
+    private static void markAsDone(String user_input) {
+        int doneTaskIndex = getDoneTaskIndex(user_input);
+        t[doneTaskIndex].markAsDone();
+        doneMessage(doneTaskIndex);
     }
 
-    public static void changeFile(int num) {
+    private static void addNewTask(String user_input) {
+        t[num_of_goals] = new Task(user_input);
+    }
 
-        try {
-            writeToFile("/Users/chenlingcui/Desktop/CS2113/duke.txt", "[" + t[0].getSymbol() + "]" + "[" + t[0].getStatusIcon() + "] " + t[0].getDescription() + "(" + t[0].getPrep() + ":" + t[0].getDdl() + ")");
-            appendToFile("/Users/chenlingcui/Desktop/CS2113/duke.txt", "\n");
-        } catch (IOException e) {
-            System.out.println("Something went wrong: " + e.getMessage());
+    private static void addNewDeadline(String user_input) {
+        t[num_of_goals] = new Deadline(user_input.substring(DEADLINE_START, getDeadlineIndex(user_input)), getDeadline(user_input));
+    }
+
+    private static void addNewEvent(String user_input) {
+        t[num_of_goals] = new Event(user_input.substring(EVENT_START, getDeadlineIndex(user_input)), getDeadline(user_input));
+    }
+
+    private static void addNewTodo(String user_input) {
+        t[num_of_goals] = new Todo(user_input.substring(TODO_START, user_input.length()));
+    }
+
+    private static boolean emptyInput(String user_input, Command command) {
+        switch (command) {
+            case TODO:
+                return (user_input.substring(TODO_START, user_input.length()).strip().equals(""));
+            case EVENT:
+                return user_input.substring(EVENT_START, getDeadlineIndex(user_input)).strip().equals("");
+            case DEADLINE:
+                return (user_input.substring(DEADLINE_START, getDeadlineIndex(user_input)).strip().equals(""));
         }
-        for (int i = 1; i < num; i++) {
-            try {
-                appendToFile("/Users/chenlingcui/Desktop/CS2113/duke.txt", "[" + t[i].getSymbol() + "]" + "[" + t[i].getStatusIcon() + "] " + t[i].getDescription() + "(" + t[i].getPrep() + ":" + t[i].getDdl() + ")");
-                appendToFile("/Users/chenlingcui/Desktop/CS2113/duke.txt", "\n");
-            } catch (IOException e) {
-                System.out.println("Something went wrong: " + e.getMessage());
-            }
+        return false;
+    }
+
+    private static boolean validEventTime(String user_input) throws InvalidEventException {
+        if (user_input.substring(getDeadlineIndex(user_input), getDeadlineIndex(user_input) + 3).equals("/at")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean validDeadlineTime(String user_input) throws InvalidDeadlineException {
+        if (user_input.substring(getDeadlineIndex(user_input), getDeadlineIndex(user_input) + 3).equals("/by")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static void checkValidInput(String user_input, Command command) throws EmptyInputException, InvalidEventException, InvalidDeadlineException {
+        if (emptyInput(user_input, command)) {
+            throw new EmptyInputException();
         }
 
+        switch (command) {
+            case EVENT:
+                if (validEventTime(user_input)) {
+                    break;
+                } else {
+                    throw new InvalidEventException();
+                }
+            case DEADLINE:
+                if (validDeadlineTime(user_input)) {
+                    break;
+                } else {
+                    throw new InvalidDeadlineException();
+                }
+        }
     }
+
+    public static void executeCommand(String input, Command c) throws InvalidCommandException, EmptyInputException, InvalidEventException, InvalidDeadlineException {
+        switch (c) {
+            case LIST:
+                listTaskMsg();
+                enumerateTasks();
+                break;
+            case DONE:
+                markAsDone(input);
+                break;
+            case TODO:
+                checkValidInput(input, c);
+                plusNumOfGoals();
+                addNewTodo(input);
+                repeatTaskAdded();
+                break;
+            case EVENT:
+                checkValidInput(input, c);
+                plusNumOfGoals();
+                addNewEvent(input);
+                repeatTaskAdded();
+                break;
+            case DEADLINE:
+                checkValidInput(input, c);
+                plusNumOfGoals();
+                addNewDeadline(input);
+                repeatTaskAdded();
+                break;
+            case INVALID:
+                throw new InvalidCommandException();
+
+        }
+    }
+
 
 
     public static void main(String[] args) {
-        loadFile();
         show_welcome_msg();
-        Scanner sc = new Scanner(System.in);
-        String user_input = sc.nextLine();
-        int num_of_goals = 0;
 
-        while (!user_input.equals("bye")) {
+        user_input = getString(in);
+        Command command;
 
-            if (user_input.equals("list")) {
-                show_list(totalNum());
+        while (!isBye()) {
+            if (isList()) {
+                command = Command.LIST;
+            } else if (isDone()) {
+                command = Command.DONE;
+            } else if (isTodo()) {
+                command = Command.TODO;
+            } else if (isEvent()) {
+                command = Command.EVENT;
+            } else if (isDeadline()) {
+                command = Command.DEADLINE;
             } else {
-                int spacing = user_input.indexOf(" ");
-                int slash = user_input.indexOf("/");
-                //System.out.println(slash);
-                String user_done = user_input.substring(0, spacing);
-                //check whether user has done some tasks
-                if (user_done.equals("done")) {
-                    String number = user_input.substring(spacing + 1);
-                    int number_converted = Integer.parseInt(number);
-                    t[number_converted - 1].markAsDone();
-                    System.out.println("____________________________________________________________\n" +
-                            "     Nice! I've marked this task as done: ");
-                    System.out.println("[" + t[number_converted - 1].getSymbol() + "]" + "[" + t[number_converted - 1].getStatusIcon() + "] " + t[number_converted - 1].getDescription() + "(" + t[number_converted - 1].getPrep() + ":" + t[number_converted - 1].getDdl() + ")");
-                    System.out.println("____________________________________________________________");
-
-                } else if (user_done.equals("delete")) {
-                    String number = user_input.substring(spacing + 1);
-                    int number_converted = Integer.parseInt(number);
-                    delete(number_converted - 1);
-                    num_of_goals = num_of_goals - 1;
-                } else if (slash != -1) {
-
-                    String taskKind = userDone(user_done);
-                    if (!taskKind.equals("")) {
-                        // System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                        //break;
-
-                        String taskName = user_input.substring(spacing + 1, slash);
-
-                        if (!noInput(taskName)) {
-                            String period = user_input.substring(slash + 1);
-                            //System.out.println(period);
-                            String prep = period.substring(0, period.indexOf(" "));
-                            // System.out.println(prep);
-                            String ddl = period.substring(period.indexOf(" ") + 1);
-                            //System.out.println(ddl);
-
-                            t[num_of_goals] = new Task(taskName);
-                            t[num_of_goals].setSymbol(taskKind);
-                            t[num_of_goals].setDdl(ddl);
-                            t[num_of_goals].setPrep(prep);
-                            num_of_goals++;
-
-                            printTask(taskKind, taskName, num_of_goals, ddl, prep);
-                        }
-                    }
-
-
-                } else {
-
-                    String taskKind = userDone(user_done);
-                    if (!taskKind.equals("")) {
-                        //System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                        //break;
-
-                        String taskName = user_input.substring(spacing + 1);
-
-                        if (!noInput(taskName)) {
-                            t[num_of_goals] = new Task(taskName);
-                            t[num_of_goals].setSymbol(taskKind);
-                            //t[num_of_goals].setDdl(ddl);
-                            num_of_goals++;
-
-                            printTask(taskKind, taskName, num_of_goals, null, null);
-                        }
-
-
-                    }
-
-                }
-
+                command = Command.INVALID;
             }
-            changeFile(num_of_goals);
-            user_input = sc.nextLine();
+
+            try {
+                executeCommand(user_input, command);
+            } catch (InvalidCommandException e) {
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+            } catch (EmptyInputException e) {
+                System.out.println("OOPS!!! The description of a new task cannot be empty.");
+            } catch (StringIndexOutOfBoundsException e) {
+                System.out.println("OOPS!!! You need to add time for new Event or Deadline with '/at' or '/by'!!");
+            } catch (InvalidEventException e) {
+                System.out.println("OOPS!!! You need to add time for new Event with keyword '/at'!!");
+            } catch (InvalidDeadlineException e) {
+                System.out.println("OOPS!!! You need to add time for new Deadline with keyword '/by'!!");
+            }
+
+            System.out.println();
+            user_input = getString(in);
         }
 
 
-        System.out.println("____________________________________________________________\n" +
-                "     Bye. Hope to see you again soon!\n" +
-                "    ____________________________________________________________");
+        show_exit_msg();
     }
-
-
 }
+
+
+
+
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,61 +1,27 @@
 public class Task {
     protected String description;
     protected boolean isDone;
-    protected String symbol = null;
-    protected String ddl = null;
-    protected String preposition = null;
-
-
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        System.out.println("Got it. I've added this task: ");
     }
 
-    public void setIsDone_or_not(boolean done) {
-        isDone = done;
+    public String getStatusIcon() {
+        return (isDone ? "Y" : "N");
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
-    }
-
     public void markAsDone() {
         isDone = true;
     }
 
-    public String getSymbol() {
-        return symbol;
+    public String toString() {
+        return ("[" + this.getStatusIcon() + "] " + description);
     }
-
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
-
-    public String getDdl() {
-        return ddl;
-    }
-
-    public void setDdl(String ddl) {
-        this.ddl = ddl;
-    }
-
-    public String getPrep() {
-        return this.preposition;
-    }
-
-    public void setPrep(String prep) {
-        this.preposition = prep;
-    }
-
-
-    //...
 }
+
