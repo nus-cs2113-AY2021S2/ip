9776c4892f30494554b003d6ccd00b7a8024ce84
NoorSarrah <e0424913@u.nus.edu>
2021-02-19 21:25:01 +0800

    Level-6: Delete tasks function & ArrayList

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
-    public static Task[] tasks = new Task[100];
+    //public static Task[] tasks = new Task[100];
     public static int size = 0;
-
-
+    public static ArrayList<Task> tasks = new ArrayList<>();
     public static void main(String[] args) {
+        printWelcomeMessage();
+        Scanner myObj = new Scanner(System.in);
+        String command = myObj.nextLine();
+
+        while (!command.equals("bye")) {
+            // prints a checklist
+            if (command.equals("list")) {
+                printList();
+
+                // marks current task as completed
+            } else if (command.contains("done")) {
+                taskCompleted(command);
+
+                // adds tasks into list
+            } else if (command.contains("delete")) {
+                deleteTasks(command);
+            } else {
+                addTasks(command);
+            }
+            command = myObj.nextLine();
+        }
+        // exit program when input=bye
+        printDash();
+        System.out.println("Bye. Hope to see you again soon!");
+        printDash();
+    }
+
+    public static void printWelcomeMessage() {
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,32 +43,9 @@ public class Duke {
 
         // print welcome message
         System.out.println("Hello from\n" + logo);
-        System.out.println("-".repeat(50));
+        printDash();
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
-        printDash();
-
-        Scanner myObj = new Scanner(System.in);
-        String command = myObj.nextLine();
-
-        while (!command.equals("bye")) {
-            // prints a checklist
-            if (command.equals("list")) {
-                printList();
-            }
-            // marks current task as completed
-            else if (command.contains("done")) {
-                taskCompleted(command);
-            }
-            // adds tasks into list
-            else {
-                addTasks(command);
-            }
-            command = myObj.nextLine();
-        }
-        // exit program when input=bye
-        printDash();
-        System.out.println("Bye. Hope to see you again soon!");
         printDash();
     }
 
@@ -59,33 +65,45 @@ public class Duke {
             description = description.strip();
             runEvent(description);
         }
-        System.out.println(tasks[size++].toString());
-        System.out.println("\tNow you have " + size + " tasks in the list");
+        System.out.println(tasks.get(tasks.size()-1).toString());
+        System.out.println("\tNow you have " + tasks.size() + " tasks in the list");
         printDash();
     }
+    public static void deleteTasks(String command) {
 
-    // mark and display task that is completed
+        command = command.replace("delete", " ");
+        command = command.strip();
+
+        int count = Integer.parseInt(command);
+        --count; // array starts from 0
+        System.out.println("\tNoted. I've removed this task:\n" + tasks.get(count).toString());
+
+        tasks.remove(tasks.get(count));
+        System.out.println("\tNow you have " + tasks.size() + " tasks in the list");
+    }
+
     public static void taskCompleted(String command) {
         // remove done from string
         command = command.replace("done", " ");
         command = command.strip();
-        // convert string 2 into int 2
+
         int count = Integer.parseInt(command);
         --count; // array starts from 0
-        tasks[count].markAsDone();
+        tasks.get(count).markAsDone();
+
         printDash();
-        System.out.println("Nice! I've marked this task as done: ");
-        System.out.println(tasks[count].toString());
+        System.out.println("\tNice! I've marked this task as done: ");
+        System.out.println(tasks.get(count).toString());
         printDash();
     }
 
     // print list as a checklist when command=list
     public static void printList() {
         printDash();
-        if (size != 0) {
+        if (tasks.size() != 0) {
             System.out.println("\tHere are the tasks in your list: ");
-            for (int i = 1; i <= size; ++i) {
-                System.out.println("\t" + i + "." + tasks[i - 1].toString());
+            for (int i = 1; i <= tasks.size(); ++i) {
+                System.out.println("\t" + i + "." + tasks.get(i - 1).toString());
             }
         } else {
             System.out.println("\tYou do not have any pending task.");
@@ -95,16 +113,16 @@ public class Duke {
 
     public static void runDeadline(String description) {
         String[] details = description.split(" /by");
-        tasks[size] = new Deadline(details[0], details[1]);
+        tasks.add(new Deadline(details[0], details[1]));
     }
 
     public static void runTodo(String description) {
-        tasks[size] = new Todo(description);
+        tasks.add(new Todo(description));
     }
 
     public static void runEvent(String description) {
         String[] details = description.split(" /at");
-        tasks[size] = new Event(details[0], details[1]);
+        tasks.add(new Event(details[0], details[1]));
     }
 
     public static void printDash() {
