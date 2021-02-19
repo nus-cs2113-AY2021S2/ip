676fb265c8df7bbf6fdd6feca6f408f4734d20af
kangxinwang <juliawangkangxin@gmail.com>
2021-02-19 21:01:18 +0800

    fixed bugs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,14 @@
-package duke.command;
-import java.io.File;
+import duke.command.ArrayIndexOutOfBoundsException;
 import duke.task.Deadline;
 import duke.task.Event;
 import duke.task.Task;
 import duke.task.Todo;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -23,7 +22,7 @@ public class Duke {
     private static int tasksCount = 0;
     private static ArrayList<Task> tasks = new ArrayList<>();
     private static String line = "*********************************************";
-    private static final String FILE_PATH = "tasks.txt";
+    private static String filePath = "data/tasks.txt";
 
     private static void helloMessage() {
         System.out.println(line);
@@ -40,91 +39,40 @@ public class Duke {
     }
 
     public static void printList(ArrayList<Task> tasks){
-        int count=1;
         System.out.println("Here are the tasks in your list: ");
-        for(int i=0; i<tasksCount;i++) {
-            System.out.println(count+"."+tasks.get(i).toString());
-            count++;
+        for (int i = 0; i < tasksCount; i++) {
+            System.out.printf("%d.%s\n", i + 1, tasks.get(i).toString());
         }
         System.out.println(line);
-    }
-    public static void writeFile(){
-        try{
-            FileWriter fw = new FileWriter(FILE_PATH);
-            if(Files.notExists(Path.of(FILE_PATH))){
-                Files.createFile(Path.of(FILE_PATH));
-            }
-            for(int i=0; i<tasksCount;i++){
-                fw.write(tasks.get(i).toString()+System.lineSeparator());
-            }
-            fw.close();
-        }catch(IOException e){
-            System.out.println(e.getMessage());
-        }
-    }
-    public static void readFile() throws IOException {
-        try{
-            File f = new File(FILE_PATH); // create a File for the given file path
-            Scanner scanner = new Scanner(f); // create a Scanner using the File as the source
-            while (scanner.hasNext()) {
-                String text = scanner.nextLine();
-                System.out.println(text);
-                if(text.startsWith("[D]")){
-                    String dueDate = text.substring(text.indexOf("(by:")+5, text.length()-1);
-                    String getDescription = text.substring(7,text.indexOf("(")-1);
-                    Deadline deadline = new Deadline(getDescription,dueDate);
-                    tasks.add(tasksCount,deadline);
-                    if(text.contains("[X]")){
-                        tasks.get(tasksCount-1).markAsDone();
-                    }
-                } else if(text.startsWith("[E]")){
-                    String timing = text.substring(text.indexOf("(at:")+5, text.length()-1);
-                    String getDescription = text.substring(7,text.indexOf("(")-1);
-                    Event event = new Event(getDescription,timing);
-                    tasks.add(tasksCount,event);
-                    if(text.contains("[X]")){
-                        tasks.get(tasksCount-1).markAsDone();
-                    }
-                } else if(text.startsWith("[T]")){
-                    String getDescription = text.substring(7);
-                    Todo todo = new Todo(getDescription);
-                    tasks.add(tasksCount,todo);
-                    if(text.contains("[X]")){
-                        tasks.get(tasksCount-1).markAsDone();
-                    }
-                }
-            }
-        }catch(FileNotFoundException e){
-            System.out.println(Files.createFile(Paths.get("tasks.txt")));
-        }
     }
 
     public static void deleteTask(String input){
         try {
             int newNum = Integer.parseInt(input.substring(DELETE_LENGTH));
-            if (newNum >= tasksCount || newNum < 0) {
+            if (newNum > tasksCount || newNum < 0) {
                 throw new ArrayIndexOutOfBoundsException();
             }
-            System.out.println("Noted. I've removed this task:" + "\n"+ tasks.get(newNum - 1).toString());
+
+            System.out.println("Noted. I've removed this task:" + "\n"+ tasks.get(newNum-1).toString());
             tasks.remove(newNum-1);
             tasksCount--;
-            System.out.println("Now you have "+ tasksCount +" tasks in the list.");
-            writeFile();
+            printTotalTasks();
+
         }catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("☹ OOPS!!! The task number is invalid!");
             System.out.println(line);
         }
     }
 
-    public static void markAsDone(String input){
+    public static void markTaskAsDone(String input){
         try {
             int newNum = Integer.parseInt(input.substring(DONE_LENGTH));
-            if (newNum >= tasksCount || newNum < 0) {
+            if (newNum > tasksCount || newNum < 0) {
                 throw new ArrayIndexOutOfBoundsException();
             }
             System.out.println("Nice! I've marked this task as done:");
             tasks.get(newNum - 1).markAsDone();
-            System.out.println(tasks.get(newNum - 1).toString());
+            System.out.println(tasks.get(newNum-1).toString());
             System.out.println(line);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("☹ OOPS!!! The task number is invalid!");
@@ -133,7 +81,7 @@ public class Duke {
 
     }
 
-    public static void addTodo(String newTask){
+    public static void addTodo(String newTask) {
         try {
             newTask = newTask.substring(TODO_LENGTH);
             System.out.println("Got it. I've added this task: ");
@@ -147,10 +95,11 @@ public class Duke {
             System.out.println(line);
         }
     }
-    public static void addDeadline(String input){
+
+    public static void addDeadline(String input) {
         String[] split = input.split(" /by");
         System.out.println("Got it. I've added this task: ");
-        Deadline deadline = new Deadline(split[0],split[1]);
+        Deadline deadline = new Deadline(split[0], split[1]);
         tasks.add(deadline);
         tasksCount++;
         System.out.println(deadline.toString());
@@ -162,17 +111,75 @@ public class Duke {
         System.out.println(line);
     }
 
-    public static void addEvent(String input){
+    public static void addEvent(String input) {
         String[] split = input.split(" /at");
         System.out.println("Got it. I've added this task: ");
-        Event event = new Event(split[0],split[1]);
+        Event event = new Event(split[0], split[1]);
         tasks.add(event);
         tasksCount++;
         System.out.println(event.toString());
         printTotalTasks();
     }
+    public static void readFile(){
+        try {
+            if(Files.exists(Paths.get(filePath))){
+                File f = new File(filePath);
+                Scanner sc = new Scanner(f);
+                while (sc.hasNext()) {
+                    String text = sc.nextLine();
+                    if (text.startsWith("[T]")) {
+                        String getDescription = text.substring(7);
+                        tasks.add(tasksCount, new Todo(getDescription));
+                        tasksCount++;
+                        if (text.substring(4,5).compareTo("\u2718")==0) {
+                            tasks.get(tasksCount - 1).markAsDone();
+                        }
+                    } else if (text.startsWith("[E]")) {
+                        String getDescription = text.substring(7, text.indexOf("(") - 1);
+                        String event = text.substring(text.indexOf("(") + 5, text.indexOf(")"));
+                        tasks.add(tasksCount, new Event(getDescription, event));
+                        tasksCount++;
+                        if (text.substring(4,5).compareTo("\u2718")==0) {
+                            tasks.get(tasksCount - 1).markAsDone();
+                        }
+                    } else if (text.startsWith("[D]")) {
+                        String description = text.substring(7, text.indexOf("(") - 1);
+                        String deadline = text.substring(text.indexOf("(") + 5, text.indexOf(")"));
+                        tasks.add(tasksCount,new Deadline(description, deadline));
+                        tasksCount++;
+                        if (text.substring(4,5).compareTo("\u2718")==0) {
+                            tasks.get(tasksCount - 1).markAsDone();
+                        }
+                    }
 
-    public static void main(String[] args) throws IOException {
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static void writeFile(){
+        try {
+            if (Files.notExists(Paths.get("data/"))) {
+                Files.createDirectory(Paths.get("data/"));
+            } else if (Files.notExists(Paths.get(filePath))) {
+                Files.createFile(Paths.get(filePath));
+            }
+            FileWriter fw = new FileWriter(filePath);
+            for (int i=0; i<tasksCount;i++) {
+                fw.write(tasks.get(i).toString() + System.lineSeparator());
+            }
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Error! Unable to write to file");
+        }
+
+
+
+    }
+    public static void main(String[] args){
         helloMessage();
         readFile();
         String input;
@@ -184,10 +191,11 @@ public class Duke {
             } else if(input.equals("list")) {
                 printList(tasks);
             } else if(input.startsWith("done")) {
-                markAsDone(input);
+                markTaskAsDone(input);
                 writeFile();
             } else if(input.startsWith("todo")){
                 addTodo(input);
+                writeFile();
             } else if(input.startsWith("deadline")){
                 input = input.substring(DEADLINE_LENGTH);
                 addDeadline(input);
@@ -203,6 +211,7 @@ public class Duke {
                 System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 System.out.println(line);
             }
+
         }
     }
 
