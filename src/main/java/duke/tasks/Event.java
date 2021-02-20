1ee7bc6b4980f19fcbcdb011097d0194d2f8999c
lamzf1998 <73275294+lamzf1998@users.noreply.github.com>
2021-02-20 17:55:32 +0800

    Update Event.java

--- src/main/java/duke/tasks/Event.java
+++ src/main/java/duke/tasks/Event.java
@@ -20,7 +20,7 @@ public class Event extends Task {
         return atDate;
     }
 
-    public LocalDateTime getDatetime() {
+    public LocalDateTime getDateTime() {
         return dateTime;
     }
 
