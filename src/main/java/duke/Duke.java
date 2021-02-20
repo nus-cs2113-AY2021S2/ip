e84020d464c3c09af162bb0b32a14575cb68bc5b
jadenwjh <wongjunhong9723@gmail.com>
2021-02-20 18:25:54 +0800

    Add greetUser method

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,8 +1,13 @@
 package duke;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import duke.common.Strings;
 import duke.parser.Parser;
+import duke.storage.Storage;
+import duke.ui.Ui;
 
 public class Duke {
 
@@ -10,7 +15,16 @@ public class Duke {
      * Entry point of the application.
      */
     public static void main(String[] args) {
+        greetUser();
         Scanner scanner = new Scanner(System.in);
         Parser.handleCommand(scanner);
+    }
+
+    private static void greetUser() {
+        Storage.read();
+        List<String> messages = new ArrayList<>();
+        messages.add(Strings.HELLO_MESSAGE);
+        messages.add(Strings.ASSIST_MESSAGE);
+        Ui.reply(messages);
     }
 }
