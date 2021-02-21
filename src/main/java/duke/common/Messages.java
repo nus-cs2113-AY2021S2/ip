49bfc2501b4e47858fa1a9366e67b1a017b60b6c
jadenwjh <wongjunhong9723@gmail.com>
2021-02-22 00:15:38 +0800

    Edit exception messages to show user

--- src/main/java/duke/common/Messages.java
+++ src/main/java/duke/common/Messages.java
@@ -3,23 +3,22 @@ package duke.common;
 public class Messages {
 
     public static final String HORIZONTAL_LINE = "    ___________________________________________";
-    public static final String HELLO_MESSAGE = "Hello! I'm Duke [Version 0.1]";
+    public static final String HELLO_MESSAGE = "Hello! I'm Duke [Version 0.2]";
     public static final String ASSIST_MESSAGE = "What can I do for you?";
     public static final String BYE_MESSAGE = "Bye. Hope to see you again soon!";
     public static final String DONE_MESSAGE = "Nice! I've marked this task as done:";
     public static final String ADDED_MESSAGE = "Got it. I've added this task:";
     public static final String DELETED_MESSAGE = "Noted. I've removed this task:";
     public static final String FOUND_TASK_MESSAGE = "Here are the matching tasks in your list:";
-
     public static final String INTERNAL_ERROR_MESSAGE = "Duke has malfunctioned...";
     public static final String INVALID_COMMAND_MESSAGE = "Do not understand your command.";
     public static final String COMPLETE_TASK_ERROR_MESSAGE = "There are no tasks to complete.";
     public static final String EMPTY_TASK_LIST_MESSAGE = "You currently have no tasks.";
     public static final String TODO_EMPTY_MESSAGE = "Description of a todo cannot be empty!";
     public static final String DEADLINE_EMPTY_MESSAGE = "Description of deadline can't be empty!";
-    public static final String DEADLINE_EMPTY_TIME_MESSAGE = "Deadline missing!";
+    public static final String DEADLINE_EMPTY_TIME_MESSAGE = "Please specify a deadline";
     public static final String EVENT_EMPTY_MESSAGE = "Description of a event cannot be empty!";
-    public static final String EVENT_EMPTY_TIME_MESSAGE = "Event missing!";
+    public static final String EVENT_EMPTY_TIME_MESSAGE = "Please specify a start and end time";
     public static final String TASK_DOES_NOT_EXIST = "Task does not exist!";
     public static final String INDEX_NUMBER_INPUT_MESSAGE = "Please input a valid number!";
     public static final String EMPTY_KEYWORD_MESSAGE = "Please input a keyword";
