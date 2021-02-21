fbe624f157b22f71de2d79a19b936d037f5322bb
jianningzhuang <jianning.personal@gmail.com>
2021-02-21 21:57:10 +0800

    output date if valid format

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -1,13 +1,37 @@
 package duke.task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
     protected String by;
+    protected LocalDate date;
+    protected boolean hasDate = false;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        LocalDate parsedDate = parseDate(by);
+        if (parsedDate != null) {
+            hasDate = true;
+            this.date = parsedDate;
+        }
     }
+
+
+    public LocalDate parseDate(String line) {
+        LocalDate parsedDate = null;
+        try {
+            parsedDate = LocalDate.parse(line);
+        } catch (DateTimeParseException e) {
+            //System.out.println("invalid date format");
+        }
+        return parsedDate;
+    }
+
+
 
     public String getBy() {
         return this.by;
@@ -15,6 +39,9 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
+        if (hasDate) {
+            return "[D]" + super.toString() + " (by: " + date.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
+        }
         return "[D]" + super.toString() + " (by: " + this.by + ")";
     }
 
--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -1,12 +1,33 @@
 package duke.task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
 
     protected String at;
+    protected LocalDate date;
+    protected boolean hasDate = false;
 
     public Event(String description, String at) {
         super(description);
         this.at = at;
+        LocalDate parsedDate = parseDate(at);
+        if (parsedDate != null) {
+            hasDate = true;
+            this.date = parsedDate;
+        }
+    }
+
+    public LocalDate parseDate(String line) {
+        LocalDate parsedDate = null;
+        try {
+            parsedDate = LocalDate.parse(line);
+        } catch (DateTimeParseException e) {
+            //System.out.println("invalid date format");
+        }
+        return parsedDate;
     }
 
     public String getAt() {
@@ -15,6 +36,9 @@ public class Event extends Task {
 
     @Override
     public String toString() {
+        if (hasDate) {
+            return "[E]" + super.toString() + " (at: " + date.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
+        }
         return "[E]" + super.toString() + " (at: " + this.at + ")";
     }
 }
