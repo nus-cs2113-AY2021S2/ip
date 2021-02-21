e01a41dc4cc6b3666dd675689aefdea6e36b7bdd
LeeHanYongAndy <andyleehy2502@gmail.com>
2021-02-21 15:59:34 +0800

    update JavDoc and Code Quality for Storage.java

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -9,11 +9,18 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.StringJoiner;
 
+/**
+ * Storage class for handling the loading and storing of file to the hard disk.
+ */
 public class Storage {
     private static final Constant constant = new Constant();
     private static final Printer printer = new Printer();
     private static final TaskHandler taskHandler = new TaskHandler();
 
+    /**
+     * Try-catch block for attempting to load a file if it exist.
+     * Print the relevant welcome message and file loading status.
+     */
     public static void initFile() {
         printer.printWelcomeMessage();
         try {
@@ -29,6 +36,11 @@ public class Storage {
         }
     }
 
+    /**
+     * Load and read the file input and then add them to the task list.
+     *
+     * @throws IOException if the file is unable to load.
+     */
     private static void tryRunFile() throws IOException {
         File sourceFile = new File(constant.FILE_PATH);
         String fileData;
@@ -43,6 +55,13 @@ public class Storage {
         }
     }
 
+    /**
+     * Add the type of task to the task list based on the input record.
+     * Default print out any unexpected data found during the process.
+     *
+     * @param fileData is the record of 1 task from the file.
+     * @param taskNumber is the current index in the task list managed by taskHandler.
+     */
     private static void processFileData(String fileData, int taskNumber) {
         String[] splitFileData = fileData.split(" \\| ");
         String fileInput, fileDescription, fileTiming;
@@ -70,20 +89,38 @@ public class Storage {
         }
     }
 
+    /**
+     * Mark the task as done or undone based on the input records.
+     *
+     * @param taskNumber is the current index in the task list managed by taskHandler.
+     * @param splitFileData contains the information such as type, status, and description of a task.
+     */
     private static void checkFileInputDoneStatus(int taskNumber, String[] splitFileData) {
         if (splitFileData[1].equals("X")) {
             taskHandler.markDone(taskNumber);
         }
     }
 
+    /**
+     * Extract a single task information based on the requirement given in param.
+     *
+     * @param splitFileData contains the information such as type, status, and description of a task.
+     * @param start is the index of the first block to concatenate.
+     * @param end is the index of the last block to concatenate.
+     * @return the information requested by the calling method (task description or timing field).
+     */
     private static String extractFileInfo(String[] splitFileData, int start, int end) {
         StringJoiner joiner = new StringJoiner(" ");
-        for(int i = start; i < end; i++) {
+        for (int i = start; i < end; i++) {
             joiner.add(splitFileData[i]);
         }
         return joiner.toString();
     }
 
+    /**
+     * Try-catch block for attempting to save the current task list into a file.
+     * Prints an error if error an occur during the saving process.
+     */
     public static void saveFile() {
         try {
             tryWriteToFiles();
@@ -92,12 +129,22 @@ public class Storage {
         }
     }
 
+    /**
+     * Write the records in the task list to the file.
+     *
+     * @throws IOException if an error if error an occur during the writing to file process.
+     */
     private static void tryWriteToFiles() throws IOException {
         FileWriter fw = new FileWriter(constant.FILE_PATH);
         fw.write(convertToFileInput());
         fw.close();
     }
 
+    /**
+     * Convert the task list to the saving format.
+     *
+     * @return a string containing the format for writing in the file by calling method.
+     */
     private static String convertToFileInput() {
         return taskHandler.toFileFormat();
     }
