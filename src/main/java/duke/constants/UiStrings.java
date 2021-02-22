f190efb78e781a6cecd46031b6417c052a8d777b
ivanchongzhien <icze2009@gmail.com>
2021-02-22 17:35:05 +0800

    Implement level 8: add date formatting methods in parser Add new methods to check user input and convert to date format if neccessary

--- src/main/java/duke/constants/UiStrings.java
+++ src/main/java/duke/constants/UiStrings.java
@@ -5,23 +5,25 @@ package duke.constants;
  */
 
 public class UiStrings {
-    // Messages for announcing error
-    public static final String ENTER_HELP_FOR_LIST_OF_COMMANDS = "Enter \"help\" for a list of available commands and format\n";
-    public static final String JOB_NUMBER_TOO_SMALL = "Enter a valid job number. Use the list command to view your current tasks.";
-    public static final String JOB_NUMBER_TOO_BIG = "You don't have that many jobs! Use the list command to view your current tasks.";
-    public static final String LIST_FULL_WARNING = "List is full!" +'\n'+"Use the \"list\" command to view your tasks." + '\n';
-
+    
     // Messages for ... todo
     public static final String PROMPT_ENTER_BYE_TO_EXIT = "To exit, enter \"bye\"";
-    
     
     // Command formats for help message
     public static final String LIST_COMMAND_FORMAT = "LIST - \n" + "FORMAT: list";
     public static final String DONE_COMMAND_FORMAT = "DONE - \n" + "FORMAT: done [(int) number]";
     public static final String TODO_COMMAND_FORMAT = "TODO - \n" + "FORMAT: todo [(str) description]";
-    public static final String DEADLINE_COMMAND_FORMAT = "DEADLINE - \n" + "FORMAT: deadline [(str) description] /by [(str) deadline]";
-    public static final String EVENT_COMMAND_FORMAT = "EVENT - \n" + "FORMAT: event [(str) description] /at [(str) time]";
+    public static final String DEADLINE_COMMAND_FORMAT = "DEADLINE - \n" + "FORMAT: deadline [(str) description] /by [(str) time/*date]";
+    public static final String EVENT_COMMAND_FORMAT = "EVENT - \n" + "FORMAT: event [(str) description] /at [(str) time/*date]";
     public static final String DELETE_COMMAND_FORMAT = "DELETE - \n" + "FORMAT: delete [(int) index]";
+    public static final String DATE_FORMAT = "Date FORMAT: dd-mm-yyyy";
+
+    // Messages for announcing error
+    public static final String ENTER_HELP_FOR_LIST_OF_COMMANDS = "Enter \"help\" for a list of available commands and format\n";
+    public static final String JOB_NUMBER_TOO_SMALL = "Enter a valid job number. Use the list command to view your current tasks.";
+    public static final String JOB_NUMBER_TOO_BIG = "You don't have that many jobs! Use the list command to view your current tasks.";
+    public static final String LIST_FULL_WARNING = "List is full!" +'\n'+"Use the \"list\" command to view your tasks." + '\n';
+    public static final String INVALID_DATE_FORMAT_WARNING = "Invalid date/ wrong date format. " + DATE_FORMAT;
     
     // Output formatting
     public static final String STRAIGHT_LINE = "____________________________________________________________";
