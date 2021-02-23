265d931748a426052fc2cfff88d665b27f1ab7ee
ivanchongzhien <icze2009@gmail.com>
2021-02-24 00:49:31 +0800

    JavaDocs: exceptions class

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -1,5 +1,8 @@
 package duke.exception;
 
+/**
+ * Parent class to contain all exceptions specific to Duke.
+ */
 public class DukeException extends Exception {
     // nothing to add here
 }
--- src/main/java/duke/exception/FullListException.java
+++ src/main/java/duke/exception/FullListException.java
@@ -1,5 +1,8 @@
 package duke.exception;
 
+/**
+ * Signals an error caused by running commands to add tasks to a full list.
+ */
 public class FullListException extends DukeException {
     // nothing to be done
 }
