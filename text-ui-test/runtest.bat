1fb9a1ebb0e420de433c23ff39630519f7fa2682
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-02-24 01:22:21 +0800

    Implemeted batch file for Automated Testing of Text UIs

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -10,13 +10,33 @@ REM delete duke.txt from previous previous run
 if exist duke.txt del duke.txt
 
 REM compile the code into the bin folder
-javac  -encoding "UTF-8" -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
 )
 REM no error here, errorlevel == 0
 
-REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
-REM Compare the output to the expected output compare files manually as there are special characters which cmd doesn't recognise
+REM run the program, feed commands from input1.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ..\bin Duke < input1.txt > ACTUAL.TXT
+
+REM compare the output to the first expected output
+FC ACTUAL.TXT expected1.txt
+
+REM run the program, feed commands from input2.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ..\bin Duke < input2.txt > ACTUAL.TXT
+
+REM compare the output to the first expected output
+FC ACTUAL.TXT expected2.txt
+
+REM run the program, feed commands from input3.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ..\bin Duke < input3.txt > ACTUAL.TXT
+
+REM compare the output to the first expected output
+FC ACTUAL.TXT expected3.txt
+
+REM run the program, feed commands from input3.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ..\bin Duke < input4.txt > ACTUAL.TXT
+
+REM compare the output to the first expected output
+FC ACTUAL.TXT expected4.txt
