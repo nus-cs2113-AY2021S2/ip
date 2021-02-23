c10fd00d12f21df0815adeb70194f5e0e66f718f
Wei Sheng Ong <ongweisheng30@gmail.com>
2021-02-23 15:33:32 +0800

    Add TaskManager class

---
+++ src/main/java/task/TaskManager.java
@@ -0,0 +1,28 @@
+package task;
+
+import java.util.ArrayList;
+
+public class TaskManager {
+
+    private static final ArrayList<Task> taskList = new ArrayList<>();
+
+    public Task getTask(int index) {
+        return taskList.get(index);
+    }
+
+    public void addTask(Task task) {
+        taskList.add(task);
+    }
+
+    public void deleteTask(int index) {
+        taskList.remove(index);
+    }
+
+    public int getTaskCount() {
+        return taskList.size();
+    }
+
+    public ArrayList<Task> getTaskList() {
+        return taskList;
+    }
+}
