8eff11f7966ebece09133df7d02b2713288f2d97
kewenlok <e0360650@u.nus.edu>
2021-02-24 01:21:42 +0800

    Modify access to keyword with getter method

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -18,7 +18,7 @@ public class FindCommand extends Command {
 
     @Override
     public void execute(TaskList taskList, Ui ui, Storage storage) throws DukeException {
-        ui.showMatchingTasks(taskList.find(keyword));
+        ui.showMatchingTasks(taskList.find(getKeyword()));
     }
 
     @Override
