8b06ca6bca5fdebeffec4ad547236f9c1e7577bf
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-02-24 01:58:58 +0800

    Implement sh file for Automated Testing of Text UIs

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-# use command "dos2unix runtest.sh" to convert file to unix
+# use command "dos2unix runtest.sh" to convert file to unix if end of file not found
+
+
 # create bin directory if it doesn't exist
 if [ ! -d "../bin" ]
 then
@@ -26,20 +28,66 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin Duke < input1.txt > ACTUAL.TXT
 
 # convert to UNIX format
-cp EXPECTED-LINUX.TXT EXPECTED-UNIX.TXT
+cp expected1.txt EXPECTED-UNIX.TXT
 dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED-UNIX.TXT
 # shellcheck disable=SC2181
-if [ $? -eq 0 ]
-then
+if [ $? -eq 0 ]; then
+    echo "Test 1 result: PASSED"
+else
+    echo "Test 2 result: FAILED"
+fi
+
+# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ../bin Duke < input2.txt > ACTUAL.TXT
+
+# convert to UNIX format
+cp expected2.txt EXPECTED-UNIX.TXT
+dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+
+# compare the output to the expected output
+diff ACTUAL.TXT EXPECTED-UNIX.TXT
+# shellcheck disable=SC2181
+if [ $? -eq 0 ]; then
+    echo "Test 2 result: PASSED"
+else
+    echo "Test 2 result: FAILED"
+fi
+
+# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ../bin Duke < input3.txt > ACTUAL.TXT
+
+# convert to UNIX format
+cp expected3.txt EXPECTED-UNIX.TXT
+dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+
+# compare the output to the expected output
+diff ACTUAL.TXT EXPECTED-UNIX.TXT
+# shellcheck disable=SC2181
+if [ $? -eq 0 ]; then
     echo "Test result: PASSED"
-    exit 0
 else
     echo "Test result: FAILED"
 fi
-exit 1
+
+# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ../bin Duke < input4.txt > ACTUAL.TXT
+
+# convert to UNIX format
+cp expected4.txt EXPECTED-UNIX.TXT
+dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+
+# compare the output to the expected output
+diff ACTUAL.TXT EXPECTED-UNIX.TXT
+# shellcheck disable=SC2181
+if [ $? -eq 0 ]; then
+    echo "Test result: PASSED"
+else
+    echo "Test result: FAILED"
+fi
+exit 0
