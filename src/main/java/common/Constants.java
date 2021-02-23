9ad59cf4dbfb029fd6a70e8f7a17b998411529c2
Wei Sheng Ong <ongweisheng30@gmail.com>
2021-02-23 15:32:46 +0800

    Add common package
    Classes in the package
    * Constants
    * Messages

---
+++ src/main/java/common/Constants.java
@@ -0,0 +1,17 @@
+package common;
+
+public class Constants {
+
+    public static final int ARRAY_OFFSET = 1;
+    public static final String INDENTATION = "  ";
+
+    // Commands
+    public static final String COMMAND_TODO_WORD = "todo";
+    public static final String COMMAND_EVENT_WORD = "event";
+    public static final String COMMAND_DEADLINE_WORD = "deadline";
+    public static final String COMMAND_LIST_WORD = "list";
+    public static final String COMMAND_EXIT_WORD = "exit";
+    public static final String COMMAND_DONE_WORD = "done";
+    public static final String COMMAND_DELETE_WORD = "delete";
+    public static final String COMMAND_HELP_WORD = "help";
+}
