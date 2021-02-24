3b23d29b173ec3cd1ff12c3f4401af8d9ed7c1e7
ManikaHennedige <mhennedige@gmail.com>
2021-02-24 18:35:10 +0800

    Add DateTime parsing functionality

--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -1,7 +1,12 @@
 package duke.task;
 
+import duke.exception.InvalidDateTimeException;
 import duke.exception.MissingEventDurationException;
 import duke.exception.MissingTaskDescriptionException;
+
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Event extends Deadline {
 
@@ -9,20 +14,27 @@ public class Event extends Deadline {
         super(description, eventDuration);
     }
 
+
     public String toString(){
-        return "Event : " + super.toBaseString() + " || Happening on: " + this.getDateTime();
+        return "Event : " + super.toBaseString() + " || Happening on: " + this.printDateTime();
     }
 
     // Exceptions
 
-    public static void checkEventInput(String[] taskDetails) throws MissingEventDurationException, MissingTaskDescriptionException {
+    public static void checkEventInput(String[] taskDetails) throws MissingEventDurationException,
+            MissingTaskDescriptionException, InvalidDateTimeException {
+        try {
+            DateTimeFormatter parseFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm a");
+            LocalDateTime.parse(taskDetails[1], parseFormatter);
+        } catch (DateTimeException e) {
+            throw new InvalidDateTimeException();
+        }
         if (taskDetails[0] == null){
             throw new MissingTaskDescriptionException();
         }
         if (taskDetails[1] == null) {
             throw new MissingEventDurationException();
         }
-
     }
 
 }
