da8358e513d248acf912a2e26276a6e1e07ab9e4
JoviYeung92 <joviyeung1992@gmail.com>
2021-02-26 02:13:39 +0800

    Level-8

--- src/java/tasks/DeadLines.java
+++ src/java/tasks/DeadLines.java
@@ -1,6 +1,9 @@
 package tasks;
 
 import tasks.Task;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class DeadLines extends Task {
 
@@ -15,6 +18,14 @@ public class DeadLines extends Task {
     public void printStatus() {
         System.out.print("[D]");
         super.printStatus();
-        System.out.println("(by: "+by+")");
+        try {
+            String words[] = by.split(" ");
+            String date = words[0];
+            String time = words[1];
+            LocalDate d1 = LocalDate.parse(date);
+            System.out.println("(by: " + d1.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ", " + time + ")");
+        } catch(DateTimeParseException e){
+            System.out.println("(by: "+by+")");
+        }
     }
 }
