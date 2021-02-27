f361db3289884c172f88c77c32102ffe5f90de86
Emkay16 <mabelkohjx@gmail.com>
2021-02-27 17:06:04 +0800

    Add Ui class and move methods to class

--- src/main/java/duke/Constants.java
+++ src/main/java/duke/Constants.java
@@ -8,32 +8,68 @@ public class Constants {
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
 
-    public static final String WELCOME_MESSAGE = LINE + "Hello from\n" + LOGO
-            + "What can I do for you today?\n" + LINE;
-    public static final String BYE_MESSAGE = LINE + "Goodbye! Hope to see you around soon!\n" + LINE;
-    public static final String HELP_MESSAGE = LINE + "Add todo:\n"
-            + "Command prefix: NONE\n"
-            + "Argument(s): task\n\n"
-            + "Show tasks:\n"
-            + "Command prefix: list\n\n"
-            + "Check task off:\n"
-            + "Command prefix: done\n"
-            + "Argument(s): task number\n";
-    public static final String EMPTY_LIST_MESSAGE = "There are not tasks in the list\n";
-    public static final String ADD_MESSAGE = " added to list\n";
-    public static final String NO_DESCRIPTION_MESSAGE = "The description cannot be empty!\n";
-    public static final String NO_DEADLINE_MESSAGE = "Please indicate a deadline after \"/by\"\n";
-    public static final String NO_TIME_MESSAGE = "Please indicate the event time after \"/at\"\n";
-    public static final String TASK_CHECKED_MESSAGE = "Task checked off!\n";
-    public static final String TASK_ALREADY_CHECKED_MESSAGE = " is already checked off\n";
-    public static final String ALL_TASKS_CHECKED_MESSAGE = "All remaining tasks have been checked off!\n";
-    public static final String TASK_UNCHECKED_MESSAGE = "Task is no longer checked off\n";
-    public static final String TASK_NOT_CHECKED_MESSAGE = " is not checked\n";
-    public static final String TASK_DELETED_MESSAGE = "This task has been removed: \n";
-    public static final String INVALID_ARGUMENT_MESSAGE = "Invalid argument!\n";
+    public static final String WELCOME_MESSAGE = "Hello from\n" + LOGO
+            + "What can I do for you today?\n"
+            + "Type 'help' for list of commands and usage";
+    public static final String BYE_MESSAGE = "Goodbye! Hope to see you around soon!";
+    public static final String UNDONE_TASKS_REMAINING_MESSAGE = "There are %d undone task(s) on the list";
+    public static final String EMPTY_LIST_MESSAGE = "There are no tasks in the list";
+    public static final String ADD_MESSAGE = "\"%s\" added to list";
+    public static final String NO_DESCRIPTION_MESSAGE = "The description cannot be empty!";
+    public static final String DEADLINE_DUE_DATE_MESSAGE = "Please complete by: %s";
+    public static final String NO_DEADLINE_MESSAGE = "Please indicate a deadline after \"/by\"";
+    public static final String EVENT_TIME_MESSAGE = "It occurs at: %s";
+    public static final String NO_TIME_MESSAGE = "Please indicate the event time after \"/at\"";
+    public static final String TASK_CHECKED_MESSAGE = "Task %d checked off!\n    %s %s";
+    public static final String TASK_ALREADY_CHECKED_MESSAGE = "Task %d: \"%s\" is already checked off";
+    public static final String ALL_TASKS_CHECKED_MESSAGE = "All remaining tasks have been checked off!";
+    public static final String TASK_UNCHECKED_MESSAGE = "Task %d is no longer checked off\n    $s %s";
+    public static final String TASK_NOT_CHECKED_MESSAGE = "Task %d: \"%s\" is not checked";
+    public static final String TASK_DOES_NOT_EXIST_MESSAGE = "Task %d does not exist!";
+    public static final String TASK_DELETED_MESSAGE = "Task %d:%s has been removed";
+    public static final String INVALID_ARGUMENT_MESSAGE = "Invalid argument!";
     public static final String INVALID_COMMAND_MESSAGE = "I am sorry, I do not recognise that command\n"
-            + "Please try again\n";
-    public static final String GENERIC_ERROR_MESSAGE = "Something went wrong!\n";
+            + "Please try again";
+    public static final String GENERIC_ERROR_MESSAGE = "Something went wrong!";
+
+    public static final String TODO_INFO = "Add todo:\n"
+            + "     Command prefix: todo\n"
+            + "     Argument(s): description\n"
+            + "     Usage: todo <description>\n"
+            + "     Example: todo CS2113T lecture 5 quiz\n";
+    public static final String EVENT_INFO = "Add event:\n"
+            + "     Command prefix:  event\n"
+            + "     Argument(s): description, date\n"
+            + "     Usage: event <description> /at <date>\n"
+            + "     Example: event CS2113T finals /at 29/4 1pm\n";
+    public static final String DEADLINE_INFO = "Add deadline:\n"
+            + "     Command prefix: deadline\n"
+            + "     Argument(s): description, due date\n"
+            + "     Usage: deadline <description> /by <due date>\n"
+            + "     Example: deadline release v2.0 jar /by 5/3 2359\n";
+    public static final String LIST_INFO = "Show list of tasks:\n"
+            + "     Command: list\n"
+            + "     Usage: list\n";
+    public static final String DONE_INFO = "Check off completed task:\n"
+            + "     Command prefix: done\n"
+            + "     Argument(s): task index (as seen from list)\n"
+            + "     Usage: done <task index>\n"
+            + "     Example: done 3\n";
+    public static final String UNDO_INFO = "Uncheck task: \n"
+            + "     Command prefix: undo\n"
+            + "     Argument(s): task index (as seen from list)\n"
+            + "     Usage: undo <task index>\n"
+            + "     Example: undo 3\n";
+    public static final String DELETE_INFO = "Remove task:\n"
+            + "! Note that deleting a task will cause the indices of tasks after in to change !\n"
+            + "     Command prefix: delete\n"
+            + "     Argument(s): task index (as seen from list)\n"
+            + "     Usage: delete <task index>\n"
+            + "     Example: delete 3\n";
+    public static final String BYE_INFO = "Exit Duke:\n"
+            + "     Command: bye\n"
+            + "     Usage: bye";
+
 
     public static final String FILENAME = "data.txt";
 }
--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -14,10 +14,10 @@ public class Duke {
     public static void main(String[] args) {
         ArrayList<Task> list = new ArrayList<>();
         int taskCount = DataStorage.readFile(list);
-        printWelcome();
+        Ui.printWelcomeMessage();
         interact(list, taskCount);
         DataStorage.writeSaveData(list);
-        printBye();
+        Ui.printByeMessage();
     }
 
     public static void interact(ArrayList<Task> list, int taskCount) {
@@ -33,7 +33,7 @@ public class Duke {
         while (!command.equalsIgnoreCase("bye")) {
             switch (command.toLowerCase()) {
             case "help":
-                System.out.print(Constants.LINE + Constants.HELP_MESSAGE + Constants.LINE);
+                Ui.printHelpMessage();
                 break;
             case "list":
                 printList(list, taskCount);
@@ -57,7 +57,7 @@ public class Duke {
                 delete(list, taskCount, arguments);
                 break;
             default:
-                System.out.print(Constants.LINE + Constants.INVALID_COMMAND_MESSAGE + Constants.LINE);
+                Ui.printInvalidCommandMessage();
             }
             input = scan.nextLine();
 
@@ -69,10 +69,10 @@ public class Duke {
 
     public static int addToDo(String input, ArrayList<Task> list, int index) {
         if(input == null) {
-            System.out.print(Constants.LINE + Constants.NO_DESCRIPTION_MESSAGE + Constants.LINE);
+            Ui.printInvalidArgumentMessage(Constants.NO_DESCRIPTION_MESSAGE);
         } else {
             list.add(new ToDo(input));
-            System.out.print(Constants.LINE + "\"" + input + "\"" + Constants.ADD_MESSAGE + Constants.LINE);
+            Ui.printToDoAdded(input);
             index++;
         }
         return index;
@@ -80,21 +80,19 @@ public class Duke {
 
     public static int addDeadline(String input, ArrayList<Task> list, int index) {
         if (input == null) {
-            System.out.print(Constants.LINE + Constants.NO_DESCRIPTION_MESSAGE + Constants.LINE);
+            Ui.printInvalidArgumentMessage(Constants.NO_DESCRIPTION_MESSAGE);
         } else if(input.toLowerCase().contains("/by")) {
             try {
                 String desc = input.substring(0, input.toLowerCase().indexOf("/by") - 1);
                 String dueDate = input.substring(input.toLowerCase().indexOf("/by") + 4);
                 list.add(new Deadline(desc, dueDate));
-                System.out.print(Constants.LINE + "\"" + desc + "\"" + Constants.ADD_MESSAGE +
-                        "Please complete by: " + dueDate + "\n" + Constants.LINE);
+                Ui.printDeadlineAdded(desc, dueDate);
                 index++;
             } catch (Exception e) {
-                System.out.print(Constants.LINE + Constants.INVALID_ARGUMENT_MESSAGE + Constants.LINE);
+                Ui.printGenericErrorMessage();
             }
         } else {
-            System.out.print(Constants.LINE + Constants.INVALID_ARGUMENT_MESSAGE
-                    + Constants.NO_DEADLINE_MESSAGE + Constants.LINE);
+            Ui.printInvalidArgumentMessage(Constants.NO_DEADLINE_MESSAGE);
         }
 
         return index;
@@ -102,36 +100,39 @@ public class Duke {
 
     public static int addEvent(String input, ArrayList<Task> list, int taskCount) {
         if (input == null) {
-            System.out.print(Constants.LINE + Constants.NO_DESCRIPTION_MESSAGE + Constants.LINE);
+            Ui.printInvalidArgumentMessage(Constants.NO_DESCRIPTION_MESSAGE);
         } else if(input.toLowerCase().contains("/at")) {
             try {
-                String desc = input.substring(0, input.toLowerCase().indexOf("/at")-1);
-                String date = input.substring(input.toLowerCase().indexOf("/at")+4);
-                list.add(new Event(desc, date));
-                System.out.print(Constants.LINE + "\"" + desc + "\"" + Constants.ADD_MESSAGE +
-                        "It occurs at: " + date + "\n" + Constants.LINE);
+                String description = input.substring(0, input.toLowerCase().indexOf("/at")-1);
+                String time = input.substring(input.toLowerCase().indexOf("/at")+4);
+                list.add(new Event(description, time));
+                Ui.printEventAdded(description, time);
                 taskCount++;
             } catch (Exception e) {
-                System.out.print(Constants.LINE + Constants.INVALID_ARGUMENT_MESSAGE + Constants.LINE);
+                Ui.printGenericErrorMessage();
             }
         } else {
-            System.out.print(Constants.LINE + Constants.INVALID_ARGUMENT_MESSAGE + Constants.NO_TIME_MESSAGE + Constants.LINE);
+                Ui.printInvalidArgumentMessage(Constants.NO_TIME_MESSAGE);
         }
         return taskCount;
     }
 
     public static void printList(ArrayList<Task> list, int taskCount) {
         if(taskCount == 0) {
-            System.out.print(Constants.LINE + Constants.EMPTY_LIST_MESSAGE + Constants.LINE);
+            Ui.printEmptyListMessage();
         } else {
-            System.out.print(Constants.LINE);
+            ArrayList<String> taskList = new ArrayList<>();
+            StringBuilder task = new StringBuilder();
             for (int i = 0; i < list.size(); i++) {
                 if (i < 9) {
-                    System.out.print(" ");
+                    task.append(" ");
                 }
-                System.out.println(i + 1 + list.get(i).toString());
+                task.append(i+1).append(list.get(i).toString());
+                taskList.add(task.toString());
+                task.setLength(0);
             }
-            System.out.print("There are " + Task.getTasksRemaining() + " undone task(s) on the list\n" + Constants.LINE);
+            taskList.add(String.format(Constants.UNDONE_TASKS_REMAINING_MESSAGE, Task.getTasksRemaining()));
+            Ui.dukePrinter(taskList);
         }
     }
 
@@ -140,22 +141,17 @@ public class Duke {
             int taskNo = Integer.parseInt(input);
             if (taskNo <= max && taskNo > 0) { //no. is valid
                 if (list.get(taskNo - 1).getStatus()) {
-                    System.out.print(Constants.LINE + "Task \"" + list.get(taskNo - 1).getDesc() + "\""
-                            + Constants.TASK_ALREADY_CHECKED_MESSAGE + Constants.LINE);
+                    Ui.printTaskAlreadyCheckedMessage(taskNo, list.get(taskNo - 1).getDesc());
                 } else {
                     list.get(taskNo - 1).check();
-                    System.out.print(Constants.LINE + Constants.TASK_CHECKED_MESSAGE);
-                    System.out.println("  " + list.get(taskNo - 1).getStatusSymbol() + list.get(taskNo - 1).getDesc());
-                    if (Task.getTasksRemaining() == 0) {
-                        System.out.print(Constants.ALL_TASKS_CHECKED_MESSAGE);
-                    }
-                    System.out.print(Constants.LINE);
+                    Ui.printTaskChecked(taskNo, list.get(taskNo - 1).getStatusSymbol(),
+                            list.get(taskNo - 1).getDesc(), Task.getTasksRemaining());
                 }
             } else {
-                printInvalidArgumentMessage();
+                Ui.printInvalidArgumentMessage(String.format(Constants.TASK_DOES_NOT_EXIST_MESSAGE, taskNo));
             }
         } catch (Exception e) {
-            printInvalidArgumentMessage();
+            Ui.printGenericErrorMessage();
         }
     }
 
@@ -164,19 +160,17 @@ public class Duke {
             int taskNo = Integer.parseInt(input);
             if (taskNo <= max && taskNo > 0) { //no. is valid
                 if (!list.get(taskNo - 1).getStatus()) {
-                    System.out.print(Constants.LINE + "Task \"" + list.get(taskNo - 1).getDesc() + "\""
-                            + Constants.TASK_NOT_CHECKED_MESSAGE + Constants.LINE);
+                    Ui.printTaskNotCheckedMessage(taskNo, list.get(taskNo - 1).getDesc());
                 } else {
                     list.get(taskNo - 1).uncheck();
-                    System.out.print(Constants.LINE + Constants.TASK_UNCHECKED_MESSAGE);
-                    System.out.println("  " + list.get(taskNo - 1).getStatusSymbol() + list.get(taskNo - 1).getDesc());
-                    System.out.print(Constants.LINE);
+                    Ui.printTaskUnchecked(taskNo, list.get(taskNo - 1).getStatusSymbol(),
+                            list.get(taskNo - 1).getDesc());
                 }
             } else {
-                printInvalidArgumentMessage();
+                Ui.printInvalidArgumentMessage(String.format(Constants.TASK_DOES_NOT_EXIST_MESSAGE, taskNo));
             }
         } catch (Exception e) {
-            printInvalidArgumentMessage();
+            Ui.printGenericErrorMessage();
         }
     }
 
@@ -184,27 +178,14 @@ public class Duke {
         try {
             int taskNo = Integer.parseInt(input);
             if (taskNo <= max && taskNo > 0) {
-                System.out.print(Constants.LINE + Constants.TASK_DELETED_MESSAGE
-                        + list.get(taskNo - 1).toString() + "\n" + Constants.LINE);
+                Ui.printTaskDeleted(taskNo, list.get(taskNo - 1).toString());
                 list.get(taskNo - 1).remove();
                 list.remove(taskNo - 1);
             } else {
-                printInvalidArgumentMessage();
+                Ui.printInvalidArgumentMessage(String.format(Constants.TASK_DOES_NOT_EXIST_MESSAGE, taskNo));
             }
         } catch (Exception e) {
-            printInvalidArgumentMessage();
+            Ui.printGenericErrorMessage();
         }
-    }
-
-    private static void printWelcome() {
-        System.out.print(Constants.WELCOME_MESSAGE);
-    }
-
-    private static void printBye() {
-        System.out.print(Constants.BYE_MESSAGE);
-    }
-
-    private static void printInvalidArgumentMessage() {
-        System.out.print(Constants.LINE + Constants.INVALID_ARGUMENT_MESSAGE + Constants.LINE);
     }
 }
