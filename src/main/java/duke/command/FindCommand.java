23dbb34ddcb9ce4b09d4a947eab5fb1158f04b96
kewenlok <e0360650@u.nus.edu>
2021-02-28 19:15:40 +0800

    Add JavaDoc for FindCommand class

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -1,13 +1,23 @@
 package duke.command;
 
-import duke.DukeException;
 import duke.Storage;
 import duke.TaskList;
 import duke.Ui;
 
+/**
+ * Handles find command.
+ */
 public class FindCommand extends Command {
+    /**
+     * Keyword used for searching in tasks.
+     */
     private String keyword;
 
+    /**
+     * Constructs a new find command.
+     *
+     * @param keyword
+     */
     public FindCommand(String keyword) {
         this.keyword = keyword;
     }
@@ -16,11 +26,24 @@ public class FindCommand extends Command {
         return keyword;
     }
 
+    /**
+     * Filter tasks which contains keyword specified in find command.
+     * Prints out resulting filtered tasks in task list.
+     *
+     * @param taskList Task list consisting of all tasks.
+     * @param ui User interface for printing result.
+     * @param storage Storage for storing task list data.
+     */
     @Override
-    public void execute(TaskList taskList, Ui ui, Storage storage) throws DukeException {
+    public void execute(TaskList taskList, Ui ui, Storage storage) {
         ui.showMatchingTasks(taskList.find(getKeyword()));
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @return false
+     */
     @Override
     public boolean isExit() {
         return false;
