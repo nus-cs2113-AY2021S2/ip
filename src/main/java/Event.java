c9dd745a33fee155e9f759f95a7483f7605ee5bb
DESKTOP-5J2V28Q\user <e0426220@u.nus.edu>
2021-03-03 02:00:37 +0800

    add comments to most classes and methods

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,35 @@
+/**
+ * Representation for an event
+ */
 public class Event extends Task{
     protected String at;
 
+    /**
+     * Instantiates an event
+     *
+     * @param description event description
+     * @param at time the event is on.
+     */
     public Event(String description, String at) {
         super(description);
         this.at = at;
     }
 
+    /**
+     * Returns a formatted event for user to view
+     *
+     * @return formatted event string
+     */
     @Override
     public String toString() {
         return "[E]" + super.getDescription() + " (at: " + at + ")";
     }
 
+    /**
+     * Returns a formatted event for saving
+     *
+     * @return formatted event string
+     */
     @Override
     public String toSaveFormat() { return (super.toSaveFormat() + " | " + at); }
 }
