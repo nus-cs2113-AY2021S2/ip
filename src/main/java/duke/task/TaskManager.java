8e9fe98044d73a508be72f6272631053dab6c116
jovanhuang <jhtc98@hotmail.com>
2021-03-02 18:26:29 +0800

    Give users a way to find a task by searching for a keyword.

--- src/main/java/duke/task/TaskManager.java
+++ src/main/java/duke/task/TaskManager.java
@@ -16,21 +16,24 @@ public class TaskManager {
     private static final String EVENT_COMMAND = "event";
     public static final String DONE_COMMAND = "done";
     public static final String DELETE_COMMAND = "delete";
+    public static final String FIND_COMMAND = "find";
 
     private static final String ADDED_TASK_MESSAGE = "Got it. I have added this task: ";
     private static final String MARKED_TASK_AS_DONE_MESSAGE = "Nice! I've marked this task as done: ";
 
     private FileManager fileManager;
+    private StringParser stringParser;
     public TaskManager() {
         fileManager = new FileManager();
+        stringParser = new StringParser();
     }
 
     public void handleTask(String input) throws CommandNotFoundException {
-        String firstWord = StringParser.getFirstWord(input);
+        String firstWord = stringParser.getFirstWord(input);
         switch (firstWord){
         case DONE_COMMAND:
             try {
-                int taskNumberDone = StringParser.getTaskNumberDone(input);
+                int taskNumberDone = stringParser.getTaskNumberDone(input);
                 markTaskAsDone(taskNumberDone);
                 fileManager.updateDataFile();
             } catch (IOException e) {
@@ -39,7 +42,7 @@ public class TaskManager {
             break;
         case TODO_COMMAND:
             try{
-                String taskDescription = StringParser.getIndexOfStringAfterWhiteSpace(input);
+                String taskDescription = stringParser.getIndexOfStringAfterWhiteSpace(input);
                 Todo t = new Todo(taskDescription);
                 t.checkIfToDoDescriptionExists(taskDescription);
                 taskArrayList.add(t);
@@ -55,8 +58,8 @@ public class TaskManager {
             
         case DEADLINE_COMMAND:
             try {
-                String dueDate = StringParser.getStringAfterSlash(input);
-                String taskDescription = StringParser.getStringAfterWhiteSpaceAndBeforeSlash(input);
+                String dueDate = stringParser.getStringAfterSlash(input);
+                String taskDescription = stringParser.getStringAfterWhiteSpaceAndBeforeSlash(input);
                 Deadline d = new Deadline(taskDescription,dueDate);
                 d.checkIfDeadlineDescriptionExists(taskDescription);
                 taskArrayList.add(d);
@@ -71,8 +74,8 @@ public class TaskManager {
             break;
         case EVENT_COMMAND:
             try {
-                String eventPeriod = StringParser.getStringAfterSlash(input);
-                String taskDescription = StringParser.getStringAfterWhiteSpaceAndBeforeSlash(input);
+                String eventPeriod = stringParser.getStringAfterSlash(input);
+                String taskDescription = stringParser.getStringAfterWhiteSpaceAndBeforeSlash(input);
                 Event e = new Event(taskDescription,eventPeriod);
                 taskArrayList.add(e);
                 printMessageAfterTaskIsAdded(e);
@@ -83,7 +86,7 @@ public class TaskManager {
             break;
         case DELETE_COMMAND:
             try {
-                int taskNumberDeleted = StringParser.getTaskNumberDeleted(input);
+                int taskNumberDeleted = stringParser.getTaskNumberDeleted(input);
                 System.out.println("Noted. I've removed this task:\n" + taskArrayList.get(taskNumberDeleted-1));
                 taskArrayList.remove(taskNumberDeleted-1);
                 Task.decreaseTaskCount();
@@ -93,9 +96,25 @@ public class TaskManager {
             System.out.println("IOError at DELETE Command");
         }
             break;
+        case FIND_COMMAND:
+            String keyword = stringParser.getKeyword(input);
+            ArrayList<Task> matches = find(keyword);
+            printMatchingTasks(matches);
+            break;
         default:
             throw new CommandNotFoundException();
         }
+
+    }
+
+    private ArrayList<Task> find(String keyword) {
+        ArrayList<Task> matches = new ArrayList<>();
+        for (Task task: taskArrayList) {
+            if (task.getDescription().contains(keyword)){
+                matches.add(task);
+            }
+        }
+        return matches;
     }
 
 
@@ -123,6 +142,17 @@ public class TaskManager {
         int TaskCount = 0;
         MainUI.printDivider();
         System.out.println("Here are the tasks in your list:");
+        for (Task task: taskArrayList){
+            TaskCount +=1;
+            System.out.println(TaskCount + "." + task);
+        }
+        MainUI.printDivider();
+    }
+
+    public void printMatchingTasks(ArrayList<Task> taskArrayList){
+        int TaskCount = 0;
+        MainUI.printDivider();
+        System.out.println("Here are the matching tasks in your list:");
         for (Task task: taskArrayList){
             TaskCount +=1;
             System.out.println(TaskCount + "." + task);
