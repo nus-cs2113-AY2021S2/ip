f3e6d594334c73661fe389a0522d4c545f5610cc
jovanhuang <jhtc98@hotmail.com>
2021-03-02 16:27:01 +0800

    Refactor and reformat codes. (Remove long functions)

--- src/main/java/duke/command/MainUI.java
+++ src/main/java/duke/command/MainUI.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-
 public class MainUI {
     public static final String LIST_COMMAND = "list";
     public static final String BYE_COMMAND = "bye";
@@ -19,9 +18,14 @@ public class MainUI {
     public static boolean isEnding = false;
 
     public static ArrayList<Task> taskArrayList = new ArrayList<>();
+    public static final String LOGO_STRING = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
 
 
-    public static void displayUI(Scanner in){
+    public static void displayUI(Scanner in) {
         runProgram(in);
     }
 
@@ -33,34 +37,32 @@ public class MainUI {
         while (sc.hasNext()) {
             String taskSentence = sc.nextLine();
             char taskCategory = taskSentence.charAt(1);
-            boolean isDone = taskSentence.substring(4,5).compareTo("\u2713") == 0;
+            boolean isDone = taskSentence.substring(4, 5).compareTo("\u2713") == 0;
             switch (taskCategory) {
             case 'T':
                 String toDoDescription = taskSentence.substring(7);
                 Todo t = new Todo(toDoDescription);
-                if (isDone){
+                if (isDone) {
                     t.markAsDone();
                 }
                 taskArrayList.add(t);
                 break;
             case 'D':
                 int dueDateStartingIndex = taskSentence.indexOf("(by:");
-                String dueDate = taskSentence.substring(dueDateStartingIndex + 5, taskSentence.length()-1);
-                String deadlineDescription = taskSentence.substring(7,dueDateStartingIndex);
-//                System.out.println(dueDateStartingIndex+":"+dueDate);
-                Deadline d = new Deadline(deadlineDescription,dueDate);
-                if (isDone){
+                String dueDate = taskSentence.substring(dueDateStartingIndex + 5, taskSentence.length() - 1);
+                String deadlineDescription = taskSentence.substring(7, dueDateStartingIndex);
+                Deadline d = new Deadline(deadlineDescription, dueDate);
+                if (isDone) {
                     d.markAsDone();
                 }
                 taskArrayList.add(d);
                 break;
             case 'E':
                 int durationStartingIndex = taskSentence.indexOf("(at:");
-                String duration = taskSentence.substring(durationStartingIndex + 5, taskSentence.length()-1);
-                String eventDescription = taskSentence.substring(7,durationStartingIndex);
-//                System.out.println(durationStartingIndex+":"+duration);
-                Event e = new Event(eventDescription,duration);
-                if (isDone){
+                String duration = taskSentence.substring(durationStartingIndex + 5, taskSentence.length() - 1);
+                String eventDescription = taskSentence.substring(7, durationStartingIndex);
+                Event e = new Event(eventDescription, duration);
+                if (isDone) {
                     e.markAsDone();
                 }
                 taskArrayList.add(e);
@@ -78,31 +80,17 @@ public class MainUI {
         try {
             File data = new File("data/duke.txt");
             if (data.createNewFile()) {
-                System.out.println("Your file that stores all your tasks is not found! (data/duke.txt) :(");
-                System.out.println("Creating new file now to store your future tasks! :)");
-                System.out.println("File created: " + data.getName());
-                printDivider();
+                printMessageForCreatingNewFile(data);
             } else {
                 loadData();
             }
-
         } catch (IOException e) {
-            System.out.println("We couldn't find the 'data' folder in your directory. This data folder will store your tasks.");
-            System.out.println("Creating 'data' folder now...");
-            File relativePathFileName = new File(DIRECTORY_NAME);
-            String fullPath = relativePathFileName.getAbsolutePath();
-            File absolutePathFileName = new File(fullPath);
-            boolean bool = absolutePathFileName.mkdirs();
-            if(bool) {
-                System.out.println("'data' folder created successfully");
-            } else {
-                System.out.println("Sorry, couldn't create 'data' folder in your directory");
-            }
+            createDirectory();
         }
         printWelcomeMessage();
-        while (!isEnding){
+        while (!isEnding) {
             String input = in.nextLine();
-            switch (input){
+            switch (input) {
             case LIST_COMMAND:
                 TaskManager.printAllTasks(taskArrayList);
                 break;
@@ -120,13 +108,29 @@ public class MainUI {
         }
     }
 
+    private static void createDirectory() {
+        System.out.println("We couldn't find the 'data' folder in your directory. This data folder will store your tasks.");
+        System.out.println("Creating 'data' folder now...");
+        File relativePathFileName = new File(DIRECTORY_NAME);
+        String fullPath = relativePathFileName.getAbsolutePath();
+        File absolutePathFileName = new File(fullPath);
+        boolean bool = absolutePathFileName.mkdirs();
+        if (bool) {
+            System.out.println("'data' folder created successfully");
+        } else {
+            System.out.println("Sorry, couldn't create 'data' folder in your directory");
+        }
+    }
+
+    private static void printMessageForCreatingNewFile(File data) {
+        System.out.println("Your file that stores all your tasks is not found! (data/duke.txt) :(");
+        System.out.println("Creating new file now to store your future tasks! :)");
+        System.out.println("File created: " + data.getName());
+        printDivider();
+    }
+
     public static void printWelcomeMessage() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + LOGO_STRING);
         printDivider();
         System.out.println("Hello, I'm Duke");
         System.out.println("What can I do for you?");
@@ -134,14 +138,14 @@ public class MainUI {
         printDivider();
     }
 
-    public static void stopProgram(){
+    public static void stopProgram() {
         isEnding = true;
         System.out.println("Bye. Hope to see you again soon!");
         printDivider();
         System.exit(0);
     }
 
-    public static void printDivider(){
+    public static void printDivider() {
         System.out.println("________________________________");
     }
 
