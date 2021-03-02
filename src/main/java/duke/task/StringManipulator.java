15ca8a7b69b155aeb176a2fefe56fdef68c21d80
jovanhuang <jhtc98@hotmail.com>
2021-03-02 16:36:02 +0800

    Declare magic numbers as Constants.

--- src/main/java/duke/task/StringManipulator.java
+++ src/main/java/duke/task/StringManipulator.java
@@ -1,10 +1,10 @@
 package duke.task;
 
 public class StringManipulator {
-    
-    public StringManipulator(){
-        // TODO: 02-Feb-21
-    }
+
+    public static final int OUT_OF_BOUND_INDEX = -1;
+    public static final int DONE_TASK_NUMBER_INDEX = 5;
+    public static final int DELETED_TASK_NUMBER_INDEX = 7;
 
     public static String getFirstWord(String input){
         return input.split(" ")[0];
@@ -16,7 +16,7 @@ public class StringManipulator {
 
     public static String getIndexOfStringAfterWhiteSpace(String input){
         int firstWhiteSpaceIndex = input.indexOf(" ");
-        if (firstWhiteSpaceIndex == -1){
+        if (firstWhiteSpaceIndex == OUT_OF_BOUND_INDEX){
             return input.substring(input.length());
         }
         return input.substring(firstWhiteSpaceIndex+1);
@@ -25,20 +25,20 @@ public class StringManipulator {
     public static String getStringAfterWhiteSpaceAndBeforeSlash(String input){
         int firstWhiteSpaceIndex = input.indexOf(" ");
         int firstSlashIndex = input.indexOf("/");
-        if (firstWhiteSpaceIndex == -1) {
+        if (firstWhiteSpaceIndex == OUT_OF_BOUND_INDEX) {
             return input.substring(input.length());
         }
-        if (firstSlashIndex == -1) {
+        if (firstSlashIndex == OUT_OF_BOUND_INDEX) {
             return input.substring(firstWhiteSpaceIndex+1, input.length());
         }
         return input.substring(firstWhiteSpaceIndex+1, firstSlashIndex);
     }
 
     public static int getTaskNumberDone(String input){
-        return Integer.parseInt(input.substring(5));
+        return Integer.parseInt(input.substring(DONE_TASK_NUMBER_INDEX));
     }
 
     public static int getTaskNumberDeleted(String input){
-        return Integer.parseInt(input.substring(7));
+        return Integer.parseInt(input.substring(DELETED_TASK_NUMBER_INDEX));
     }
 }
