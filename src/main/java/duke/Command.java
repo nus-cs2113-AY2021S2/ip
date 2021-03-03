9598a843e50a8af8b2daed71d2fc95a28181672f
hussain1998 <e0406406@u.nus.edu>
2021-03-03 19:07:28 +0800

    Update A-JavaDoc

--- src/main/java/duke/Command.java
+++ src/main/java/duke/Command.java
@@ -1,5 +1,8 @@
 package duke;
 
+/**
+ * Types of commands available in Duke.
+ */
 public enum Command {
     BYE, DONE, ERROR, LIST, TODO, DEADLINE, EVENT, DELETE, FIND,
     DONE_ERROR, DELETE_ERROR, FIND_ERROR, ADD_ERROR
