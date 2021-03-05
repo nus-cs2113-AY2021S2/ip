28bda25c2a1013bc6bb4d8e6744489ee3393c634
beaniestanley <beanbeanstanley@gmail.com>
2021-03-05 14:57:53 +0800

    Add Level-7: Save

--- src/main/java/Classes/Event.java
+++ src/main/java/Classes/Event.java
@@ -3,14 +3,19 @@ package Classes;
 public class Event extends Task {
     protected String at;
 
-    public Event(String description, String at) {
-        super(description);
+    public Event(String description, boolean isDone, String at) {
+        super(description, isDone);
         this.at= at;
     }
 
     @Override
     public String toString() {
         return "[E]" + super.toString() + "(at: " + this.at + ")";
+    }
+
+    public String formatString() {
+        int done = (isDone ? 1 : 0);
+        return "E-" + done + "-" + description + "-" + at + "\n";
     }
 }
 
