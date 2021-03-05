bed39c715e585ade70453a1c7d75ba5f6a1b9356
limwenfeng <wilfred.limwf@gmail.com>
2021-03-05 15:14:06 +0800

    added DukeUI class

---
+++ src/main/java/Duke/DukeUI.java
@@ -0,0 +1,45 @@
+package Duke;
+import java.util.Scanner;
+
+
+public class DukeUI {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";;
+
+    public static void printLine() {
+        System.out.println("-------------------------------------------");
+    }
+
+    public static void print(String output) {
+        System.out.println(output);
+    }
+
+    public String[] getUserInput() {
+        String userInput = scanner.nextLine();
+        return splitInputIntoString(userInput);
+    }
+
+    private static String[] splitInputIntoString(String userInput) {
+        String[] listOfInputs = userInput.split(" ", 2);
+        if (listOfInputs.length == 1) {
+            listOfInputs = new String[]{userInput, "filler"};
+        }
+        return listOfInputs;
+    }
+
+    public void printWelcomeMessage() {
+        print("Hello from\n" + LOGO);
+        printLine();
+        print("Hello! I'm Duke");
+        print("What can I do for you?");
+    }
+
+    public void printExitingMessage() {
+        print("Bye. Hope to see you again soon!");
+    }
+}
