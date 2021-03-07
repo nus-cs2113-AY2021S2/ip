382a854b5624e958017101958c785937652ff937
violinyap <violinyap@gmail.com>
2021-03-07 13:28:58 +0800

    update data

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -45,7 +45,7 @@ public class Duke {
      * executes the program Duke
      */
     public static void main(String[] args){
-        new Duke("data/tasks.txt").run();
+        new Duke("tasks.txt").run();
     }
 
 
